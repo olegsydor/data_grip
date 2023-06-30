7f1ffb86d114b594ac6de377428be46207065c50
Oleh Sydor <ole.sydor@gmail.com>
2023-06-30 14:00:08 +0300

    Regular auto-commit at 14:00:08 on 30/06/2023

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console.sql
@@ -1,3 +1,32 @@
+-- function getuserpermissions(p_user_role in character, p_user_name in varchar2 default null) return ref_cursor is
+drop function if exists dash360.get_user_permissions;
+create function dash360.get_user_permissions(in_user_role varchar(1), in_user_name varchar(30) default null)
+    returns table
+            (
+                user_name   varchar(30),
+                first_name  varchar(30),
+                last_name   varchar(100),
+                is_locked   varchar(1),
+                create_time timestamp,
+                description varchar(255)
+            )
+    language plpgsql
+as
+$fx$
+begin
+    return query
+        select ui.user_name, ui.first_name, ui.last_name, ui.is_locked, ui.date_start as create_time, uf.description
+        from dwh.d_user_identifier ui
+                 left join dwh.d_user2user_function uuf
+                 join dwh.d_user_function uf on uuf.function_id = uf.function_id on ui.user_id = uuf.user_id
+        where ui.is_active
+          and ui.user_role = in_user_role
+          and case when in_user_name is null then true else ui.user_name = in_user_name end
+        order by ui.is_locked, ui.user_name, uf.description;
+end;
+$fx$;
+select * from dash360.get_user_permissions('P', 'BARC.jloftus')
+
 
      SELECT null as  "StrategyInID",
        SILS.TRANSACTION_ID as "TransactionID",
