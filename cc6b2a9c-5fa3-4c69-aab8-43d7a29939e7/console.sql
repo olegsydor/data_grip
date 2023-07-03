6e746669b1ab42572c8f548710d95d4834f0cb71
Oleh Sydor <ole.sydor@gmail.com>
2023-07-03 16:00:08 +0300

    Regular auto-commit at 16:00:08 on 03/07/2023

--- cc6b2a9c-5fa3-4c69-aab8-43d7a29939e7/console.sql
+++ cc6b2a9c-5fa3-4c69-aab8-43d7a29939e7/console.sql
@@ -5,7 +5,7 @@ with MD as (select sr.routing_table_id,
 --             where sr.ROUTING_TABLE_ID = 24876
             group by sr.routing_table_id
 
-	union ALL
+	union
 
     select sd1.routing_table_id,
            max(st.start_date_id) as max_date
@@ -98,7 +98,40 @@ select sr.routing_table_id,
 
 
 
-select *--em.routing_table_id--, e.max_date
+select rt.routing_table_id                   as routing_table_id,
+       rt.routing_table_name                 as routing_table_name,
+       rt.routing_table_desc                 as routing_table_desc,
+       case
+           when rt.instrument_type_id = 'O' then 'Option'
+           when rt.instrument_type_id = 'E' then 'Equity'
+           when rt.instrument_type_id = 'M' then 'Multileg'
+           else rt.instrument_type_id end    as instrument_type,
+       ts.target_strategy_name               as target_strategy,
+       rt.fee_sensitivity                    as fee_sensitivity,
+       case
+           when rt.intended_scope_of_use = 'D' then 'Default'
+           when rt.intended_scope_of_use = 'A' then 'Account-specific'
+           when rt.intended_scope_of_use = 'T' then 'Trading firm-specific'
+           else rt.intended_scope_of_use end as routing_table_scope,
+       ac.account_class_name                 as account_class,
+       cg.capacity_group_name                as capacity_group,
+       case
+           when rt.routing_table_type = 'C' then 'Instrument Class'
+           when rt.routing_table_type = 'T' then 'Global'
+           when rt.routing_table_type = 'S' then 'Symbol'
+           when rt.routing_table_type = 'L' then 'Symbol List'
+           else rt.routing_table_type end    as routing_table_type,
+       case
+           when rt.instr_class_id = 'NN' then 'Nickel Non-Premium'
+           when rt.instr_class_id = 'NP' then 'Nickel Premium'
+           when rt.instr_class_id = 'PN' then 'Penny Non-Premium'
+           when rt.instr_class_id = 'PP' then 'Penny Premium'
+           else rt.instr_class_id end        as instrument_class,
+       rt.symbol_list_id                     as symbol_list,
+       rt.root_symbol                        as symbol,
+       rt.symbol_suffix                      as symbol_sfx,
+       md.max_date                           as last_used_date
+
 from genesis2.routing_table rt
          left join (select sr.routing_table_id,
                                    max(st.start_date_id) as max_date
@@ -110,7 +143,7 @@ where rt.routing_table_id = 20469;
 
 
 select rt.ROUTING_TABLE_ID,
-       (select first_value(st.start_date_id) over (partition by sr.transaction_id order by st.START_DATE_ID) max_date_id
+       (/*+ no_push_pred(i) */select first_value(st.start_date_id) over (partition by sr.transaction_id order by st.START_DATE_ID) max_date_id
                             from strategy_routing_table sr
                                      join strategy_transaction st on (sr.transaction_id = st.transaction_id)
                             where sr.ROUTING_TABLE_ID = rt.ROUTING_TABLE_ID
@@ -120,11 +153,61 @@ from genesis2.routing_table rt
          where r is not null;
 
 
-select rt.ROUTING_TABLE_ID, r.*
+select rt.routing_table_id                   as routing_table_id,
+       rt.routing_table_name                 as routing_table_name,
+       rt.routing_table_desc                 as routing_table_desc,
+       case
+           when rt.instrument_type_id = 'O' then 'Option'
+           when rt.instrument_type_id = 'E' then 'Equity'
+           when rt.instrument_type_id = 'M' then 'Multileg'
+           else rt.instrument_type_id end    as instrument_type,
+       ts.target_strategy_name               as target_strategy,
+       rt.fee_sensitivity                    as fee_sensitivity,
+       case
+           when rt.intended_scope_of_use = 'D' then 'Default'
+           when rt.intended_scope_of_use = 'A' then 'Account-specific'
+           when rt.intended_scope_of_use = 'T' then 'Trading firm-specific'
+           else rt.intended_scope_of_use end as routing_table_scope,
+       ac.account_class_name                 as account_class,
+       cg.capacity_group_name                as capacity_group,
+       case
+           when rt.routing_table_type = 'C' then 'Instrument Class'
+           when rt.routing_table_type = 'T' then 'Global'
+           when rt.routing_table_type = 'S' then 'Symbol'
+           when rt.routing_table_type = 'L' then 'Symbol List'
+           else rt.routing_table_type end    as routing_table_type,
+       case
+           when rt.instr_class_id = 'NN' then 'Nickel Non-Premium'
+           when rt.instr_class_id = 'NP' then 'Nickel Premium'
+           when rt.instr_class_id = 'PN' then 'Penny Non-Premium'
+           when rt.instr_class_id = 'PP' then 'Penny Premium'
+           else rt.instr_class_id end        as instrument_class,
+       rt.symbol_list_id                     as symbol_list,
+       rt.root_symbol                        as symbol,
+       rt.symbol_suffix                      as symbol_sfx,
+       md.max_date                           as last_used_date
 from genesis2.routing_table rt
-join        (select sr.ROUTING_TABLE_ID, max(st.start_date_id)
+join        ( /*+ no_push_pred(i) */select sr.ROUTING_TABLE_ID, max(st.start_date_id) max_date
                             from strategy_routing_table sr
                                      join strategy_transaction st on (sr.transaction_id = st.transaction_id)
+                            WHERE st.START_DATE_ID is not null
                             group by sr.ROUTING_TABLE_ID
-    )  r on rt.routing_table_id = r.ROUTING_TABLE_ID
+    )  md on md.routing_table_id = rt.ROUTING_TABLE_ID
+	left outer join target_strategy ts on rt.target_strategy = ts.target_strategy
+	left outer join account_class ac on rt.account_class_id = ac.account_class_id
+	left outer join capacity_group cg on rt.capacity_group_id = cg.capacity_group_id
+where
+	rt.is_deleted = 'N'
+order by
+	rt.instrument_type_id,
+	rt.target_strategy,
+	rt.routing_table_name;
+
+
+select sr.ROUTING_TABLE_ID, max(st.start_date_id) max_date
+                            from strategy_routing_table sr
+                                     join strategy_transaction st on (sr.transaction_id = st.transaction_id)
+                            WHERE st.START_DATE_ID is not null
+                            and sr.routing_table_id = 20469
+                            group by sr.ROUTING_TABLE_ID;
 
