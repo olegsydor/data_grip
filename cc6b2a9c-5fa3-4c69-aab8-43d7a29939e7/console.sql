6850034e40c6edfb3c88f3aac501fbfbb968b01d
Oleh Sydor <ole.sydor@gmail.com>
2023-07-06 13:00:08 +0300

    Regular auto-commit at 13:00:08 on 06/07/2023

--- cc6b2a9c-5fa3-4c69-aab8-43d7a29939e7/console.sql
+++ cc6b2a9c-5fa3-4c69-aab8-43d7a29939e7/console.sql
@@ -214,3 +214,115 @@ select sr.ROUTING_TABLE_ID, max(st.start_date_id) max_date
 
 
 select * from genesis2.REPORTING_SL.getAllocations(null, null,date '2023-07-03', date '2023-07-04');
+
+
+select
+	bai."TradeDate" as "Date",
+	tf.trading_firm_name as "TradingFirm",
+	a.account_name as "AccountName",
+	bai."OPRASymbol" as "OSI Symbol",
+	bai."DisplayInstrumentID" as "Symbol",
+	case when(
+			bai."MaturityYear" is not null
+			and bai."MaturityMonth" is not null
+			and bai."MaturityDay" is not null
+		) then bai."MaturityDay" || '/' || bai."MaturityMonth" || '/' || bai."MaturityYear"
+	end as "Expiration",
+	bai."OpenClose",
+	bai."InstrumentType",
+	(
+		case
+			when bai."Side" = '1' then 'B'
+			when bai."Side" = '2' then 'S'
+			else 'T'
+		end
+	) "Side",
+	cast((CASE
+		when(
+			bai."AllocQty" is null
+			or bai."AllocQty" = 0
+		) then bai."DayCumQty"
+		else bai."AllocQty"
+	END) AS INTEGER) as "Total Quantity",
+	to_CHAR(
+		round( bai."DayAvgPx", 5 )
+	) as "Average Price",
+	NVL(
+		AE.ALLOC_QTY,
+		bai."DayCumQty"
+	) as "Allocated Quantity",
+	round(( bai."AccDashCommissionAmount" / bai."DayCumQty" )* NVL( AE.ALLOC_QTY, bai."DayCumQty" ), 6 ) as "Commission",
+	round(( bai."AliasMakerTakerFeeAmount" / bai."DayCumQty" )* NVL( AE.ALLOC_QTY, bai."DayCumQty" ), 6 ) as "Maker/Taker",
+	round(( bai."AliasTransactionFeeAmount" / bai."DayCumQty" )* NVL( AE.ALLOC_QTY, bai."DayCumQty" ), 6 ) as "Transaction",
+	round(( bai."AliasTradeProcessingFeeAmount" / bai."DayCumQty" )* NVL( AE.ALLOC_QTY, bai."DayCumQty" ), 6 ) as "Trade Processing",
+	round(( bai."AliasRoyaltyFeeAmount" / bai."DayCumQty" )* NVL( AE.ALLOC_QTY, bai."DayCumQty" ), 6 ) as "Royalty",
+	ca.clearing_account_number as "CMTA"
+from
+	BUNDLE_ALLOCATION_INFO_STORAGE bai
+left join account a ON bai."AccountID" = a.account_id
+left join trading_firm tf ON a.trading_firm_id = tf.trading_firm_id
+left join ALLOCATION_INSTRUCTION ALIN ON ALIN.BUNDLE_ID = bai."BundleID" and ALIN.ALLOC_SCOPE = 'B'	and ALIN.IS_DELETED <> 'Y'
+left join ALLOCATION_ENTRY AE ON ALIN.ALLOCATION_INSTRUCTION_ID = AE.ALLOCATION_INSTRUCTION_ID and AE.ALLOC_QTY > 0
+left join CLEARING_ACCOUNT ca ON ca.clearing_account_id = AE.clearing_account_id and ca.MARKET_TYPE = bai."InstrumentType" and ca.IS_DELETED <> 'Y'
+where 1=1
+-- 	and (p_tf_id IS NULL OR a.trading_firm_id=p_tf_id)
+-- 	and (p_accounts IS NULL OR a.ACCOUNT_ID MEMBER p_accounts)
+  	and bai."TradeDate" BETWEEN :l_trunc_start_date AND :l_trunc_end_date
+
+union all
+select
+	oai."TradeDate" as "Date",
+	tf.trading_firm_name as "TradingFirm",
+	a.account_name as "AccountName",
+	oai."OPRASymbol" as "OSI Symbol",
+	oai."DisplayInstrumentID" as "Symbol",
+	case
+		when(
+			oai."MaturityYear" is not null
+			and oai."MaturityMonth" is not null
+			and oai."MaturityDay" is not null
+		) then oai."MaturityDay" || '/' || oai."MaturityMonth" || '/' || oai."MaturityYear"
+	end as "Expiration",
+	oai."OpenClose",
+	oai."InstrumentType",
+	(
+		case
+			when oai."Side" = '1' then 'B'
+			when oai."Side" = '2' then 'S'
+			else 'T'
+		end
+	) "Side",
+	cast((CASE
+		when(
+			oai."AllocQty" is null
+			or oai."AllocQty" = 0
+		) then oai."DayCumQty"
+		else oai."AllocQty"
+	END) AS INTEGER) as "Total Quantity",
+	TO_CHAR(
+		round( oai."DayAvgPx", 5 )
+	) as "Average Price",
+	NVL(
+		AE.ALLOC_QTY,
+		oai."DayCumQty"
+	) as "Allocated Quantity",
+	round(( oai."AccDashCommissionAmount" / oai."DayCumQty" )* NVL( AE.ALLOC_QTY, oai."DayCumQty" ), 6 ) as "Commission",
+	round(( oai."AliasMakerTakerFeeAmount" / oai."DayCumQty" )* NVL( AE.ALLOC_QTY, oai."DayCumQty" ), 6 ) as "Maker/Taker",
+	round(( oai."AliasTransactionFeeAmount" / oai."DayCumQty" )* NVL( AE.ALLOC_QTY, oai."DayCumQty" ), 6 ) as "Transaction",
+	round(( oai."AliasTradeProcessingFeeAmount" / oai."DayCumQty" )* NVL( AE.ALLOC_QTY, oai."DayCumQty" ), 6 ) as "Trade Processing",
+	round(( oai."AliasRoyaltyFeeAmount" / oai."DayCumQty" )* NVL( AE.ALLOC_QTY, oai."DayCumQty" ), 6 ) as "Royalty",
+	ca.clearing_account_number as "CMTA"
+from
+	ORDER_ALLOCATION_INFO_STORAGE oai
+left join account a ON oai."AccountID" = a.account_id
+left join trading_firm tf ON a.trading_firm_id = tf.trading_firm_id
+left join ALLOCATION_INSTRUCTION ALIN ON ALIN.ORDER_ID = oai."OrderID" and ALIN.ALLOC_SCOPE = 'S' and ALIN.IS_DELETED <> 'Y'
+left join ALLOCATION_ENTRY AE ON ALIN.ALLOCATION_INSTRUCTION_ID = AE.ALLOCATION_INSTRUCTION_ID and AE.ALLOC_QTY > 0
+left join CLEARING_ACCOUNT ca ON ca.clearing_account_id = AE.clearing_account_id
+	and ca.MARKET_TYPE = oai."InstrumentType"
+	and ca.IS_DELETED <> 'Y'
+where
+	oai."BundleID" is null
+-- 	AND (p_tf_id IS NULL OR a.trading_firm_id=p_tf_id)
+-- 	AND (p_accounts IS NULL OR a.ACCOUNT_ID MEMBER p_accounts)
+	AND oai."TradeDate" BETWEEN :l_trunc_start_date AND :l_trunc_end_date;
