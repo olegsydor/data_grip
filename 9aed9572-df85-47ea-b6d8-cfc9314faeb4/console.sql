1e19121a0588656062b726ad89c75af350b06008
Oleh Sydor <ole.sydor@gmail.com>
2023-07-10 15:00:09 +0300

    Regular auto-commit at 15:00:09 on 10/07/2023

--- 9aed9572-df85-47ea-b6d8-cfc9314faeb4/console.sql
+++ 9aed9572-df85-47ea-b6d8-cfc9314faeb4/console.sql
@@ -1,7 +1,28 @@
-SELECT
-       co.cl_ord_id                                                                       AS orderid,
-       co.route_destination,
-       co.payload ->>'IsFlex'
+drop function if exists staging.get_timestamp_from_date_ts;
+create or replace function staging.get_timestamp_from_date_ts(in_date_id int4 default null, in_ts text default null)
+    returns timestamp
+    language plpgsql
+as
+$fx$
+begin
+    return to_timestamp(in_date_id::text || in_ts, 'YYYYMMDDHH24:MI:SS.US');
+exception
+    when others then
+        return null;
+end;
+$fx$;
+
+select staging.get_timestamp_from_date_ts(20230506, '25:50:09.010453')
+select :order_trade_date::text, :f_timestamp,
+       to_timestamp(:order_trade_date::text || :f_timestamp, 'YYYYMMDDHH24:MI:SS.US')
+
+SELECT co.cl_ord_id                                                                            AS orderid,
+       co.payload -> 'OriginatorOrder',
+       co.payload ->> 'IsFlex',
+       co.crossing_side,
+       case
+           when is_q_time then staging.get_timestamp_from_date_ts(order_trade_date,
+                                                                  q_time) end as boxqooannouncedtime
 FROM (SELECT DISTINCT ON (cl.cl_ord_id) cl.order_id,
                                         cl.chain_id,
                                         cl.parent_order_id,
@@ -16,7 +37,25 @@ FROM (SELECT DISTINCT ON (cl.cl_ord_id) cl.order_id,
                                         cl.secondary_order_id,
                                         cl.cross_order_id,
                                         big.payload                     AS big_payload,
-                                        cl.route_destination
+                                        case
+                                            when cl.route_destination = 'BOX QOO' and cl.payload ->> 'IsFlex' = 'Y'
+                                                then true::boolean
+                                            else false end as is_q_time,
+                                        cl.order_trade_date::int,
+                                        case
+                                            when (cl.route_destination = 'BOX QOO' and cl.payload ->> 'IsFlex' = 'Y')
+                                                then
+                                                case
+                                                    when cl.crossing_side = 'O'::bpchar
+                                                        then cl.payload #>> '{OriginatorOrder,OrderNotes}'::text[]
+                                                    when cl.crossing_side = 'C'::bpchar
+                                                        then (select cl.payload #>> '{OriginatorOrder,OrderNotes}'
+                                                              from blaze7.client_order co2
+                                                              WHERE co2.cross_order_id = cl.cross_order_id
+                                                                and co2.crossing_side = 'O'
+                                                              order by cl.chain_id desc
+                                                              limit 1)
+                                                    end end             as q_time
       FROM blaze7.client_order cl
                LEFT JOIN LATERAL ( SELECT regexp_replace(co2.payload::text, '\\u0000'::text, ''::text,
                                                          'g'::text)::json AS payload
@@ -26,4 +65,5 @@ FROM (SELECT DISTINCT ON (cl.cl_ord_id) cl.order_id,
                                    LIMIT 1) big ON true
       WHERE true
         AND (cl.record_type = ANY (ARRAY ['0'::bpchar, '2'::bpchar]))
+        and cl.route_destination = 'BOX QOO'
       ORDER BY cl.cl_ord_id, cl.chain_id DESC) co;
