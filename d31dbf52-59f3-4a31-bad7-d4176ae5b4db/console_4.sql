63eb9e20cb1b97ff03c33edadc2d07335889a700
Oleh Sydor <ole.sydor@gmail.com>
2023-07-11 15:00:08 +0300

    Regular auto-commit at 15:00:08 on 11/07/2023

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_4.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_4.sql
@@ -1,3 +1,51 @@
+CREATE OR REPLACE FUNCTION dash360.get_street_orders_trade_activity(in_account_ids integer[] DEFAULT '{}'::integer[],
+                                                                    in_exchange_ids character varying[] DEFAULT '{}'::character varying(6)[],
+                                                                    in_start_date date DEFAULT CURRENT_DATE,
+                                                                    in_end_date date DEFAULT CURRENT_DATE)
+    RETURNS TABLE
+            (
+                parent_client_order_id    character varying,
+                client_order_id           character varying,
+                exch_order_id             character varying,
+                order_id                  bigint,
+                side                      character,
+                order_qty                 integer,
+                price                     numeric,
+                time_in_force             character varying,
+                instrument_name           character varying,
+                last_trade_date           timestamp without time zone,
+                account_name              character varying,
+                sub_system_id             character varying,
+                multileg_reporting_type   character,
+                exec_id                   bigint,
+                ref_exec_id               bigint,
+                exch_exec_id              character varying,
+                exec_time_orig            timestamp without time zone,
+                exec_time                 text,
+                exec_type                 character varying,
+                order_status              character varying,
+                leaves_qty                bigint,
+                cum_qty                   bigint,
+                avg_px                    numeric,
+                last_qty                  bigint,
+                last_px                   numeric,
+                bust_qty                  bigint,
+                last_mkt                  character varying,
+                last_mkt_name             character varying,
+                text_                     character varying,
+                trade_liquidity_indicator character varying,
+                is_busted                 character,
+                exec_broker               character varying,
+                exchange_id               character varying,
+                exchange_name             character varying
+            )
+    LANGUAGE plpgsql
+AS
+$function$
+declare
+-- 2023-06-29 https://dashfinancial.atlassian.net/browse/DS-6950
+begin
+    return query
         select po.client_order_id                     as parent_client_order_id,
                co.client_order_id,
                co.exch_order_id,
@@ -32,8 +80,8 @@
                ex.exec_broker,
                ex.exchange_id,
                e.exchange_name
-        from dwh.execution ex
-                 join dwh.client_order co on co.order_id = ex.order_id
+/*        from dwh.execution ex
+                 inner join dwh.client_order co on co.order_id = ex.order_id
                  inner join dwh.client_order po on po.order_id = co.parent_order_id
                  inner join dwh.d_account acc on co.account_id = acc.account_id
                  inner join dwh.d_instrument inst on co.instrument_id = inst.instrument_id
@@ -44,11 +92,48 @@
                  left join dwh.d_exchange e on e.exchange_id = ex.exchange_id and e.is_active
                  left join dwh.d_exec_type et on et.exec_type = ex.exec_type
         where ex.exec_type in ('F', 'H')
-          and co.account_id = any('{63384,63109,54131}')
---           and case when coalesce(in_account_ids, '{}') = '{}' then true else co.account_id = any (in_account_ids) end
---           and case when coalesce(in_exchange_ids, '{}') = '{}' then true else ex.exchange_id = any (in_exchange_ids) end
---           and ex.exec_date_id >= public.get_dateid(in_start_date)
---           and ex.exec_date_id <= public.get_dateid(in_end_date + 1)
-          and ex.exec_date_id >= public.get_dateid(:in_start_date::date)
-          and ex.exec_date_id < public.get_dateid(:in_end_date::date + 1 )
+          and case when coalesce(in_account_ids, '{}') = '{}' then true else co.account_id = any (in_account_ids) end
+          and case when coalesce(in_exchange_ids, '{}') = '{}' then true else ex.exchange_id = any (in_exchange_ids) end
+          and ex.exec_date_id >= public.get_dateid(in_start_date)
+          and ex.exec_date_id <= public.get_dateid(in_end_date + 1)
           and co.parent_order_id is not null;
+*/
+        from dwh.execution ex
+                 join lateral (select client_order_id,
+                                      exch_order_id,
+                                      order_id,
+                                      side,
+                                      order_qty,
+                                      price,
+                                      parent_order_id,
+                                      account_id,
+                                      instrument_id,
+                                      time_in_force_id,
+                                      sub_system_unq_id,
+                                      multileg_reporting_type,
+                                      create_date_id
+                               from dwh.client_order co
+                               where co.order_id = ex.order_id
+                                 and case
+                                         when coalesce(in_account_ids, '{}') = '{}' then true
+                                         else co.account_id = any (in_account_ids) end
+                                 and co.parent_order_id is not null
+                                 and co.create_date_id <= ex.exec_date_id
+                               limit 1) co on true
+                 inner join dwh.client_order po
+                            on po.order_id = co.parent_order_id and po.create_date_id <= co.create_date_id
+                 inner join dwh.d_account acc on co.account_id = acc.account_id
+                 inner join dwh.d_instrument inst on co.instrument_id = inst.instrument_id
+                 left join dwh.d_time_in_force tif on co.time_in_force_id = tif.tif_id
+                 left join dwh.d_order_status os on os.order_status = ex.order_status
+                 left join dwh.d_sub_system dss on dss.sub_system_unq_id = co.sub_system_unq_id
+                 left join dwh.d_last_market lm on lm.last_mkt = ex.last_mkt and lm.is_active
+                 left join dwh.d_exchange e on e.exchange_id = ex.exchange_id and e.is_active
+                 left join dwh.d_exec_type et on et.exec_type = ex.exec_type
+        where ex.exec_type in ('F', 'H')
+          and case when coalesce(in_exchange_ids, '{}') = '{}' then true else ex.exchange_id = any (in_exchange_ids) end
+          and ex.exec_date_id >= public.get_dateid(in_start_date)
+          and ex.exec_date_id <= public.get_dateid(in_end_date + 1);
+end;
+$function$
+;
