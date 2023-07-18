6eb88e77e8cd0fcdce670ff0a61c709adfed2698
Oleh Sydor <ole.sydor@gmail.com>
2023-07-18 13:00:09 +0300

    Regular auto-commit at 13:00:08 on 18/07/2023

---
+++ 471cc74d-c526-482e-b21b-90a348ef067e/console.sql
@@ -0,0 +1,5 @@
+select CAST('2023-07-18 14:00:11.123456' AS datetime2)
+
+ALTER TABLE Blaze7_dev.dbo.TOrderMisc1_EDW ALTER COLUMN BoxQOOAnnouncedTime datetime2(3) NULL;
+ALTER TABLE Blaze7_dev.dbo.TOrderMisc1_EDW ALTER COLUMN BoxQOOAnnouncedTime datetime2 NULL;
+ALTER TABLE Blaze7_dev.dbo.TOrderMisc1_EDW ALTER COLUMN BoxQOOAnnouncedTime datetime2 NULL;
