5da2000129705a248581430743d3069b8778582a
Oleh Sydor <ole.sydor@gmail.com>
2023-07-18 19:00:17 +0300

    Regular auto-commit at 19:00:17 on 18/07/2023

--- 471cc74d-c526-482e-b21b-90a348ef067e/console.sql
+++ 471cc74d-c526-482e-b21b-90a348ef067e/console.sql
@@ -18,4 +18,12 @@ select *
 --delete
 from TOrderMisc1_EDW
 where date_id = 20230718
-    and pg_order_id = 535822291003523072
+    and pg_order_id = 535793635686367234
+
+
+
+select 'max_processed_time_leg' as [key],
+coalesce(max(pg_db_create_time), '2020-01-01 00:00:00') as [value]
+from [dbo].[tlegs_edw]
+where date_id = '"+context.p_date_id+"'
+and pg_entity = '"+context.pg_entity+"'
--- 9aed9572-df85-47ea-b6d8-cfc9314faeb4/console.sql
+++ 9aed9572-df85-47ea-b6d8-cfc9314faeb4/console.sql
@@ -11,3 +11,83 @@ _db_create_time at time zone 'US/Central' as _db_create_time
 from blaze7.tprices_edw
 where _db_create_time::date = '2023-07-18'
     and "_order_id" = 535822291003523072;
+
+
+select to_char(boxqooannouncedtime, 'YYYY-MM-DD HH24:MI:SS.US'), *
+from blaze7.tordermisc1_edw
+where "_db_create_time"::date = '2023-07-18'
+and "_order_id" = 535793635686367234;
+
+
+
+SELECT id,
+       clordid_to_guid(orderid)                                                  as orderid,
+       legrefid::text,
+       round(price::bigint / 10000.0, 4)                                         as price,
+       legcount::int,
+       legnumber::int,
+       dashsecurityid,
+       basecode,
+       typecode,
+       expirationdate::date,
+       round(strike::numeric, 6)                                                 as strike,
+       side::int,
+       ratio::int,
+       multiplier::int,
+       quantity::int,
+       filled::int,
+       invested,
+       round(avgprice::bigint / 100000000.0, 8)                                  as avgprice,
+       stockquantity::bigint,
+       stockopenquantity::int,
+       stockfilled::int,
+       stockcancelled::bigint,
+       optionquantity::int,
+       optionopenquantity::int,
+       optionfilled::int,
+       optioncancelled::int,
+       mindatetime::timestamp at time zone 'UTC' at time zone 'US/Central'       as mindatetime,
+       maxdatetime::timestamp at time zone 'UTC' at time zone 'US/Central'       as maxdatetime,
+       firstfilldatetime::timestamp at time zone 'UTC' at time zone 'US/Central' as firstfilldatetime,
+       lastfilldatetime::timestamp at time zone 'UTC' at time zone 'US/Central'  as lastfilldatetime,
+       statuscode,
+       timeinforcecode,
+       openclose,
+       systemid,   -- no data
+       orderidint, -- no data
+       rootcode,
+       prevfillquantity::int,
+       stockprevfillquantity::int,
+       optionprevfillquantity::int,
+       clordid_to_guid(legorderid)                                               as legorderid,
+       _order_id::bigint,
+       _chain_id,
+       _db_create_time::timestamp at time zone 'UTC' at time zone 'US/Central'   as _db_create_time,
+       orderid                                                                   as cl_ord_id
+select mx.*, *
+FROM blaze7.tlegs_edw as x
+left join lateral(SELECT rep.db_create_time as max_rep_time FROM blaze7.order_report rep WHERE rep.order_id = x._order_id AND rep.chain_id = x._chain_id order by rep.db_create_time desc limit 1) mx on true
+where to_char(x._db_create_time, 'YYYYMMDD')::int = '20230718'::int
+AND x._db_create_time >  '2023-07-18 06:15:15.237'::timestamp at time zone 'US/Central' at time zone 'UTC' - interval '10 minute'
+and mx.max_rep_time >  '2023-07-18 06:15:15.237'::timestamp at time zone 'US/Central' at time zone 'UTC' - interval '10 minute';
+
+select *
+FROM blaze7.tlegs_edw as x
+         left join lateral (SELECT rep.db_create_time as max_rep_time
+                            FROM blaze7.order_report rep
+                            WHERE rep.order_id = x._order_id
+                              AND rep.chain_id = x._chain_id
+                            order by rep.db_create_time desc
+                            limit 1) mx on true
+where to_char(x._db_create_time, 'YYYYMMDD')::int = '"+context.p_date_id+"'::int
+  AND x._db_create_time > '"+context.max_processed_time_leg+"'::timestamp at time zone 'US/Central' at time zone
+                          'UTC' - interval '10 minute'
+  and mx.max_rep_time > '"+context.max_processed_time_leg+"'::timestamp at time zone 'US/Central' at time zone
+                        'UTC' - interval '10 minute';
+
+
+select *
+FROM blaze7.torder_edw as x
+where x._db_create_time >= '20230718'::date
+and x._db_create_time < '''20230718'::date + interval '1 day'
+AND x._last_mod_time::timestamp >  '"+context.max_processed_time_order+"'::timestamp at time zone 'US/Central' at time zone 'UTC' - interval '10 minute';
