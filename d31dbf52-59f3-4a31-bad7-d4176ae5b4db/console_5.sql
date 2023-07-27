108cd1ea41da76400ff295e7e8193edc10747d64
Oleh Sydor <ole.sydor@gmail.com>
2023-07-27 20:00:09 +0300

    Regular auto-commit at 20:00:08 on 27/07/2023

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_5.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_5.sql
@@ -31,65 +31,44 @@ select yc.parent_order_id                                                  as "P
            when hsd.instrument_type_id = 'E' then 'Equity'
            when hsd.instrument_type_id = 'O' then 'Option'
            end                                                             as "Security Type",
-       hsd.underlying_symbol                                as "Root Symbol",
-       yc.client_id                                         as "Client ID",
-       cf.customer_or_firm_name                             as "Capacity",
-       dss.sub_strategy                                     as "Sub Strategy",
-       coalesce(exd.ex_destination_desc, co.ex_destination) as "Ex Dest",
-       fc.fix_comp_id                                       as "Sending Firm",
-       lst_ex.exec_type_description as "Event Type",
+       hsd.underlying_symbol                                               as "Root Symbol",
+       yc.client_id                                                        as "Client ID",
+       cf.customer_or_firm_name                                            as "Capacity",
+       dss.sub_strategy                                                    as "Sub Strategy",
+       coalesce(exd.ex_destination_desc, co.ex_destination)                as "Ex Dest",
+       fc.fix_comp_id                                                      as "Sending Firm",
+       lst_ex.exec_type_description                                        as "Event Type",
        lst_ex.text_                                                        as "Free Text",
-       null::text as "Reject Reason",
+       null::text                                                          as "Reject Reason",
        co.max_floor                                                        as "Max Floor",
-       lst_ex.exec_broker as "Exec Broker",
+       lst_ex.exec_broker                                                  as "Exec Broker",
        co.clearing_firm_id                                                 as "CMTA",
        case when yc.multileg_reporting_type = '1' then 'N' else 'Y' end    as "Is Mleg",
        case when yc.cross_order_id is not null then 'Y' else 'N' end       as "Is Cross",
-       coa.auction_id as "ATS Auction ID", -- ??
---        cross_
-
-
-
-
-
-
-
-
-       tf.trading_firm_name                                                as "Trading Firm",
-       ex.exchange_name                                                    as "Exchange Name",
-       hsd.opra_symbol                                                     as "OSI Symbol",
-       hsd.put_call                                                        as "Put/Call",
-       hsd.strike_px                                                       as "Strike",
-       case
-           when co.exec_instruction like '1%' then 'NH' -- Not Held
-           when co.exec_instruction like '5%' then 'H' -- Held
-           else 'NH'
-           end                                                             as "Held Status",
-       coalesce(fm_ex.tag_6376, fm_co.tag_6376)                            as "Alternative Compliance ID",
-       coalesce(fm_ex.tag_376, fm_co.tag_6376)                             as "Compliance ID",
-       coalesce(fm_ex.tag_21, fm_co.tag_21)                                as "Handling Instructions",
-       coalesce(fm_ex.tag_18, fm_co.tag_18)                                as "Execution Instructions",
+       coa.auction_id                                                      as "ATS Auction ID", -- ??
+       null::text                                                          as "Cross Ord Type",
+       co.fee_sensitivity                                                  as "Fee Sensitivity",
+       coalesce(fm_ex.tag_21, fm_co.tag_21)                                as "Handle Inst",
+       co.locate_broker                                                    as "Locate Broker",
        co.co_client_leg_ref_id                                             as "Leg ID",
-''
+       a.broker_dealer_mpid                                                as "MPID",           --??
+       null::text                                                          as "OCC Opt Data",
+       doc.order_capacity_name                                             as "Ord Capacity",
+       hsd.opra_symbol                                                     as "OSI Symbol",
+       ds.sub_system_id                                                   as "Sub System",
+       co.session_eligibility                                              as "Session Eligibility",
+       co.sweep_style                                                      as "Sweep Style",
+       tf.trading_firm_name                                                as "Trading Firm"
 from data_marts.f_yield_capture yc
          join dwh.d_account a on (a.account_id = yc.account_id)
-         join dwh.client_order co on (co.create_date_id between :in_start_date_id and :in_end_date_id and
-                                      co.create_date_id = yc.status_date_id and
-                                      co.order_id = yc.order_id)
-         left join dwh.client_order orig
-                   on (orig.create_date_id between :in_start_date_id and :in_end_date_id and
-                       orig.create_date_id = yc.status_date_id and
-                       orig.order_id = co.orig_order_id)
+         join dwh.client_order co on (co.create_date_id between :in_start_date_id and :in_end_date_id and co.create_date_id = yc.status_date_id and co.order_id = yc.order_id)
+         left join dwh.client_order orig on (orig.create_date_id between :in_start_date_id and :in_end_date_id and orig.create_date_id = yc.status_date_id and orig.order_id = co.orig_order_id)
          join dwh.d_trading_firm tf on (tf.trading_firm_unq_id = a.trading_firm_unq_id)
          left join dwh.d_time_in_force tif on tif.is_active and tif.tif_id = yc.time_in_force_id
          left join dwh.d_order_type ot on ot.order_type_id = yc.order_type_id
          join dwh.historic_security_definition_all hsd on (hsd.instrument_id = yc.instrument_id)
          left join data_marts.d_sub_strategy dss on yc.sub_strategy_id = dss.sub_strategy_id
-         left join dwh.d_ex_destination exd on (exd.ex_destination_code = co.ex_destination and
-                                                coalesce(exd.exchange_id, '') =
-                                                coalesce(yc.exchange_id, '') and
-                                                exd.instrument_type_id = yc.instrument_type_id and
-                                                exd.is_active)
+         left join dwh.d_ex_destination exd on (exd.ex_destination_code = co.ex_destination and coalesce(exd.exchange_id, '') = coalesce(yc.exchange_id, '') and exd.instrument_type_id = yc.instrument_type_id and exd.is_active)
          left join dwh.d_customer_or_firm cf on (cf.customer_or_firm_id = co.customer_or_firm_id)
          left join dwh.d_exchange ex on (ex.exchange_unq_id = yc.exchange_unq_id)
          left join lateral
@@ -121,8 +100,10 @@ from data_marts.f_yield_capture yc
                             where fm.fix_message_id = lst_ex.fix_message_id
                               and fm.date_id between :in_start_date_id and :in_end_date_id
                             limit 1) fm_ex on true
-left join dwh.d_fix_connection fc on fc.fix_connection_id = co.fix_connection_id and fc.is_active
-left join dwh.client_order2auction coa on coa.order_id = co.order_id and coa.create_date_id = co.create_date_id
+         left join dwh.d_fix_connection fc on fc.fix_connection_id = co.fix_connection_id and fc.is_active
+         left join dwh.client_order2auction coa on coa.order_id = co.order_id and coa.create_date_id = co.create_date_id
+         left join dwh.d_order_capacity doc on doc.order_capacity_id = co.order_capacity_id
+         left join dwh.d_sub_system ds on ds.sub_system_unq_id = co.sub_system_unq_id and ds.is_active
 where yc.status_date_id between :in_start_date_id and :in_end_date_id
   and case
           when :in_row_type is null then true
@@ -132,16 +113,33 @@ where yc.status_date_id between :in_start_date_id and :in_end_date_id
   and yc.account_id = any ('{26594,26668,26675,64854}')
   and yc.multileg_reporting_type in ('1', '2')
 -- order by co.create_date_id, co.order_id
--- ;
+;
+
 
 select * from dwh.d_account
 where account_name in ('5CG05976', '5CG05464', '5CG05518', '5CG05523');
 
 
-select  from data_marts.f_yield_capture
-where status_date_id = 20230726;
+select * from data_marts.f_yield_capture
+where status_date_id = 20230726
+and order_id in (109649403,109644969,109649404,109737892,109649482)
+;
 
-select "CrossOrderID", "CrossType" from
+select "CrossOrderID", "CrossType", "ClOrdID" from
 dwh.historic_order_details_storage x
 where "Status_Date_id" = 20230726
-and "CrossOrderID" is not null
+and "CrossOrderID" is not null;
+
+select * from dwh.client_order co
+             where true
+-- and create_date_id = 20230726
+-- and client_order_id = 'DHAA1507-20230726'
+and cross_order_id = 107162675
+
+select cross_order_id, count(distinct order_type_id)
+from dwh.client_order co
+where true
+  and create_date_id > 20230701
+group by cross_order_id
+having count(distinct order_type_id) > 1
+limit 10
