da7cc52aac0ac25ca12a7601fc4379a96f155087
Oleh Sydor <ole.sydor@gmail.com>
2023-12-14 15:00:09 +0200

    Regular auto-commit at 15:00:09 on 14/12/2023

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/getAmeritradeGTD.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/getAmeritradeGTD.sql
@@ -1,12 +1,74 @@
 
 
+create function dash360.report_rps_ofp0011_gtd()
+    returns table(ret_row text)
+    language plpgsql
+    as $$
 
- FUNCTION getAmeritradeGTD(p_date IN DATE) RETURN REF_CURSOR IS
-  rs REF_CURSOR;
-  BEGIN
-    OPEN rs FOR
+    begin
 
-	select
+    create temp table tmp_ameri /*on commit drop*/ as
+
+    select CL.ORDER_ID || '|' ||--Order Number
+           CL.MULTILEG_REPORTING_TYPE,
+           cl.no_legs,            --||'|'|| --Order Leg ID
+           CL.CREATE_TIME,        --||'|'|| --Order Entry Date
+           CL.SIDE,               --||'|'||--Action
+           CL.ORDER_TYPE_id,      --||'|'||--Type
+           CL.ORDER_QTY,          --||'|'||
+           EX.LEAVES_QTY,         --||'|'||
+           di.INSTRUMENT_TYPE_ID, --||'|'||
+           di.INSTRUMENT_TYPE_ID,
+           di.SYMBOL,             --||'|'||
+           os.ROOT_SYMBOL,        --||'|'||
+           oc.PUT_CALL,           --||'|'||
+           CL.OPEN_CLOSE,         --||'|'||
+           di.INSTRUMENT_TYPE_ID,
+           di.LAST_TRADE_DATE,    --||'|'||
+           oc.STRIKE_PRICE,       --||'|'||
+           CL.PRICE,              --||'|'||
+           CL.STOP_PRICE,         --||'|'||
+           CL.TIME_IN_FORCE_id,   --||'|'||
+           CL.EXPIRE_TIME,        --||'|'||
+           fmj.t432,
+           cl.exec_instruction
+    from dwh.gtc_order_status gtc
+             join dwh.client_order cl
+                  on cl.order_id = gtc.order_id and cl.create_date_id = gtc.create_date_id
+             join dwh.d_instrument di on di.instrument_id = cl.instrument_id
+             join lateral (select ex.exec_id as exec_id,
+                                  ex.avg_px,
+                                  ex.leaves_qty,
+                                  ex.order_status
+                           from dwh.execution ex
+                           where gtc.order_id = ex.order_id
+                             and ex.order_status <> '3'
+                             and ex.exec_date_id >= gtc.create_date_id
+                           order by ex.exec_time desc
+                           limit 1) ex on true
+             left join lateral (select fix_message ->> '432' as t432
+                                from fix_capture.fix_message_json fmj
+                                where true
+                                  and fmj.fix_message_id = cl.fix_message_id
+                                  and fmj.date_id = cl.create_date_id
+                                  and fix_message ->> '432' is not null
+                                limit 1) fmj on true
+             left join dwh.d_option_contract oc on oc.instrument_id = cl.instrument_id
+             left join d_option_series os on (oc.option_series_id = os.option_series_id)
+    where true
+		  and AC.TRADING_FIRM_ID in ('OFP0011')
+		  and CL.PARENT_ORDER_ID is NULL
+		  and CL.TIME_IN_FORCE = '6'
+		  and CL.TRANS_TYPE <> 'F'
+		  and CL.MULTILEG_REPORTING_TYPE <> '3'
+		  and CL.ORDER_ID not in (select ORDER_ID from EXECUTION where ORDER_STATUS in ('2','4','8'))
+		  and EX.EXEC_ID = (select max(EXEC_ID) from EXECUTION where ORDER_ID = CL.ORDER_ID and ORDER_STATUS <> '3')
+		  and to_char(NVL(CL.EXPIRE_TIME, to_date((select FIELD_VALUE from FIX_MESSAGE_FIELD where FIX_MESSAGE_ID = CL.FIX_MESSAGE_ID and TAG_NUM = 432),'YYYYMMDD')),'YYYYMMDD') > to_char(p_date, 'YYYYMMDD');
+
+
+
+
+/*	select
 		case
 			when REC_TYPE = 'TRL' then REC||'|'||to_char(REC_NUM-2)
 			else REC
@@ -14,7 +76,15 @@
 	from (
 	  select REC, REC_TYPE, OUT_ORD_FLAG, rownum as REC_NUM
 	  from (
-		 select 'DET' as REC_TYPE, 2 as OUT_ORD_FLAG,
+
+
+		  select  'HDR' as REC_TYPE, 1 as OUT_ORD_FLAG,
+		  'LIQUID2A'||'|'||to_char(systimestamp, 'MMDDYYYY') as REC
+		  from dual
+
+		  union all
+
+   select 'DET' as REC_TYPE, 2 as OUT_ORD_FLAG,
 			 CL.ORDER_ID||'|'||--Order Number
 			 decode(CL.MULTILEG_REPORTING_TYPE,'1','0','2',OLN.LEG_NUMBER)||'|'|| --Order Leg ID
 			 to_char(CL.CREATE_TIME,'MMDDYYYY')||'|'|| --Order Entry Date
@@ -36,40 +106,9 @@
 			 case
 			  when CL.EXEC_INST like '%G%' then 'AON'
 			  when CL.EXEC_INST like '%1%' then 'NH'
-			 end
+			 end;
 
-		as REC
-
-		  from CLIENT_ORDER CL
-		  inner join INSTRUMENT I on I.INSTRUMENT_ID = CL.INSTRUMENT_ID
-		  inner join EXECUTION EX on CL.ORDER_ID = EX.ORDER_ID
-		  left join
-				(select ORDER_ID,
-					CO_MULTILEG_ORDER_ID MULTILEG_ORDER_ID,
-					dense_rank() over (partition by CO_MULTILEG_ORDER_ID order by ORDER_ID) LEG_NUMBER
-		  from CLIENT_ORDER
-		  where MULTILEG_REPORTING_TYPE = '2') OLN on (OLN.ORDER_ID = CL.ORDER_ID)
-		  left join DRV_OPTION DO on (DO.INSTRUMENT_ID = CL.INSTRUMENT_ID)
-		  inner join ACTIVE_ACCOUNT_SNAPSHOT AC on (CL.ACCOUNT_ID = AC.ACCOUNT_ID )
-
-
-		  where 1=1
-		  and AC.TRADING_FIRM_ID in ('OFP0011')
-		  and CL.PARENT_ORDER_ID is NULL
-		  and CL.TIME_IN_FORCE = '6'
-		  and CL.TRANS_TYPE <> 'F'
-		  and CL.MULTILEG_REPORTING_TYPE <> '3'
-		  and CL.ORDER_ID not in (select ORDER_ID from EXECUTION where ORDER_STATUS in ('2','4','8'))
-		  and EX.EXEC_ID = (select max(EXEC_ID) from EXECUTION where ORDER_ID = CL.ORDER_ID and ORDER_STATUS <> '3')
-		  and to_char(NVL(CL.EXPIRE_TIME, to_date((select FIELD_VALUE from FIX_MESSAGE_FIELD where FIX_MESSAGE_ID = CL.FIX_MESSAGE_ID and TAG_NUM = 432),'YYYYMMDD')),'YYYYMMDD') > to_char(p_date, 'YYYYMMDD')
-
-		  union all
-
-		  select  'HDR' as REC_TYPE, 1 as OUT_ORD_FLAG,
-		  'LIQUID2A'||'|'||to_char(systimestamp, 'MMDDYYYY') as REC
-		  from dual
-
-		  union all
+   union all
 
 		  select  'TRL' as REC_TYPE, 3 as OUT_ORD_FLAG,
 		  'Total Number of  Records' as REC
@@ -78,6 +117,7 @@
 	  )
 	)
 	order by OUT_ORD_FLAG;
-
+*/
 	RETURN rs;
   END getAmeritradeGTD;
+$$
