b2182be6b53d276db10ed507182022b9455fe327
Oleh Sydor <ole.sydor@gmail.com>
2023-12-14 17:00:11 +0200

    Regular auto-commit at 17:00:11 on 14/12/2023

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/getAmeritradeGTD.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/getAmeritradeGTD.sql
@@ -1,35 +1,45 @@
-
-
-create function dash360.report_rps_ofp0011_gtd()
-    returns table(ret_row text)
+select * from dash360.report_rps_ofp0011_gtd()
+create or replace function dash360.report_rps_ofp0011_gtd()
+    returns table
+            (
+                ret_row text
+            )
     language plpgsql
-    as $$
+as
+$$
+declare
+    l_account_ids int4[];
+    l_row_cnt     int4;
 
-    begin
+begin
+    select array_agg(account_id)
+    into l_account_ids
+    from dwh.mv_active_account_snapshot ac
+    where ac.is_active
+      and AC.TRADING_FIRM_ID in ('OFP0011');
 
-    create temp table tmp_ameri /*on commit drop*/ as
+--     select '{64998}' into l_account_ids;
 
-    select CL.ORDER_ID, -- || '|' ||--Order Number
-           CL.MULTILEG_REPORTING_TYPE,
-           cl.no_legs,            --||'|'|| --Order Leg ID
-           CL.CREATE_TIME,        --||'|'|| --Order Entry Date
-           CL.SIDE,               --||'|'||--Action
-           CL.ORDER_TYPE_id,      --||'|'||--Type
-           CL.ORDER_QTY,          --||'|'||
-           EX.LEAVES_QTY,         --||'|'||
-           di.INSTRUMENT_TYPE_ID, --||'|'||
-           di.INSTRUMENT_TYPE_ID,
-           di.SYMBOL,             --||'|'||
-           os.ROOT_SYMBOL,        --||'|'||
-           oc.PUT_CALL,           --||'|'||
-           CL.OPEN_CLOSE,         --||'|'||
-           di.INSTRUMENT_TYPE_ID,
-           di.LAST_TRADE_DATE,    --||'|'||
-           oc.STRIKE_PRICE,       --||'|'||
-           CL.PRICE,              --||'|'||
-           CL.STOP_PRICE,         --||'|'||
-           CL.TIME_IN_FORCE_id,   --||'|'||
-           CL.EXPIRE_TIME,        --||'|'||
+    create temp table tmp_ameri on commit drop as
+    select cl.order_id,
+           cl.multileg_reporting_type,
+           cl.no_legs,
+           cl.create_time,
+           cl.side,
+           cl.order_type_id,
+           cl.order_qty,
+           ex.leaves_qty,
+           di.instrument_type_id,
+           di.symbol,
+           os.root_symbol,
+           oc.put_call,
+           cl.open_close,
+           di.last_trade_date,
+           oc.strike_price,
+           cl.price,
+           cl.stop_price,
+           cl.time_in_force_id,
+           cl.expire_time,
            fmj.t432,
            cl.exec_instruction
     from dwh.gtc_order_status gtc
@@ -55,66 +65,71 @@ create function dash360.report_rps_ofp0011_gtd()
              left join dwh.d_option_contract oc on oc.instrument_id = cl.instrument_id
              left join d_option_series os on (oc.option_series_id = os.option_series_id)
     where true
-		  and gtc.account_id in (select account_id from dwh.mv_active_account_snapshot ac where ac.is_active and AC.TRADING_FIRM_ID in ('OFP0011'))
-		  and CL.PARENT_ORDER_ID is NULL
-		  and gtc.TIME_IN_FORCE_id = '6'
-		  and CL.TRANS_TYPE <> 'F'
-		  and CL.MULTILEG_REPORTING_TYPE <> '3'
-       and not exists (select null from dwh.execution ex where ex.order_id = gtc.order_id and ex.exec_date_id >= gtc.create_date_id and ex.order_status in ('2','4','8'))
-and gtc.close_date_id is null;
+      and gtc.account_id = any (l_account_ids)
+      and cl.parent_order_id is null
+      and gtc.time_in_force_id = '6'
+      and cl.trans_type <> 'F'
+      and cl.multileg_reporting_type <> '3'
+      and not exists (select null
+                      from dwh.execution ex
+                      where ex.order_id = gtc.order_id
+                        and ex.exec_date_id >= gtc.create_date_id
+                        and ex.order_status in ('2', '4', '8'))
+      and gtc.close_date_id is null;
+
+    get diagnostics l_row_cnt = row_count;
+
+    return query
+        select 'LIQUID2A' || '|' || to_char(current_date, 'MMDDYYYY');
+
+    return query
+        select array_to_string(array [
+                                   order_id::text,
+                                   case multileg_reporting_type
+                                       when '1' then '0'::text
+                                       when '2' then no_legs::text end, --order leg id
+                                   to_char(create_time, 'MMDDYYYY'), --Order Entry Date
+                                   case side
+                                       when '1' then 'B'
+                                       when '2' then 'S'
+                                       when '5' then 'SS'
+                                       when '6' then 'SS' end, --Action
+                                   case order_type_id
+                                       when '1' then 'M'
+                                       when '2' then 'L'
+                                       when '3' then 'S'
+                                       when '4' then 'SL'
+                                       else 'L' end, --Type
+                                   order_qty::text,
+                                   leaves_qty::text,
+                                   instrument_type_id::text,
+                                   case instrument_type_id when 'E' then symbol end,
+                                   case instrument_type_id when 'O' then root_symbol end,
+                                   case put_call when '0' then 'P' when '1' then 'C' end,
+                                   open_close,
+                                   case instrument_type_id when 'O' then to_char(last_trade_date, 'MMDDYY') end,
+                                   to_char(strike_price, 'FM99990D0099'),
+                                   to_char(price, 'FM99990D0099'),
+                                   to_char(stop_price, 'FM99990D0099'),
+                                   case time_in_force_id
+                                       when '1' then 'GTC'
+                                       when '6' then 'GTD'
+                                       else time_in_force_id end,
+                                   to_char(coalesce(expire_time, to_date(t432, 'YYYYMMDD')), 'MMDDYYYY'),
+                                   case
+                                       when exec_instruction like '%G%' then 'AON'
+                                       when exec_instruction like '%1%' then 'NH'
+                                       end
+                                   ], '|', '') as rec
+        from tmp_ameri;
+
+    return query
+        select 'Total Number of Records' || '|' || l_row_cnt::text;
+
+end;
+$$;
 
 
-
-/*	select
-		case
-			when REC_TYPE = 'TRL' then REC||'|'||to_char(REC_NUM-2)
-			else REC
-		end
-	from (
-	  select REC, REC_TYPE, OUT_ORD_FLAG, rownum as REC_NUM
-	  from (
-
-
-		  select  'HDR' as REC_TYPE, 1 as OUT_ORD_FLAG,
-		  'LIQUID2A'||'|'||to_char(systimestamp, 'MMDDYYYY') as REC
-		  from dual
-
-		  union all
-
-   select 'DET' as REC_TYPE, 2 as OUT_ORD_FLAG,
-			 CL.ORDER_ID||'|'||--Order Number
-			 decode(CL.MULTILEG_REPORTING_TYPE,'1','0','2',OLN.LEG_NUMBER)||'|'|| --Order Leg ID
-			 to_char(CL.CREATE_TIME,'MMDDYYYY')||'|'|| --Order Entry Date
-			 decode(CL.SIDE,'1','B','2','S','5','SS','6','SS')||'|'||--Action
-			 decode(CL.ORDER_TYPE,'1','M','2','L','3','S','4','SL','L')||'|'||--Type
-			 CL.ORDER_QTY||'|'||
-			 EX.LEAVES_QTY||'|'||
-			 I.INSTRUMENT_TYPE_ID||'|'||
-			 decode(I.INSTRUMENT_TYPE_ID,'E',I.SYMBOL)||'|'||
-			 decode(I.INSTRUMENT_TYPE_ID,'O',DO.ROOT_SYMBOL)||'|'||
-			 decode(DO.PUT_CALL,'0','P','1','C')||'|'||
-			 CL.OPEN_CLOSE||'|'||
-			 decode(I.INSTRUMENT_TYPE_ID,'O',to_char(I.LAST_TRADE_DATE,'MMDDYY'))||'|'||
-			 to_char(DO.STRIKE_PRICE, 'FM99990D0099', 'NLS_NUMERIC_CHARACTERS = ''. ''')||'|'||
-			 to_char(CL.PRICE, 'FM99990D0099', 'NLS_NUMERIC_CHARACTERS = ''. ''')||'|'||
-			 to_char(CL.STOP_PRICE, 'FM99990D0099', 'NLS_NUMERIC_CHARACTERS = ''. ''')||'|'||
-			 decode(CL.TIME_IN_FORCE,'1','GTC','6','GTD',CL.TIME_IN_FORCE)||'|'||
-			 to_char(NVL(CL.EXPIRE_TIME, to_date((select FIELD_VALUE from FIX_MESSAGE_FIELD where FIX_MESSAGE_ID = CL.FIX_MESSAGE_ID and TAG_NUM = 432),'YYYYMMDD')),'MMDDYYYY')||'|'||
-			 case
-			  when CL.EXEC_INST like '%G%' then 'AON'
-			  when CL.EXEC_INST like '%1%' then 'NH'
-			 end;
-
-   union all
-
-		  select  'TRL' as REC_TYPE, 3 as OUT_ORD_FLAG,
-		  'Total Number of  Records' as REC
-		  from dual
-
-	  )
-	)
-	order by OUT_ORD_FLAG;
-*/
-	RETURN rs;
-  END getAmeritradeGTD;
-$$
+select * from dwh.gtc_order_status gtc
+where true
+  and gtc.account_id in (49730,49729,63849,19875,58772,57035,57054,28519,19874,69611)
