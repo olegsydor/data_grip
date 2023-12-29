285237db029ec94d6b5c1d5644ac3c6482c834f2
Oleh Sydor <ole.sydor@gmail.com>
2023-12-29 17:00:09 +0200

    Regular auto-commit at 17:00:09 on 29/12/2023

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_24.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_24.sql
@@ -1,20 +1,33 @@
 13735825550
 ClOrdID: Ajklvtdrnt1x
+200475 - 20230601
+54560 - 2023128
 
-select cl.exchange_id, par.sub_strategy_desc, cl.*
+select cl.*
 from dwh.client_order cl
-join lateral (select sub_strategy_desc from dwh.client_order par where par.order_id = cl.parent_order_id and sub_strategy_desc in ('SENSOR', 'DMA') limit 1) par on true
-where cl.create_date_id >= 20230601
-    and cl.create_date_id < 20230701
---   and cl.sub_strategy_desc = 'SENSOR'
---   and cl.account_id in (select account_id from dwh.d_account where trading_firm_id = 'baml')
+where cl.create_date_id = 20231228
+--   and cl.create_date_id < 20230701
   and cl.exchange_id in
       ('ARCAML', 'BATSML', 'BATYML', 'EDGAML', 'EDGXML', 'EPRLML', 'IEXML', 'LTSEML', 'MEMXML', 'NQBXML', 'NSDQML',
        'NSXML', 'NYSEML', 'XASEML', 'XCHIML', 'XPSXML')
--- and cl.order_id = 13735825550
   and parent_order_id is not null
-and par.sub_strategy_desc in ('SENSOR', 'DMA')
+and exists (select sub_strategy_desc
+                       from dwh.client_order par
+                       where par.order_id = cl.parent_order_id
+                         and par.sub_strategy_desc in ('SENSOR', 'DMA')
+                         and par.create_date_id <= cl.create_date_id
+                         and par.sub_strategy_desc in ('SENSOR', 'DMA')
+                       );
+
 
 
 select sub_strategy_id, * from data_marts.f_yield_capture
 where order_id = 13735825550;
+
+
+select par.order_id
+from dwh.client_order par
+         join dwh.client_order str on str.parent_order_id = par.order_id and str.create_date_id >= par.create_date_id
+    and par.sub_strategy_desc in ('SENSOR', 'DMA')
+    and str.create_date_id >= 20230601 and
+                                      str.create_date_id < 20230701;
