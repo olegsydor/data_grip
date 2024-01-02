69a3946025d07740e85a6e0e8adb811c8c2b6bd5
Oleh Sydor <ole.sydor@gmail.com>
2024-01-02 18:00:08 +0200

    Regular auto-commit at 18:00:08 on 02/01/2024

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_24.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_24.sql
@@ -35,7 +35,7 @@ Marketable equity DMA orders routed to BAML Softbot routes list below -I would l
 Equity SENSOR BEST IOC orders routed to BAML Softbot routes list below - I would like any street orders that were filled/partially filled
 as well as the associated parent order ID and routing table name.
 */
-select par.order_id
+select par.order_id, par.transaction_id, str.order_id, str.transaction_id
 from dwh.client_order par
          join dwh.client_order str on str.parent_order_id = par.order_id and str.create_date_id >= par.create_date_id
          left join lateral (select ls.ask_price, ls.bid_price, ls.ask_quantity, ls.bid_quantity
@@ -45,32 +45,24 @@ from dwh.client_order par
                               and ls.start_date_id = to_char(par.create_time, 'YYYYMMDD')::int4
                             limit 1
     ) md on true
---          left join lateral ( select ex.order_id
---                                   , max(ex.exec_type)    as max_exec_type
---                                   , max(ex.order_status) as max_order_status
---                              from dwh.execution ex
---                              where ex.exec_date_id >= 20210101
---                                and ex.exec_date_id <= par.create_date_id
---                                and ex.order_id = par.order_id
---                                and ex.exec_type = 'F'
---                              and ex.is_busted = 'N'
--- --                                and ex.order_status in ('1', '2')
---                              group by ex.order_id
---                              limit 1
---     ) ex on true
-join lateral (select * from dwh.flat_trade_record ftr where ftr.order_id = par.order_id and ftr.date_id >= par.create_date_id limit 1) ftr on true
+         join lateral (select null
+                       from dwh.flat_trade_record ftr
+                       where ftr.order_id = par.order_id
+                         and ftr.date_id >= par.create_date_id
+                         and ftr.is_busted = 'N'
+                       limit 1) ftr on true
 
 where str.create_date_id = 20231228
   and str.exchange_id in
       ('ARCAML', 'BATSML', 'BATYML', 'EDGAML', 'EDGXML', 'EPRLML', 'IEXML', 'LTSEML', 'MEMXML', 'NQBXML', 'NSDQML',
        'NSXML', 'NYSEML', 'XASEML', 'XCHIML', 'XPSXML')
   and par.sub_strategy_desc = 'DMA'
-  and ex.max_order_status in ('1', '2')
   and case
           when par.order_type_id = '1' then true
           when par.side = '1' and par.price >= md.ask_price then true
           when par.side <> '1' and par.price <= md.bid_price then true
           else false end;
+
 
 select dt.target_strategy_desc, fyc.is_marketable, fyc.exchange_id, fyc.* from data_marts.f_yield_capture fyc
          join dwh.d_target_strategy dt on fyc.sub_strategy_id = dt.target_strategy_id
