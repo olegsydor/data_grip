47876058d538ee0ebdab4d7eb82e6ffa53c6f20f
Oleh Sydor <ole.sydor@gmail.com>
2024-01-04 16:00:08 +0200

    Regular auto-commit at 16:00:08 on 04/01/2024

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_24.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_24.sql
@@ -50,16 +50,17 @@ select ftr.date_id,                                                             
        str.order_qty, -- route_qty
        str.price                       as price,     -- price
        ftr.last_px,                                                                                         -- fill_px
-       ftr.last_qty                                                                                         -- fill_qty
+       ftr.last_qty,                                                                                         -- fill_qty,
+md.bid_qty, md.bid_price, md.ask_qty, md.ask_price
 from dwh.client_order par
          join dwh.client_order str on str.parent_order_id = par.order_id and str.create_date_id >= par.create_date_id
-         left join lateral (select ls.ask_price, ls.bid_price, ls.ask_quantity, ls.bid_quantity
-                            from dwh.l1_snapshot ls
-                            where ls.transaction_id = par.transaction_id
-                              and ls.exchange_id = 'NBBO'
-                              and ls.start_date_id = to_char(par.create_time, 'YYYYMMDD')::int4
-                            limit 1
-    ) md on true
+         left join lateral (select *
+                            from dwh.get_routing_market_data(in_transaction_id := par.transaction_id,
+                                                             in_exchange_id := 'NBBO',
+                                                             in_multileg_reporting_type := par.multileg_reporting_type,
+                                                             in_instrument_id := par.instrument_id,
+                                                             in_date_id := par.create_date_id)
+                            limit 1) md on true
          join lateral (select *
                        from dwh.flat_trade_record ftr
                        where ftr.order_id = par.order_id
@@ -67,7 +68,7 @@ from dwh.client_order par
                          and ftr.is_busted = 'N'
                        limit 1) ftr on true
          join dwh.d_account ac on ac.account_id = par.account_id
-         join dwh.d_trading_firm tf on tf.trading_firm_id = ac.trading_firm_unq_id
+         join dwh.d_trading_firm tf on tf.trading_firm_unq_id = ac.trading_firm_unq_id
     join dwh.d_instrument di on di.instrument_id = str.instrument_id
 where str.create_date_id between 20231228 and 20231231
   and str.exchange_id in
@@ -79,6 +80,63 @@ where str.create_date_id between 20231228 and 20231231
           when par.side = '1' and par.price >= md.ask_price then true
           when par.side <> '1' and par.price <= md.bid_price then true
           else false end;
+
+
+select ftr.date_id          as date_id,         -- date_id
+       str.process_time     as execution_time,  -- execution_Time
+       par.order_id         as parent_order_id, -- parent_order_id
+       str.order_id         as order_id,        -- order_id
+       str.process_time     as route_time,      -- route_time
+       str.exchange_id      as exchange_id,     -- exchange_id
+       tf.trading_firm_name as firm_name,       -- firm Name
+       ftr.side             as side,            -- side
+       di.symbol            as symbol,          -- symbol
+       str.order_qty        as route_qty,       -- route_qty
+       str.price            as price,           -- price
+       ftr.last_px          as fill_px,         -- fill_px
+       ftr.last_qty         as fill_qty,        -- fill_qty,
+       md.*
+into trash.so_dma_orders
+from dwh.client_order str
+         join lateral (select *
+                       from dwh.client_order par
+                       where str.parent_order_id = par.order_id
+                         and str.create_date_id >= par.create_date_id
+                         and par.sub_strategy_desc = 'DMA'
+                       limit 1) par on true
+
+         left join lateral (select *
+                            from dwh.get_routing_market_data(in_transaction_id := par.transaction_id,
+                                                             in_exchange_id := 'NBBO',
+                                                             in_multileg_reporting_type := par.multileg_reporting_type,
+                                                             in_instrument_id := par.instrument_id,
+                                                             in_date_id := par.create_date_id)
+                            limit 1) md on true
+         join lateral (select *
+                       from dwh.flat_trade_record ftr
+                       where ftr.order_id = par.order_id
+                         and ftr.date_id >= par.create_date_id
+                         and ftr.is_busted = 'N'
+                       limit 1) ftr on true
+         join dwh.d_account ac on ac.account_id = par.account_id
+         join dwh.d_trading_firm tf on tf.trading_firm_unq_id = ac.trading_firm_unq_id
+         join dwh.d_instrument di on di.instrument_id = str.instrument_id
+where str.create_date_id between 20230601 and 20231231
+  and str.exchange_id in
+      ('ARCAML', 'BATSML', 'BATYML', 'EDGAML', 'EDGXML', 'EPRLML', 'IEXML', 'LTSEML', 'MEMXML', 'NQBXML', 'NSDQML',
+       'NSXML', 'NYSEML', 'XASEML', 'XCHIML', 'XPSXML')
+  and str.parent_order_id is not null
+  and case
+          when par.order_type_id = '1' then true
+          when par.side = '1' and par.price >= md.ask_price then true
+          when par.side <> '1' and par.price <= md.bid_price then true
+          else false end;
+
+
+
+
+
+
 
 
 
@@ -120,3 +178,8 @@ where order_id in (14104242610,
 14101711298
 );
 
+select client_order_id, order_id, * from dwh.client_order cl
+where cl.client_order_id in (
+'10Z2286802212271',
+'10Z2286802211136',
+'10Z2286802211151')
