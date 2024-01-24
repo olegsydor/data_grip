73b404714167d0db81d6bd23554486ee9baccfe4
Oleh Sydor <ole.sydor@gmail.com>
2024-01-24 12:00:09 +0200

    Regular auto-commit at 12:00:09 on 24/01/2024

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_38.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_38.sql
@@ -62,6 +62,7 @@ from preagg
 
 
 -----------------------
+drop table t_so;
 create temp table t_so as
 select co.create_date_id,
        co.order_id as parent_order_id,
@@ -72,15 +73,16 @@ select co.create_date_id,
        co.client_order_id,
        co.fix_connection_id,
        str.order_id,
-       str.create_time
+       str.create_time,
+       vhfm.t_10057
 from dwh.client_order co
-         join lateral (select fix_message_id, fix_message ->> '9000' tgt_strategy
+         join lateral (select fix_message_id, fix_message ->> '9000' tgt_strategy, fix_message ->> '10057' as t_10057
                        from fix_capture.fix_message_json fmj
                        where fmj.date_id = co.create_date_id
                          and fmj.fix_message_id = co.fix_message_id
-                         and fix_message ->> '9000' is not null --= 'VOLHEDGE'
-                         and fix_message ->> '10057' is not null
-                         and fix_message ->> '35' not in ('8', '9')
+--                          and fix_message ->> '9000' is not null --= 'VOLHEDGE'
+--                          and fix_message ->> '10057' is not null
+                         and fmj.message_type not in ('8', '9')
                          and fmj.date_id = :date_id
                        limit 1) vhfm on true
          join dwh.client_order str on str.parent_order_id = co.order_id and str.create_date_id >= co.create_date_id and
@@ -89,7 +91,7 @@ from dwh.client_order co
 where co.create_date_id = :date_id
   and co.parent_order_id is null;
 
-create index on t_so (parent_order_id)
+create index on t_so (parent_order_id);
 
 select so.parent_order_id as order_id
                      , count(1)              street_cnt
@@ -119,4 +121,38 @@ select clo.create_date_id
            else 1 end
     as street_order_eps
 from preagg
-         join clo on clo.order_id = preagg.order_id
+         join clo on clo.order_id = preagg.order_id;
+
+create temp table t_os as
+select clo.create_date_id
+     , clo.order_id
+     , clo.orig_order_id
+     , clo.account_id
+     , clo.client_order_id
+     , clo.fix_connection_id
+     , di.instrument_type_id
+     , vhfm.tgt_strategy,
+       str.min_create_time,
+       str.max_create_time,
+       str.street_cnt
+from dwh.client_order clo
+join dwh.d_instrument di on di.instrument_id = clo.instrument_id
+join lateral (select fix_message_id, fix_message ->> '9000' tgt_strategy, fix_message ->> '10057' as t_10057
+                       from fix_capture.fix_message_json fmj
+                       where fmj.date_id = clo.create_date_id
+                         and fmj.fix_message_id = clo.fix_message_id
+--                          and fix_message ->> '9000' is not null --= 'VOLHEDGE'
+--                          and fix_message ->> '10057' is not null
+                         and fmj.message_type not in ('8', '9')
+                         and fmj.date_id = :date_id
+                       limit 1) vhfm on true
+join lateral (select so.parent_order_id as order_id
+                     , count(1)              street_cnt
+                     , min(so.create_time)   min_create_time
+                     , max(so.create_time)   max_create_time
+                from dwh.client_order so
+                where so.parent_order_id = clo.order_id
+                and so.create_date_id >= :date_id
+                group by 1) str on true
+where create_date_id = :date_id
+               and parent_order_id is null
