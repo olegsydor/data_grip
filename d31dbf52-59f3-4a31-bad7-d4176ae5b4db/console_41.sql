a850c80c6106452b45534b4632041a9e55dcfe99
Oleh Sydor <ole.sydor@gmail.com>
2024-01-26 14:03:58 +0200

    Regular auto-commit at 14:03:56 on 26/01/2024

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_41.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_41.sql
@@ -184,45 +184,51 @@ FROM trash.so_fyc_perf_new
                  inner join dwh.d_instrument i on i.instrument_id = po.instrument_id
                  left join dwh.d_target_strategy dss on po.sub_strategy_id = dss.target_strategy_id
                  inner join data_marts.d_account acc on (acc.account_id = po.account_id)
-                 join lateral (
-            select first_value(fyc.wave_no) over w                 as wave_no_first,
-                   last_value(fyc.wave_no) over w                  as wave_no_last,
+                 join lateral ( select first_value(fyc.wave_no) over w                 as wave_no_first,
+                                       last_value(fyc.wave_no) over w                  as wave_no_last,
 
-                   first_value(fyc.multileg_reporting_type) over w as multileg_reporting_type_first,
-                   last_value(fyc.multileg_reporting_type) over w  as multileg_reporting_type_last,
+                                       first_value(fyc.multileg_reporting_type) over w as multileg_reporting_type_first,
+                                       last_value(fyc.multileg_reporting_type) over w  as multileg_reporting_type_last,
 
-                   first_value(fyc.transaction_id) over w          as transaction_id_first,
-                   last_value(fyc.transaction_id) over w           as transaction_id_last,
+                                       first_value(fyc.transaction_id) over w          as transaction_id_first,
+                                       last_value(fyc.transaction_id) over w           as transaction_id_last,
 
-                   first_value(fyc.status_date_id) over w          as status_date_id_first,
-                   last_value(fyc.status_date_id) over w           as status_date_id_last,
+                                       first_value(fyc.status_date_id) over w          as status_date_id_first,
+                                       last_value(fyc.status_date_id) over w           as status_date_id_last,
 
-                   first_value(fyc.instrument_id) over w          as instrument_id_first,
-                   last_value(fyc.instrument_id) over w           as instrument_id_last
+                                       first_value(fyc.instrument_id) over w           as instrument_id_first,
+                                       last_value(fyc.instrument_id) over w            as instrument_id_last
 
-            from data_marts.f_yield_capture fyc
-            where fyc.parent_order_id = po.order_id
-              and fyc.parent_order_id is not null
-              and fyc.status_date_id >= :start_status_date_id
-            window w as (partition by parent_order_id order by wave_no)
-            order by wave_no desc limit 1
+                                from data_marts.f_yield_capture fyc
+                                where fyc.parent_order_id = po.order_id
+                                  and fyc.parent_order_id is not null
+                                  and fyc.status_date_id >= :start_status_date_id
+                                window w as (partition by parent_order_id order by wave_no)
+                                order by wave_no desc
+                                limit 1
             ) mx on true
-             left join lateral (select ls.ask_price, ls.bid_price, ls.ask_quantity as ask_qty, ls.bid_quantity as bid_qty
-                                from dwh.l1_snapshot ls
-                                where ls.transaction_id = transaction_id_first
-                                  and ls.exchange_id = 'NBBO'
-                                  and ls.start_date_id = status_date_id_first--to_char(clo.create_time, 'YYYYMMDD')::int4
+                 left join lateral (select ls.ask_price,
+                                           ls.bid_price,
+                                           ls.ask_quantity as ask_qty,
+                                           ls.bid_quantity as bid_qty
+                                    from dwh.l1_snapshot ls
+                                    where ls.transaction_id = transaction_id_first
+                                      and ls.exchange_id = 'NBBO'
+                                      and ls.start_date_id = status_date_id_first--to_char(clo.create_time, 'YYYYMMDD')::int4
 --                                    and ls.start_date_id = str.create_date_id
-                                limit 1
-        ) min_wave on true
-             left join lateral (select ls.ask_price, ls.bid_price, ls.ask_quantity as ask_qty, ls.bid_quantity as bid_qty
-                                from dwh.l1_snapshot ls
-                                where ls.transaction_id = transaction_id_last
-                                  and ls.exchange_id = 'NBBO'
-                                  and ls.start_date_id = status_date_id_last--to_char(clo.create_time, 'YYYYMMDD')::int4
+                                    limit 1
+            ) min_wave on true
+                 left join lateral (select ls.ask_price,
+                                           ls.bid_price,
+                                           ls.ask_quantity as ask_qty,
+                                           ls.bid_quantity as bid_qty
+                                    from dwh.l1_snapshot ls
+                                    where ls.transaction_id = transaction_id_last
+                                      and ls.exchange_id = 'NBBO'
+                                      and ls.start_date_id = status_date_id_last--to_char(clo.create_time, 'YYYYMMDD')::int4
 --                                    and ls.start_date_id = str.create_date_id
-                                limit 1
-        ) max_wave on true
+                                    limit 1
+            ) max_wave on true
         where po.parent_order_id is null
           and po.status_date_id >= :start_status_date_id
           and po.status_date_id <= :end_status_date_id
@@ -232,7 +238,7 @@ FROM trash.so_fyc_perf_new
           and po.account_id in
               (24993, 19676, 52064, 36679, 52101, 51465, 51464, 63695, 52061, 52062, 52066, 52067, 52063, 36680, 36675,
                36681, 52065, 58770, 70279, 19681, 19634)
-        and po.order_id = 13317023312
+          and po.order_id = 13317023312
         order by po.exec_time;
 end;
 
@@ -344,7 +350,164 @@ select *,
                                                                                  in_order_id := fyc.parent_order_id,
                                                                                  in_multileg_reporting_type := fyc.multileg_reporting_type),
                                                                          in_date_id := fyc.status_date_id)).*
-from trash.so_fyc_perf_new fyc
+from trash.so_fyc_perf fyc
 where multileg_reporting_type <> '1';
 
-select
+------------------------------------------------------------------------------------------------------------------------
+
+  create temp table t_report as
+        select po.exec_time,
+               po.routed_time,
+               po.status_date_id,
+               po.transaction_id,
+               po.order_id,                                                                  -- 5
+               po.client_order_id,
+               po.multileg_reporting_type,
+               tif.tif_name,
+               ot.order_type_name,
+               po.order_price,                                                               -- 10
+               po.day_order_qty,
+               po.day_cum_qty,
+               po.day_avg_px,
+               po.side,
+               po.client_id,                                                                 -- 15
+               acc.account_name,
+               po.is_marketable,
+               po.cross_order_id,
+               po.instrument_type_id    as                           sec_type_id,
+               i.display_instrument_id,                                                      -- 20
+               i.last_trade_date,
+               dss.target_strategy_name as                           sub_strategy,
+               po.num_exch,
+               po.nbbo_bid_price,
+               po.nbbo_bid_quantity,      -- 25
+               po.nbbo_ask_price,
+               po.nbbo_ask_quantity,
+               mx.*
+        from data_marts.f_yield_capture po
+                 inner join dwh.d_time_in_force tif on tif.is_active and tif.tif_id = po.time_in_force_id
+                 inner join dwh.d_order_type ot on ot.order_type_id = po.order_type_id
+                 inner join dwh.d_instrument i on i.instrument_id = po.instrument_id
+                 left join dwh.d_target_strategy dss on po.sub_strategy_id = dss.target_strategy_id
+                 inner join data_marts.d_account acc on (acc.account_id = po.account_id)
+                 join lateral ( select --first_value(fyc.wave_no) over w                 as wave_no_first,
+                                       last_value(fyc.wave_no) over w                  as wave_no_last,
+
+                                       first_value(fyc.multileg_reporting_type) over w as multileg_reporting_type_first,
+                                       last_value(fyc.multileg_reporting_type) over w  as multileg_reporting_type_last,
+
+                                       first_value(fyc.transaction_id) over w          as transaction_id_first,
+                                       last_value(fyc.transaction_id) over w           as transaction_id_last,
+
+                                       first_value(fyc.status_date_id) over w          as status_date_id_first,
+                                       last_value(fyc.status_date_id) over w           as status_date_id_last,
+
+                                       first_value(fyc.instrument_id) over w           as instrument_id_first,
+                                       last_value(fyc.instrument_id) over w            as instrument_id_last
+
+                                from data_marts.f_yield_capture fyc
+                                where fyc.parent_order_id = po.order_id
+                                  and fyc.parent_order_id is not null
+                                  and fyc.status_date_id >= :start_status_date_id
+                                window w as (partition by parent_order_id order by wave_no)
+                                order by wave_no desc
+                                limit 1
+            ) mx on true
+        where po.parent_order_id is null
+          and po.status_date_id >= :start_status_date_id
+          and po.status_date_id <= :end_status_date_id
+          and po.multileg_reporting_type in ('1', '2')
+          and po.instrument_type_id = :in_instrument_type_id
+          and po.TIME_IN_FORCE_id IN ('0', '2', '3', '4')
+          and po.account_id in
+              (24993, 19676, 52064, 36679, 52101, 51465, 51464, 63695, 52061, 52062, 52066, 52067, 52063, 36680, 36675,
+               36681, 52065, 58770, 70279, 19681, 19634);
+
+select po.exec_time,
+               po.routed_time,
+               po.status_date_id,
+               po.transaction_id,
+               po.order_id,                                                                  -- 5
+               po.client_order_id,
+               po.multileg_reporting_type,
+               po.tif_name,
+               po.order_type_name,
+               po.order_price,                                                               -- 10
+               po.day_order_qty,
+               po.day_cum_qty,
+               po.day_avg_px,
+               po.side,
+               po.client_id,                                                                 -- 15
+               po.account_name,
+               po.is_marketable,
+               po.cross_order_id,
+               po.sec_type_id,
+               po.display_instrument_id,                                                      -- 20
+               po.last_trade_date,
+               po.sub_strategy,
+               po.num_exch,
+               coalesce(po.nbbo_bid_price, min_wave.bid_price)       nbbo_bid_price,
+               coalesce(po.nbbo_bid_quantity, min_wave.bid_qty)::int nbbo_bid_quantity,      -- 25
+               coalesce(po.nbbo_ask_price, min_wave.ask_price)       nbbo_ask_price,
+               coalesce(po.nbbo_ask_quantity, min_wave.ask_qty)::int nbbo_ask_quantity,
+               po.order_id as parent_order_id_of_street,
+               po.wave_no_last as wave_no,
+               min_wave.bid_price                                    first_wave_nbbo_bid_px, -- 30
+               max_wave.bid_price                                    last_wave_nbbo_bid_px,
+               min_wave.ask_price                                    first_wave_nbbo_ask_px,
+               max_wave.ask_price                                    last_wave_nbbo_ask_px,
+               min_wave.bid_qty                                      first_wave_nbbo_bid_qty,
+               max_wave.bid_qty                                      last_wave_nbbo_bid_qty, -- 35
+               min_wave.ask_qty                                      first_wave_nbbo_ask_qty,
+               max_wave.ask_qty                                      last_wave_nbbo_ask_qty
+, min2.*
+,max2.*
+from t_report po
+  left join lateral (select ls.ask_price,
+                                           ls.bid_price,
+                                           ls.ask_quantity as ask_qty,
+                                           ls.bid_quantity as bid_qty
+                                    from dwh.l1_snapshot ls
+                                    where ls.transaction_id = transaction_id_first
+                                      and ls.exchange_id = 'NBBO'
+                                      and ls.start_date_id = status_date_id_first--to_char(clo.create_time, 'YYYYMMDD')::int4
+--                                    and ls.start_date_id = str.create_date_id
+                                    limit 1
+            ) min_wave on true
+                 left join lateral (select ls.ask_price,
+                                           ls.bid_price,
+                                           ls.ask_quantity as ask_qty,
+                                           ls.bid_quantity as bid_qty
+                                    from dwh.l1_snapshot ls
+                                    where ls.transaction_id = transaction_id_last
+                                      and ls.exchange_id = 'NBBO'
+                                      and ls.start_date_id = status_date_id_last--to_char(clo.create_time, 'YYYYMMDD')::int4
+--                                    and ls.start_date_id = str.create_date_id
+                                    limit 1
+            ) max_wave on true
+  left join lateral (select ask_price,
+                            bid_price,
+                            ask_qty,
+                            bid_qty
+                     from dwh.get_routing_market_data(in_transaction_id := transaction_id_first,
+                                                      in_exchange_id := 'NBBO',
+                                                      in_multileg_reporting_type := '2',
+                                                      in_instrument_id := dwh.get_multileg_head_instrument_id(
+                                                              in_order_id := po.order_id,
+                                                              in_multileg_reporting_type := '2'),
+                                                      in_date_id := status_date_id_first)
+                     where po.multileg_reporting_type_first = '2'
+                     limit 1) min2 on true
+  left join lateral (select ask_price,
+                            bid_price,
+                            ask_qty,
+                            bid_qty
+                     from dwh.get_routing_market_data(in_transaction_id := transaction_id_last,
+                                                      in_exchange_id := 'NBBO',
+                                                      in_multileg_reporting_type := '2',
+                                                      in_instrument_id := dwh.get_multileg_head_instrument_id(
+                                                              in_order_id := po.order_id,
+                                                              in_multileg_reporting_type := '2'),
+                                                      in_date_id := status_date_id_last)
+                     where po.multileg_reporting_type_last = '2'
+                     limit 1) max2 on true
