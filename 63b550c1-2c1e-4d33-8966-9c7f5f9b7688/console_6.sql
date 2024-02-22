027653a154d69a1d6a579cc9078fd2af76c54b92
Oleh Sydor <ole.sydor@gmail.com>
2024-02-22 09:00:07 +0200

    Regular auto-commit at 09:00:07 on 22/02/2024

--- 63b550c1-2c1e-4d33-8966-9c7f5f9b7688/console_6.sql
+++ 63b550c1-2c1e-4d33-8966-9c7f5f9b7688/console_6.sql
@@ -1,63 +1,6 @@
-drop table if exists training.check_big_operation;
-create table training.check_big_operation
-(
-    big_operation_id int8      not null
-        constraint big_operation_id_pk primary key,
-    operation_number int4      not null,
-    operation_text   text,
-    db_create_time   timestamp not null default clock_timestamp(),
-    db_update_time   timestamp
-);
+select *--relname, n_live_tup, n_dead_tup, last_vacuum, last_autovacuum, last_analyze, last_autoanalyze
+from pg_stat_user_tables;
 
-select * from training.f_big_operation_processing()
-
-create or replace function training.f_big_operation_processing()
-    returns int4
-    language plpgsql
-as
-$fn$
-declare
-    l_row_ins int4;
-    l_row_del int4 := 0;
-begin
-    -- insert/update part
-
-    create temp table t_ins on commit drop as
-    select distinct round(random() * 150000000) as id,
-                    round(random() * 10000)     as num,
-                    md5(random()::text)         as txt
-    from generate_series(1, 100000) s(i);
-
-    insert into training.check_big_operation (big_operation_id, operation_number, operation_text)
-    select distinct on (id) id,
-                            num,
-                            txt
-    from t_ins
-    on conflict (big_operation_id) do update
-        set operation_number = excluded.operation_number,
-            operation_text   = excluded.operation_text,
-            db_update_time   = clock_timestamp();
-
-    get diagnostics l_row_ins = row_count;
-
-    -- delete part
-    delete
-    from training.check_big_operation
-    where big_operation_id in (select round(random() * 150000000) as id
-                               from generate_series(1, 10000) s(i));
-
-    get diagnostics l_row_del = row_count;
-
-    return l_row_ins - l_row_del;
-end;
-$fn$;
-
-select count(*) from training.check_big_operation;
-
-select * from training.check_big_operation
-where db_update_time is not null;
-vacuum full training.check_big_operation;
-analyze training.check_big_operation;
 
 SELECT current_database()                                                          AS current_database,
        s3.schemaname,
@@ -191,6 +134,7 @@ ORDER BY ((s3.tblpages::double precision - s3.est_tblpages) * s3.bs::double prec
                  END) DESC;
 
 
+
 WITH table_opts AS (SELECT c_1.oid,
                            c_1.relname,
                            c_1.relfrozenxid,
@@ -285,15 +229,3 @@ WHERE (v.autovacuum_vacuum_threshold::double precision +
       s.n_mod_since_analyze::double precision
    OR age(v.relfrozenxid)::bigint > v.autovacuum_freeze_max_age
    OR mxid_age(v.relminmxid)::bigint > v.autovacuum_multixact_freeze_max_age;
-
-select *--relname, n_live_tup, n_dead_tup, last_vacuum, last_autovacuum, last_analyze, last_autoanalyze
-from pg_stat_user_tables
-order by n_dead_tup desc;
-
-SELECT *
-FROM pg_stat_activity;
-
-CREATE EXTENSION pg_stat_statements;
-
-SELECT *
-FROM pg_stat_statements;
