ac45e080eb5b82e81de5f11accc8039ebed12cfb
Oleh Sydor <ole.sydor@gmail.com>
2024-02-27 20:00:09 +0200

    Regular auto-commit at 20:00:09 on 27/02/2024

--- f1ad5881-6661-44d6-9cc9-ebb289d5f0f6/console_3.sql
+++ f1ad5881-6661-44d6-9cc9-ebb289d5f0f6/console_3.sql
@@ -149,6 +149,8 @@ begin
                            ' execution flow update', l_row_cnt, 'U')
     into l_step_id;
 
+    create index on staging.gtc_base_modif (order_id);
+
     -- parent flow
     insert into staging.gtc_base_modif (order_id, close_date_id, order_status, closing_reason, client_order_id,
                                         multileg_reporting_type)
@@ -171,7 +173,8 @@ begin
                            order by exec_id desc
                            limit 1) iex_par on true
     where gtc.close_date_id is null
-      and str.parent_order_id is not null;
+      and str.parent_order_id is not null
+      and not exists (select null from staging.gtc_base_modif bm where bm.order_id = gtc.order_id);
 
     get diagnostics l_row_cnt = row_count;
     select public.load_log(l_load_id, l_step_id,
@@ -190,7 +193,8 @@ begin
            multileg_reporting_type
     from dwh.gtc_order_status gtc
     where gtc.close_date_id is null
-      and gtc.last_trade_date::date < l_end_date_id::text::date;
+      and gtc.last_trade_date::date < l_end_date_id::text::date
+      and not exists (select null from staging.gtc_base_modif bm where bm.order_id = gtc.order_id);
 
     get diagnostics l_row_cnt = row_count;
 
@@ -215,7 +219,8 @@ begin
                            limit 1) t on true
     where true
       and gtc.close_date_id is null
-      and gtc.multileg_reporting_type = '3';
+      and gtc.multileg_reporting_type = '3'
+      and not exists (select null from staging.gtc_base_modif bm where bm.order_id = gtc.order_id);
 
     get diagnostics l_row_cnt = row_count;
     select public.load_log(l_load_id, l_step_id,
@@ -241,7 +246,8 @@ begin
                            limit 1) t on true
     where true
       and gtc.close_date_id is null
-      and gtc.multileg_reporting_type = '2';
+      and gtc.multileg_reporting_type = '2'
+      and not exists (select null from staging.gtc_base_modif bm where bm.order_id = gtc.order_id);
 
     get diagnostics l_row_cnt = row_count;
 
@@ -275,6 +281,8 @@ begin
                            l_row_cnt_total,
                            'U')
     into l_step_id;
+
+    select count(*) into l_row_cnt_total from staging.gtc_base_modif;
     return l_row_cnt_total;
 end;
 $function$
@@ -288,217 +296,3 @@ select * from trash.so_gtc_update_daily();
 
 select * from staging.gtc_base_modif;
 select * from trash.so_gtc_update_daily2()
-
--------
-CREATE OR REPLACE FUNCTION trash.so_gtc_update_daily2(p_start_date_id integer DEFAULT NULL::integer, p_end_date_id integer DEFAULT NULL::integer)
- RETURNS integer
- LANGUAGE plpgsql
- SET application_name TO 'ETL: GTC update process'
-AS $function$
--- 2022-09-02 https://dashfinancial.atlassian.net/browse/DS-5561 add logic to close gtc by parent executions
--- 2023-05-01 https://dashfinancial.atlassian.net/browse/DS-3581 closing heads after closing all legs
--- 2023-05-16 https://dashfinancial.atlassian.net/browse/DS-6745 closing head after at least one of legs was closed and then closing all other legs if the head was closed
--- 2023-08-17 https://dashfinancial.atlassian.net/browse/DS-7183 PD added multileg_order_id condition
--- 2024-01-08 https://dashfinancial.atlassian.net/browse/DS-7800 OS added a condition to prevent input end_date later then today
--- 2024-01-11 https://dashfinancial.atlassian.net/browse/DS-7809 OS added logging into dwh.fact_last_load_time for zabbix monitoring
--- 2024-02-27 https://dashfinancial.atlassian.net/browse/DS-8029 OS added instrument_id and multileg_reporting_type into the flow and performance improvement for gtc_update
-declare
-    l_row_cnt       int;
-    l_row_cnt_total int;
-    l_load_id       int;
-    l_step_id       int;
-    l_start_date_id int4;
-    l_end_date_id   int4;
-begin
-    l_start_date_id = coalesce(p_start_date_id, to_char(public.get_last_workdate(), 'YYYYMMDD')::int);
-    l_end_date_id = coalesce(p_end_date_id, to_char(current_date, 'YYYYMMDD')::int);
-    select nextval('public.load_timing_seq') into l_load_id;
-    l_step_id := 1;
-    select public.load_log(l_load_id, l_step_id,
-                           'dwh.gtc_update_daily for ' || l_start_date_id::text || ' - ' || l_end_date_id::text ||
-                           ' STARTED===', 0, 'O')
-    into l_step_id;
-    if l_end_date_id > to_char(current_date, 'YYYYMMDD')::int then
-        select public.load_log(l_load_id, l_step_id,
-                               'dwh.gtc_update_daily for ' || l_start_date_id::text || ' - ' || l_end_date_id::text ||
-                               ' Stopped as the end_date is further than today', -1, 'E')
-        into l_step_id;
-        return -1;
-    end if;
-    -- Aggregating data
-    -- street/execution flow
-    create temp table base_modif /*on commit drop*/ as
-    select co.order_id,
-           iex.close_date_id as close_date_id,
-           iex.order_status  as order_status,
-           'E'               as closing_reason
-    from dwh.gtc_order_status co
-             join lateral (select to_char(iex.exec_time, 'YYYYMMDD')::int4 as close_date_id,
-                                  iex.order_status
-                           from dwh.execution iex
-                           where true
-                             and iex.order_id = co.order_id
-                             and iex.order_status in ('2', '4', '8')
-                             and exec_date_id between l_start_date_id and l_end_date_id
-                           order by exec_id desc
-                           limit 1) iex on true
-    where co.close_date_id is null;
---     update dwh.gtc_order_status co
---     set exec_time      = base.exec_time,
---         order_status   = base.order_status,
---         close_date_id  = to_char(base.exec_time, 'YYYYMMDD')::int4,
---         db_update_time = clock_timestamp(),
---         closing_reason = 'E'
---     from base
---     where base.order_id = co.order_id
---       and co.close_date_id is null;
-    get diagnostics l_row_cnt = row_count;
-    select public.load_log(l_load_id, l_step_id,
-                           'dwh.gtc_update_daily for ' || l_start_date_id::text || ' - ' || l_end_date_id::text ||
-                           ' execution flow update', l_row_cnt, 'U')
-    into l_step_id;
-    l_row_cnt_total = l_row_cnt;
-    -- parent flow
-    insert into base_modif (order_id, close_date_id, order_status, closing_reason)
-    select co.order_id,
-           iex_par.close_date_id    as close_date_id,
-           iex_par.order_status as order_status,
-           'P'                  as closing_reason
-    from dwh.gtc_order_status co
-             join dwh.client_order str
-                  on str.order_id = co.order_id and str.create_date_id = co.create_date_id
-             join lateral (select to_char(iex.exec_time, 'YYYYMMDD')::int4 as close_date_id,
-                                  iex.order_status
-                           from dwh.execution iex
-                           where true
-                             and iex.order_id = str.parent_order_id
-                             and iex.order_status in ('2', '4', '8')
-                             and exec_date_id between l_start_date_id and l_end_date_id
-                           order by exec_id desc
-                           limit 1) iex_par on true
-    where co.close_date_id is null
-      and str.parent_order_id is not null;
---     update dwh.gtc_order_status co
---     set exec_time      = base.exec_time,
---         order_status   = base.order_status,
---         close_date_id  = to_char(base.exec_time, 'YYYYMMDD')::int4,
---         db_update_time = clock_timestamp(),
---         closing_reason = 'P'
---     from base
---     where base.order_id = co.order_id
---       and co.close_date_id is null;
-    get diagnostics l_row_cnt = row_count;
-    select public.load_log(l_load_id, l_step_id,
-                           'dwh.gtc_update_daily for ' || l_start_date_id::text || ' - ' || l_end_date_id::text ||
-                           ' parent flow update', l_row_cnt, 'U')
-    into l_step_id;
-    l_row_cnt_total = l_row_cnt_total + l_row_cnt;
-    -- instrument flow
-    insert into base_modif (order_id, close_date_id, order_status, closing_reason)
-    select gtc.order_id, to_char(last_trade_date, 'YYYYMMDD')::int4, gtc.order_status, 'I'
-    from dwh.gtc_order_status gtc
-    where gtc.close_date_id is null
-      and gtc.last_trade_date::date < l_end_date_id::text::date;
---     update dwh.gtc_order_status co
---     set close_date_id  = to_char(last_trade_date, 'YYYYMMDD')::int4,
---         db_update_time = clock_timestamp(),
---         closing_reason = 'I'
---     where co.close_date_id is null
---       and last_trade_date::date < l_end_date_id::text::date;
-    get diagnostics l_row_cnt = row_count;
-    l_row_cnt_total = l_row_cnt_total + l_row_cnt;
-    select public.load_log(l_load_id, l_step_id,
-                           'dwh.gtc_update_daily for ' || l_start_date_id::text || ' - ' || l_end_date_id::text ||
-                           ' instrument/client order update', l_row_cnt, 'U')
-    into l_step_id;
-    -- head of multileg
-    insert into base_modif(order_id, close_date_id, order_status, closing_reason)
-    select gtc.order_id,
-           t.close_date_id,
-           gtc.order_status,
-           'L'
-    from dwh.gtc_order_status gtc
-             join lateral (select gos.close_date_id
-                           from dwh.gtc_order_status gos
-                           where gos.client_order_id = gtc.client_order_id
---                                            and c.multileg_order_id = co.order_id
-                             and gos.multileg_reporting_type <> '3'
-                             and gos.close_date_id is not null
-                           limit 1) t on true
-    where true
-      and gtc.close_date_id is null
-      and gtc.multileg_reporting_type = '3';
---     update dwh.gtc_order_status gos
---     set close_date_id  = base.close_date_id,
---         db_update_time = clock_timestamp(),
---         closing_reason = 'L'
---     from base
---     where gos.order_id = base.order_id
---       and gos.create_date_id = base.create_date_id
---       and gos.close_date_id is null;
-    get diagnostics l_row_cnt = row_count;
-    select public.load_log(l_load_id, l_step_id,
-                           'dwh.gtc_update_daily for ' || l_start_date_id::text || ' - ' || l_end_date_id::text ||
-                           ' Heads of multilegs after closed leg', l_row_cnt, 'U')
-    into l_step_id;
-    l_row_cnt_total = l_row_cnt_total + l_row_cnt;
-    -- legs after the head has been closed
-    insert into base_modif(order_id, close_date_id, order_status, closing_reason)
-    select gtc.order_id,
-           t.close_date_id,
-           gtc.order_status,
-           'H'
-    from dwh.gtc_order_status gtc
-             join lateral (select gos.close_date_id
-                           from dwh.gtc_order_status gos
-                           where gos.client_order_id = gtc.client_order_id
-                             and gos.multileg_reporting_type = '3'
-                             and gos.close_date_id is not null
-                           limit 1) t on true
-    where true
-      and gtc.close_date_id is null
-      and gtc.multileg_reporting_type <> '3';
---     update dwh.gtc_order_status gos
---     set close_date_id  = base.close_date_id,
---         db_update_time = clock_timestamp(),
---         closing_reason = 'H'
---     from base
---     where gos.order_id = base.order_id
---       and gos.create_date_id = base.create_date_id
---       and gos.close_date_id is null;
-    get diagnostics l_row_cnt = row_count;
-    l_row_cnt_total = l_row_cnt_total + l_row_cnt;
-    select public.load_log(l_load_id, l_step_id,
-                           'dwh.gtc_update_daily for ' || l_start_date_id::text || ' - ' || l_end_date_id::text ||
-                           ' Legs after the heads was closed', l_row_cnt, 'U')
-    into l_step_id;
-    -- Logging into the table dwh.fact_last_load_time
-    update dwh.gtc_order_status gtc
-    set close_date_id = base.close_date_id,
-        db_update_time = clock_timestamp(),
-        closing_reason = base.closing_reason,
-        order_status = base.order_status
-    from base_modif base
-    where gtc.order_id = base.order_id
-    and gtc.close_date_id is null;
-    get diagnostics l_row_cnt = row_count;
-   perform dwh.p_upd_fact_last_load_time('GTC_ORDER_STATUS');
-   select public.load_log(l_load_id, l_step_id,
-                           'dwh.gtc_update_daily for ' || l_start_date_id::text || ' - ' || l_end_date_id::text ||
-                           ' Logging into GTC_ORDER_STATUS total', l_row_cnt, 'U')
-    into l_step_id;
-    -- End of logging into the table dwh.fact_last_load_time
- select public.load_log(l_load_id, l_step_id,
-                           'dwh.gtc_update_daily for ' || l_start_date_id::text || ' - ' || l_end_date_id::text ||
-                           ' COMPLETED===',
-                           l_row_cnt_total,
-                           'U')
-    into l_step_id;
-    return l_row_cnt_total;
-end;
-$function$
-;
-
-
-select * from dwh.gtc_order_status
-where order_id in  (283685675, 283685676,283685677);
