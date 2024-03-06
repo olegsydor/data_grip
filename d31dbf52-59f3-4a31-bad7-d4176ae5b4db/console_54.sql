52941f35611a1f1084e0fb8480de5495fb31d648
Oleh Sydor <ole.sydor@gmail.com>
2024-03-06 10:38:52 +0200

    Regular auto-commit at 10:38:52 on 06/03/2024

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_54.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_54.sql
@@ -69,64 +69,58 @@ AS
 $function$
 declare
     v_settle_date date;
+    v_offset      int;
 begin
 
-
-    --Securities and Settlement Periods
+--Securities and Settlement Periods
 --Security type Settlement Date (Prior to 09/05/17) Settlement Date (Beginning on 09/05/17)
 --Stocks 3 Market days after trade date 2 Market days after trade date
 --Exchange Traded Funds (ETFs) 3 Market days after trade date 2 Market days after trade date
 --Mutual Funds 1 Market days after trade date 1 Market days after trade date
 --Options 1 Market days after trade date 1 Market days after trade date
+-- 2024-03-06
+    v_offset := case
+                    when instrument_type = 'E' and trade_date between '2017-05-09'::date and '2024-05-27'::date then 1
+                    else 0 end;
 
-    if trade_date >= '2024-05-28' then
-        select date_range.period
-        into v_settle_date
-        from (select (generate_series(trade_date, trade_date + 10, '1 day'::interval))::date as period) date_range
-        where date_range.period not in (select banking_holiday_date from public.banking_holiday_calendar b)
-          and date_range.period not in (select holiday_date from public.holiday_calendar h)
-          and extract(dow from date_range.period) not in (0, 6)
-        order by 1
-        offset 1 limit 1;
-    elsif (instrument_type = 'E' and trade_date < to_date('09/05/2017', 'mm/dd/yyyy')) then
-        select x.period as equities_sd
-        into v_settle_date
-        from (SELECT date_range.period, ROW_NUMBER() OVER (ORDER BY date_range.period) as rownum
-              FROM (SELECT (generate_series(trade_date + 1, trade_date + 7, '1 day'::interval))::date as period) date_range
-              where date_range.period not in (select banking_holiday_date from public.banking_holiday_calendar b)
-                and date_range.period not in (select holiday_date from public.holiday_calendar h)
-                and trim(to_char(date_range.period, 'DAY')) not IN ('SUNDAY', 'SATURDAY') -- '1 -Sun 7 - Sat
-             ) x
-        where x.rownum = 3; -- Equities T+3
-    elsif (instrument_type = 'E' and trade_date >= to_date('09/05/2017', 'mm/dd/yyyy')) then
-
-        select x.period as equities_sd
-        into v_settle_date
-        from (SELECT date_range.period, ROW_NUMBER() OVER (ORDER BY date_range.period) as rownum
-              FROM (SELECT (generate_series(trade_date + 1, trade_date + 7, '1 day'::interval))::date as period) date_range
-              where date_range.period not in (select banking_holiday_date from public.banking_holiday_calendar b)
-                and date_range.period not in (select holiday_date from public.holiday_calendar h)
-                and trim(to_char(date_range.period, 'DAY')) not IN ('SUNDAY', 'SATURDAY') -- '1 -Sun 7 - Sat
-             ) x
-        where x.rownum = 2; -- Equities T+2
-    elsif (instrument_type = 'O') then
-        select x.period as options_sd
-        into v_settle_date
-        from (SELECT date_range.period, ROW_NUMBER() OVER (ORDER BY date_range.period) as rownum
-              FROM (SELECT (generate_series(trade_date + 1, trade_date + 7, '1 day'::interval))::date as period) date_range
-              where date_range.period not in (select banking_holiday_date from public.banking_holiday_calendar b)
-                and date_range.period not in (select holiday_date from public.holiday_calendar h)
-                and trim(to_char(date_range.period, 'DAY')) not IN ('SUNDAY', 'SATURDAY') -- '1 -Sun 7 - Sat
-             ) x
-        where x.rownum = 1; -- Options T+1
-
-    end if;
-
+    select date_range.period
+    into v_settle_date
+    from (select (generate_series(trade_date + 1, trade_date + 10, '1 day'::interval))::date as period) date_range
+    where date_range.period not in (select banking_holiday_date from public.banking_holiday_calendar b)
+      and date_range.period not in (select holiday_date from public.holiday_calendar h)
+      and extract(dow from date_range.period) not in (0, 6)
+    order by 1
+    limit 1 offset v_offset;
+-- raise notice '%, %, %', trade_date, v_offset, v_settle_date;
     RETURN v_settle_date;
 END;
 $function$
 ;
+select * from trash.get_settle_date_by_instrument_type('2024-05-27', 'E');
 
-select d::date, trash.get_settle_date_by_instrument_type(d::date, 'E') as new_return,
+with base as (
+select d::date, extract(dow from d), trash.get_settle_date_by_instrument_type(d::date, 'E') as new_return,
        public.get_settle_date_by_instrument_type(d::date, 'E') as old_return
 from generate_series(current_date, current_date + interval '100 days', '1 day') as x(d)
+)
+         select * from base
+                  where new_return <> old_return
+select :trade_date between '2017-05-09'::date and '2024-05-27'::date
+
+select * from get_settle_date_by_instrument_type( '2024-05-01', 'E')
+
+
+select date_range.period
+    from (select (generate_series(:trade_date + 1, :trade_date + 10, '1 day'::interval))::date as period) date_range
+    where date_range.period not in (select banking_holiday_date from public.banking_holiday_calendar b)
+      and date_range.period not in (select holiday_date from public.holiday_calendar h)
+      and extract(dow from date_range.period) not in (0, 6)
+    order by 1
+    limit 1 offset :v_offset;
+
+
+SELECT date_range.period, ROW_NUMBER() OVER(ORDER BY date_range.period) as rownum
+			     FROM (SELECT (generate_series (:trade_date + 1, :trade_date + 7, '1 day'::interval))::date as period) date_range
+			            where date_range.period not in (select banking_holiday_date from public.banking_holiday_calendar b)
+			            and date_range.period not in (select holiday_date from public.holiday_calendar h)
+			              and trim(to_char(date_range.period,'DAY')) not IN ('SUNDAY', 'SATURDAY') -- '1 -Sun 7 - Sat
