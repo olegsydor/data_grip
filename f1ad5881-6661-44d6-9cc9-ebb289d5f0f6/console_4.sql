052040081ae7347337e4bef906073be2197f4d02
Oleh Sydor <ole.sydor@gmail.com>
2024-03-06 14:38:49 +0200

    Regular auto-commit at 14:38:49 on 06/03/2024

--- f1ad5881-6661-44d6-9cc9-ebb289d5f0f6/console_4.sql
+++ f1ad5881-6661-44d6-9cc9-ebb289d5f0f6/console_4.sql
@@ -7,3 +7,22 @@ from dwh.execution ex
                          and cl.parent_order_id is not null
                        limit 1 ) cl on true
 where exec_date_id = 20240306
+
+
+status_date_id
+order_id
+create_date_id
+time_in_Force_id
+account_id
+trading_firm_id / trading_firm_unq_id !?!?!
+instrument_id
+instrument_type_id
+number_of streets
+number_of_trades
+order_qty
+street_order_qty
+last_qty / traded_qty ?????- means sum(last_qty)
+vwap ???
+pg_db_create_time
+pg_db_update_time
+process_time
