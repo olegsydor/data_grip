d6e75a9a95564589c035ed4db69b5ca43e145835
Oleh Sydor <ole.sydor@gmail.com>
2024-03-27 16:00:11 +0200

    Regular auto-commit at 16:00:11 on 27/03/2024

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_55.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_55.sql
@@ -1,4 +1,4 @@
-select * from dash360.report_surveillance_first_trade_date_accounts(20240327, 20240327);
+select * from dash360.report_surveillance_first_trade_date_accounts(20240322, 20240327);
 create or replace function dash360.report_surveillance_first_trade_date_accounts(in_start_date_id int4,
                                                                                  in_end_date_id int4
 )
@@ -13,10 +13,12 @@ $fx$
     -- You can take a look at this dwh.upd_account_first_last_trade_date function for reference.
     -- We may need to change tables to something like dwh.flat_trade_record or dwh.execution instead of using the historic tables.
     -- else, we can use the first_trade_date field
-DECLARE
-    l_row_cnt int4;
-    l_load_id int4;
-    l_step_id int4;
+declare
+    l_row_cnt    int4;
+    l_load_id    int4;
+    l_step_id    int4;
+    l_start_date date := in_start_date_id::text::date;
+    l_end_date   date := in_end_date_id::text::date;
 
 begin
     select nextval('public.load_timing_seq') into l_load_id;
@@ -26,36 +28,62 @@ begin
                            'B')
     into l_step_id;
 
+
+    create temp table t_account on commit drop
+    as
+    select tf.trading_firm_id,
+           tf.trading_firm_name,
+           tf.trading_firm_side,
+           a.account_id,
+           a.account_name,
+           a.broker_dealer_mpid,
+           a.account_holder_type,
+           a.first_trade_date
+    from dwh.d_account a
+             join dwh.d_trading_firm tf on (tf.trading_firm_unq_id = a.trading_firm_unq_id)
+    where a.first_trade_date between l_start_date and l_end_date;
+--     raise notice 'Old - %', clock_timestamp();
+
+    if current_date between l_start_date and l_end_date then
+        insert into t_account
+        select distinct on (a.account_id) tf.trading_firm_id,
+                                          tf.trading_firm_name,
+                                          tf.trading_firm_side,
+                                          a.account_id,
+                                          a.account_name,
+                                          a.broker_dealer_mpid,
+                                          a.account_holder_type,
+                                          current_date
+        from dwh.d_account a
+                 join dwh.d_trading_firm tf on (tf.trading_firm_unq_id = a.trading_firm_unq_id)
+                 join dwh.flat_trade_record ftr
+                      on (ftr.date_id = to_char(current_date, 'YYYYMMDD')::int4 and ftr.account_id = a.account_id and
+                          ftr.is_busted = 'N')
+        where a.first_trade_date is null
+          and a.is_active
+          and not exists (select null from t_account ta where ta.account_id = a.account_id);
+    end if;
+--     raise notice 'New - %', clock_timestamp();
+
     return query
         select 'Trading Firm ID,Trading Firm Name,Business Model,Account ID,Account Name,Broker/Dealer,Account Holder Type,First Trade Date';
 
     return query
         select array_to_string(ARRAY [
-                                   tf.trading_firm_id, -- as "Trading Firm ID",
-                                   tf.trading_firm_name, -- as "Trading Firm Name",
+                                   ta.trading_firm_id, -- as "Trading Firm ID",
+                                   ta.trading_firm_name, -- as "Trading Firm Name",
                                    case
-                                       when tf.trading_firm_side = '1' then 'Buy Side'
-                                       when tf.trading_firm_side = '2' then 'Sell Side'
+                                       when ta.trading_firm_side = '1' then 'Buy Side'
+                                       when ta.trading_firm_side = '2' then 'Sell Side'
                                        end, -- as "Business Model",
-                                   a.account_id::text, -- as "Account ID",
-                                   a.account_name, -- as "Account Name",
-                                   a.broker_dealer_mpid, -- as "Broker/Dealer",
-                                   a.account_holder_type, -- as "Account Holder Type",
-                                   to_char(coalesce(a.first_trade_date, current_date), 'MM/dd/yyyy') -- as "First Trade Date"
+                                   ta.account_id::text, -- as "Account ID",
+                                   ta.account_name, -- as "Account Name",
+                                   ta.broker_dealer_mpid, -- as "Broker/Dealer",
+                                   ta.account_holder_type, -- as "Account Holder Type",
+                                   to_char(ta.first_trade_date, 'MM/dd/yyyy') -- as "First Trade Date"
                                    ], ',', '')
-        from dwh.d_account a
-                 join dwh.d_trading_firm tf on (tf.trading_firm_unq_id = a.trading_firm_unq_id)
-        where (a.first_trade_date between in_start_date_id::text::date and in_end_date_id::text::date
-            or (case
-                    when in_start_date_id = to_char(current_date, 'YYYYMMDD')::int4 then a.first_trade_date is null and
-                                                                                         exists (select null
-                                                                                                 from dwh.flat_trade_record ftr
-                                                                                                 where ftr.date_id = in_start_date_id
-                                                                                                   and ftr.account_id = a.account_id
-                                                                                                   and a.is_active)
-                    else false end)
-            )
-        order by tf.trading_firm_id, a.account_name;
+        from t_account ta
+        order by ta.trading_firm_id, ta.account_name;
 
     get diagnostics l_row_cnt = row_count;
 
@@ -144,16 +172,21 @@ where first_trade_date is not null
 ;
 
 
-select a.first_trade_date, td.*, *
+select distinct on (a.account_id) *
 from dwh.d_account a
          join dwh.d_trading_firm tf on (tf.trading_firm_unq_id = a.trading_firm_unq_id)
-         left join lateral (select current_date as first_trade_date
-                            from dwh.flat_trade_record ftr
-                            where ftr.date_id = :in_start_date_id
+    join dwh.flat_trade_record ftr
+                            on
+                                ftr.date_id = :in_start_date_id
                               and ftr.account_id = a.account_id
-                              and a.is_active
-                              and is_busted = 'N'
-                              and a.first_trade_date is null
-                            limit 1) td on true
-where coalesce(a.first_trade_date, td.first_trade_date) = :in_start_date
+                              and ftr.is_busted = 'N'
+where true
+  and a.first_trade_date is null
+    and case when :in_start_date_id != to_char(current_date, 'YYYYMMDD')::int4 then a.first_trade_date = :in_start_date else true end
+and case when :in_start_date_id != to_char(current_date, 'YYYYMMDD')::int4 then true else (a.first_trade_date is null and td.first_trade_date is not null) end
 order by tf.trading_firm_id, a.account_name;
+
+
+
+    create temp table t_account on commit drop
+    as
