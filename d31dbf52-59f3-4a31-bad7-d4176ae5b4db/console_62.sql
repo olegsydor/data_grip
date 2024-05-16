fcadaa205c85f049ff422d1f786d7d49545cf370
Oleh Sydor <ole.sydor@gmail.com>
2024-05-16 21:00:10 +0300

    Regular auto-commit at 21:00:10 on 16/05/2024

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_62.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_62.sql
@@ -21,15 +21,17 @@ and cl.create_date_id = 20240423;
 select fix_message::jsonb->>'9730' as "9730", fix_message::jsonb->>'9882' as "9882", fix_message::jsonb, * from fix_capture.fix_message_json
 where client_order_id = 'CDAB2262-20240423';
 
-select case
-           when cl.STRATEGY_DECISION_REASON_CODE in ('74') and
-                ex.exchange_id in ('AMEX', 'BOX', 'CBOE', 'EDGO', 'GEMINI', 'ISE', 'MCRY', 'MIAX', 'NQBXO', 'PHLX')
-               and exists (select upper(description)
-                           from dwh.d_liquidity_indicator li
-                           where (upper(description) like '%FLASH%'
-                               or upper(description) like '%EXPOSURE%')
-                             and li.trade_liquidity_indicator = ex.trade_liquidity_indicator)
-               then 'FLASH'
-           end
-from dwh.client_order cl,
-     execution ex
+
+
+select street_trade_fix_message_id , order_id, client_order_id, *
+from dwh.flat_trade_record ftr
+where trade_record_id in (
+3507822052,
+3507821265,
+3507821057)
+
+select fmj.fix_message ->> '9730', * from fix_capture.fix_message_json fmj
+where fix_message_id in (
+31978817393,
+31978817402,
+31978817407)
