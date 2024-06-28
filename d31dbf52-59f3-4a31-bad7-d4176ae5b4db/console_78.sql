4c7591c76cd7712985ed37b5aa712fdf99d93d32
Oleh Sydor <ole.sydor@gmail.com>
2024-06-28 09:00:08 +0300

    Regular auto-commit at 09:00:07 on 28/06/2024

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_78.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_78.sql
@@ -35,10 +35,13 @@ create table trash.so_cons_pre
 as
 select *
 from consolidator.consolidator_message cm
-where cm.message_type = 8
+where cm.message_type in (8, 9)
   and cm.date_id between 20230902 and 20230929;
 
 create index on trash.so_cons_pre (cons_message_id);
+create index on trash.so_cons_pre (rfr_id, request_number);
+create index on trash.so_cons_pre (date_id);
+
 
 create table trash.so_consolidator_message as
 select ri.date_id
@@ -55,8 +58,19 @@ from trash.so_routing_instruction ri
                   and cm.date_id between 20230902 and 20230929
 where ri.date_id between 20230902 and 20230929;
 
+select * from trash.so_main -- old version
+except
+select * from trash.so_main_1;
 
-create table trash.so_main_1 as
+
+create table trash.roinformation as
+select * from consolidator.routed_order_information
+where date_id between 20230902 and 20230929
+and accepted_or_rejected = 1;
+
+create index on trash.roinformation (cons_message_id, date_id);
+
+create table trash.so_main as
 -- explain
 select cm_base.date_id
      , cm_base.route_type
@@ -83,33 +97,33 @@ select cm_base.date_id
 from trash.so_consolidator_message cm_base
          inner join lateral
     (
-    select roi.date_id
-         , roi.side
-         , roi.mechanism
-         , roi.exchange
-         , roi.volume
-         , roi.accepted_or_rejected
-         , roi.executed_volume
-         , cm.message
-         , cm.rfr_id
-         , cm.request_number--, cm_9.date_id
-         , row_number()
-           over (partition by cm.date_id, cm.rfr_id, cm.request_number, roi.side, roi.exchange order by roi.executed_volume asc nulls first) as dedup --
-    from consolidator.consolidator_message cm
-             left join consolidator.routed_order_information roi
-                       on cm.cons_message_id = roi.cons_message_id and cm.date_id = roi.date_id
-    where true
-      and cm.message_type = 9                       -- Routed Order Information
-      and cm.date_id between 20230902 and 20230929  -- 20230101 and 20230930 -- = 20231108 --
-      and roi.date_id between 20230902 and 20230929 -- 20230101 and 20230930 -- = 20231108 --
-      and cm_base.rfr_id = cm.rfr_id
-      and cm_base.request_number = cm.request_number
-      and cm_base.date_id = cm.date_id
-      and cm_base.side = roi.side                   -- get rid off roi side which is for another RouteType
-      and cm_base.exchange = roi.exchange           -- important, as it can be in several exchanges for different RouteTypes for one side, so having just a side is not enough.
-      and roi.accepted_or_rejected = 1
-    --and roi.executed_volume > 0 -- !!! WTF!!!???
-    limit 1
+        select roi.date_id
+             , roi.side
+             , roi.mechanism
+             , roi.exchange
+             , roi.volume
+             , roi.accepted_or_rejected
+             , roi.executed_volume
+             , cm.message
+             , cm.rfr_id
+             , cm.request_number--, cm_9.date_id
+             , row_number()
+               over (partition by cm.date_id, cm.rfr_id, cm.request_number, roi.side, roi.exchange order by roi.executed_volume asc nulls first) as dedup --
+        from trash.so_cons_pre cm
+                 left join trash.roinformation roi
+                           on cm.cons_message_id = roi.cons_message_id and cm.date_id = roi.date_id
+        where true
+          and cm.message_type = 9                       -- Routed Order Information
+          and cm.date_id between 20230902 and 20230929  -- 20230101 and 20230930 -- = 20231108 --
+          and roi.date_id between 20230902 and 20230929 -- 20230101 and 20230930 -- = 20231108 --
+          and cm_base.rfr_id = cm.rfr_id
+          and cm_base.request_number = cm.request_number
+          and cm_base.date_id = cm.date_id
+          and cm_base.side = roi.side                   -- get rid off roi side which is for another RouteType
+          and cm_base.exchange = roi.exchange           -- important, as it can be in several exchanges for different RouteTypes for one side, so having just a side is not enough.
+          and roi.accepted_or_rejected = 1
+        --and roi.executed_volume > 0 -- !!! WTF!!!???
+        limit 1
     ) cm_lat on true and cm_lat.dedup = 1
 where cm_base.date_id between 20230902 and 20230929 -- 20230101 and 20230930 -- = 20231108 --
   and ((cm_base.route_type = 10) --(Post then Cross) -- var SA_RR_PC_POST_ORDER = 94 // Post order as part of Post then cross
@@ -117,7 +131,78 @@ where cm_base.date_id between 20230902 and 20230929 -- 20230101 and 20230930 -- 
     (cm_base.route_type = 11) --(Flash)           -- var SA_RR_FLASH_ORDER = 96    // Flash order
         or
     (cm_base.route_type = 3 and cm_base.cancel_chaser = 'Y') --(DMA route to Post to the book) -- var SA_RR_DMA2POST_ORDER_CXL = 97 // DMA to Post order with Cancel Chaser
-        or
-    (cm_base.route_type = 12) --(Post as COA)     -- var SA_RR_POST_COA_ORDER = 98 // Post as COA order
+--         or
+--     (cm_base.route_type = 12) --(Post as COA)     -- var SA_RR_POST_COA_ORDER = 98 // Post as COA order
     )
 ;
+
+
+create table trash.mes_orders as
+select co.*
+     , main.* --t.reason_code, count(1)
+from trash.so_main main
+         left join lateral
+    ( select str.*, ec.order_status
+      from (select co.order_id, co.exchange_id, co.strtg_decision_reason_code
+            from dwh.client_order co
+            where co.create_date_id = main.date_id
+              and co.create_date_id between 20230902 and 20230929
+              and co.parent_order_id is not null -- street level
+              and co.dash_rfr_id = main.rfr_id
+              and co.order_qty = main.routed_volume
+              and co.side = main.side
+              and co.request_number = main.request_number
+              --and co.exchange_id ilike t.exch_beg||'%'
+              and co.trans_type <> 'F'           -- co.trans_type = 'F' --
+              and co.multileg_reporting_type in ('1', '2')
+              and co.strtg_decision_reason_code::varchar = left(reason_code, 2)
+            limit 1) str
+               --) co on true
+               left join lateral
+          (
+          select ec.order_id, ec.order_status
+          from dwh.execution ec
+          where ec.exec_date_id = main.date_id
+            and ec.exec_date_id between 20230902 and 20230929
+            and ec.order_id = str.order_id
+            and (ec.exec_type = '4' or ec.order_status = '4')
+          limit 1
+          ) ec on true
+      where ec.order_id is not null
+    ) co on true
+where true
+  and co.order_id is not null;
+
+
+
+create table trash.mes_orders as
+select co.*
+     , main.* --t.reason_code, count(1)
+from trash.so_main main
+         join lateral
+    (select co.order_id, co.exchange_id, co.strtg_decision_reason_code, ec.order_status
+     from dwh.client_order co
+              join dwh.execution ec on ec.exec_date_id = co.create_date_id and ec.order_id = co.order_id and
+                                       (ec.exec_type = '4' or ec.order_status = '4')
+     where co.create_date_id = main.date_id
+       and co.create_date_id between 20230902 and 20230929
+       and co.parent_order_id is not null -- street level
+       and ec.exec_date_id between 20230902 and 20230929
+       and co.dash_rfr_id = main.rfr_id
+       and co.order_qty = main.routed_volume
+       and co.side = main.side
+       and co.request_number = main.request_number
+       --and co.exchange_id ilike t.exch_beg||'%'
+       and co.trans_type <> 'F'           -- co.trans_type = 'F' --
+       and co.multileg_reporting_type in ('1', '2')
+       and co.strtg_decision_reason_code::varchar = left(reason_code, 2)
+       and ec.order_id is not null
+     limit 1) co on true
+where true;
+
+select * from trash.mes_orders;
+
+-- 2
+select route_type, count(distinct order_id)
+from trash.mes_orders
+group by route_type;
