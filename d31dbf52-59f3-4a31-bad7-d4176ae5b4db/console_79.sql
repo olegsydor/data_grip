9ba6229f23318bf06293d5db7543f26218663c27
Oleh Sydor <ole.sydor@gmail.com>
2024-07-08 18:00:15 +0300

    Regular auto-commit at 18:00:15 on 08/07/2024

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_79.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_79.sql
@@ -10,7 +10,9 @@ as
 select *
 from consolidator.consolidator_message cm
 where true
-  and cm.date_id between 20230902 and 20230929;
+  and cm.date_id between 20230902 and 20230929
+-- and cm.message ilike '%IMC%'
+;
 
 create index on trash.so_routed_order_information(date_id, cons_message_id);
 create index on trash.so_consolidator_message(rfr_id);
@@ -30,9 +32,11 @@ from consolidator.routing_instruction_message ri
 where ri.date_id between 20230902 and 20230929
   and ri.route_type in (10, 11, 3);
 
+/*
 3 = DMA route to Post to the book -> 6 = DMA to Pos
 10 = Post then Cross
 11 = Flash -> 7 = Flash
+*/
 
 drop table if exists trash.so_report_abcd;
 create table trash.so_report_abcd as
@@ -47,6 +51,15 @@ select cm.message_type,
        roi.order_sent_timestamp,
        roi.order_ack_timestamp,
        roi.mechanism,
+       case roi.mechanism
+           when 1 then 'DMA to Remove' -- excluded
+           when 2 then 'Auction'
+           when 3 then 'ATS'
+           when 4 then 'SOR'
+           when 5 then 'Consolidator Lite'
+           when 6 then 'DMA to Pos' -- 3
+           when 7 then 'Flash'
+           end           as route_type,
        cme.exchange_name as exchange,
        roi.volume,
        roi.accepted_or_rejected,
@@ -57,7 +70,6 @@ from trash.so_consolidator_message cm
          left join trash.so_routed_order_information roi
                    on cm.date_id = roi.date_id and cm.cons_message_id = roi.cons_message_id
          join consolidator.cons_message_exchange cme on roi.exchange = cme.exchange_number
-
 where true
   and cm.date_id between 20230902 and 20230929
   and roi.date_id between 20230902 and 20230929
