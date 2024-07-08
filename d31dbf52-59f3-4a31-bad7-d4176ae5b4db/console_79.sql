2ab447b7294dd98d1f7a7bb4781d6c1638e9b86b
Oleh Sydor <ole.sydor@gmail.com>
2024-07-08 17:00:11 +0300

    Regular auto-commit at 17:00:11 on 08/07/2024

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_79.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_79.sql
@@ -12,6 +12,27 @@ from consolidator.consolidator_message cm
 where true
   and cm.date_id between 20230902 and 20230929;
 
+create index on trash.so_routed_order_information(date_id, cons_message_id);
+create index on trash.so_consolidator_message(rfr_id);
+
+drop table if exists trash.so_routing_instruction;
+create table trash.so_routing_instruction as
+select ri.date_id,
+       ri.route_type,
+       ri.side,
+       ri.exchange,
+       ri.cross_exchange,
+       ri.coa_indicator,
+       ri.account,
+       ri.cons_message_id,
+       ri.cancel_chaser
+from consolidator.routing_instruction_message ri
+where ri.date_id between 20230902 and 20230929
+  and ri.route_type in (10, 11, 3);
+
+3 = DMA route to Post to the book -> 6 = DMA to Pos
+10 = Post then Cross
+11 = Flash -> 7 = Flash
 
 drop table if exists trash.so_report_abcd;
 create table trash.so_report_abcd as
@@ -32,13 +53,18 @@ select cm.message_type,
        roi.exchange_error_message,
        roi.executed_volume,
        roi.pg_db_create_time
-from consolidator.consolidator_message cm
-         left join consolidator.routed_order_information roi
+from trash.so_consolidator_message cm
+         left join trash.so_routed_order_information roi
                    on cm.date_id = roi.date_id and cm.cons_message_id = roi.cons_message_id
          join consolidator.cons_message_exchange cme on roi.exchange = cme.exchange_number
+
 where true
   and cm.date_id between 20230902 and 20230929
   and roi.date_id between 20230902 and 20230929
-  and cm.rfr_id = '100284649835'
-  and cm.message_type = 9
-  and cm.request_number = 2
+  and cm.rfr_id = '760204378479'
+  and roi.mechanism <> 1;
+
+
+  select * from trash.so_report_abcd
+  where true
+    and rfr_id = '100284649835'
