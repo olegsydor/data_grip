21450fd253479c363caa7e47cfbf949cfecef76d
Oleh Sydor <ole.sydor@gmail.com>
2024-07-08 19:00:14 +0300

    Regular auto-commit at 19:00:13 on 08/07/2024

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_79.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_79.sql
@@ -58,7 +58,7 @@ select cm.message_type,
            when 4 then 'SOR'
            when 5 then 'Consolidator Lite'
            when 6 then 'DMA to Pos' -- 3
-           when 7 then 'Flash'
+           when 7 then 'Flash' -- 11
            end           as route_type,
        cme.exchange_name as exchange,
        roi.volume,
@@ -66,17 +66,24 @@ select cm.message_type,
        roi.exchange_error_message,
        roi.executed_volume,
        roi.pg_db_create_time
+        ,
+       crw.*--route_type
 from trash.so_consolidator_message cm
          left join trash.so_routed_order_information roi
                    on cm.date_id = roi.date_id and cm.cons_message_id = roi.cons_message_id
          join consolidator.cons_message_exchange cme on roi.exchange = cme.exchange_number
+         left join lateral (select *--route_type
+                            from trash.so_consolidator_message cmw
+                                     join trash.so_routing_instruction riw
+                                          on cmw.date_id = riw.date_id and cmw.cons_message_id = riw.cons_message_id
+                            where cmw.rfr_id = cm.rfr_id
+                              and cmw.request_number = cm.request_number
+                              and cmw.message_type = '8'
+                            and riw.route_type = 11
+                            limit 1) crw on true
 where true
   and cm.date_id between 20230902 and 20230929
   and roi.date_id between 20230902 and 20230929
   and cm.rfr_id = '760204378479'
+  and cm.message_type = '9'
   and roi.mechanism <> 1;
-
-
-  select * from trash.so_report_abcd
-  where true
-    and rfr_id = '100284649835'
