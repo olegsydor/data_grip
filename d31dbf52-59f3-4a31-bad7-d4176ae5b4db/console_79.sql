9378394835be2ed70ced3b5a516a3cdf4984a885
Oleh Sydor <ole.sydor@gmail.com>
2024-07-08 15:00:13 +0300

    Regular auto-commit at 15:00:12 on 08/07/2024

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_79.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_79.sql
@@ -13,7 +13,9 @@ from consolidator.routing_instruction_message ri
 where ri.date_id between 20230902 and 20230929
   and ri.route_type in (10, 11, 3);
 
-create table trash.roinformation as
+alter table trash.roinformation rename to so_routed_order_information;
+
+create table trash.so_routed_order_information as
 select * from consolidator.routed_order_information
 where date_id between 20230902 and 20230929
 and accepted_or_rejected = 1;
@@ -23,14 +25,18 @@ create table trash.so_cons_preload
 as
 select *
 from consolidator.consolidator_message cm
-where cm.message_type in (8, 9)
+where true
+    and cm.message_type in (8, 9)
   and cm.date_id between 20230902 and 20230929
-and rfr_id in ('760204378479', '740245810547', '710317275002', '780105394924', '100284649835');
+and rfr_id in ('760204378479', '740245810547', '710317275002', '780105394924', '100284649835')
+order by rfr_id, request_number, message_type
+;
 
 
 drop table if exists trash.so_consolidator_message;
 create table trash.so_consolidator_message as
-select date_id, cons_message_id, rfr_id, request_number, route_type, side, exchange, coalesce(executed_volume, 0) as executed_volume
+select cm.*, '||', cm_lat.*
+--     date_id, cons_message_id, rfr_id, request_number, route_type, side, exchange, coalesce(executed_volume, 0) as executed_volume
 from trash.so_cons_preload cm
          join trash.so_routing_instruction ri
               using (cons_message_id, date_id)
@@ -39,10 +45,11 @@ from trash.so_cons_preload cm
         select
              roi.accepted_or_rejected,
              roi.executed_volume,
-              dense_rank()
-               over (partition by cmi.date_id, cm.rfr_id, cmi.request_number, roi.side, roi.exchange order by roi.executed_volume asc nulls first) as dedup --
+              row_number()
+               over (partition by cmi.date_id, cmi.rfr_id, cmi.request_number, roi.side, roi.exchange order by roi.executed_volume asc nulls first) as dedup --
+        , *
         from trash.so_cons_preload cmi
-                 left join trash.roinformation roi
+                 left join trash.so_routed_order_information roi
                            on cmi.cons_message_id = roi.cons_message_id and cmi.date_id = roi.date_id
         where true
           and cmi.message_type = 9                       -- Routed Order Information
@@ -53,17 +60,22 @@ from trash.so_cons_preload cm
           and cmi.date_id = cm.date_id
           and ri.side = roi.side
           and ri.exchange = roi.exchange
---           and roi.accepted_or_rejected = 1
+          and roi.accepted_or_rejected = 1
         limit 2
-    ) cm_lat on true
-where ri.date_id between 20230902 and 20230929
+    ) cm_lat on true and dedup = 1
+where true
+--     and ri.date_id between 20230902 and 20230929
   and cm.message_type = 8
   and cm.date_id between 20230902 and 20230929
   and cm.rfr_id in ('760204378479', '740245810547', '710317275002', '780105394924', '100284649835')
+  and cm.rfr_id = '100284649835'
   and cm.message ilike '%IMC%'
 --     and cm.message_type = any('{6, 7}')
-  and cm.date_id between 20230901 and 20230930
-    and coalesce(dedup, 1) = 1
-and route_type = 11
+--     and coalesce(dedup, 1) = 1
+ and route_type = 11
 order by cm.rfr_id,cm.request_number, cm.cons_message_id
 
+
+select * from trash.so_routing_instruction
+    where cons_message_id in (2356424962,2356424975,2356424969,2356424964)
+    and route_type = 11
