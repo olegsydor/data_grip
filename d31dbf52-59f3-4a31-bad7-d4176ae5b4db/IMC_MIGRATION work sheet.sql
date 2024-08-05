5d914f4bd79c054afb913ef468c49ffdcf43d07f
Oleh Sydor <ole.sydor@gmail.com>
2024-08-05 16:00:13 +0300

    Regular auto-commit at 16:00:13 on 05/08/2024

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/IMC_MIGRATION work sheet.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/IMC_MIGRATION work sheet.sql
@@ -14,7 +14,6 @@ where true
 create index on t_alp (cross_order_id, order_id);
 
 -- Day part
-drop table if exists trash.so_imc;
 create table trash.so_imc as
 select cl.order_id,
        cl.create_date_id,
@@ -49,12 +48,12 @@ select cl.order_id,
        cl.instrument_id,
        cl.fix_connection_id,
        cl.side,
-
+       cl.multileg_order_id,
        ex.exec_id                     as ex_exec_id,
        ex.exec_time                   as ex_exec_time,
        ex.exec_type                   as ex_exec_type,
        ex.cum_qty                     as ex_cum_qty,
-       ex.order_status                as order_status,
+       ex.order_status                as ex_order_status,
        ex.last_px                     as ex_last_px,
        ex.last_qty                    as ex_last_qty,
        ex.contra_account_capacity     as ex_contra_account_capacity,
@@ -130,9 +129,11 @@ where true
   and cl.multileg_reporting_type in ('1', '2')
   and ex.is_busted = 'N'
   and ex.exec_type not in ('E', 'S', 'D', 'y')
-  and cl.trans_type <> 'F'
-  and cl.create_date_id > :l_retention_date_id;
+  and cl.trans_type <> 'F';
+
 create index on trash.so_imc (order_id, ex_exec_id);
+
+
 
 create temp table t_left_orders as
 select order_id
@@ -145,12 +146,8 @@ select order_id
 from trash.so_imc;
 create index on t_left_orders (order_id);
 
-select order_id, count(*)
-    from t_left_orders
-        group by order_id
-having count(*)  > 1
-
 analyze t_left_orders;
+
 
 insert into trash.so_imc
 select cl.order_id,
@@ -186,12 +183,12 @@ select cl.order_id,
        cl.instrument_id,
        cl.fix_connection_id,
        cl.side,
-
+       cl.multileg_order_id,
        ex.exec_id                     as ex_exec_id,
        ex.exec_time                   as ex_exec_time,
        ex.exec_type                   as ex_exec_type,
        ex.cum_qty                     as ex_cum_qty,
-       ex.order_status                as order_status,
+       ex.order_status                as ex_order_status,
        ex.last_px                     as ex_last_px,
        ex.last_qty                    as ex_last_qty,
        ex.contra_account_capacity     as ex_contra_account_capacity,
@@ -276,10 +273,10 @@ where true
   and ex.is_busted = 'N'
   and ex.exec_type not in ('E', 'S', 'D', 'y')
   and cl.trans_type <> 'F'
-  and cl.create_date_id > :l_retention_date_id
-  and not exists (select null from trash.so_imc tao where tao.order_id = ex.order_id)
-;
+  and cl.create_date_id > :l_retention_date_id;
 
+drop table if exists t_left_orders;
+/*
 select order_id, ex_exec_id
 from trash.so_imc
 except
@@ -294,4 +291,235 @@ from trash.so_imc
 union all
 select count(*)
 from trash.imc_oracle_report
+*/
+-------------------------------------------
 
+create temp table t_next as
+select cl.order_id,
+       cl.transaction_id,
+       cl.create_time,
+       cl.order_qty,
+       cl.price,
+       cl.par_order_id,
+       cl.exch_order_id,
+       cl.cross_order_id,
+       cl.is_originator,
+       cl.orig_order_id,
+       cl.client_order_id,
+       cl.cl_exchange_id,
+       cl.sub_strategy_id,
+       cl.sub_strategy_desc,
+       cl.sub_system_unq_id,
+       cl.opt_exec_broker_id,
+       cl.clearing_firm_id,
+       cl.clearing_account,
+       cl.sub_account,
+       cl.open_close,
+       cl.customer_or_firm_id,
+       cl.opt_customer_firm_street,
+       cl.eq_order_capacity,
+       cl.exec_instruction,
+       cl.strtg_decision_reason_code,
+       cl.request_number,
+       cl.order_type_id,
+       cl.time_in_force_id,
+       cl.multileg_reporting_type,
+       cl.cons_payment_per_contract,
+       cl.instrument_id,
+       cl.create_date_id,
+       cl.fix_connection_id,
+       coalesce(lnb.no_legs, 1)       as no_legs,
+       case
+           when cl.multileg_reporting_type = '2'
+               then trash.get_multileg_leg_number(cl.order_id, cl.multileg_order_id) end
+                                      as leg_number,
+       cl.side,
+       cl.ex_exec_id,
+       cl.ex_exec_time,
+       cl.ex_exec_type,
+       cl.ex_cum_qty,
+       cl.ex_order_status,
+       cl.ex_last_px,
+       cl.ex_last_qty,
+       cl.ex_contra_account_capacity,
+       cl.ex_trade_liquidity_indicator,
+       cl.ex_exch_exec_id,
+       cl.ex_exchange_id,
+       cl.ex_contra_broker,
+       cl.ex_contra_trader,
+       cl.ex_secondary_order_id,
+       cl.ex_secondary_exch_exec_id,
+       cl.ac_trading_firm_id,
+       cl.ac_opt_is_fix_clfirm_processed,
+       cl.ac_opt_customer_or_firm,
+       cl.ac_account_id,
+       cl.opx_opt_exec_broker,
+       cl.ex_exec_date_id,
+       cl.ex_fix_message_id,
+       cl.par_sub_strategy_desc,
+       cl.par_client_order_id,
+       cl.par_order_type_id,
+       cl.par_time_in_force_id,
+       cl.str_cons_payment_per_contract,
+       cl.str_order_id,
+       cl.str_cross_order_id,
+       cl.STR_strtg_decision_reason_code,
+       cl.STR_request_number,
+       cl.str_create_date_id,
+
+           case
+               when cl.par_order_id is null then cl.exch_order_id
+               when cl.ac_trading_firm_id <> 'imc01' then (select exch_order_id
+                                                        from dwh.client_order
+                                                        where order_id = cl.par_order_id
+--                                                          and create_date_id = :in_date_id
+                                                        limit 1)
+               else (select exch_order_id
+                     from dwh.client_order
+                     where order_id = (select max(parent_order_id)
+                                       from dwh.client_order
+                                       where cross_order_id = cl.cross_order_id
+                                         and is_originator <> cl.is_originator
+--                                         and create_date_id = :in_date_id
+                                         )
+                       and create_date_id = :in_date_id
+                     limit 1)
+               end                        as RFR_ID,--rfr_id
+
+           case
+               when cl.par_order_id is null then (select orig.exch_order_id
+                                                     from dwh.client_order orig
+                                                     where orig.order_id = cl.orig_order_id
+--                                                       and orig.create_date_id = :in_date_id
+                                                     limit 1)
+               when cl.ac_trading_firm_id <> 'imc01' then (select orig.exch_order_id
+                                                        from client_order co
+                                                                 join dwh.client_order orig
+                                                                      on orig.order_id = co.orig_order_id
+--                                                                          and co.create_date_id = :in_date_id
+                                                        where co.order_id = cl.par_order_id
+--                                                          and orig.create_date_id = :in_date_id
+                                                        limit 1)
+               else (select orig.exch_order_id
+                     from dwh.client_order orig
+                              join dwh.client_order co on co.orig_order_id = orig.order_id
+                     where co.order_id = (select max(parent_order_id) as max_orig_parent_order_id
+                                          from dwh.client_order cr
+                                          where cr.cross_order_id = cl.cross_order_id
+                                            and cr.is_originator <> cl.is_originator
+--                                            and cr.create_date_id = :in_date_id
+                                          limit 1)
+                       and orig.create_date_id = :in_date_id
+                       and co.create_date_id = :in_date_id)
+               end                        as ORIG_RFR_ID,--orig_rfr_id
+       case
+           when cl.ex_exec_type in ('S', 'W') then orig.client_order_id
+--                   (select orig.client_order_id from client_order orig where orig.order_id = tbs.orig_order_id) -- SO MOVED TO LATERAL
+           end                        as REPLACED_ORDER_ID,
+       case
+           when cl.ex_exec_type in ('b', '4') then cxl.client_order_id
+--                   (select min(cxl.client_order_id) from client_order cxl where cxl.orig_order_id = tbs.order_id) -- SO MOVED TO LATERAL
+           end                        as cancel_order_id,
+       case
+           when cl.ex_exec_type = 'F' then
+               (select LAST_QTY from EXECUTION exc where EXEC_ID = MCT.CONTRA_EXEC_ID and exec_date_id = :in_date_id)
+           end                        as CONTRA_CROSS_EXEC_QTY,
+
+       case
+           when cl.PAR_ORDER_ID is null and cl.STR_CROSS_ORDER_ID is not null
+               then cc_str.t_alp_agg
+           when cl.PAR_ORDER_ID is not null and cl.CROSS_ORDER_ID is not null
+               then cc.t_alp_agg
+           end                        as CONTRA_CROSS_LP_ID,
+       cl.es_FIX_MESSAGE_ID              as es_fix_message_id,
+       cl.es_exec_id                     as es_exec_id,
+       case
+           when cl.ex_exec_type = 'F' then
+               case
+                   --
+                   when coalesce(cl.par_sub_strategy_desc, cl.sub_strategy_desc) = 'DMA' then 'DMA'
+                   when coalesce(cl.par_sub_strategy_desc, cl.sub_strategy_desc) in ('CSLDTR', 'RETAIL') and
+                        coalesce(cl.REQUEST_NUMBER, cl.STR_request_number, -1) between 0 and 99 then 'IMC'
+                   when coalesce(cl.par_SUB_STRATEGY_desc, cl.SUB_STRATEGY_desc) in ('CSLDTR', 'RETAIL') and
+                        coalesce(cl.REQUEST_NUMBER, cl.STR_REQUEST_NUMBER, -1) > 99 then 'Exhaust'
+                   when (
+                       coalesce(cl.par_SUB_STRATEGY_desc, cl.SUB_STRATEGY_desc) not in ('DMA', 'CSLDTR', 'RETAIL') or
+                       coalesce(cl.request_number, cl.STR_request_number, -1) = -1)
+                       then
+                       case
+                           when coalesce(cl.par_ORDER_TYPE_id, cl.ORDER_TYPE_id) in ('3', '4', '5', 'B')
+                               then 'Exhaust_IMC'
+                           when coalesce(cl.par_time_in_force_id, cl.time_in_force_id) in ('2', '7')
+                               then 'Exhaust_IMC'
+                           -- the other part is moved into the end part for billing_code!!!!!
+--                               when (staging.get_lp_list_tmp(ac.ACCOUNT_ID, I.SYMBOL, :in_date_id::text::date) is null and
+--                                     staging.get_lp_list_lite_tmp(ac.ACCOUNT_ID, OS.ROOT_SYMBOL,
+--                                                              case cl.MULTILEG_REPORTING_TYPE
+--                                                                  when '1' then 'O'
+--                                                                  when '2' then 'M' end) is null)
+--                                   then 'Exhaust_IMC'
+--                               else 'Exhaust'
+                           end
+                   else 'Other'
+                   --
+                   end
+           end                        as BILLING_CODE,
+       cl.es_contra_broker               as es_contra_broker,
+       cl.es_contra_account_capacity     as es_contra_account_capacity,
+       cl.es_contra_trader               as es_contra_trader,
+       cl.es_exchange_id                 as es_exchange_id,
+       cl.ac_account_demo_mnemonic,
+       cl.fc_acceptor_id,
+       fmj.t9730                      as str_t9730,
+       fmj_p.t9730                    as par_t9730
+from trash.so_imc cl
+
+         left join lateral (select fix_message ->> '9730' as t9730
+                            from fix_capture.fix_message_json fmj
+                            where fmj.fix_message_id = cl.es_fix_message_id
+                              and fmj.date_id = :in_date_id
+                            limit 1) fmj on true
+         left join lateral (select fix_message ->> '9730' as t9730
+                            from fix_capture.fix_message_json fmj
+                            where fmj.fix_message_id = cl.ex_fix_message_id
+                              and fmj.date_id = :in_date_id
+                            limit 1) fmj_p on true
+         left join trash.matched_cross_trades_pg mct on mct.orig_exec_id = coalesce(cl.es_exec_id, cl.ex_exec_id)
+         left join lateral (select orig.client_order_id, exch_order_id
+                            from dwh.client_order orig
+                            where orig.order_id = cl.orig_order_id
+                              and cl.ex_exec_type in ('S', 'W')
+                              and orig.create_date_id <= :in_date_id
+                              and cl.orig_order_id is not null
+                            order by orig.create_date_id desc
+                            limit 1) orig on true
+         left join lateral (select cxl.client_order_id as client_order_id
+                            from client_order cxl
+                            where cxl.orig_order_id = cl.order_id
+                              and cl.ex_exec_type in ('b', '4')
+                              and cxl.create_date_id = :in_date_id
+                              and cxl.orig_order_id is not null
+                            order by cxl.order_id
+                            limit 1) cxl on true
+         left join lateral (select cnl.no_legs
+                            from dwh.client_order cnl
+                            where cnl.order_id = cl.multileg_order_id
+                              and cnl.create_date_id = :in_date_id
+                            limit 1) lnb on true
+
+         left join lateral (select string_agg(LP_DEMO_MNEMONIC, ' ') as t_alp_agg
+                            from t_alp as cc_str
+                            where cc_str.cross_order_id = cl.STR_CROSS_ORDER_ID
+                              and cc_str.order_id <> cl.STR_ORDER_ID
+                              and cl.PAR_ORDER_ID is null
+                              and cl.STR_CROSS_ORDER_ID is not null
+                            group by cc_str.CROSS_ORDER_ID
+                            limit 1) cc_str on true
+         left join lateral (select string_agg(lp_demo_mnemonic, ' ') as t_alp_agg
+                            from t_alp as cc
+                            where cc.cross_order_id = cl.CROSS_ORDER_ID
+                              and cc.order_id <> cl.ORDER_ID
+                              and cl.PAR_ORDER_ID is not null
+                              and cl.CROSS_ORDER_ID is not null
+                            group by cc.cross_order_id
+                            limit 1) cc on true
