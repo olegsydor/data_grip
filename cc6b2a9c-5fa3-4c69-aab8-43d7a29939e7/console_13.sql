31469552c9fa4915b46a45fb1c2db208dcdff17d
Oleh Sydor <ole.sydor@gmail.com>
2024-09-04 14:00:09 +0300

    Regular auto-commit at 14:00:09 on 04/09/2024

--- cc6b2a9c-5fa3-4c69-aab8-43d7a29939e7/console_13.sql
+++ cc6b2a9c-5fa3-4c69-aab8-43d7a29939e7/console_13.sql
@@ -18,3 +18,14 @@ VALUES ('ISEF', '57', 'Simple Exposure Order - Responder', NULL, 4, 'N', current
 commit;
 
 
+update LIQUIDITY_INDICATOR
+set description = case
+                      when trade_liquidity_indicator in ('7', '5', '6', '4', '0') then '\'
+                      when trade_liquidity_indicator = '1' then 'Add/Maker'
+                      when trade_liquidity_indicator = '2' then 'Remove/Taker' end
+where exchange_id = 'ISE'
+  and trade_liquidity_indicator in ('7', '5', '1', '6', '4', '2', '0');
+
+select * from LIQUIDITY_INDICATOR
+where exchange_id = 'ISE'
+order by CAST(TRADE_LIQUIDITY_INDICATOR AS integer);
