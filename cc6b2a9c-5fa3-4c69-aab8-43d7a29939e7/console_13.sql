7f3c13cf7d3ed0059d786a16e570962ba8a88f7d
Oleh Sydor <ole.sydor@gmail.com>
2024-09-05 10:18:11 +0300

    Regular auto-commit at 10:18:11 on 05/09/2024

--- cc6b2a9c-5fa3-4c69-aab8-43d7a29939e7/console_13.sql
+++ cc6b2a9c-5fa3-4c69-aab8-43d7a29939e7/console_13.sql
@@ -2,30 +2,52 @@ select current_timestamp from dual;
 
 INSERT INTO GENESIS2.LIQUIDITY_INDICATOR
 (EXCHANGE_ID, TRADE_LIQUIDITY_INDICATOR, DESCRIPTION, "COMMENT", LIQUIDITY_INDICATOR_TYPE_ID, IS_GREY, CREATE_TIME)
-VALUES ('ISEF', '53', 'Combo Taker Against Regular - Thru NBBO', NULL, 2, 'N', current_timestamp);
+VALUES ('ISE', '53', 'Combo Taker Against Regular - Thru NBBO', NULL, 2, 'N', current_timestamp);
 INSERT INTO GENESIS2.LIQUIDITY_INDICATOR
 (EXCHANGE_ID, TRADE_LIQUIDITY_INDICATOR, DESCRIPTION, "COMMENT", LIQUIDITY_INDICATOR_TYPE_ID, IS_GREY, CREATE_TIME)
-VALUES ('ISEF', '54', 'Combo Taker Against IO - Thru NBBO', NULL, 2, 'N', current_timestamp);
+VALUES ('ISE', '54', 'Combo Taker Against IO - Thru NBBO', NULL, 2, 'N', current_timestamp);
 INSERT INTO GENESIS2.LIQUIDITY_INDICATOR
 (EXCHANGE_ID, TRADE_LIQUIDITY_INDICATOR, DESCRIPTION, "COMMENT", LIQUIDITY_INDICATOR_TYPE_ID, IS_GREY, CREATE_TIME)
-VALUES ('ISEF', '55', 'Simple Exposure Order - Upon Receipt', NULL, 4, 'N', current_timestamp);
+VALUES ('ISE', '55', 'Simple Exposure Order - Upon Receipt', NULL, 4, 'N', current_timestamp);
 INSERT INTO GENESIS2.LIQUIDITY_INDICATOR
 (EXCHANGE_ID, TRADE_LIQUIDITY_INDICATOR, DESCRIPTION, "COMMENT", LIQUIDITY_INDICATOR_TYPE_ID, IS_GREY, CREATE_TIME)
-VALUES ('ISEF', '56', 'Simple Exposure Order - Subsequent', NULL, 4, 'N', current_timestamp);
+VALUES ('ISE', '56', 'Simple Exposure Order - Subsequent', NULL, 4, 'N', current_timestamp);
 INSERT INTO GENESIS2.LIQUIDITY_INDICATOR
 (EXCHANGE_ID, TRADE_LIQUIDITY_INDICATOR, DESCRIPTION, "COMMENT", LIQUIDITY_INDICATOR_TYPE_ID, IS_GREY, CREATE_TIME)
-VALUES ('ISEF', '57', 'Simple Exposure Order - Responder', NULL, 4, 'N', current_timestamp);
+VALUES ('ISE', '57', 'Simple Exposure Order - Responder', NULL, 4, 'N', current_timestamp);
 commit;
+
+update LIQUIDITY_INDICATOR
+set trade_liquidity_indicator = case
+                                    when trade_liquidity_indicator = 'C' then '7'
+                                    when trade_liquidity_indicator = 'H' then '5'
+                                    when trade_liquidity_indicator = 'M' then '1'
+                                    when trade_liquidity_indicator = 'O' then '6'
+                                    when trade_liquidity_indicator = 'R' then '4'
+                                    when trade_liquidity_indicator = 'T' then '2'
+                                    when trade_liquidity_indicator = 'X' then '0' end
+where exchange_id = 'ISE'
+  and trade_liquidity_indicator in ('C', 'H', 'M', 'O', 'R', 'T', 'X');
+commit;
+
 
 
 update LIQUIDITY_INDICATOR
 set description = case
-                      when trade_liquidity_indicator in ('7', '5', '6', '4', '0') then '\'
                       when trade_liquidity_indicator = '1' then 'Add/Maker'
                       when trade_liquidity_indicator = '2' then 'Remove/Taker' end
 where exchange_id = 'ISE'
-  and trade_liquidity_indicator in ('7', '5', '1', '6', '4', '2', '0');
+  and trade_liquidity_indicator in ('1', '2');
 
 select * from LIQUIDITY_INDICATOR
-where exchange_id = 'ISE'
+where exchange_id = 'ISEF'
 order by CAST(TRADE_LIQUIDITY_INDICATOR AS integer);
+
+insert into LIQUIDITY_INDICATOR (EXCHANGE_ID, TRADE_LIQUIDITY_INDICATOR, DESCRIPTION, "COMMENT", LIQUIDITY_INDICATOR_TYPE_ID)
+SELECT 'ISEF', TRADE_LIQUIDITY_INDICATOR, DESCRIPTION, "COMMENT", LIQUIDITY_INDICATOR_TYPE_ID
+from LIQUIDITY_INDICATOR
+where EXCHANGE_ID = 'ISE'
+and TRADE_LIQUIDITY_INDICATOR not in ('53', '54', '55', '56', '57')
+order by CAST(TRADE_LIQUIDITY_INDICATOR AS integer);
+commit;
+
