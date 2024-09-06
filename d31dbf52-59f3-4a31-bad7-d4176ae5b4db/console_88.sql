9f6a9c1e190d6b2ef13de5fa62119faf8d2f5dd2
Oleh Sydor <ole.sydor@gmail.com>
2024-09-06 13:00:08 +0300

    Regular auto-commit at 13:00:08 on 06/09/2024

---
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_88.sql
@@ -0,0 +1,8 @@
+select * from dwh.client_order
+where true
+    and create_date_id = 20240905
+    client_order_id = '8a02ec80-4460-4b3d-8aec-ad1c768d267f ';
+
+
+select * from dwh.execution
+where execution.exch_exec_id = '40711000000002900000'
