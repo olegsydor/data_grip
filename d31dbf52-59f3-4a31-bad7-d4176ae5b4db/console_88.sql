84af7c00468f0e800ce19776e67176bfca1b494d
Oleh Sydor <ole.sydor@gmail.com>
2024-09-06 14:00:08 +0300

    Regular auto-commit at 14:00:08 on 06/09/2024

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_88.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_88.sql
@@ -5,4 +5,12 @@ where true
 
 
 select * from dwh.execution
-where execution.exch_exec_id = '40711000000002900000'
+where execution.exch_exec_id = '40711000000002900000';
+
+
+select process_time::date, process_time::time, di.symbol, * from dwh.client_order cl
+         join dwh.d_account ac on ac.account_id = cl.account_id
+                                                join dwh.d_instrument di on di.instrument_id = cl.instrument_id
+where true
+    and create_date_id >= 20240805
+    and ac.trading_firm_id = 'OFT0068'
