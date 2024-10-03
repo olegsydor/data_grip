72f05022fa948c16f7c897af41d77745cf6c0aeb
Oleh Sydor <ole.sydor@gmail.com>
2024-10-03 18:00:13 +0300

    Regular auto-commit at 18:00:13 on 03/10/2024

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_93.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_93.sql
@@ -94,7 +94,8 @@ and auction_id = 290003977001;
 
  create or replace function trash.ats_responce_side(in_start_date_id integer, in_end_date_id integer,
                                                     in_liquidity_provider_id varchar(9)[] default '{}'::varchar(9)[],
-                                                    in_is_bd bool default true, in_summary bool default true)
+                                                    in_is_bd bool default true,
+                                                    in_is_summary bool default true)
      returns table
              (
                  export_row text
@@ -114,36 +115,48 @@ and auction_id = 290003977001;
      select public.load_log(l_load_id, l_step_id, 'trash.ats_resp STARTED===', 0, 'O')
      into l_step_id;
 
+     create temp table t_ats on commit drop as
+     select tf.trading_firm_name,
+            acd.auction_date_id,
+            case when acd.side in ('1', '3') then 'buy' else 'sell' end as side,
+            acd.auction_id,
+            acd.liquidity_provider_id
+     from data_marts.f_ats_cons_details acd
+              join dwh.client_order cl
+                   on cl.order_id = acd.order_id and cl.create_date_id >= acd.auction_date_id --tf lpo
+              join dwh.d_account ac on ac.account_id = cl.account_id
+              join dwh.d_trading_firm tf on tf.trading_firm_id = cl.trading_firm_id
+     where acd.auction_date_id between in_start_date_id and in_end_date_id
+       and is_lpo_parent
+       and num_nonnulls(acd.nbbo_bid_price, acd.nbbo_ask_price) > 0
+       and is_ats_or_cons = 'A'
+       and case when in_is_bd then tf.is_broker_dealer = 'Y' else true end
+       and case
+               when in_liquidity_provider_id = '{}' then true
+               else acd.liquidity_provider_id = any (in_liquidity_provider_id) end;
+
      return query
-         select 'Date Start, Date End, Side, Bid, Ask, Auction Id, Auction Date Id, Liquidity Provider';
+         select 'Trading Firm, Auction Date, Both Responces, Bid Responce, Ask Responce, Liquidity Provider';
      return query
          select array_to_string(ARRAY [
-                                    min(acd.auction_date_id)::text, -- as min_date_id,
-                                    max(acd.auction_date_id)::text, -- as max_date_id,
+                                    trading_firm_name,
                                     case
-                                        when count(distinct acd.side) = 2 then 'both'
-                                        else case when min(acd.side) = '1' then 'buy' else 'sell' end end, -- as side,
-                                    min(acd.nbbo_bid_price)::text, -- as bid,
-                                    min(acd.nbbo_ask_price)::text, -- as ask,
-                                    acd.auction_id::text,
-                                    acd.auction_date_id::text,
-                                    acd.liquidity_provider_id
+                                        when in_is_summary
+                                            then min(auction_date_id::text) || '-' || max(auction_date_id::text)
+                                        else min(auction_date_id::text) end,
+                                    sum(case when responce = 'both' then 1 else 0 end)::text, -- as both_resp,
+                                    sum(case when responce = 'sell' then 1 else 0 end)::text, -- as sell_resp,
+                                    sum(case when responce = 'buy' then 1 else 0 end)::text, -- as buy_resp,
+                                    liquidity_provider_id
                                     ], ',', '')
-         from data_marts.f_ats_cons_details acd
-                  join dwh.client_order cl
-                       on cl.order_id = acd.order_id and cl.create_date_id >= acd.auction_date_id --tf lpo
-                  join dwh.d_account ac on ac.account_id = cl.account_id
-                  join dwh.d_trading_firm tf on tf.trading_firm_id = cl.trading_firm_id
-         where acd.auction_date_id between in_start_date_id and in_end_date_id
---    and acd.auction_id = 7790001338874
-           and is_lpo_parent
--- and cl.ex_destination = 'LIQPT'
-           and is_ats_or_cons = 'A'
-           and case when in_is_bd then tf.is_broker_dealer = 'Y' else true end
-           and case
-                   when in_liquidity_provider_id = '{}' then true
-                   else acd.liquidity_provider_id = any (in_liquidity_provider_id) end
-         group by acd.auction_id, acd.auction_date_id, acd.liquidity_provider_id;
+         from (select trading_firm_name,
+                      auction_date_id,
+                      case when count(distinct side) > 1 then 'both' else min(side) end as responce,
+                      liquidity_provider_id
+               from t_ats
+               group by trading_firm_name, auction_date_id, liquidity_provider_id, auction_id) x
+         group by trading_firm_name, liquidity_provider_id;
+
      get diagnostics l_row_cnt = row_count;
      select public.load_log(l_load_id, l_step_id, 'trash.ats_resp COMPLETE===',
                             coalesce(l_row_cnt, 0), 'O')
@@ -151,7 +164,7 @@ and auction_id = 290003977001;
  end;
  $fx$;
 
-select * from trash.ats_responce_side(20240930, 20241001)
+'select * from trash.ats_responce_side(20240930, 20241001, in_is_summary := true, in_liquidity_provider_id := '{WALLLPA}', in_is_bd := false)'
 
 select extract(epoch from timestamp with time zone '2024-10-02 20:56:32.571049 +00:00')
 union all
@@ -179,10 +192,16 @@ select extract(epoch from timestamp with time zone '2024-10-02 20:56:32.571051 +
            else acd.liquidity_provider_id = any (:in_liquidity_provider_id) end;
 
 
-select trading from (select trading_firm_name,
+select trading_firm_name,
+       auction_date_id,
+       sum(case when responce = 'both' then 1 else 0 end) as both,
+       sum(case when responce = 'sell' then 1 else 0 end) as sell,
+       sum(case when responce = 'buy' then 1 else 0 end) as buy,
+       liquidity_provider_id
+from (select trading_firm_name,
                       auction_date_id,
                       case when count(distinct side) > 1 then 'both' else min(side) end as responce,
                       liquidity_provider_id
                from t_ats
                group by trading_firm_name, auction_date_id, liquidity_provider_id, auction_id) x
-
+group by trading_firm_name, auction_date_id, liquidity_provider_id
