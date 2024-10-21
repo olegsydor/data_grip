a42f48a066d9be116767e4db5343da4248839412
Oleh Sydor <ole.sydor@gmail.com>
2024-10-21 15:00:10 +0300

    Regular auto-commit at 15:00:10 on 21/10/2024

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_95.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_95.sql
@@ -41,43 +41,93 @@ begin
                                    ], '', '');
 
 
-   end;
-$fx$
+    create temp table t_rec
+        on commit drop
+    as
+    select tr.trade_record_id,
+           1 as record_num,
+           array_to_string(ARRAY [
+                               case
+                                   when tr.side = '1' then 'B'
+                                   when tr.side in ('2', '5', '6') then 'S' end, -- as buy_sell, [1]
+                               'NCO', -- as correspondent_office [2-4]
+                               '8966731', -- as customer_account_num, --Should we hardcode? [5-10]
+                               '?', -- as account_type, --Should we hardcode? [11]
+                               rpad(tr.exchange_id, 4), -- as exchange_mnemonic, [12-15]
+                               lpad('', 2), -- as filler1, [16-17]
+                               lpad(case when tr.instrument_type_id = 'E' then di.symbol else 'OPTION' end,
+                                    9), -- as cusip_symbol, --Use Symbol? We don't provide CUISP [18-26]
+                               '8', -- as market, [27]
+                               '1', -- as blotter, [28]
+                               lpad(tr.last_qty::text, 8, '0'), -- as quantity, [29-36]
+                               lpad(floor(tr.last_px)::text, 4, '0'), -- as price_whole_number, --Need whole number [37-40]
+                               case
+                                   when tr.last_px - floor(tr.last_px) < 0
+                                       then (10000000 * (tr.last_px - floor(tr.last_px)))::text
+                                   else '.' || to_char(10000 * (tr.last_px - floor(tr.last_px)), 'FM0000??')
+                                   end, -- as price_decimal, --Need decimal [41-47]
+                               ' ', -- as price_code, [48]
+                               '9', -- as settlement_date_code, [49]
+                               ' ', -- as step_in_step_out_code, [50]
+                               'DFIN', -- as executing_broker, [51-54]
+                               '0161', -- as contra_broker, [55-58]
+                               'Y', -- as clearance_only_flag,[59]
+                               ' ', -- as compression_flag, [60]
+                               ' ', -- as cross_trade_generate_code, [61]
+                               ' ', -- as legend_code_1, [62]
+                               ' ', -- as legend_code_2, [63]
+                               ' ', -- as confirm_comparison, [64]
+                               ' ', -- as commission_or_concession_sales_credit1, [65]
+                               'A', -- as execution_account_type, [66]
+                               ' ', -- as filler2, [67]
+                               rpad('', 7), -- as commission_or_concession_sales_credit2, [68-74]
+                               rpad('', 3), -- as rr_override, [75-77]
+                               '1', -- as control_field, [78]
+                               rpad('', 2) -- as filler3 [79-80]
+                               ], '', '')
+    from dwh.flat_trade_record tr
+             join dwh.d_instrument di on (di.instrument_id = tr.instrument_id)
+    where tr.date_id between in_start_date_id and in_end_date_id
+      and tr.is_busted = 'N'
+      and tr.account_id = any (l_account_ids);
 
-s
+
+
+   end;
+$fx$;
+
+
 select
-	case when tr.side = '1' then 'B' when tr.side in ('2','5','6') then 'S' end, -- as buy_sell, [1]
-	'NCO', -- as correspondent_office [2-4]
- 	'8966731', -- as customer_account_num, --Should we hardcode? [5-10]
-	'?', -- as account_type, --Should we hardcode? [11]
-	rpad(tr.exchange_id, 4), -- as exchange_mnemonic, [12-15]
-	lpad('', 2), -- as filler1, [16-17]
-	lpad(case when tr.instrument_type_id = 'E' then di.symbol else 'OPTION' end, 9), -- as cusip_symbol, --Use Symbol? We don't provide CUISP [18-26]
-	'8', -- as market, [27]
-	'1', -- as blotter, [28]
-	tr.last_qty as quantity,
-	tr.last_px as price_whole_number, --Need whole number
-	tr.last_px as price_decimal, --Need decimal
-	null as price_code,
-	'9' as settlement_date_code,
-	null as step_in_step_out_code,
-	'DFIN' as executing_broker,
-	'0161' as contra_broker,
-	'Y' as clearance_only_flag,
-	null as compression_flag,
-	null as cross_trade_generate_code,
-	null as legend_code_1,
-	null as legend_code_2,
-	null as confirm_comparison,
-	null as commission_or_concession_sales_credit1,
-	'A' as execution_account_type,
+	case when tr.side = '1' then 'B' when tr.side in ('2','5','6') then 'S' end as buy_sell,
+	'NCO8966731' as correspondent_office,
+	'?' as customer_account_num, ----Should we hardcode?
+	'?' as account_type, --Should we hardcode?
+	tr.exchange_id as exchange_mnemonic,
+	null as filler1,
+	(case when tr.instrument_type_id = 'E' then hsd.symbol else 'OPTION' end) as cusip_symbol, --Use Symbol? We don't provide CUISP
+	'8' as market,
+	'1' as blotter,
+	to_char(tr.trade_record_time, 'MMddyyyy') as trade_date,
+	to_char(public.get_settle_date_by_instrument_type(tr.trade_record_time::date, tr.instrument_type_id), 'MMddyyyy') as settlement_date,
+	null as order_accum_code,
+	null as blank,
+	null as standard_instruction_override,
+	null as blotter_override,
+	null as special_confirm_print,
+	null as multiple_round,
+	null as cns_override,
+	null as transfer_override,
+	null as execution_time_milli_seconds,
 	null as filler2,
-	null as commission_or_concession_sales_credit2,
-	null as rr_override,
-	'1' as control_field,
-	null as filler3
+	null as x2nd_rr_override,
+	null as x2nd_rr_percentage,
+	null as x3rd_rr_override,
+	null as x3rd_rr_percentage,
+	null as trading_account,
+	null as execution_time_hhmm,
+	'3' as control_field,
+	null as execution_time_ss
 from dwh.flat_trade_record tr
-join dwh.d_instrument di on (di.instrument_id = tr.instrument_id)
+join dwh.historic_security_definition_all hsd on (hsd.instrument_id = tr.instrument_id)
 where tr.date_id = 20241007
 	and tr.is_busted = 'N'
-and tr.account_id = any(:l_account_ids)
