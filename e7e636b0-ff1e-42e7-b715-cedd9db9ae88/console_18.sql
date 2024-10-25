0bf6e34584c50b9279730b77540a5646324e1ac0
Oleh Sydor <ole.sydor@gmail.com>
2024-10-25 13:00:08 +0300

    Regular auto-commit at 13:00:08 on 25/10/2024

--- e7e636b0-ff1e-42e7-b715-cedd9db9ae88/console_18.sql
+++ e7e636b0-ff1e-42e7-b715-cedd9db9ae88/console_18.sql
@@ -1,22 +1,34 @@
 
 select client_order_id, count(*)
 	from staging.trade_record_missed_lp
-		where date_id = 20241016
+		where date_id = 20241024
 group by client_order_id
 having count(*) > 1
 
 create temp table t_new as
 select *
 	from trash.so_missed_lp
-		where date_id = 20241017
-and client_order_id = '1_106241016';
+		where date_id = 20241024
+and client_order_id = '1_0241024';
+
+
 
 create temp table t_old as
 select *
 	from staging.trade_record_missed_lp
-		where date_id = 20241016
-and client_order_id = '1_106241016';
+		where date_id = 20241024
+and client_order_id = '1_0241024';
 
+
+select *
+	from t_new
+		where date_id = 20241024
+and client_order_id = '1_0241024';
+
+select *
+	from t_old
+		where date_id = 20241024
+and client_order_id = '1_0241024';
 
 select
 --     distinct
@@ -93,85 +105,91 @@ select
        blaze_account_alias
 , edw_status
 , system_order_type_id
-from t_new tn;
+from t_new tn
+where true
+and client_order_id = '1_0241024';
 
-select 'new' as src,
-       client_order_id,
-       side,
-       openclose,
-       exch_exec_id,
-       secondary_exch_exec_id,
-       last_mkt,
-       last_qty,
-       last_px,
-       ex_destination,
-       street_order_qty::int,
-       order_qty::int,
-       multileg_reporting_type::text,
-       exec_broker,
-       cmta,
-       street_time_in_force::text,
-       opt_customer_firm,
-       is_cross_order,
-       contra_broker,
-       client_id,
-       order_price,
-       leaves_qty::int,
-       symbol,
-       strike_price,
-       put_or_call,
-       maturuty_year,
-       maturuty_month,
-       maturuty_day,
-       security_type,
-       display_instrument_id,
-       instrument_type_id,
-       activ_symbol,
-       company_name,
-       generation::int,
-       mx_gen::int
--- , edw_status
+with bth as (select 'new' as src,
+                    client_order_id,
+                    side,
+                    openclose,
+                    exch_exec_id,
+                    secondary_exch_exec_id,
+                    last_mkt,
+                    last_qty,
+                    last_px,
+                    ex_destination,
+                    street_order_qty::int,
+                    order_qty::int,
+                    multileg_reporting_type::text,
+                    exec_broker,
+                    cmta,
+                    street_time_in_force::text,
+                    opt_customer_firm,
+                    is_cross_order,
+                    contra_broker,
+                    client_id,
+                    order_price,
+                    leaves_qty::int,
+                    symbol,
+                    strike_price,
+                    put_or_call,
+                    maturuty_year,
+                    maturuty_month,
+                    maturuty_day,
+                    security_type,
+                    display_instrument_id,
+                    instrument_type_id,
+                    activ_symbol,
+                    company_name,
+                    generation::int,
+                    mx_gen::int
+             -- , edw_status
 -- , system_order_type_id
-from t_new
-union all
-select 'old' as src,
-       client_order_id,
-       side,
-       open_close,
-       exch_exec_id,
-       secondary_exch_exec_id,
-       last_mkt,
-       last_qty,
-       last_px,
-       ex_destination,
-       street_order_qty,
-       order_qty,
-       multileg_reporting_type::text,
-       exec_broker,
-       cmta,
-       street_time_in_force,
-       opt_customer_firm,
-       is_cross_order,
-       contra_broker,
-       client_id,
-       order_price,
-       leaves_qty,
-       symbol,
-       strike_price,
-       put_or_call,
-       maturity_year,
-       maturity_month,
-       maturity_day,
-       security_type,
-       display_instrument_id,
-       instrument_type_id,
-       activ_symbol,
-       companyname,
-       generation,
-       mx_gen
-from t_old
-
-
+             from t_new
+             where true
+               and client_order_id = '1_0241024'
+             union all
+             select 'old' as src,
+                    client_order_id,
+                    side,
+                    open_close,
+                    exch_exec_id,
+                    secondary_exch_exec_id,
+                    last_mkt,
+                    last_qty,
+                    last_px,
+                    ex_destination,
+                    street_order_qty,
+                    order_qty,
+                    multileg_reporting_type::text,
+                    exec_broker,
+                    cmta,
+                    street_time_in_force,
+                    opt_customer_firm,
+                    is_cross_order,
+                    contra_broker,
+                    client_id,
+                    order_price,
+                    leaves_qty,
+                    symbol,
+                    strike_price,
+                    put_or_call,
+                    maturity_year,
+                    maturity_month,
+                    maturity_day,
+                    security_type,
+                    display_instrument_id,
+                    instrument_type_id,
+                    activ_symbol,
+                    companyname,
+                    generation,
+                    mx_gen
+             from t_old
+             where true
+               and client_order_id = '1_0241024')
+select * from bth
+order by secondary_exch_exec_id, src
 
 ---
 select *
