1dfe55bc9a7c37a23e4cbdd8eb555ce77cd606fe
Oleh Sydor <ole.sydor@gmail.com>
2024-10-28 10:00:09 +0200

    Regular auto-commit at 10:00:09 on 28/10/2024

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_97.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_97.sql
@@ -1,9 +1,10 @@
 select *
-into trash.so_worm_trades
-from eod_reports.export_rbc_trades_tbl(in_start_date_id=>20181001, in_end_date_id=>20181231)
+into table trash.so_worm_orders
+from eod_reports.export_rbc_orders_tbl(in_start_date_id=>20181001, in_end_date_id=>20181231)
 ;
 select array_agg(v)
-from (select distinct "Sending Firm" as v
-      from trash.so_worm_trades
+from (select *--distinct "Liq Ind Description" as v
+      from trash.so_worm_orders
 --       where "Ask Px" is null
-      limit 5) x
+      limit 50) x
+
