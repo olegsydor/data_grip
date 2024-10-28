2aae2fa1397ff685974fac1ca4c81c1305887adb
Oleh Sydor <ole.sydor@gmail.com>
2024-10-28 11:00:09 +0200

    Regular auto-commit at 11:00:09 on 28/10/2024

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_97.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_97.sql
@@ -3,8 +3,9 @@ into table trash.so_worm_orders
 from eod_reports.export_rbc_orders_tbl(in_start_date_id=>20181001, in_end_date_id=>20181231)
 ;
 select array_agg(v)
-from (select *--distinct "Liq Ind Description" as v
+from (select distinct "Underlying Symbol" as v
       from trash.so_worm_orders
 --       where "Ask Px" is null
-      limit 50) x
+--         order by 1 nulls first
+      limit 5) x
 
