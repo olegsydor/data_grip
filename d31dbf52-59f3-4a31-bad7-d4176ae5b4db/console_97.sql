1822f5383cb806b4935f544814a7b99c4922b0b6
Oleh Sydor <ole.sydor@gmail.com>
2024-11-06 14:00:08 +0200

    Regular auto-commit at 14:00:08 on 06/11/2024

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_97.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_97.sql
@@ -1,5 +1,4 @@
-select *
-into table trash.so_worm_orders
+select * from trash.so_worm_orders
 from eod_reports.export_rbc_orders_tbl(in_start_date_id=>20181001, in_end_date_id=>20181231)
 ;
 select array_agg(v)
@@ -9,3 +8,5 @@ from (select distinct "Underlying Symbol" as v
 --         order by 1 nulls first
       limit 5) x
 
+select *
+from eod_reports.export_rbc_trades_tbl(in_start_date_id=>20181001, in_end_date_id=>20181231)
