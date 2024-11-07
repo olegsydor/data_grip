c8dd6d6dd626ba6c845fc30cc9e0abc5e241df86
Oleh Sydor <ole.sydor@gmail.com>
2024-11-07 20:00:08 +0200

    Regular auto-commit at 20:00:08 on 07/11/2024

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_99.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_99.sql
@@ -1,4 +1,4 @@
-with order_ids_cte as (select co.create_date_id, co.order_id, di.instrument_type_id
+with order_ids_cte as (select co.create_date_id, co.order_id, di.instrument_type_id, di.display_instrument_id
                        from dwh.client_order po
                                 join dwh.client_order co on (co.create_date_id = po.create_date_id and
                                                              (co.order_id = po.order_id or co.parent_order_id = po.order_id))
@@ -29,73 +29,72 @@ select --array_to_string(ARRAY [
        case when co.parent_order_id is null then 'Parent' else 'Child' end, -- as "Row Type",
        a.account_name,                                                      -- as "Account",
        to_char(co.create_time, 'MM/DD/YYYY'),                               -- as "Creation Date",
-       to_char(co.process_time, 'MM/DD/YYYY'),                               -- as "Event Date",
+       to_char(co.process_time, 'MM/DD/YYYY'),                              -- as "Event Date",
        to_char(co.create_time, 'HH24:MI:SS.US'),                            -- as "Creation Time",
-       to_char(co.process_time, 'HH24:MI:SS.US'),                            -- as "Routed Time",
-       to_char(yc.exec_time, 'HH24:MI:SS.US'),                              -- as "Event Time",
+       to_char(co.process_time, 'HH24:MI:SS.US'),                           -- as "Routed Time",
+       to_char(lst_ex.exec_time, 'HH24:MI:SS.US'),                          -- as "Event Time",
        lst_ex.order_status_description,                                     -- as "Order Status",
-       yc.client_order_id,                                                  -- as "Cl Ord ID",
+       co.client_order_id,                                                  -- as "Cl Ord ID",
        orig.client_order_id,                                                -- as "Orig Cl Ord ID",
-/*                                   case
-                                       when yc.side = '1' then 'Buy'
-                                       when yc.side in ('2', '5', '6') then 'Sell'
-                                       else ''
-                                       end, -- as "Side",
-                                   yc.order_qty::text, -- as "Ord Qty",
-                                   yc.day_cum_qty::text, -- as "Ex Qty",
-                                   yc.day_leaves_qty::text, -- as "Lvs Qty",
-                                   hsd.display_instrument_id, -- as "Symbol",
-                                   to_char(hsd.maturity_date, 'MM/DD/YYYY'), -- as "Expiration Day",
-                                   round(yc.order_price, 6)::text, -- as "Price",
-                                   round(yc.avg_px, 6)::text, -- as "Avg Px",
-                                   tif.tif_name, -- as "TIF",
-                                   ot.order_type_name, -- as "Ord Type",
-                                   case
-                                       when co.open_close = 'O' then 'Open'
-                                       when co.open_close = 'C' then 'Close'
-                                       else '' end, -- as "O/C",
-                                   case
-                                       when hsd.instrument_type_id = 'E' then 'Equity'
-                                       when hsd.instrument_type_id = 'O' then 'Option'
-                                       end, -- as "Security Type",
-                                   hsd.underlying_symbol, -- as "Root Symbol",
-                                   yc.client_id, -- as "Client ID",
-                                   cf.customer_or_firm_name, -- as "Capacity",
-                                   dss.sub_strategy, -- as "Sub Strategy",
-                                   coalesce(exd.ex_destination_desc, co.ex_destination), -- as "Ex Dest",
-                                   fc.fix_comp_id, -- as "Sending Firm",
-                                   lst_ex.exec_type_description, -- as "Event Type",
-                                   lst_ex.exec_text, -- as "Free Text",
-                                   null::text, -- as "Reject Reason",
-                                   co.max_floor::text, -- as "Max Floor",
-                                   lst_ex.exec_broker, -- as "Exec Broker",
-                                   co.clearing_firm_id, -- as "CMTA",
-                                   case when yc.multileg_reporting_type = '1' then 'N' else 'Y' end, -- as "Is Mleg",
-                                   case when yc.cross_order_id is not null then 'Y' else 'N' end, -- as "Is Cross",
-                                   coa.auction_id::text, -- as "ATS Auction ID", -- ??
-                                   case
-                                       when cro.cross_type = 'C' then 'Customer Match'
-                                       when cro.cross_type in ('F', '2') then 'Facilitation'
-                                       when cro.cross_type in ('P', '4') then 'Price Improvement Mechanism'
-                                       when cro.cross_type = 'S' then 'Solicitation'
-                                       when cro.cross_type = 'Q' then 'Qualified Contingent Cross'
-                                       when cro.cross_type = '1' then 'Solicitation or Customer Match Order'
-                                       else cro.cross_type
-                                       end, -- as "Cross Ord Type",
-                                   co.fee_sensitivity::text, -- as "Fee Sensitivity",
-            --coalesce(fm_ex.tag_21, fm_co.tag_21)                                , -- as "Handle Inst",
-                                   co.handl_inst::varchar, -- as "Handle Inst",
-                                   co.locate_broker, -- as "Locate Broker",
-                                   co.co_client_leg_ref_id, -- as "Leg ID",
-                                   a.broker_dealer_mpid, -- as "MPID",           --??
-                                   co.occ_optional_data, -- as "OCC Opt Data", --fix_message->>'10441'
-                                   doc.order_capacity_name, -- as "Ord Capacity",
-                                   hsd.opra_symbol, -- as "OSI Symbol",
-                                   ds.sub_system_id, -- as "Sub System",
-                                   co.session_eligibility::varchar, -- as "Session Eligibility",
-                                   co.sweep_style::varchar, -- as "Sweep Style",
+       case
+           when co.side = '1' then 'Buy'
+           when co.side in ('2', '5', '6') then 'Sell'
+           else ''
+           end,                                                             -- as "Side",
+       co.order_qty::text,                                                  -- as "Ord Qty",
+       ftr.day_cum_qty::text,                                               -- as "Ex Qty",
+       ftr.leaves_qty::text,                                                -- as "Lvs Qty",
+       hsd.display_instrument_id,                                           -- as "Symbol",
+       to_char(hsd.maturity_date, 'MM/DD/YYYY'),                            -- as "Expiration Day",
+       round(co.price, 6)::text,                                            -- as "Price",
+       round(ftr.avg_px, 6)::text,                                          -- as "Avg Px",
+       tif.tif_name,                                                        -- as "TIF",
+       ot.order_type_name,                                                  -- as "Ord Type",
+       case
+           when co.open_close = 'O' then 'Open'
+           when co.open_close = 'C' then 'Close'
+           else '' end,                                                     -- as "O/C",
+       case
+           when hsd.instrument_type_id = 'E' then 'Equity'
+           when hsd.instrument_type_id = 'O' then 'Option'
+           end,                                                             -- as "Security Type",
+       hsd.underlying_symbol,                                               -- as "Root Symbol",
+       co.client_id_text,                                                   -- as "Client ID",
+       cf.customer_or_firm_name,                                            -- as "Capacity",
+       dss.sub_strategy,                                                    -- as "Sub Strategy",
+       coalesce(exd.ex_destination_desc, co.ex_destination),                -- as "Ex Dest",
+       fc.fix_comp_id,                                                      -- as "Sending Firm",
+       lst_ex.exec_type_description,                                        -- as "Event Type",
+       lst_ex.exec_text,                                                    -- as "Free Text",
+       null::text,                                                          -- as "Reject Reason",
+       co.max_floor::text,                                                  -- as "Max Floor",
+       lst_ex.exec_broker,                                                  -- as "Exec Broker",
+       co.clearing_firm_id,                                                 -- as "CMTA",
+       case when co.multileg_reporting_type = '1' then 'N' else 'Y' end,    -- as "Is Mleg",
+       case when co.cross_order_id is not null then 'Y' else 'N' end,       -- as "Is Cross",
+       coa.auction_id::text,                                                -- as "ATS Auction ID", -- ??
+       case
+           when cro.cross_type = 'C' then 'Customer Match'
+           when cro.cross_type in ('F', '2') then 'Facilitation'
+           when cro.cross_type in ('P', '4') then 'Price Improvement Mechanism'
+           when cro.cross_type = 'S' then 'Solicitation'
+           when cro.cross_type = 'Q' then 'Qualified Contingent Cross'
+           when cro.cross_type = '1' then 'Solicitation or Customer Match Order'
+           else cro.cross_type
+           end,                                                             -- as "Cross Ord Type",
+       co.fee_sensitivity::text,                                            -- as "Fee Sensitivity",
+       --coalesce(fm_ex.tag_21, fm_co.tag_21)                                , -- as "Handle Inst",
+       co.handl_inst::varchar,                                              -- as "Handle Inst",
+       co.locate_broker,                                                    -- as "Locate Broker",
+       co.co_client_leg_ref_id,                                             -- as "Leg ID",
+       a.broker_dealer_mpid,                                                -- as "MPID",           --??
+       co.occ_optional_data,                                                -- as "OCC Opt Data", --fix_message->>'10441'
+       doc.order_capacity_name,                                             -- as "Ord Capacity",
+       hsd.opra_symbol,                                                     -- as "OSI Symbol",
+       ds.sub_system_id,                                                    -- as "Sub System",
+       co.session_eligibility::varchar,                                     -- as "Session Eligibility",
+       co.sweep_style::varchar,                                             -- as "Sweep Style",
 
- */
        tf.trading_firm_name                                                 -- as "Trading Firm"
 --                                     ], ',', '')
 from order_ids_cte oic
@@ -105,6 +104,7 @@ from order_ids_cte oic
 --                  left join data_marts.f_yield_capture pyc
 --                            on (pyc.status_date_id between in_start_date_id and in_end_date_id and
 --                                pyc.status_date_id = yc.status_date_id and pyc.order_id = yc.parent_order_id)
+
          join dwh.client_order co on co.create_date_id = oic.create_date_id and co.order_id = oic.order_id and
                                      co.create_date_id between :in_start_date_id and :in_end_date_id
          left join dwh.client_order pyc on pyc.create_date_id between :in_start_date_id and :in_end_date_id and
@@ -121,7 +121,7 @@ from order_ids_cte oic
          join dwh.d_trading_firm tf on (tf.trading_firm_unq_id = a.trading_firm_unq_id)
          left join dwh.d_time_in_force tif on tif.is_active and tif.tif_id = co.time_in_force_id
          left join dwh.d_order_type ot on ot.order_type_id = co.order_type_id
---                  join dwh.historic_security_definition_all hsd on (hsd.instrument_id = yc.instrument_id)
+         join dwh.historic_security_definition_all hsd on (hsd.instrument_id = co.instrument_id)
          left join data_marts.d_sub_strategy dss on co.sub_strategy_id = dss.sub_strategy_id
          left join dwh.d_ex_destination exd on (exd.ex_destination_code = co.ex_destination and
                                                 coalesce(exd.exchange_id, '') = coalesce(co.exchange_id, '') and
@@ -131,7 +131,7 @@ from order_ids_cte oic
          left join dwh.d_exchange ex on (ex.exchange_unq_id = co.exchange_unq_id)
          left join lateral
     (
-    select os.order_status_description, ex.exec_text, ex.fix_message_id, exec_type_description, exec_broker
+    select os.order_status_description, ex.exec_text, ex.fix_message_id, exec_type_description, exec_broker, exec_time
     from dwh.execution ex
              left join dwh.d_order_status os on (os.order_status = ex.order_status and os.is_active)
              left join dwh.d_exec_type et on (et.exec_type = ex.exec_type)
@@ -142,6 +142,30 @@ from order_ids_cte oic
     order by ex.exec_id desc
     limit 1
     ) lst_ex on true
+         left join lateral (
+    select max(ftr.date_id)                                                                     as date_id,
+           max(ftr.cmta)                                                                        as cmta,
+           ftr.open_close,
+           max(ftr.order_process_time)                                                          as PROCESS_TIME,
+           ftr.order_id                                                                         as order_id,
+           ftr.instrument_id,
+           ftr.account_id,
+           ftr.side,
+           sum(last_qty)                                                                        as day_cum_qty,
+           sum(last_px)                                                                         as last_px,
+           avg(last_qty),
+           --avg(last_px) as avg_px,
+           case sum(last_qty) when 0 then null else sum(last_qty * last_px) / sum(last_qty) end as avg_px,
+           max(opt_customer_firm)                                                               as CUSTOMER_OR_FIRM_ID,
+           min(ftr.leaves_qty)                                                                  as leaves_qty
+    from dwh.flat_trade_record ftr
+    where ftr.date_id between :in_start_date_id and :in_end_date_id
+      and ftr.date_id = co.create_date_id
+      and ftr.order_id = co.order_id
+      and is_busted = 'N'
+      and ftr.order_id > 0
+    group by ftr.open_close, ftr.order_id, ftr.instrument_id, ftr.account_id, ftr.side
+    ) ftr on true
          left join dwh.d_fix_connection fc on fc.fix_connection_id = co.fix_connection_id --and fc.is_active
          left join dwh.client_order2auction coa
                    on coa.create_date_id between :in_start_date_id and :in_end_date_id and
