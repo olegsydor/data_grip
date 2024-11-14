b008c9b3d81ed4f4a7515347cdd4d538400f6601
Oleh Sydor <ole.sydor@gmail.com>
2024-11-14 09:00:08 +0200

    Regular auto-commit at 09:00:08 on 14/11/2024

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_95.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_95.sql
@@ -369,7 +369,118 @@ select * from dash360.report_fintech_eod_pershing_ps_trade_file(in_start_date_id
 select * from dash360.report_fintech_pershing_ps_trade_file(in_start_date_id := 20241022, in_end_date_id:= 20241022, in_account_ids := '{15826, 69422}');
 
 
-drop table t_rec;
+select char_length(ret_row), ret_row from dash360.report_fintech_eod_pershing_ps_trade_file(in_start_date_id := 20241107, in_end_date_id:= 20241107, in_account_ids := '{8112}');
+
+select rpad(:symb, 9) -- as cusip;
 
 
-select * from t_rec
+    create temp drop table t_rec
+--          on commit drop
+    as
+    select tr.trade_record_id,
+           case
+               when tr.side = '1' then 'B'
+               when tr.side in ('2', '5', '6') then 'S' end as side,
+           tr.last_qty,
+           tr.trade_record_time,
+           1                                                as record_num,
+           array_to_string(ARRAY [
+                               case
+                                   when tr.side = '1' then 'B'
+                                   when tr.side in ('2', '5', '6') then 'S' end, -- as buy_sell, [1]
+                               'NCO', -- as correspondent_office [2-4]
+                               '896673', -- as customer_account_num, --Should we hardcode? [5-10]
+                               '1', -- as account_type, --Should we hardcode? [11]
+--                                rpad(tr.exchange_id, 4), -- as exchange_mnemonic, [12-15]
+                               rpad('', 4), -- as exchange_mnemonic, [12-15]
+                               lpad('', 2), -- as filler1, [16-17]
+                               rpad(case when tr.instrument_type_id = 'E' then di.symbol else 'OPTION' end,
+                                    9), -- as cusip_symbol, --Use Symbol? We don't provide CUISP [18-26]
+                               '8', -- as market, [27]
+                               '1', -- as blotter, [28]
+                               lpad(tr.last_qty::text, 8, '0'), -- as quantity, [29-36]
+                               lpad(floor(tr.last_px)::text, 4, '0'), -- as price_whole_number, --Need whole number [37-40]
+                               to_char(round(10000000 * (last_px - floor(last_px))), 'FM0000000'), -- as price_decimal, --Need decimal [41-47]
+                               ' ', -- as price_code, [48]
+                               '9', -- as settlement_date_code, [49]
+                               ' ', -- as step_in_step_out_code, [50]
+                               'DFIN', -- as executing_broker, [51-54]
+                               '0161', -- as contra_broker, [55-58]
+                               'Y', -- as clearance_only_flag,[59]
+                               ' ', -- as compression_flag, [60]
+                               ' ', -- as cross_trade_generate_code, [61]
+                               ' ', -- as legend_code_1, [62]
+                               ' ', -- as legend_code_2, [63]
+                               ' ', -- as confirm_comparison, [64]
+                               ' ', -- as commission_or_concession_sales_credit1, [65]
+                               'A', -- as execution_account_type, [66]
+                               ' ', -- as filler2, [67]
+                               rpad('', 7), -- as commission_or_concession_sales_credit2, [68-74]
+                               rpad('', 3), -- as rr_override, [75-77]
+                               '1', -- as control_field, [78]
+                               rpad('', 2) -- as filler3 [79-80]
+                               ], '', '')                   as ret_row
+    from dwh.flat_trade_record tr
+             join dwh.d_instrument di on (di.instrument_id = tr.instrument_id)
+    where tr.date_id between :in_start_date_id and :in_end_date_id
+      and tr.is_busted = 'N'
+      and di.instrument_type_id = any(:l_instrument_type_ids)
+      and tr.account_id = any (:l_account_ids);
+
+    insert into t_rec
+    select tr.trade_record_id,
+           case
+               when tr.side = '1' then 'B'
+               when tr.side in ('2', '5', '6') then 'S' end as side,
+           tr.last_qty,
+           tr.trade_record_time,
+           3                                                as record_num,
+           array_to_string(ARRAY [
+                               case
+                                   when tr.side = '1' then 'B'
+                                   when tr.side in ('2', '5', '6') then 'S' end, -- as buy_sell, [1]
+                               'NCO', -- as correspondent_office [2-4]
+                               '896673', -- as customer_account_num, --Should we hardcode? [5-10]
+                               '1', -- as account_type, --Should we hardcode? [11]
+--                                rpad(tr.exchange_id, 4), -- as exchange_mnemonic, [12-15]
+                               rpad('', 4), -- as exchange_mnemonic, [12-15]
+                               lpad('', 2), -- as filler1, [16-17]
+                               rpad(case when tr.instrument_type_id = 'E' then di.symbol else 'OPTION' end,
+                                    9), -- as cusip_symbol, --Use Symbol? We don't provide CUISP [18-26]
+                               '8', -- as market, [27]
+                               '1', -- as blotter, [28]
+                               to_char(tr.trade_record_time, 'MMDDYY'), -- as trade_date, [29-34]
+                               to_char(public.get_settle_date_by_instrument_type(tr.trade_record_time::date,
+                                                                                 tr.instrument_type_id),
+                                       'MMDDYY'), -- as settlement_date, [35-40]
+                               ' ', -- as order_accum_code, [41]
+                               ' ', -- as blank, [42]
+                               ' ', -- as standard_instruction_override, [43]
+                               ' ', -- as blotter_override, [44]
+                               ' ', -- as special_confirm_print, [45]
+                               ' ', -- as multiple_round, [46]
+                               ' ', -- as cns_override, [47]
+                               ' ', -- as transfer_override, [48]
+                               lpad('', 3), -- as execution_time_milli_seconds, [49-51]
+                               lpad('', 2), -- as filler2, [52-53]
+                               lpad('', 3), -- as x2nd_rr_override, [54-56]
+                               lpad('', 2), -- as x2nd_rr_percentage, [57-58]
+                               lpad('', 3), -- as x3rd_rr_override, [59-61]
+                               lpad('', 2), -- as x3rd_rr_percentage, [62-63]
+                               lpad('', 10), -- as trading_account,[64-73]
+--                               lpad('', 4), -- as execution_time_hhmm,[74-77]
+                               lpad(to_char(tr.trade_record_time, 'HH24MI'), 4), -- as execution_time_hhmm,[74-77]
+                               '3', -- as control_field, [78]
+                               lpad('', 2) -- as execution_time_ss [79-80]
+                               ], '', '')                   as ret_row
+    from dwh.flat_trade_record tr
+             join dwh.d_instrument di on (di.instrument_id = tr.instrument_id)
+    where tr.date_id between :in_start_date_id and :in_end_date_id
+      and di.instrument_type_id = any(:l_instrument_type_ids)
+      and tr.is_busted = 'N'
+      and tr.account_id = any (:l_account_ids);
+
+
+select ret_row, trade_record_time from t_rec
+order by trade_record_id, record_num;
+
