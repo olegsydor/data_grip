3adf1c762eb3486c633cc149b23495c516a13cd4
Oleh Sydor <ole.sydor@gmail.com>
2024-11-22 16:19:51 +0200

    Regular auto-commit at 16:19:51 on 22/11/2024

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_102.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_102.sql
@@ -27,7 +27,7 @@ with order_ids_cte as (select co.create_date_id,
                          and case
                                  when coalesce(:in_symbols, '{}') = '{}' then true
                                  else di.symbol = any (:in_symbols) end)
-   , all_rows as (select co.order_id, -- +
+   , all_rows as (select co.order_id,
                          coalesce(pyc.client_order_id, co.client_order_id)                 as "Parent Cl Ord ID",
                          a.account_name                                                    as "Account",
                          case
@@ -50,20 +50,10 @@ with order_ids_cte as (select co.create_date_id,
                       pyc.create_date_id between :in_start_date_id and :in_end_date_id and
                       pyc.create_date_id = co.create_date_id and pyc.order_id = co.parent_order_id
                            join dwh.d_account a on (a.account_id = co.account_id)
-                           left join lateral (select orig.client_order_id
-                                              from dwh.client_order orig
-                                              where orig.create_date_id between :in_start_date_id and :in_end_date_id
-                                                and orig.create_date_id = co.create_date_id
-                                                and orig.order_id = co.orig_order_id
-                                              limit 1) orig on true
                            join dwh.d_trading_firm tf on (tf.trading_firm_unq_id = a.trading_firm_unq_id)
-                           left join dwh.d_time_in_force tif
-                                     on tif.is_active and tif.tif_id = oic.time_in_force_id
-                           left join dwh.d_order_type ot on ot.order_type_id = co.order_type_id
                            join dwh.historic_security_definition_all hsd
                                 on (hsd.instrument_id = co.instrument_id)
-                           left join data_marts.d_sub_strategy dss
-                                     on co.sub_strategy_id = dss.sub_strategy_id
+
                            left join dwh.d_ex_destination exd
                                      on (exd.ex_destination_code = co.ex_destination and
                                          coalesce(exd.exchange_id, '') =
@@ -71,8 +61,6 @@ with order_ids_cte as (select co.create_date_id,
                                          exd.instrument_type_id =
                                          oic.instrument_type_id and
                                          exd.is_active)
-                           left join dwh.d_customer_or_firm cf
-                                     on (cf.customer_or_firm_id = co.customer_or_firm_id)
                            left join dwh.d_exchange ex on (ex.exchange_unq_id = co.exchange_unq_id)
                            left join lateral
                       (
@@ -95,56 +83,21 @@ with order_ids_cte as (select co.create_date_id,
                       order by ex.exec_id desc
                       limit 1
                       ) lst_ex on true
-                           left join lateral (
-                      select sum(last_qty)       as                                         day_cum_qty,
-                             sum(ftr.last_qty * ftr.last_px) / nullif(sum(ftr.last_qty), 0) avg_px,
-                             min(ftr.leaves_qty) as                                         leaves_qty
-                      from flat_trade_record ftr
-                      where true
-                        and ftr.date_id = co.create_date_id
-                        and ftr.date_id between :in_start_date_id and :in_end_date_id
-                        and ftr.street_order_id = co.order_id
-                        and ftr.is_busted = 'N'
-                        and co.parent_order_id is not null
-                      ) ftr on true
-                           left join lateral (
-                      select sum(last_qty)       as                                         day_cum_qty,
-                             sum(ftr.last_qty * ftr.last_px) / nullif(sum(ftr.last_qty), 0) avg_px,
-                             min(ftr.leaves_qty) as                                         leaves_qty
-                      from flat_trade_record ftr
-                      where true
-                        and ftr.date_id = co.create_date_id
-                        and ftr.date_id between :in_start_date_id and :in_end_date_id
-                        and ftr.order_id = co.order_id
-                        and ftr.is_busted = 'N'
-                        and co.parent_order_id is null
-                      ) ftr_par on true
-                           left join dwh.d_fix_connection fc on
-                      fc.fix_connection_id = co.fix_connection_id --and fc.is_active
-                           left join dwh.client_order2auction coa
-                                     on coa.create_date_id between :in_start_date_id and :in_end_date_id and
-                                        coa.create_date_id = co.create_date_id and
-                                        coa.order_id = co.order_id
-                           left join dwh.d_order_capacity doc
-                                     on doc.order_capacity_id = co.eq_order_capacity
-                           left join dwh.d_sub_system ds on
-                      ds.sub_system_unq_id = co.sub_system_unq_id --and ds.is_active
-                           left join dwh.cross_order cro on
-                      cro.cross_order_id = co.cross_order_id and co.cross_order_id is not null
                   where co.create_date_id between :in_start_date_id and :in_end_date_id
                     and co.multileg_reporting_type in ('1', '2')
                     and co.trans_type <> 'F')
-select count(order_id)                    as order_count,
-       count(distinct "Parent Cl Ord ID") as parent_order_count,
+select "Ex Dest",
        "Account",
        "Security Type",
-       "Ex Dest",
        "Event Type",
        "Free Text",
        "Reject Reason",
        "Is Mleg",
        "Is Cross",
-       "Trading Firm"
+       "Trading Firm",
+       array_agg(distinct "Parent Cl Ord ID") as parent_orders,
+       count(distinct "Parent Cl Ord ID")     as parent_order_count,
+       count(order_id)                        as order_count
 from all_rows
 group by "Account", "Security Type", "Ex Dest", "Event Type", "Free Text", "Reject Reason", "Is Mleg", "Is Cross",
          "Trading Firm"
