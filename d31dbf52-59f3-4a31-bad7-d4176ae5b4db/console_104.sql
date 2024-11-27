02c79b624cbad09bfd827657e83c36d37042b479
Oleh Sydor <ole.sydor@gmail.com>
2024-11-27 12:00:08 +0200

    Regular auto-commit at 12:00:08 on 27/11/2024

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_104.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_104.sql
@@ -4,8 +4,30 @@ select cl_exchange_id,
 --        substr(str_t9730, 2, 1) as counterparty_range_for_saphire,
        par_t9730,
        counterparty_range,
-       *
+           case
+               when tbs.ex_exec_type = 'F' then
+                   case
+                       when tbs.par_order_id is not null then tbs.ex_contra_account_capacity
+                       else tbs.es_contra_account_capacity
+                       end
+               end                                                                                     as COUNTERPARTY_RANGE,
 from dash_reporting.imc_final x
 where true
   and ex_exchange_id ~~* any (array ['%MIAX%', '%EMLD%','%SPHR%', '%MPRL%'])
   and cl_exchange_id is null
+
+
+select
+               case
+               when tbs.ex_exec_type = 'F' then
+                   case
+--                        when tbs.ex_exchange_id ~~* any (array ['%MIAX%', '%EMLD%','%SPHR%', '%MPRL%']) then substr(coalesce(str_t9730, par_t9730), 2, 1)
+                       when tbs.ex_exchange_id ~~* any (array ['%MIAX%', '%EMLD%','%SPHR%', '%MPRL%']) then substr(str_t9730, 2, 1)
+                       when tbs.par_order_id is not null then tbs.ex_contra_account_capacity
+                       else tbs.es_contra_account_capacity
+                       end
+               end                                                                                     as COUNTERPARTY_RANGE,
+*
+from dash_reporting.imc_base_ext_md tbs
+where true
+and tbs.ex_exchange_id ~~* any (array ['%MIAX%', '%EMLD%','%SPHR%', '%MPRL%'])
