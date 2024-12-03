d891c5707d9ebc5ce72eed204e3104ec0746988b
Oleh Sydor <ole.sydor@gmail.com>
2024-12-03 17:00:08 +0200

    Regular auto-commit at 17:00:08 on 03/12/2024

--- f9447fdb-8def-4b3f-af2b-edc8e5d1d9d5/ALLOC_UAT.sql
+++ f9447fdb-8def-4b3f-af2b-edc8e5d1d9d5/ALLOC_UAT.sql
@@ -245,6 +245,7 @@ begin
         SELECT ftr.first_orig_trade_record_id,
                qty.last_qty         as first_qty,
                alin.alloc_instr_id,
+--                ae.al
                ftr.last_qty,
                ae.alloc_qty,
                alin.side,
@@ -274,7 +275,9 @@ begin
                                             tr.opt_customer_firm,-- coalesce(tr.street_account_name,'') street_account_name
                                             tr.last_qty,
                                             staging.last_orig_trade_record_id_today(tr.trade_record_id,
-                                                                                    tr.date_id) as first_orig_trade_record_id
+                                                                                    tr.date_id) as first_orig_trade_record_id,
+                                         tr.trade_record_id
+
                                      from genesis2.alloc_instr2trade_record aitr
                                               inner join genesis2.trade_record tr
                                                          on aitr.trade_record_id = tr.trade_record_id
@@ -283,6 +286,7 @@ begin
                                                              and case
                                                                      when :in_exec_broker is null then true
                                                                      else tr.exec_broker = :in_exec_broker end
+                                     and tr.exec_broker is not null
                                      where aitr.alloc_instr_id = alin.alloc_instr_id
                                        and aitr.date_id = alin.date_id
                                      limit 1) ftr on true
@@ -346,7 +350,80 @@ end;
 $fn$
 ;
 
+select * from genesis2.alloc_instr2trade_record
+where alloc_instr_id = -51678
+
+select last_qty, * from genesis2.trade_record
+where trade_record_id in (2346593078, 2346593027, 2346592823)
 
 select staging.last_orig_trade_record_id_today(2346593043, 20241129);
 
-select last_qty, * from trade_record where trade_record_id = 2346592826
+select last_qty, * from trade_record where trade_record_id = 2346592826;
+
+
+SELECT ftr.first_orig_trade_record_id,
+       qty.last_qty         as first_qty,
+       alin.alloc_instr_id,
+       ftr.orig_trade_record_id,
+       ftr.trade_record_id,
+       ftr.last_qty,
+       ae.alloc_qty,
+       alin.side,
+       alin.avg_px,
+       acc.opt_is_fix_clfirm_processed,
+       ftr.cmta             AS ftr_cmta,
+       ca.cmta              AS ca_cmta,
+       alin.date_id,
+       os.min_tick_increment,
+       acc.opt_penny_commission,
+       acc.opt_nickel_commission,
+       os.root_symbol,
+       oc.put_call,
+       oc.maturity_year,
+       oc.maturity_month,
+       oc.maturity_day,
+       oc.strike_price,
+       alin.open_close,
+       acc.opt_is_fix_custfirm_processed,
+       ftr.opt_customer_firm,
+       acc.opt_customer_or_firm,
+       ae.occ_actionable_id as occ_actionable_id --, ftr.street_account_name
+FROM genesis2.allocation_instruction_entry ae
+         JOIN genesis2.allocation_instruction alin
+              ON alin.alloc_instr_id = ae.alloc_instr_id AND alin.is_deleted <> 'Y'
+         inner join lateral (select tr.cmta,
+                                    tr.opt_customer_firm,-- coalesce(tr.street_account_name,'') street_account_name
+                                    tr.last_qty,
+                                    staging.last_orig_trade_record_id_today(tr.trade_record_id,
+                                                                            tr.date_id) as first_orig_trade_record_id,
+                                    tr.trade_record_id,
+                                    tr.orig_trade_record_id
+
+                             from genesis2.alloc_instr2trade_record aitr
+                                      inner join genesis2.trade_record tr
+                                                 on aitr.trade_record_id = tr.trade_record_id
+                                                     and aitr.date_id = tr.date_id
+--                                                              and tr.is_busted = 'N'
+                                                     and case
+                                                             when :in_exec_broker is null then true
+                                                             else tr.exec_broker = :in_exec_broker end
+                                                     and tr.exec_broker is not null
+                             where aitr.alloc_instr_id = alin.alloc_instr_id
+                               and aitr.date_id = alin.date_id
+--                                      limit 1
+    ) ftr on true
+         join lateral (select last_qty
+                       from genesis2.trade_record tr
+                       where tr.trade_record_id = ftr.first_orig_trade_record_id
+                       limit 1) qty on true
+         JOIN genesis2.clearing_account ca
+              ON (ca.clearing_account_id = ae.clearing_account_id /*AND ca.is_deleted <> 'Y'*/
+                  AND ca.clearing_account_type = '1' AND ca.market_type = 'O')
+         JOIN genesis2.account acc ON (acc.account_id = ca.account_id AND acc.is_deleted <> 'Y' AND
+                                       acc.opt_report_to_mpid = 'MLCB' AND acc.trading_firm_id <> 'cantor')
+         JOIN genesis2.option_contract oc ON oc.instrument_id = alin.instrument_id
+         JOIN genesis2.option_series os ON os.option_series_id = oc.option_series_id
+         JOIN genesis2.instrument i ON i.instrument_id = alin.instrument_id
+WHERE alin.date_id between :in_start_date_id and :in_end_date_id
+order by ftr.first_orig_trade_record_id, alin.alloc_instr_id desc, ftr.trade_record_id;
+
