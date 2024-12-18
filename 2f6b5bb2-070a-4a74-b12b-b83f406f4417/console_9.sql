d0b40c095a6c315a71399403e810b93cd68c44cc
Oleh Sydor <ole.sydor@gmail.com>
2024-12-18 20:00:08 +0200

    Regular auto-commit at 20:00:08 on 18/12/2024

--- 2f6b5bb2-070a-4a74-b12b-b83f406f4417/console_9.sql
+++ 2f6b5bb2-070a-4a74-b12b-b83f406f4417/console_9.sql
@@ -9,9 +9,19 @@ alter table training.pack add column decs_id int;
 alter table training.pack add column reqs_id int;
 
 insert into training.pack (pack_id, pack_approve_dec, pack_approve_req)
-values (1, true, false);
+values (2, false, true);
 
 
 select * from training.pack;
-update training.pack
-set pack_approve_dec = case when :in_decs_id is not null then true else pack_approve_dec end
+
+update training.pack dst
+set pack_approve_dec = case when :in_decs_id is not null then true else dst.pack_approve_dec end,
+    pack_approve_req = case when :in_reqs_id is not null then true else dst.pack_approve_req end,
+    pack_status_id   = case
+                           when ((dst.reqs_id is not null and dst.pack_approve_req) or dst.reqs_id is null) then 1
+                           when ((dst.decs_id is not null and dst.pack_approve_dec) or dst.decs_id is null) then 1
+                           else src.pack_status_id end
+from training.pack src
+where src.pack_id = dst.pack_id
+  and dst.pack_id = 1
+
