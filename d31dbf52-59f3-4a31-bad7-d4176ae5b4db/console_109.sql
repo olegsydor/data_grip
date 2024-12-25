882aadd5bd77a91ef23c27979f07f6de63f25cbb
Oleh Sydor <ole.sydor@gmail.com>
2024-12-25 13:00:08 +0200

    Regular auto-commit at 13:00:08 on 25/12/2024

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_109.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_109.sql
@@ -1,12 +1,12 @@
 -- DROP FUNCTION dash360.dash360_report_parent_order_metrics(_int8, varchar, int4, int4, bpchar);
 
-CREATE OR REPLACE FUNCTION trash.dash360_report_parent_order_metrics(account_ids bigint[] DEFAULT '{}'::bigint[],
-                                                                       instrument_type_id character varying DEFAULT NULL::character varying(1),
-                                                                       start_status_date_id integer DEFAULT NULL::integer,
-                                                                       end_status_date_id integer DEFAULT NULL::integer,
-                                                                       is_demo character DEFAULT 'N'::bpchar,
-                                                                       trading_firm_ids character varying[] DEFAULT '{}'::character varying[])
-    RETURNS TABLE
+create or replace function trash.dash360_report_parent_order_metrics(account_ids bigint[] default '{}'::bigint[],
+                                                                       instrument_type_id character varying default null::character varying(1),
+                                                                       start_status_date_id integer default null::integer,
+                                                                       end_status_date_id integer default null::integer,
+                                                                       is_demo character default 'N'::bpchar,
+                                                                       trading_firm_ids character varying[] default '{}'::character varying[])
+    returns table
             (
                 exec_time               timestamp without time zone,
                 routed_time             timestamp without time zone,
@@ -55,17 +55,17 @@ $function$
     -- https://dashfinancial.atlassian.net/browse/DS-9346 SO 20241225 added trading_firm_ids as an input parameter
 declare
     in_instrument_type_id char := $2;
-    l_account_ids int4[];
+    l_account_ids int8[];
 begin
         if coalesce(account_ids, '{}') = '{}' and coalesce(trading_firm_ids, '{}') = '{}' then
         l_account_ids := '{}';
     else
-        select array_agg(account_id)
+        select array_agg(account_id::int8)
         into l_account_ids
         from dwh.d_account
         where true
            and case when coalesce(trading_firm_ids, '{}') <> '{}'::varchar[] then trading_firm_id = ANY (trading_firm_ids) else true end
-           and case when coalesce(account_ids, '{}') <> '{}'::integer[] then account_id = ANY (account_ids) else true end;
+           and case when coalesce(account_ids, '{}') <> '{}'::int8[] then account_id = ANY (account_ids) else true end;
     end if;
 
     return query
@@ -164,3 +164,63 @@ begin
 end;
 $function$
 ;
+---
+select po.account_id, acc.trading_firm_id
+from data_marts.f_yield_capture po
+                 inner join dwh.d_time_in_force tif on tif.is_active and tif.tif_id = po.time_in_force_id
+                 inner join dwh.d_order_type ot on ot.order_type_id = po.order_type_id
+                 inner join dwh.d_instrument i on i.instrument_id = po.instrument_id
+                 left join dwh.d_target_strategy dss on po.sub_strategy_id = dss.target_strategy_id
+                 inner join dwh.d_account acc on (acc.account_id = po.account_id)
+                 inner join lateral (select str.parent_order_id,
+                                            last_value(str.wave_no) over w            as wave_no,
+                                            first_value(str.nbbo_bid_price) over w    as first_wave_nbbo_bid_px,
+                                            last_value(str.nbbo_bid_price) over w     as last_wave_nbbo_bid_px,
+                                            first_value(str.nbbo_ask_price) over w    as first_wave_nbbo_ask_px,
+                                            last_value(str.nbbo_ask_price) over w     as last_wave_nbbo_ask_px,
+                                            first_value(str.nbbo_bid_quantity) over w as first_wave_nbbo_bid_qty,
+                                            last_value(str.nbbo_bid_quantity) over w  as last_wave_nbbo_bid_qty,
+                                            first_value(str.nbbo_ask_quantity) over w as first_wave_nbbo_ask_qty,
+                                            last_value(str.nbbo_ask_quantity) over w  as last_wave_nbbo_ask_qty
+                                     from data_marts.f_yield_capture str
+                                     where str.parent_order_id = po.order_id
+                                       and str.status_date_id >= :start_status_date_id
+                                       and str.status_date_id <= :end_status_date_id
+                                       and str.parent_order_id is not null
+                                       and str.status_date_id = po.status_date_id
+                                     window w as (partition by str.parent_order_id order by str.wave_no)
+                                     order by str.wave_no desc
+                                     limit 1
+            ) parent_waves on true
+                 left join lateral (select *
+                                    from dwh.get_routing_market_data(in_transaction_id := po.transaction_id,
+                                                                     in_exchange_id := 'NBBO',
+                                                                     in_multileg_reporting_type := '3',
+                                                                     in_instrument_id := 3,
+                                                                     in_date_id := po.status_date_id)
+                                    where po.multileg_reporting_type = '2'
+                                      and num_nonnulls(parent_waves.first_wave_nbbo_bid_px,
+                                                       parent_waves.first_wave_nbbo_ask_px,
+                                                       parent_waves.first_wave_nbbo_bid_qty,
+                                                       parent_waves.first_wave_nbbo_ask_qty) = 0
+                                    limit 1
+            ) head_md on true
+        where po.parent_order_id is null
+          and po.status_date_id >= :start_status_date_id
+          and po.status_date_id <= :end_status_date_id
+          and po.multileg_reporting_type in ('1', '2')
+          and po.instrument_type_id = :in_instrument_type_id
+          and po.time_in_force_id in ('0', '2', '3', '4')
+          and po.account_id = any (:l_account_ids);
+
+
+select *
+from trash.dash360_report_parent_order_metrics(account_ids := '{63887}', instrument_type_id := 'O',
+                                               start_status_date_id := 20241224, end_status_date_id := 20241224,
+                                               trading_firm_ids := '{"dftdesk02"}');
+
+
+select *
+from dash360.dash360_report_parent_order_metrics(account_ids := '{}', instrument_type_id := 'O',
+                                               start_status_date_id := 20241224, end_status_date_id := 20241224),
+                                               trading_firm_ids := '{"dftdesk02"}')
