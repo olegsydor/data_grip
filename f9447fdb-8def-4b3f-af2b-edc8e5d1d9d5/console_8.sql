aeddab52012fabbd81838d030bb66a746156c583
Oleh Sydor <ole.sydor@gmail.com>
2025-01-03 16:00:08 +0200

    Regular auto-commit at 16:00:08 on 03/01/2025

--- f9447fdb-8def-4b3f-af2b-edc8e5d1d9d5/console_8.sql
+++ f9447fdb-8def-4b3f-af2b-edc8e5d1d9d5/console_8.sql
@@ -366,6 +366,6 @@ select * from dash360.so_allocations_instruction_trades(in_alloc_instr_id := -53
 select staging.all_orig_trade_record_id_today( 2346619764, 20241210);
 
 
-select * from dash360.so_allocations_snapshot (in_date_id:=20250103, in_account_ids:='{257078}');
+    select * from dash360.so_allocations_snapshot (in_date_id:=20250103, in_account_ids:='{257078}');
 
 select * from dash360.allocations_snapshot (in_date_id:=20241223, in_account_ids:='{257078}');
