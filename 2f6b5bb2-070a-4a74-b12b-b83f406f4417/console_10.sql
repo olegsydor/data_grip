111542780e38774a1ba891fcb41b184eaba21c67
Oleh Sydor <ole.sydor@gmail.com>
2025-01-08 13:31:50 +0200

    Regular auto-commit at 13:31:50 on 08/01/2025

--- 2f6b5bb2-070a-4a74-b12b-b83f406f4417/console_10.sql
+++ 2f6b5bb2-070a-4a74-b12b-b83f406f4417/console_10.sql
@@ -17,7 +17,7 @@ as
 $trg$
 begin
     perform public.send('oleh.sydor@iongroup.com', 'EOS ETL',
-                        new.date_id::text || ': ' || new.node_name || '. ' || 'Ready to reporting');
+                        to_char(clock_timestamp(), 'YYYY-MM-DD HH24:MI:SS') ||'. date_id-'||new.date_id::text || ': ' || new.node_name || '. ' || 'Ready to reporting');
     return new;
 end;
 $trg$;
@@ -99,4 +99,12 @@ select public.send('oleh.sydor@iongroup.com', 'EOS ETL', 'Ready to reporting') i
 
 select * from public.mail_to_send;
 
-select * from inc_hft.load_finish
+select * from inc_hft.load_finish;
+
+insert into inc_hft.load_finish(date_id, node_name)
+values (20250108, 'node_2')
+on conflict (date_id) do nothing
+
+
+
+
