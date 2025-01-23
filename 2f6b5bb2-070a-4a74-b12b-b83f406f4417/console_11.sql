9609b7a6b0fdcc877b42902f33b14f8baa7207f1
Oleh Sydor <ole.sydor@gmail.com>
2025-01-23 14:00:08 +0200

    Regular auto-commit at 14:00:08 on 23/01/2025

---
+++ 2f6b5bb2-070a-4a74-b12b-b83f406f4417/console_11.sql
@@ -0,0 +1,30 @@
+create table training.t1
+(
+    id  int8 primary key,
+    txt text
+);
+
+create table training.t2
+(
+    id    bigserial primary key,
+    t1_id int8
+        constraint t2_t1_fk references training.t1 (id),
+    txt   text
+);
+
+insert into training.t1 (id, txt)
+values (1, 'txt1'),
+       (2, 'txt2'),
+       (3, 'txt3');
+
+insert into training.t2 (t1_id, txt)
+values (1, 'text'),
+       (1, 'text2'),
+       (2, 'text4'),
+       (2, 'text4');
+
+select *
+from training.t1
+         left join training.t2 on t2.t1_id = t1.id
+
+
