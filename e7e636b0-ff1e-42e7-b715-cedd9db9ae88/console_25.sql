f5a46e4a8bdb4a917dcde57a60ee435d03525385
Oleh Sydor <ole.sydor@gmail.com>
2025-01-29 18:00:08 +0200

    Regular auto-commit at 18:00:08 on 29/01/2025

--- e7e636b0-ff1e-42e7-b715-cedd9db9ae88/console_25.sql
+++ e7e636b0-ff1e-42e7-b715-cedd9db9ae88/console_25.sql
@@ -1,110 +1,95 @@
-select
-	real_exch.exchange_name as exchange_name,
-	i.display_instrument_id2 as symbol,
-	i.last_trade_date as expiration_date,
-	tr.side,
-	cie.open_close,
-	cie.last_px as last_px,
-	cie.exec_broker as gup,
-	cie.cmta,
-	cie.opt_customer_firm,
-	cie.clearing_account_number,
-	cie.sub_account,
-	cie.account_id,
-	tr.client_id,
-	sum(cie.last_qty) as last_qty, -- affected qty
-    sum(cie.last_qty) + sum(coalesce(t.last_qty, 0))::bigint total_qty, -- total qty
-    cie.electronic_report_status report_status,
-    cie.street_account_name,
-    cie.street_exec_broker,
-    cie.client_commission_rate,
-    real_exch.exchange_id as exchange_id,
-    i.instrument_type_id,
-    cie.branch_sequence_number,
-    cie.trade_text,
-    cie.frequent_trader_id
-	from clearing_instruction_entry cie
-	--inner join clearing_instruction ci on ci.clearing_instr_id = cie.clearing_instr_id
-    inner join trade_record tr on tr.trade_record_id = cie.trade_record_id
-	left join instrument i on i.instrument_id = tr.instrument_id
-    left join exchange exch on 	exch.exchange_id = tr.exchange_id
-    left join exchange real_exch on real_exch.exchange_id = exch.real_exchange_id
--- Commented due to performnce improvement https://dashfinancial.atlassian.net/browse/DS-284
---    left join lateral (select sum(total.last_qty) last_qty
---	                   from trade_record total
---						where total.date_id = tr.date_id
---						  and date_id = l_date_id
---						  and total.is_busted = 'N'
---						  and total.trade_record_id not in (select	trade_record_id
---															from clearing_instruction_entry
---															where clearing_instr_id = cie.clearing_instr_id)
---						  and total.orig_trade_record_id not in (select	trade_record_id
---																 from clearing_instruction_entry
---																 where clearing_instr_id = cie.clearing_instr_id)
---						  and total.account_id = cie.account_id
---						  and total.instrument_id = tr.instrument_id
---						  and total.side = tr.side
---						  and total.last_px = cie.last_px
---						  and coalesce(total.open_close, '')= coalesce(cie.open_close, '')
---						  and coalesce(total.client_id, '')= coalesce(tr.client_id, '')
---						  and coalesce(total.exec_broker, '')= coalesce(cie.exec_broker, '')
---						  and coalesce(total.cmta, '')= coalesce(cie.cmta, '')
---						  and coalesce(total.opt_customer_firm, '')= coalesce(cie.opt_customer_firm, '') ) t on
---						1 = 1
-    left join  (select total.account_id, total.instrument_id , total.side, total.last_px,
-						coalesce(total.open_close, '') as open_close
-					  , coalesce(total.client_id, '') as client_id
-					  , coalesce(total.exec_broker, '') as exec_broker
-					  , coalesce(total.cmta, '') cmta
-					  , coalesce(total.opt_customer_firm, '') opt_customer_firm
-				      , sum(total.last_qty)last_qty
-	                   from trade_record total
-						where total.date_id = :l_date_id
-						  and date_id = :l_date_id
-						  and total.is_busted = 'N'
-						  and total.trade_record_id not in (select	trade_record_id
-															from clearing_instruction_entry
-															where clearing_instr_id = :in_clearing_instr_id)
-						  and total.orig_trade_record_id not in (select	trade_record_id
-																 from clearing_instruction_entry
-																 where clearing_instr_id = :in_clearing_instr_id)
+select array_agg(trade_record_id)
+from clearing_instruction_entry
+where clearing_instr_id = :in_clearing_instr_id
+--'{3961223943,3961223963,3961224093,3961224107,3961224110,3961224111,3961224140,3961223932,3961223933,3961223944,3961223945,3961223946,3961223961,3961223962,3961223964,3961223973,3961223974,3961223975,3961223983,3961223996,3961223997,3961224004,3961224005,3961224006,3961224021,3961224038,3961224039,3961224040,3961224056,3961224057,3961224058,3961224059,3961224070,3961224072,3961224073,3961224083,3961224094,3961224095,3961224108,3961224109,3961224125,3961224126,3961224139,3961224141,3961224142}'
+select real_exch.exchange_name                                    as exchange_name,
+       i.display_instrument_id2                                   as symbol,
+       i.last_trade_date                                          as expiration_date,
+       tr.side,
+       cie.open_close,
+       cie.last_px                                                as last_px,
+       cie.exec_broker                                            as gup,
+       cie.cmta,
+       cie.opt_customer_firm,
+       cie.clearing_account_number,
+       cie.sub_account,
+       cie.account_id,
+       tr.client_id,
+       sum(cie.last_qty)                                          as last_qty,  -- affected qty
+       sum(cie.last_qty) + sum(coalesce(trt.last_qty, 0))::bigint as total_qty, -- total qty
+       cie.electronic_report_status                               as report_status,
+       cie.street_account_name,
+       cie.street_exec_broker,
+       cie.client_commission_rate,
+       real_exch.exchange_id                                      as exchange_id,
+       i.instrument_type_id,
+       cie.branch_sequence_number,
+       cie.trade_text,
+       cie.frequent_trader_id
+from clearing_instruction_entry cie
+         --inner join clearing_instruction ci on ci.clearing_instr_id = cie.clearing_instr_id
+         inner join trade_record tr on tr.trade_record_id = cie.trade_record_id
+         left join instrument i on i.instrument_id = tr.instrument_id
+         left join exchange exch on exch.exchange_id = tr.exchange_id
+         left join exchange real_exch on real_exch.exchange_id = exch.real_exchange_id
 
-			             group by total.account_id, total.instrument_id , total.side, total.last_px,
-						coalesce(total.open_close, '')
-					  , coalesce(total.client_id, '')
-					  , coalesce(total.exec_broker, '')
-					  , coalesce(total.cmta, '')
-					  , coalesce(total.opt_customer_firm, '')) t on	(t.account_id = cie.account_id
-																  and t.instrument_id = tr.instrument_id
-																  and t.side = tr.side
-																  and t.last_px = cie.last_px
-																  and t.open_close= coalesce(cie.open_close, '')
-																  and t.client_id= coalesce(tr.client_id, '')
-																  and t.exec_broker= coalesce(cie.exec_broker, '')
-																  and t.cmta= coalesce(cie.cmta, '')
-																  and t.opt_customer_firm= coalesce(cie.opt_customer_firm, ''))
+         left join (select total.account_id
+                         , total.instrument_id
+                         , total.side
+                         , total.last_px
+                         , coalesce(total.open_close, '')  as    open_close
+                         , coalesce(total.client_id, '')   as    client_id
+                         , coalesce(total.exec_broker, '') as    exec_broker
+                         , coalesce(total.cmta, '')              cmta
+                         , coalesce(total.opt_customer_firm, '') opt_customer_firm
+                         , sum(total.last_qty)                   last_qty
+                    from trade_record total
+                    where total.date_id = :l_date_id
+                      and date_id = :l_date_id
+                      and total.is_busted = 'N'
+                      and total.trade_record_id not in (select trade_record_id
+                                                        from clearing_instruction_entry
+                                                        where clearing_instr_id = :in_clearing_instr_id)
+                      and total.orig_trade_record_id not in (select trade_record_id
+                                                             from clearing_instruction_entry
+                                                             where clearing_instr_id = :in_clearing_instr_id)
 
-	where cie.clearing_instr_id = :in_clearing_instr_id
-	and tr.date_id =:l_date_id
-	group by cie.account_id,
-			real_exch.exchange_id,
-			i.instrument_id,
-			i.instrument_type_id,
-			i.last_trade_date,
-			tr.side,
-			tr.client_id,
-			cie.open_close,
-			cie.last_px,
-			cie.exec_broker,
-			cie.cmta,
-			cie.opt_customer_firm,
-			cie.clearing_account_number,
-			cie.sub_account,
-			real_exch.exchange_name,
-		    cie.electronic_report_status,
-		    cie.street_account_name,
-            cie.street_exec_broker,
-            cie.client_commission_rate,
-            cie.branch_sequence_number,
-            cie.trade_text,
-            cie.frequent_trader_id;
+                    group by total.account_id, total.instrument_id, total.side, total.last_px
+                           , total.open_close
+                           , total.client_id
+                           , total.exec_broker
+                           , total.cmta
+                           , total.opt_customer_firm) trt on (trt.account_id = cie.account_id
+    and trt.instrument_id = tr.instrument_id
+    and trt.side = tr.side
+    and trt.last_px = cie.last_px
+    and trt.open_close = coalesce(cie.open_close, '')
+    and trt.client_id = coalesce(tr.client_id, '')
+    and trt.exec_broker = coalesce(cie.exec_broker, '')
+    and trt.cmta = coalesce(cie.cmta, '')
+    and trt.opt_customer_firm = coalesce(cie.opt_customer_firm, ''))
+
+where cie.clearing_instr_id = :in_clearing_instr_id
+  and tr.date_id = :l_date_id
+group by cie.account_id,
+         real_exch.exchange_id,
+         i.instrument_id,
+         i.instrument_type_id,
+         i.last_trade_date,
+         tr.side,
+         tr.client_id,
+         cie.open_close,
+         cie.last_px,
+         cie.exec_broker,
+         cie.cmta,
+         cie.opt_customer_firm,
+         cie.clearing_account_number,
+         cie.sub_account,
+         real_exch.exchange_name,
+         cie.electronic_report_status,
+         cie.street_account_name,
+         cie.street_exec_broker,
+         cie.client_commission_rate,
+         cie.branch_sequence_number,
+         cie.trade_text,
+         cie.frequent_trader_id;
--- f9447fdb-8def-4b3f-af2b-edc8e5d1d9d5/BOFA Make changes here only.sql
+++ f9447fdb-8def-4b3f-af2b-edc8e5d1d9d5/BOFA Make changes here only.sql
@@ -13,6 +13,7 @@ function dash360.so_allocations_snapshot(int8[], int4, bpchar) -> dash360.alloca
 function dash360.so_allocations_instruction_delete(int4, int4) -> dash360.allocations_instruction_delete(int4, int4) | dash360.so_allocations_instruction_delete_bkp(int4, int4)
 function trash.report_alloc_instr_trade_record(int4, text) -> dash360.report_alloc_instr_trade_record(int4, text)
 function staging.zabbix_monitor_ptm_missed_r(int4)
+function staging.fix_ptm_missed_r(int4)
 */
 -----------------
 -- TABLES
@@ -1542,6 +1543,51 @@ end;
 
 $fx$;
 comment on function staging.zabbix_monitor_ptm_missed_r is 'The script returns 1 if trade_records exist with missed status R, and zero otherwise';
+
+
+create or replace function staging.fix_ptm_missed_r(in_date_id int4 default public.get_dateid(current_date))
+    returns int4
+    language plpgsql
+as
+$fx$
+
+declare
+    l_load_id int;
+    l_step_id int;
+    l_row_cnt int;
+begin
+    select nextval('public.load_timing_seq') into l_load_id;
+    l_step_id := 1;
+    select public.load_log(l_load_id, l_step_id,
+                           'fix_ptm_missed_r for ' || in_date_id::text || ' STARTED ====', 0, 'O')
+    into l_step_id;
+    with base as (select tr.is_billed, tr.trade_record_id, tr.orig_trade_record_id, tr.exec_id
+                  from genesis2.trade_record tr
+                  where date_id = in_date_id
+                    and tr.is_billed = 'N'
+                    and tr.orig_trade_record_id is not null
+                    and exists(select null
+                               from genesis2.trade_record tri
+                               where tri.date_id = in_date_id
+                                 and tri.exec_id = tr.exec_id
+                                 and tri.is_billed = 'R'
+                                 and tri.trade_record_id < tr.trade_record_id))
+    update genesis2.trade_record tr
+    set is_billed = 'R'
+    from base
+    where tr.trade_record_id = base.trade_record_id
+      and tr.date_id = in_date_id
+      and tr.is_billed <> 'R';
+    get diagnostics l_row_cnt = row_count;
+    select public.load_log(l_load_id, l_step_id,
+                           'fix_ptm_missed_r for ' || in_date_id::text || ' COMPLETED ====', l_row_cnt, 'E')
+    into l_step_id;
+    return l_row_cnt;
+end;
+
+$fx$;
+comment on function staging.fix_ptm_missed_r is 'The script fix the issue when trade_records exist with missed status R';
+
 
 -- drop function if exists dash360.allocations_instruction_trades_bkp(int4);
 -- alter function dash360.allocations_instruction_trades rename to allocations_instruction_trades_bkp;
