bd1c2c8aec5a0f5e223be77cbba34f7fd3db2bba
Oleh Sydor <ole.sydor@gmail.com>
2025-02-01 17:00:10 +0200

    Regular auto-commit at 17:00:10 on 01/02/2025

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_112.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_112.sql
@@ -20,23 +20,84 @@ where da.account_id in (select distinct cl.account_id
                           and (cl.create_time::time between '03:00'::time and '09:30'::time
                             or cl.create_time::time between '16:00'::time and '20:00'::time));
 
-select cl.account_id, ac.account_name, cl.trading_firm_id, tf.trading_firm_name,
-       case when cl.create_time between '03:00'::time and '09:30'::time then 1 else 0 end as case_1,
-       case when cl.create_time between '16:00'::time and '20:00'::time then 1 else 0 end as case_2,
-       case when cl.create_time > '09:30'::time and cl.create_time < '16:00'::time and not exists (select null from dwh.execution ex where ex.exec_date_id = :in_date_id and )
-from dwh.client_order cl
-join dwh.d_account ac on ac.account_id = cl.account_id
-join dwh.d_trading_firm tf on tf.trading_firm_id = cl.trading_firm_id
 
 
+create function trash.so_non_working_time_account(in_date_id int4)
+    returns table
+            (
+                date_id           int4,
+                account_id        int4,
+                account_name      varchar(30),
+                trading_firm_id   varchar(9),
+                trading_firm_name varchar(60),
+                case_1            int8,
+                case_2            int8,
+                case_3            int8
+            )
+    language plpgsql
+as
+$fx$
+declare
 
+begin
+    return query
+        select in_date_id,
+               cl.account_id,
+               ac.account_name,
+               cl.trading_firm_id,
+               tf.trading_firm_name,
+               sum(case when cl.create_time::time between '03:00'::time and '09:30'::time then 1 else 0 end) as case_1,
+               sum(case when cl.create_time::time between '16:00'::time and '20:00'::time then 1 else 0 end) as case_2,
+               sum(case
+                       when cl.create_time::time > '09:30'::time and cl.create_time::time < '16:00'::time and
+                            not exists (select null
+                                        from dwh.execution ex
+                                        where ex.exec_date_id = in_date_id
+                                          and ex.exec_type in ('4', 'F', '8'))
+                           then 1
+                       else 0 end)                                                                           as case_3
+
+        from dwh.client_order cl
+                 join dwh.d_account ac on ac.account_id = cl.account_id
+                 join dwh.d_trading_firm tf on tf.trading_firm_id = cl.trading_firm_id
+        where cl.create_date_id = in_date_id
+        group by cl.account_id,
+                 ac.account_name,
+                 cl.trading_firm_id,
+                 tf.trading_firm_name,
+                 in_date_id
+        having (sum(case when cl.create_time::time between '03:00'::time and '09:30'::time then 1 else 0 end) > 0
+            or sum(case when cl.create_time::time between '16:00'::time and '20:00'::time then 1 else 0 end) > 0
+            or sum(case
+                       when cl.create_time::time > '09:30'::time and cl.create_time::time < '16:00'::time and
+                            not exists (select null
+                                        from dwh.execution ex
+                                        where ex.exec_date_id = in_date_id
+                                          and ex.exec_type in ('4', 'F', '8'))
+                           then 1
+                       else 0 end) > 0
+                   );
+end;
+$fx$;
+
+
+insert into trash.non_working_hours_account
+select *
+from trash.so_non_working_time_account(20240702)
+on conflict (date_id, account_id) do nothing
+
+alter table trash.non_working_hours_account
+add constraint date_id_account_id_unq unique (date_id, account_id)
+
+
+select * from dwh.d_exec_type
 select cl.*, da.account_name, dtf.trading_firm_name from t_01 cl
 join dwh.d_account da on da.account_id = cl.account_id
 join dwh.d_trading_firm dtf on dtf.trading_firm_id = cl.trading_firm_id;
 
 
 
-
+select * from trash.non_working_hours_account
 
 
 
