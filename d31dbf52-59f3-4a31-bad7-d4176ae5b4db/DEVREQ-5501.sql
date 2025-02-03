5aa52e32ddbdbdfb9e8db867e2cbd6e981219889
Oleh Sydor <ole.sydor@gmail.com>
2025-02-03 17:00:14 +0200

    Regular auto-commit at 17:00:14 on 03/02/2025

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/DEVREQ-5501.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/DEVREQ-5501.sql
@@ -94,23 +94,23 @@ begin
                                    tr.fix_comp_id,-- Sending Firm,
                                    tr.principal_amount::text, -- Principal Amount,
                                    tr.tcce_maker_taker_fee_amount::text,-- M/T Fee,
-                                   '-1',-- M/T Fee/Unit,
+                                   to_char(round(-1, 4), 'FM999990.0000'),-- M/T Fee/Unit,
                                    to_char(round(tr.tcce_transaction_fee_amount, 4), 'FM999990.0000'),-- Transaction Fee,
                                    tr.tcce_trade_Processing_Fee_Amount::text,-- Trade Processing Fee,
                                    tr.tcce_royalty_fee_amount::text,-- Royalty Fee,
                                    tr.tcce_mss_fee_amount::text,-- MSS Fee,
-                                   '-1', -- MSS Fee/Unit,
+                                   to_char(round(-1, 4), 'FM999990.0000'), -- MSS Fee/Unit,
                                    to_char(round(tr.tcce_option_regulatory_fee_amount, 4), 'FM999990.0000'),-- Option Reg Fee,
                                    to_char(round(tr.tcce_occ_fee_amount, 4), 'FM999990.0000'), -- OCC Fee,
                                    to_char(round(tr.tcce_sec_fee_amount, 4), 'FM999990.0000'), -- SEC Fee,
                                    to_char(round(tr.tcce_account_dash_commission_amount, 4), 'FM999990.0000'),-- Account Dash Commission,
                                    to_char(round(tr.tcce_account_execution_cost, 4), 'FM999990.0000'),-- Account Exec Cost,
-                                   '-1', -- Account Exec Cost/Unit,
+                                   to_char(round(-1, 4), 'FM999990.0000'), -- Account Exec Cost/Unit,
                                    to_char(round(tr.tcce_firm_dash_commission_amount, 4), 'FM999990.0000'),-- Firm Dash Commission,
                                    to_char(round(tr.tcce_firm_execution_cost, 4), 'FM999990.0000'),-- Firm Exec Cost,
-                                   '-1',-- Firm Exec Cost/Unit'
-                                   ''
-                                   -------------------------------------
+                                   to_char(round(-1, 4), 'FM999990.0000') -- Firm Exec Cost/Unit'
+
+            -------------------------------------
 --                                    tr.trade_record_id::text,
 --                                    tr.orig_trade_record_id::text,
 --                                    tr.trade_record_time::text,
@@ -124,8 +124,7 @@ begin
 --                                    tr.compliance_id,
 --                                    oc.put_call, -- as put_call,
 --                                    oc.strike_price::text -- as strike_px
-                                   ], ',', ''),
-               *
+                                   ], ',', '')
         from dwh.flat_trade_record tr
                  inner join dwh.d_account acc on (tr.account_id = acc.account_id and acc.is_active)
                  inner join dwh.d_instrument i on (i.instrument_id = tr.instrument_id)
@@ -148,12 +147,11 @@ begin
                                       and fmj.date_id = to_char(tr.street_order_process_time, 'YYYYMMDD')::int4
                                     limit 1) fmj on true
                  left join dwh.d_sub_system dss on dss.sub_system_id = tr.subsystem_id and dss.is_active
-        where tr.date_id between :in_start_date_id and :in_end_date_id
+        where tr.date_id between in_start_date_id and in_end_date_id
           and i.symbol not in
               ('ZVZZT', 'ZWZZT', 'CBO', 'CBX', 'IBO', 'IGZ', 'ZBZX', 'ZTEST', 'ZTST', 'ZZZ', 'ZZK', 'ZVV')
           and tr.is_busted = 'N'
---           and tr.account_id = any (:l_account_ids)
-          and tr.exec_id = 62444091583
+          and tr.account_id = any (l_account_ids)
         order by tr.trade_record_time;
     get diagnostics l_row_cnt = row_count;
 
@@ -164,6 +162,7 @@ end;
 $function$
 ;
 
+select * from dash360.report_fintech_eod_ofp0058_trades(20250128, 20250128)
 
 
 select * from dwh.execution
