672d4bd8c6c499070d6e232190bc1473f48b435f
Oleh Sydor <ole.sydor@gmail.com>
2025-02-05 15:00:07 +0200

    Regular auto-commit at 15:00:07 on 05/02/2025

--- f9447fdb-8def-4b3f-af2b-edc8e5d1d9d5/console_8.sql
+++ f9447fdb-8def-4b3f-af2b-edc8e5d1d9d5/console_8.sql
@@ -3367,3 +3367,131 @@ comment on function staging.zabbix_monitor_ptm_missed_r is 'The script fix the i
 
 select * from staging.zabbix_monitor_ptm_missed_r();
 select * from staging.fix_ptm_missed_r();
+
+select * from dash360.allocations_snapshot('{262707}', 20250204) WHERE trade_record_id IN ('2346694938');
+
+drop table if exists t_trade_record;
+  create temp table t_trade_record
+    as
+    select distinct on (atr.trade_record_id, br.to_report, br.alloc_instr_id) atr.trade_record_id,
+                                                                              br.to_report,
+                                                                              br.alloc_instr_id,
+                                                                              br.db_create_time,
+                                                                              'B' as alloc_rep_type
+    from dash_reporting.bofa_allocation_report br
+             join genesis2.alloc_instr2trade_record atr
+                  on atr.alloc_instr_id = br.alloc_instr_id and atr.date_id = br.date_id
+    where br.date_id = :in_date_id
+    union all
+    select btr.trade_record_id, to_report, 0, btr.db_create_time, 'T' as alloc_rep_type
+    from dash_reporting.bofa_trade_record btr
+    where btr.date_id = :in_date_id;
+
+    create index on t_trade_record (trade_record_id);
+
+select exec_id, exch_exec_id, * from genesis2.trade_record
+where trade_record_id = 2346694938
+
+        select tr.date_id,
+               tr.trade_record_id::int8,
+               tr.account_id::int4,
+               tr.instrument_id,
+               tr.side,
+               tr.open_close,
+               tr.last_px                                                  as avg_px,
+               tr.last_qty                                                 as exec_qty,
+               i.display_instrument_id,
+               i.last_trade_date::date,
+               i.instrument_type_id,
+               null                                                        as alloc_instr_id,
+               null                                                        as alloc_time,
+               false                                                       as is_allocated,
+               false                                                       as is_bundle,
+               tr.cmta,
+               tr.exec_broker,
+               case i.instrument_type_id
+                   when 'O' then tr.last_qty * tr.last_px * os.contract_multiplier
+                   else tr.last_qty * tr.last_px
+                   end                                                        principal_amount,
+               case
+                   when acc.trading_firm_id = 'cornerstn' and i.instrument_type_id = 'E' then coalesce(CCRU.rate, 0)
+                   else CCRU.rate end                                      as client_commission_rate,
+               null::varchar                                               as user_name,
+               tr.blaze_account_alias,
+               coalesce(tr.street_trade_record_time, tr.trade_record_time) as street_exec_time,
+               ----------------
+               i.last_trade_date                                           as expiration_date,
+               tr.opt_customer_firm,
+               coalesce(nullif(tr.is_billed, 'N'), rep.to_report)          as reported_status,
+               case when coalesce(nullif(tr.is_billed, 'N'), rep.to_report) = 'R' then
+               coalesce((select bar.db_create_time, *
+                        from dash_reporting.bofa_allocation_report bar
+                                 join genesis2.alloc_instr2trade_record aitr
+                                      on aitr.date_id = bar.date_id and aitr.alloc_instr_id = bar.alloc_instr_id
+                                 join genesis2.trade_record tri
+                                      on tri.date_id = bar.date_id and tri.trade_record_id = aitr.trade_record_id
+                        where true
+--                           and tri.exch_exec_id = tr.exch_exec_id
+                          and tri.exec_id = tr.exec_id
+                          and tri.is_billed = 'R'
+                        order by 1
+                        limit 1), rep.db_create_time) end                  as reported_time,
+               bas.claimed_by                                              as claimed_by,
+               bas.claim_status                                            as claim_status,
+               case when tr.is_billed = 'R' then true end                  as is_prev_reported
+
+        from genesis2.trade_record tr
+                 inner join genesis2.instrument i on (tr.instrument_id = i.instrument_id)
+                 left join genesis2.account acc on acc.account_id = tr.account_id
+                 left join (select ai2tr.trade_record_id, a.alloc_instr_id, a.date_id
+                            from genesis2.allocation_instruction a
+                                     inner join genesis2.alloc_instr2trade_record ai2tr
+                                                on (a.alloc_instr_id = ai2tr.alloc_instr_id and ai2tr.date_id = a.date_id)
+                            where true
+                              and case when in_account_ids = '{}' then true else a.account_id = any (in_account_ids) end
+                              and a.date_id = in_date_id
+                              and a.is_deleted = 'N') allocated_trades
+                           on allocated_trades.trade_record_id = TR.TRADE_RECORD_ID
+                 left join lateral (select rep.to_report, rep.db_create_time
+                                    from t_trade_record rep
+                                    where rep.trade_record_id = tr.trade_record_id
+                                    limit 1) rep on true
+                 left join genesis2.option_contract oc on i.instrument_id = oc.instrument_id
+                 left join genesis2.option_series os on oc.option_series_id = os.option_series_id
+                 left join lateral (select bas.claimed_by, bas.claim_status
+                                    from dash_reporting.bofa_allocation_instruction_status bas
+                                    where bas.alloc_instr_id = allocated_trades.alloc_instr_id
+                                      and bas.date_id = allocated_trades.date_id
+                                      and 1 = 2
+                                    limit 1) bas on true
+                 left join lateral (select L1.rate
+                                    from (SELECT row_number()
+                                                 over (partition by tl.trade_record_id , book_record_type_id , billing_entity order by cr.priority ) as rn,
+                                                 tl.rate
+                                          FROM genesis2.trade_level_book_record tl
+                                                   inner join genesis2.book_record_creator cr
+                                                              on tl.book_record_creator_id = cr.book_record_creator_id
+                                          WHERE tl.date_id = in_date_id
+                                            AND book_record_type_id = 'CCRU'
+                                            and tl.trade_record_id = tr.trade_record_id) L1
+                                    where rn = 1) CCRU on true
+        where tr.date_id = in_date_id
+          and case when in_account_ids = '{}' then true else tr.account_id = any (in_account_ids) end
+          and tr.is_busted = 'N'
+          and allocated_trades.alloc_instr_id is NULL
+          and case
+                  when in_reported_status = 'R' then rep.to_report = 'R'
+                  when in_reported_status = 'U' then rep.to_report in ('U', 'C')
+                  when in_reported_status is null then true end;
+
+
+select bar.db_create_time
+                        from dash_reporting.bofa_allocation_report bar
+                                 join genesis2.alloc_instr2trade_record aitr
+                                      on aitr.date_id = bar.date_id and aitr.alloc_instr_id = bar.alloc_instr_id
+                                 join genesis2.trade_record tri
+                                      on tri.date_id = bar.date_id and tri.trade_record_id = aitr.trade_record_id
+                        where true
+--                           and tri.exch_exec_id = tr.exch_exec_id
+                          and tri.exec_id = tr.exec_id
+                          and tri.is_billed = 'R'
