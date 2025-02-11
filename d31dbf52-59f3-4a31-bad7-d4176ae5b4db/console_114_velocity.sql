11f6e4625bd7460de0420089de46462718cfce52
Oleh Sydor <ole.sydor@gmail.com>
2025-02-11 16:54:15 +0200

    Regular auto-commit at 16:54:15 on 11/02/2025

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_114_velocity.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_114_velocity.sql
@@ -11,93 +11,182 @@ where opra_symbol = 'TTCF 210917P00020000'
 select * from dwh.d_option_series
 where root_symbol = 'TTCF 210917P00020000'
 
+select * from dash360.report_fintech_eod_velocity_option_trade(20220208, 20220208)
+drop function dash360.report_fintech_eod_velocity_option_trade;
+create function dash360.report_fintech_eod_velocity_option_trade(in_start_date_id int4, in_end_date_id int4)
+    returns table
+            (
+                entry_type         text,
+                trd_type           text,
+                trade_dt           int4,
+                settle_dt          text,
+                exec_dt            text,
+                symbol             varchar(30),
+                qty                int8,
+                price              numeric(16, 8),
+                g_amt              text,
+                comm               numeric,
+                sec_fee            text,
+                exch_fee           text,
+                clr_fee            text,
+                ecn_fee            text,
+                brk_fee            text,
+                occ_fee            text,
+                oth_fee            text,
+                "corr"             text,
+                office             text,
+                acct_no            text,
+                acct_type          text,
+                sub_acct_nosub     text,
+                contra_code        text,
+                contra_corr        text,
+                contra_office      text,
+                contra_acct_no     text,
+                contra_acct_type   text,
+                contra_sub_acct_no text,
+                blot_exch_cd       text,
+                blot_clr_typ       text,
+                blot_method        text,
+                trd_tag            text,
+                descr              text,
+                memo1              text,
+                memo2              text,
+                tax_lot            text,
+                lot_tr_no          text,
+                buy_interest       text,
+                finc_yield         text,
+                yield_type         text,
+                capacity           text,
+                ac_grp_cd          text,
+                trailer_codes      text,
+                rr_cd              text,
+                ad_cd              text,
+                an_cd              text,
+                tr_cd              text,
+                currency           text,
+                set_currency       text,
+                set_country        text,
+                set_location       text,
+                reported_price     text,
+                sales_credit       text,
+                discretion_flg     text,
+                solicited          text,
+                comm_type          text,
+                taxlot_tr_no       text,
+                cl_order_id        text,
+                order_id           text,
+                exec_id            text,
+                memo3              text
+            )
+    language plpgsql
+as
+$fx$
+
+declare
+    l_load_id     int;
+    l_step_id     int;
+    l_row_cnt     int;
+    l_account_ids int4[];
+
+begin
+    select nextval('public.load_timing_seq') into l_load_id;
+    l_step_id := 1;
+    select public.load_log(l_load_id, l_step_id,
+                           'report_fintech_eod_velocity_option_trade for ' || in_start_date_id::text ||
+                           '-' || in_end_date_id::text || ' STARTED ====', 0, 'O')
+    into l_step_id;
 
 
-        select tr.trade_record_id::bigint,
-               tr.orig_trade_record_id::bigint,
-               tr.trade_record_time,
-               tr.exec_id::bigint,
-               tr.order_id::bigint,
-               tr.street_order_id::bigint,
-               tr.client_order_id,
-               tr.street_client_order_id,
-               tf.trading_firm_name,
-               acc.account_name,
-               tr.side,
-               tr.open_close,
-               tr.last_qty,
-               tr.last_px,
-               lm.last_mkt_name                            as last_mkt,
-               tr.trade_liquidity_indicator,
-               li.description                              as trade_liquidity_indicator_text,
-               tr.ex_destination,
-               tr.sub_strategy,
-               cf.customer_or_firm_name,
-               tr.exec_broker,
-               tr.cmta,
-               tr.client_id,
-               tr.multileg_reporting_type,
-               tr.is_cross_order,
-               tr.exch_exec_id,
-               tr.secondary_exch_exec_id,
-               tr.fix_comp_id,
-               tr.tcce_account_dash_commission_amount,
-               tr.tcce_account_execution_cost,
-               tr.tcce_firm_dash_commission_amount,
-               tr.tcce_firm_execution_cost,
-               tr.tcce_mss_fee_amount,
-               tr.tcce_maker_taker_fee_amount,
-               tr.tcce_occ_fee_amount,
-               tr.tcce_option_regulatory_fee_amount,
-               tr.tcce_royalty_fee_amount,
-               tr.tcce_sec_fee_amount,
-               tr.tcce_transaction_fee_amount,
-               tr.tcce_trade_Processing_Fee_Amount,
-               i.instrument_type_id                        as sec_type,
-               i.symbol,
-               i.display_instrument_id,
-               i.last_trade_date,
-               coalesce(e.real_exchange_id, e.exchange_id) as real_exchange_id,
-               e.exchange_name,
-               real_exch.exchange_name                     as real_exchange_name,
-               tr.principal_amount,
-               tr.ask_price                                as execution_time_ask_price,
-               tr.bid_price                                as execution_time_bid_price,
-               tr.ask_qty                                  as execution_time_ask_qty,
-               tr.bid_qty                                  as execution_time_bid_qty,
-               tr.routing_time_ask_price,
-               tr.routing_time_bid_price,
-               tr.routing_time_ask_qty,
-               tr.routing_time_bid_qty,
-               tr.trade_record_reason,
-               tr.fee_sensitivity,
-               tr.optional_data,
-               oc.opra_symbol,
-               tr.compliance_id,
-               oc.put_call                                 as put_call,
-               oc.strike_price                             as strike_px
+    select array_agg(d_account.account_id)
+    into l_account_ids
+    from dwh.d_account
+    where d_account.trading_firm_id in ('OFP0045', 'OFP0054', 'velcap01', 'velocltam', 'veloclear')
+      and is_active;
+
+    return query
+        select 'TRD'                                                             as entry_type,
+               tr.open_close || case
+                                    when tr.side = '1' then 'B'
+                                    when tr.side in ('2', '5', '6') then 'S' end as trd_type,
+               tr.date_id                                                        as trade_dt,
+               null                                                              as settle_dt,
+               null                                                              as exec_dt,
+               oc.opra_symbol                                                    as symbol,
+               sum(tr.last_qty)                                                  as qty,
+               sum(tr.last_qty * tr.last_px) / nullif(sum(tr.last_qty), 0)       as price,
+               null                                                              as g_amt,
+               sum(tr.tcce_account_dash_commission_amount)                       as comm,
+               null                                                              as sec_fee,
+               null                                                              as exch_fee,
+               null                                                              as clr_fee,
+               null                                                              as ecn_fee,
+               null                                                              as brk_fee,
+               null                                                              as occ_fee,
+               null                                                              as oth_fee,
+               'VCWC'                                                            as corr,
+               'ALC'                                                             as office,
+               '545764'                                                          as acct_no,
+               'P'                                                               as acct_type,
+               null                                                              as sub_acct_noSub,
+               'DASH'                                                            as contra_code,
+               null                                                              as contra_corr,
+               null                                                              as contra_office,
+               null                                                              as contra_acct_no,
+               null                                                              as contra_acct_type,
+               null                                                              as contra_sub_acct_no,
+               'CMTA IN TO VELOCITY'                                             as blot_exch_cd,
+               'CMTA IN TO VELOCITY'                                             as blot_clr_typ,
+               'CMTA IN TO VELOCITY'                                             as blot_method,
+               '545764'                                                          as trd_tag,
+               null                                                              as descr,
+               null                                                              as memo1,
+               null                                                              as memo2,
+               null                                                              as tax_lot,
+               null                                                              as lot_tr_no,
+               null                                                              as buy_interest,
+               null                                                              as finc_yield,
+               null                                                              as yield_type,
+               'A'                                                               as capacity,
+               null                                                              as ac_grp_cd,
+               null                                                              as trailer_codes,
+               null                                                              as rr_cd,
+               null                                                              as ad_cd,
+               null                                                              as an_cd,
+               null                                                              as tr_cd,
+               null                                                              as currency,
+               null                                                              as set_currency,
+               null                                                              as set_country,
+               null                                                              as set_location,
+               null                                                              as reported_price,
+               null                                                              as sales_credit,
+               null                                                              as discretion_flg,
+               null                                                              as solicited,
+               null                                                              as comm_type,
+               null                                                              as taxlot_tr_no,
+               null                                                              as cl_order_id,
+               null                                                              as order_id,
+               null                                                              as exec_id,
+               null                                                              as memo3
         from dwh.flat_trade_record tr
                  inner join dwh.d_account acc on (tr.account_id = acc.account_id and acc.is_active)
                  inner join dwh.d_instrument i on (i.instrument_id = tr.instrument_id)
-                 left join dwh.d_exchange e on (tr.exchange_id = e.exchange_id and e.is_active = true)
-                 left join dwh.d_exchange real_exch
-                           on (real_exch.exchange_id = coalesce(e.real_exchange_id, e.exchange_id) and
-                               real_exch.is_active = true)
+
                  inner join dwh.d_trading_firm tf on (acc.trading_firm_id = tf.trading_firm_id and tf.is_active)
                  left join dwh.d_option_contract oc on (i.instrument_id = oc.instrument_id)
                  left join dwh.d_option_series os on (oc.option_series_id = os.option_series_id)
-                 left join dwh.d_liquidity_indicator li
-                           on (tr.trade_liquidity_indicator = li.trade_liquidity_indicator and
-                               real_exch.exchange_id = li.exchange_id and li.is_active)
-                 left join dwh.d_customer_or_firm cf on (cf.customer_or_firm_id = opt_customer_firm and cf.is_active)
-                 left join dwh.d_last_market lm on (lm.last_mkt = tr.last_mkt and lm.is_active)
-        where tr.date_id between :in_start_date_id and :in_end_date_id -- in_start_date_id, in_end_date_i
-          and i.symbol not in
-              ('ZVZZT', 'ZWZZT', 'CBO', 'CBX', 'IBO', 'IGZ', 'ZBZX', 'ZTEST', 'ZTST', 'ZZZ', 'ZZK', 'ZVV')
+        where tr.date_id between in_start_date_id and in_end_date_id
           and tr.is_busted = 'N'
-          and acc.account_id = any ('{54657,63783,64663,64880,64881,64983,64984,65121,65122,67947,68347,68349,68388,68475,68476,68578,68579,68848,70037,70262,70268,71372,73518,73527,70036,72054,63782,64662,64980,64981,68348,68350,70749}')
+          and acc.account_id = any (l_account_ids)
+        group by tr.open_close, tr.side, tr.date_id, tr.order_id, oc.opra_symbol, tr.order_id
+        order by tr.order_id;
 
 
-select array_agg(d_account.account_id) from dwh.d_account
-    where d_account.trading_firm_id in ('OFP0045', 'OFP0054', 'velcap01', 'velocltam', 'veloclear')
-and is_active
+    select nextval('public.load_timing_seq') into l_load_id;
+    l_step_id := 1;
+    select public.load_log(l_load_id, l_step_id,
+                           'report_fintech_eod_velocity_option_trade for ' || in_start_date_id::text ||
+                           '-' || in_end_date_id::text || ' COMPLETED ====', 0, 'O')
+    into l_step_id;
+end;
+$fx$
+
