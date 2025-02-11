9dfc45fd4d1b3c33a7af88134139c5e0f03b5c3a
Oleh Sydor <ole.sydor@gmail.com>
2025-02-11 19:54:13 +0200

    Regular auto-commit at 19:54:13 on 11/02/2025

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_114.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_114.sql
@@ -1,6 +1,6 @@
 -- DROP FUNCTION dash360.report_fintech_eod_traiana_broker_fills(int4, int4, _int4, _varchar, text, numeric);
 
-CREATE OR REPLACE FUNCTION dash360.report_fintech_eod_traiana_broker_fills(in_start_date_id integer DEFAULT public.get_dateid(CURRENT_DATE), in_end_date_id integer DEFAULT public.get_dateid(CURRENT_DATE), in_account_ids integer[] DEFAULT NULL::integer[], in_trading_firm_ids character varying[] DEFAULT '{}'::character varying[], in_exec_broker text DEFAULT NULL::text, in_comm_rate numeric DEFAULT 0)
+CREATE FUNCTION dash360.report_fintech_eod_traiana_broker_fills_(in_start_date_id integer DEFAULT public.get_dateid(CURRENT_DATE), in_end_date_id integer DEFAULT public.get_dateid(CURRENT_DATE), in_account_ids integer[] DEFAULT NULL::integer[], in_trading_firm_ids character varying[] DEFAULT '{}'::character varying[], in_exec_broker text DEFAULT NULL::text, in_comm_rate numeric DEFAULT 0)
  RETURNS TABLE(ret_row text)
  LANGUAGE plpgsql
 AS $function$
@@ -25,9 +25,8 @@ begin
         l_account_ids := '{}';
     else
         select array_agg(account_id)
---         into l_account_ids
+        into l_account_ids
         from dwh.d_account
-        where a
         where true
           and case
                   when coalesce(in_trading_firm_ids, '{}') <> '{}'::varchar[]
@@ -42,75 +41,57 @@ begin
         select 'Trade date,Trade Ref,Order ID,Action,Executing Broker,Client,Side,Fut/Opt,Ticker,BBG,Exchange,Maturity Date,Prompt,Strike,Put/Call,Quantity,Price,Expiry,Commission';
 
     return query
-        select --array_to_string(array [
---                                    to_char(ex.exec_time, 'YYYYMMDD'), -- as "Trade date",
-                                   cl.order_id::text, -- as "Trade Ref",
---                                    cl.client_order_id, -- as "Order ID",
-            ex.exec_id,
-                                   ex.exec_type,
-                                   case ex.exec_type
-                                       when 'F' then 'New' -- New
-                                       when '4' then 'Cancel' -- Cancel
-                                       when '5' then 'Amend' -- Replace
-                                       when 'W' then 'Amend' -- Replace
-                                       end, -- as "Action",
-
---                                    :in_exec_broker, -- as "Executing Broker",
---                                    upper(cl.trading_firm_id), -- as "Client",
---                                    case cl.side
---                                        when '1' then 'Buy'
---                                        when '2' then 'Sell'
---                                        when '3' then 'Buymin'
---                                        when '5' then 'SellShort'
---                                        end, -- as "Side",
---                                    'Opt', -- as "Fut/Opt",
---                                    di.symbol, -- as "Ticker",
---                                    oc.opra_symbol, -- as "BBG", --OSI
--- --             cl.exchange_id -- as "Exchange",
---                                    exc.mic_code, -- as "Exchange",
---                                    to_char(oc.maturity_year, 'FM0000') || to_char(OC.maturity_month, 'FM00') ||
---                                    to_char(OC.maturity_day, 'FM00'), -- as "Maturity Date",
---                                    to_char(OC.maturity_year, 'FM0000') ||
---                                    to_char(OC.maturity_month, 'FM00'), -- as "Prompt",
+        select array_to_string(array [
+                                   ftr.date_id::text, -- as "Trade date",
+                                   ftr.order_id::text, -- as "Trade Ref",
+                                   ftr.client_order_id, -- as "Order ID",
+                                   case
+                                       when ftr.is_busted = 'Y' then 'Cancel'
+                                       when ftr.orig_trade_record_id is not null then 'Amend'
+                                       else 'New' end, --as "Action",
+                                   in_exec_broker, -- as "Executing Broker",
+                                   upper(ftr.trading_firm_id), -- as "Client",
+                                   case ftr.side
+                                       when '1' then 'Buy'
+                                       when '2' then 'Sell'
+                                       when '3' then 'Buymin'
+                                       when '5' then 'SellShort'
+                                       end, -- as "Side",
+                                   'Opt', -- as "Fut/Opt",
+                                   di.symbol, -- as "Ticker",
+                                   oc.opra_symbol, -- as "BBG", --OSI
+--             cl.exchange_id -- as "Exchange",
+                                   exc.mic_code, -- as "Exchange",
+                                   to_char(oc.maturity_year, 'FM0000') || to_char(OC.maturity_month, 'FM00') ||
+                                   to_char(OC.maturity_day, 'FM00'), -- as "Maturity Date",
+                                   to_char(OC.maturity_year, 'FM0000') ||
+                                   to_char(OC.maturity_month, 'FM00'), -- as "Prompt",
                                    oc.strike_price::text, -- as "Strike",
                                    case oc.put_call when '0' then 'Put' when '1' then 'Call' end, -- as "Put/Call",
-                                   CL.order_qty::text, -- as "Quantity",
-                                   cl.price::text, -- as "Price",
+                                   ftr.order_qty::text, -- as "Quantity",
+                                   ftr.last_px::text, -- as "Price",
                                    to_char(OC.maturity_year, 'FM0000') || to_char(OC.maturity_month, 'FM00') ||
                                    to_char(OC.maturity_day, 'FM00'), -- as "Expiry"
-                                  case
-                                      when :in_exec_broker is not distinct from 'DASH'
-                                          then ftr.tcce_account_dash_commission_amount::text end, -- as "Commission
-                                   to_char(:in_comm_rate, 'FM999990.0099')
-                               --    ], ',', '')
-        from dwh.client_order cl
-                 inner join dwh.d_fix_connection fc on (fc.fix_connection_id = cl.fix_connection_id)
-                 join dwh.d_instrument di on di.instrument_id = cl.instrument_id
-                 inner join dwh.execution ex on cl.order_id = ex.order_id and ex.exec_date_id >= cl.create_date_id
-                 left join dwh.d_exchange exc on exc.exchange_id = cl.exchange_id and exc.is_active
-                 inner join dwh.d_option_contract oc on (oc.instrument_id = cl.instrument_id)
+--                                   case
+--                                       when in_exec_broker is not distinct from 'DASH'
+--                                           then ftr.tcce_account_dash_commission_amount::text end -- as "Commission
+                                   to_char(in_comm_rate, 'FM999990.0099')
+                                   ], ',', '')
+        from dwh.flat_trade_record ftr
+                 join dwh.d_instrument di on di.instrument_id = ftr.instrument_id
+--                  inner join dwh.execution ex on cl.order_id = ex.order_id and ex.exec_date_id >= cl.create_date_id
+                 left join dwh.d_exchange exc on exc.exchange_id = ftr.exchange_id and exc.is_active
+                 inner join dwh.d_option_contract oc on (oc.instrument_id = ftr.instrument_id)
                  inner join dwh.d_option_series os on (oc.option_series_id = os.option_series_id)
                  inner join dwh.d_instrument ui on ui.instrument_id = os.underlying_instrument_id
-                 left join lateral (select ftr.tcce_account_dash_commission_amount
-                                    from dwh.flat_trade_record ftr
-                                    where ftr.exec_id = ex.exec_id
-                                      and ex.order_id = ftr.order_id
-                                      and ex.exec_date_id = ftr.date_id
-                                    limit 1
-            ) ftr on true
 
-        where cl.create_date_id between :in_start_date_id and :in_end_date_id
-          and case when :l_account_ids = '{}' then true else cl.account_id = any (:l_account_ids) end
-          and cl.parent_order_id is null
-          and cl.multileg_reporting_type in ('1', '2')
+
+        where ftr.date_id between in_start_date_id and in_end_date_id
+          and case when l_account_ids = '{}' then true else ftr.account_id = any (l_account_ids) end
+--           and cl.parent_order_id is null
+          and ftr.multileg_reporting_type in ('1', '2')
           and di.instrument_type_id = 'O'
-          and ex.is_busted = 'N'
---           and ex.exec_type not in ('3', 'a', '5', 'E')
---           and ex.exec_type in ('F', '4', 'W', '5')
-          and ex.exec_type in ('F', '4', '5')
-          and cl.trans_type <> 'F'
---           and case when in_exec_broker is null then true else ex.exec_broker = in_exec_broker end
-        order by cl.order_id, ex.exec_id;
+        order by ftr.order_id, ftr.exec_id;
     get diagnostics l_row_cnt = row_count;
 
     select public.load_log(l_load_id, l_step_id,
@@ -120,3 +101,8 @@ begin
 end ;
 $function$
 ;
+
+select *
+from dash360.report_fintech_eod_traiana_broker_fills_(in_start_date_id := 20241101, in_end_date_id := 20241102,
+                                                     in_account_ids := '{70621}', in_exec_broker := 'DASH',
+                                                     in_comm_rate := 1)
