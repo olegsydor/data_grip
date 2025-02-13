e94f273281ab74409e4c935c4d337e5bf4d15b47
Oleh Sydor <ole.sydor@gmail.com>
2025-02-13 14:00:08 +0200

    Regular auto-commit at 14:00:08 on 13/02/2025

--- 2f6b5bb2-070a-4a74-b12b-b83f406f4417/console_14.sql
+++ 2f6b5bb2-070a-4a74-b12b-b83f406f4417/console_14.sql
@@ -17,8 +17,8 @@ where id in (5)
 select * from blaze7.src
 select * from blaze7.dst;
 
-select max(id)
-from blaze7.src
+select max(ts)
+from blaze7.dst
 
 select ts
 from blaze7.src
@@ -27,6 +27,6 @@ limit 1
 
 select * from blaze7.src
 where ((id > 5)
-or (ts > '2025-02-13 08:29:58.418223'));
+or (id < 5 and ts > '2025-02-13 08:29:58.418223'));
 
 select unnest(array[2346722287,2346722288,2346722301,2346722302,2346722303,2346722307,2346722308,2346722309,2346722310,2346722312,2346722313,2346722340,2346722342,2346722348,2346722352,2346722365,2346722370])
