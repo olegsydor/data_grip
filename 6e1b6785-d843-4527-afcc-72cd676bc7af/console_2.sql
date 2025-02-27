940633c594f6144c6d6c7ee64d4c6f91bcddd17e
Oleh Sydor <ole.sydor@gmail.com>
2025-02-27 11:00:08 +0200

    Regular auto-commit at 11:00:08 on 27/02/2025

--- 6e1b6785-d843-4527-afcc-72cd676bc7af/console_2.sql
+++ 6e1b6785-d843-4527-afcc-72cd676bc7af/console_2.sql
@@ -151,7 +151,7 @@ BEGIN
     -- 1-st execution on the next day should find all gaps if they'll be found...
      l_local_rfq_id  := ( select coalesce(max(q.rfq_id), -1) as local_rfq_id
                           from data_marts.f_rfq_details q
-                          where q.auction_date_id = l_cur_date_id ) - 100000; -- why this 10000 is here if we do not have on conflict update later in insert?
+                          where q.auction_date_id = l_cur_date_id ) - 100000;
      l_max_rfq_id := (select max(r.rfq_id) FROM dwh.request_for_quote r
                                            where r.auction_date_id = l_cur_date_id);
 
