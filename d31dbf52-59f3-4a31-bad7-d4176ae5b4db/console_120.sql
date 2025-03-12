c850e006cf1c0897a4639ede790d35fd6aee7b78
Oleh Sydor <ole.sydor@gmail.com>
2025-03-12 17:00:09 +0200

    Regular auto-commit at 17:00:09 on 12/03/2025

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_120.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_120.sql
@@ -320,3 +320,267 @@ where true
 select * from tmp_606_isi_bill_changes
 where aux_tag_street is not null;
 
+
+
+
+--------------------- ROLL BACK
+drop function if exists dash360.report_isi_bill_changes_monthly;
+CREATE OR REPLACE FUNCTION dash360.report_isi_bill_changes_monthly(p_start_date_id integer DEFAULT NULL::integer, p_end_date_id integer DEFAULT NULL::integer, p_trading_firm_ids character varying[] DEFAULT '{}'::character varying[])
+ RETURNS TABLE(export_row text)
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+  select_stmt     text;
+  sql_params      text;
+  l_row_cnt       integer;
+
+  l_start_date_id  integer;
+  l_end_date_id    integer;
+  l_gtc_date_id    integer;
+  l_trading_firm_ids character varying[];
+
+   l_load_id        integer;
+   l_step_id        integer;
+
+
+begin
+  /*https://dashfinancial.atlassian.net/browse/DEVREQ-2469
+   *
+   * 2024-05-21 DS DEVREQ-4314 Exclude BLAZE/DASH OMS routes on "Billing ExecutionID to Tag17 cross reference"
+   *
+   *
+   * */
+  select nextval('public.load_timing_seq') into l_load_id;
+  l_step_id:=1;
+
+   select public.load_log(l_load_id, l_step_id, 'dash360.report_isi_bill_changes_monthly STARTED===', 0, 'O')
+   into l_step_id;
+
+   if p_start_date_id is not null and p_end_date_id is not null
+    then
+      l_start_date_id := p_start_date_id;
+      l_end_date_id := p_end_date_id;
+    else
+      l_start_date_id := (to_char(date_trunc(NOW() - interval '1 month'), 'YYYYMMDD'))::integer;
+      l_end_date_id := (to_char(date_trunc(NOW()) - interval '1 day' , 'YYYYMMDD'))::integer;
+
+   end if;
+
+   l_gtc_date_id := to_char((to_date(l_start_date_id::varchar, 'YYYYMMDD') - interval '6 months'), 'YYYYMMDD')::integer;
+
+   l_trading_firm_ids := case when p_trading_firm_ids = '{}' then ARRAY['isigroup'] else p_trading_firm_ids end;
+
+
+    select public.load_log(l_load_id, l_step_id, left(' trading_firm_ids = '||l_trading_firm_ids::varchar, 200), 0, 'O')
+   into l_step_id;
+    select public.load_log(l_load_id, l_step_id, ' Period: l_start_date_id = '||l_start_date_id::varchar||', l_end_date_id = '||l_end_date_id::varchar, 0, 'O')
+   into l_step_id;
+
+     execute 'DROP TABLE IF EXISTS tmp_606_isi_bill_changes;';
+       create temp table tmp_606_isi_bill_changes with (parallel_workers = 4) ON COMMIT drop as
+        select to_char(tr.trade_record_time, 'YYYY-MM-DD') as date_
+          , tr.order_id
+          , tr.trade_record_id as report_id
+          , tr.exch_exec_id as tag_17
+          , tr.secondary_exch_exec_id as street_tag_17
+          --
+          , to_char(tr.order_process_time, 'YYYYMMDD')::integer as order_date_id
+          , jo.fix_message ->> '143' as t_143
+          , tr.ex_destination
+        from dwh.flat_trade_record tr
+          left join fix_capture.fix_message_json jo on tr.order_fix_message_id = jo.fix_message_id and jo.date_id = to_char(tr.order_process_time, 'YYYYMMDD')::integer
+        where tr.date_id between l_start_date_id and p_end_date_id
+          and tr.account_id in (select ac.account_id from dwh.d_account ac where ac.trading_firm_id = ANY (l_trading_firm_ids))
+          and tr.is_busted = 'N'
+          and not (tr.ex_destination = 'BRKPT' and coalesce(jo.fix_message ->> '143', '-1') <> 'DASH-CBOE') -- DEVREQ-4314 Exclude any execution on orders routed to non-DASH DASHOMS orders.
+        ;
+        GET DIAGNOSTICS l_row_cnt = ROW_COUNT;
+        select public.load_log(l_load_id, l_step_id, 'tmp_606_isi_bill_changes - Initial load', l_row_cnt, 'I')
+       into l_step_id;
+
+
+     execute 'analyze tmp_606_isi_bill_changes';
+   -- execute 'DROP TABLE IF EXISTS trash.sdn_tmp_606_isi_bill_changes;';
+   -- create table trash.sdn_tmp_606_isi_bill_changes as
+   -- select * from tmp_606_isi_bill_changes;
+
+   RETURN QUERY
+    select 'DATE,ORDERID,REPORTID,TAG17' as roe --,SECONDARY TAG17
+    union all
+    select
+      coalesce(s.date_::varchar, '')                        ||','||  --
+      coalesce(s.order_id::varchar, '')                     ||','||  --
+      coalesce(s.report_id::varchar, '')                    ||','||  --
+      coalesce(s.tag_17::varchar, '')                                --
+      --coalesce(s.street_tag_17::varchar, '')                         --
+
+      as roe
+    from
+      (
+        select date_, order_id, report_id, tag_17 --, street_tag_17
+        from tmp_606_isi_bill_changes
+        order by 1,2,3
+      ) s
+    ;
+
+   select public.load_log(l_load_id, l_step_id, 'dash360.report_isi_bill_changes_monthly COMPLETE===', coalesce(l_row_cnt,0), 'O')
+   into l_step_id;
+
+end;
+$function$;
+
+drop function if exists dash360.report_isi_bill_changes_monthly_mod;
+CREATE OR REPLACE FUNCTION dash360.report_isi_bill_changes_monthly_mod(p_start_date_id integer DEFAULT NULL::integer, p_end_date_id integer DEFAULT NULL::integer, p_trading_firm_ids character varying[] DEFAULT '{}'::character varying[], p_add_exchange_order_id character DEFAULT 'N'::bpchar)
+ RETURNS TABLE(export_row text)
+ LANGUAGE plpgsql
+AS $function$
+    -- https://dashfinancial.atlassian.net/browse/DEVREQ-2469
+    -- 2024-05-21 DS DEVREQ-4314 Exclude BLAZE/DASH OMS routes on "Billing ExecutionID to Tag17 cross reference"
+    -- 2025-03-06 OS https://dashfinancial.atlassian.net/browse/DEVREQ-5704
+DECLARE
+
+    l_row_cnt          integer;
+    l_start_date_id    integer;
+    l_end_date_id      integer;
+    l_gtc_date_id      integer;
+    l_trading_firm_ids character varying[];
+    l_account_ids      int4[];
+    l_load_id          integer;
+    l_step_id          integer;
+
+begin
+
+
+    select nextval('public.load_timing_seq') into l_load_id;
+    l_step_id := 1;
+
+    select public.load_log(l_load_id, l_step_id, 'dash360.report_isi_bill_changes_monthly STARTED===', 0,
+                           'O')
+    into l_step_id;
+
+    if p_start_date_id is not null and p_end_date_id is not null
+    then
+        l_start_date_id := p_start_date_id;
+        l_end_date_id := p_end_date_id;
+    else
+        l_start_date_id := (to_char(date_trunc(NOW() - interval '1 month'), 'YYYYMMDD'))::integer;
+        l_end_date_id := (to_char(date_trunc(NOW()) - interval '1 day', 'YYYYMMDD'))::integer;
+
+    end if;
+
+    l_gtc_date_id :=
+            to_char((to_date(l_start_date_id::varchar, 'YYYYMMDD') - interval '6 months'), 'YYYYMMDD')::integer;
+
+    l_trading_firm_ids := case when p_trading_firm_ids = '{}' then ARRAY ['isigroup'] else p_trading_firm_ids end;
+
+
+    select array_agg(ac.account_id)
+    into l_account_ids
+    from dwh.d_account ac
+    where ac.trading_firm_id = any (l_trading_firm_ids);
+
+
+    select public.load_log(l_load_id, l_step_id, left(' trading_firm_ids = ' || l_trading_firm_ids::varchar, 200), 0,
+                           'O')
+    into l_step_id;
+    select public.load_log(l_load_id, l_step_id,
+                           ' Period: l_start_date_id = ' || l_start_date_id::varchar || ', l_end_date_id = ' ||
+                           l_end_date_id::varchar, 0, 'O')
+    into l_step_id;
+    select public.load_log(l_load_id, l_step_id, ' Account_ids: ' || left(l_account_ids::text, 50), 0, 'O')
+    into l_step_id;
+
+    DROP TABLE IF EXISTS tmp_606_isi_bill_changes;
+    create temp table tmp_606_isi_bill_changes with (parallel_workers = 4)
+--                                                ON COMMIT drop
+    as
+    select to_char(tr.trade_record_time, 'YYYY-MM-DD') as "Date",
+           coalesce(tr.client_order_id, '')            as "OrderID",
+           coalesce(tr.secondary_order_id, '')         as "ExchOrderID",
+  case
+           when (tr.instrument_type_id, tr.exchange_id, dex.cat_exchange_id) = ('E', 'XASE', 'AMER') then jos.t_9483
+           when (tr.instrument_type_id, tr.exchange_id, dex.cat_exchange_id) = ('E', 'ARCAE', 'ARCA') then jos.t_9483
+           when (tr.instrument_type_id, tr.exchange_id, dex.cat_exchange_id) = ('E', 'XCHI', 'CHX') then jos.t_9483
+           when (tr.instrument_type_id, tr.exchange_id, dex.cat_exchange_id) = ('E', 'NSX', 'NSX') then jos.t_9483
+           when (tr.instrument_type_id, tr.exchange_id, dex.cat_exchange_id) = ('E', 'NYSE', 'NYSE') then jos.t_9483
+           when (tr.instrument_type_id, tr.exchange_id, dex.cat_exchange_id) = ('E', 'XPSX', 'PSX') then jos.t_9483
+           when (tr.instrument_type_id, tr.exchange_id, dex.cat_exchange_id) = ('O', 'AMEXP', 'AMEROP') then jos.t_9483
+           when (tr.instrument_type_id, tr.exchange_id, dex.cat_exchange_id) = ('O', 'ARCAP', 'ARCAOP') then jos.t_9483
+           when (tr.instrument_type_id, tr.exchange_id, dex.cat_exchange_id) = ('E', 'EPRL', 'PEARLEQ') then jos.t_1003
+           when (tr.instrument_type_id, tr.exchange_id, dex.cat_exchange_id) = ('O', 'EMLD', 'EMLD') then jos.t_1003
+           when (tr.instrument_type_id, tr.exchange_id, dex.cat_exchange_id) = ('O', 'MIAX', 'MIAMI') then jos.t_1003
+           when (tr.instrument_type_id, tr.exchange_id, dex.cat_exchange_id) = ('O', 'MPRL', 'PEARL') then jos.t_1003
+           when (tr.instrument_type_id, tr.exchange_id, dex.cat_exchange_id) = ('E', 'MEMX', 'MEMX') then jos.t_880
+           when (tr.instrument_type_id, tr.exchange_id, dex.cat_exchange_id) = ('O', 'MXOP', 'MEMXOP') then jos.t_880
+           end as aux_tag_street,
+           coalesce(tr.secondary_exch_exec_id, '')     as "ReportID",
+           coalesce(tr.exch_exec_id, '')               as "Tag17"
+    from dwh.flat_trade_record tr
+             left join lateral (select jo.fix_message ->> '143'  as t_143
+                                from fix_capture.fix_message_json jo
+                                where tr.order_fix_message_id = jo.fix_message_id
+                                  and jo.date_id = to_char(tr.order_process_time, 'YYYYMMDD')::integer
+                                limit 1) jo on true
+     left join lateral (select jo.fix_message ->> '143'  as t_143,
+                                   jo.fix_message ->> '9483' as t_9483,
+                                   jo.fix_message ->> '1003' as t_1003,
+                                   jo.fix_message ->> '880'  as t_880
+                            from fix_capture.fix_message_json jo
+                            where tr.street_trade_fix_message_id = jo.fix_message_id
+                              and jo.date_id = to_char(tr.order_process_time, 'YYYYMMDD')::integer
+                            limit 1) jos on true
+             left join dwh.d_exchange dex on dex.exchange_id = tr.exchange_id and dex.is_active
+    where true
+      and tr.date_id between l_start_date_id and p_end_date_id
+      and tr.account_id = any (l_account_ids)
+      and tr.is_busted = 'N'
+--   and not (tr.ex_destination = 'BRKPT' and coalesce(jo.fix_message ->> '143', '-1') is distinct from 'DASH-CBOE')
+      and case
+              when tr.ex_destination = 'BRKPT' and coalesce(jo.t_143, '-1') is distinct from 'DASH-CBOE'
+                  then false
+              else true end;
+
+    GET DIAGNOSTICS l_row_cnt = ROW_COUNT;
+    select public.load_log(l_load_id, l_step_id, 'tmp_606_isi_bill_changes - Initial load', l_row_cnt, 'I')
+    into l_step_id;
+
+
+    analyze tmp_606_isi_bill_changes;
+
+    if p_add_exchange_order_id = 'Y' then
+        RETURN QUERY
+            select 'Date,OrderID,ReportID,Tag17,ExchOrderID';
+        return query
+            select s."Date" || ',' ||
+                   s."OrderID" || ',' ||
+                   coalesce(aux_tag_street, s."ReportID") || ',' ||
+                   s."Tag17" || ',' ||
+                   s."ExchOrderID"
+            from tmp_606_isi_bill_changes s
+            order by s."Date", s."ExchOrderID", s."ReportID";
+    else
+        RETURN QUERY
+            select 'Date,OrderID,ReportID,Tag17';
+        return query
+            select s."Date" || ',' ||
+                   s."OrderID" || ',' ||
+                   coalesce(aux_tag_street, s."ReportID") || ',' ||
+                   s."Tag17"
+            from tmp_606_isi_bill_changes s
+            order by s."Date", s."ReportID";
+    end if;
+
+    select public.load_log(l_load_id, l_step_id, 'dash360.report_isi_bill_changes_monthly (modified) COMPLETE===',
+                           coalesce(l_row_cnt, 0), 'O')
+    into l_step_id;
+
+end;
+$function$;
+
+
+create temp table t02 as
+select *
+from dash360.report_isi_bill_changes_monthly_mod(20241201, 20241231, '{socgen01,LPTF286,socbridge}', p_add_exchange_order_id := 'Y');
+
+Exchanges_ExecID to Tag17 cross reference
+select * from t02
