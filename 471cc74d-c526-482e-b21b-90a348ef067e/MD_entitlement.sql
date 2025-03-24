8b403d95bfa1fd5242d07845e53991aced2c9e6f
Oleh Sydor <ole.sydor@gmail.com>
2025-03-24 18:00:09 +0200

    Regular auto-commit at 18:00:09 on 24/03/2025

--- 471cc74d-c526-482e-b21b-90a348ef067e/MD_entitlement.sql
+++ 471cc74d-c526-482e-b21b-90a348ef067e/MD_entitlement.sql
@@ -80,39 +80,39 @@ BEGIN
     FROM Inserted I
     WHERE TEntityMarketDataEntitlement_EDW.[id] = I.id;
 end;
-
+go;
 
 create table [dbo].[TUserMarketDataEntitlement_EDW]
 (
-    id             int      not null,
-    entity_id      int      not null,
-    user_id        int      not null,
-    created_by     int      not null,
+    id             int            not null,
+    entity_id      int            not null,
+    user_id        int            not null,
+    created_by     int            not null,
     creation_time  [datetime2](3) not null,
-    is_deleted     bit      not null,
-    deleted_by     int      null,
+    is_deleted     bit            not null,
+    deleted_by     int            null,
     deleted_time   [datetime2](3) null,
-    index_nyse     bit      not null,
-    index_nasdaq   bit      not null,
-    index_cboe     bit      not null,
-    index_ftse     bit      not null,
-    cob_amxo       bit      not null,
-    cob_xcbo       bit      not null,
-    cob_edgo       bit      not null,
-    cob_xisx       bit      not null,
-    cob_xmio       bit      not null,
-    cob_arco       bit      not null,
-    cob_c2ox       bit      not null,
-    cob_xpho       bit      not null,
-    bi_amxo        bit      not null,
-    bi_xcbo        bit      not null,
-    bi_arco        bit      not null,
-    bi_xbox        bit      not null,
-    bi_xpho        bit      not null,
-    equity_nyse    bit      not null,
-    equity_nasdaq  bit      not null,
-    option_opra    bit      not null,
-    db_create_time datetime not null default getdate()
+    index_nyse     bit            not null,
+    index_nasdaq   bit            not null,
+    index_cboe     bit            not null,
+    index_ftse     bit            not null,
+    cob_amxo       bit            not null,
+    cob_xcbo       bit            not null,
+    cob_edgo       bit            not null,
+    cob_xisx       bit            not null,
+    cob_xmio       bit            not null,
+    cob_arco       bit            not null,
+    cob_c2ox       bit            not null,
+    cob_xpho       bit            not null,
+    bi_amxo        bit            not null,
+    bi_xcbo        bit            not null,
+    bi_arco        bit            not null,
+    bi_xbox        bit            not null,
+    bi_xpho        bit            not null,
+    equity_nyse    bit            not null,
+    equity_nasdaq  bit            not null,
+    option_opra    bit            not null,
+    db_create_time datetime       not null default getdate()
         constraint pk_TUserMarketDataEntitlement_EDW primary key (id)
 );
 
@@ -163,12 +163,7 @@ BEGIN
     FROM Inserted I
     WHERE TUserMarketDataEntitlement_EDW.[id] = I.id;
 end;
-
-
-
-
----
-
+go;
 
 CREATE TABLE [dbo].[TEntityCatDefaults_EDW]
 (
@@ -185,29 +180,30 @@ CREATE TABLE [dbo].[TEntityCatDefaults_EDW]
         constraint pk_TEntityCatDefaults_EDW primary key (id)
 );
 
-    CREATE TRIGGER [dbo].[TEntityCatDefaults_EDW_UPSERT]
-        ON [dbo].[TEntityCatDefaults_EDW]
-        INSTEAD OF INSERT
-        AS
-    BEGIN
-        INSERT INTO [dbo].[TEntityCatDefaults_EDW] (id, entity_id, created_by, creation_time, is_deleted, deleted_by,
-                                                    deleted_time, fdid, account_holder_type)
+CREATE TRIGGER [dbo].[TEntityCatDefaults_EDW_UPSERT]
+    ON [dbo].[TEntityCatDefaults_EDW]
+    INSTEAD OF INSERT
+    AS
+BEGIN
+    INSERT INTO [dbo].[TEntityCatDefaults_EDW] (id, entity_id, created_by, creation_time, is_deleted,
+                                                deleted_by,
+                                                deleted_time, fdid, account_holder_type)
 
-        SELECT id,
-               entity_id,
-               created_by,
-               creation_time,
-               is_deleted,
-               deleted_by,
-               deleted_time,
-               fdid,
-               account_holder_type
-        FROM inserted I
-        where not exists (select 1 from dbo.TEntityCatDefaults_EDW L where L.id = I.id);
-        UPDATE [dbo].[TEntityCatDefaults_EDW]
-        SET [is_deleted]   = I.[is_deleted]
-          , [deleted_by]   = I.[deleted_by]
-          , [deleted_time] = I.[deleted_time]
-        FROM Inserted I
-        WHERE TEntityCatDefaults_EDW.[id] = I.id;
-    end;
+    SELECT id,
+           entity_id,
+           created_by,
+           creation_time,
+           is_deleted,
+           deleted_by,
+           deleted_time,
+           fdid,
+           account_holder_type
+    FROM inserted I
+    where not exists (select 1 from dbo.TEntityCatDefaults_EDW L where L.id = I.id);
+    UPDATE [dbo].[TEntityCatDefaults_EDW]
+    SET [is_deleted]   = I.[is_deleted]
+      , [deleted_by]   = I.[deleted_by]
+      , [deleted_time] = I.[deleted_time]
+    FROM Inserted I
+    WHERE TEntityCatDefaults_EDW.[id] = I.id;
+end;
