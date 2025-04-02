ebdb069365b8a175a3f18224a0a057783a4ca1d0
Oleh Sydor <ole.sydor@gmail.com>
2025-04-02 15:00:08 +0300

    Regular auto-commit at 15:00:08 on 02/04/2025

---
+++ 08ded3e0-c11f-430e-8455-1d00b42f1cfb/console.sql
@@ -0,0 +1,77 @@
+CREATE OR REPLACE FUNCTION dash360.report_billing_belved01_ecr(in_start_date_id int4, in_end_date_id int4)
+    RETURNS TABLE
+            (
+                roe text
+            )
+    LANGUAGE plpgsql
+    PARALLEL SAFE LEAKPROOF
+AS
+$fn$
+declare
+    l_load_id int8;
+    l_step_id int;
+    l_row_cnt integer;
+
+begin
+
+    select nextval('public.load_timing_seq') into l_load_id;
+    l_step_id := 1;
+
+    select public.load_log(l_load_id, l_step_id, 'dash360.report_billing_belved01_ecr STARTED===', 0, 'O')
+    into l_step_id;
+
+    select
+-- tcb.date,
+to_char(tr.trade_record_time, 'MM/DD/YYYY')                                                              as "Date",
+to_char(tr.trade_record_time, 'HH24:MI:SS.MS')                                                           as "Exec Time",
+tr.account_name                                                                                          as "Account",
+tr.client_id                                                                                             as "Client ID",
+tr.client_order_id                                                                                       as "Cl Ord ID",
+tr.exch_exec_id                                                                                          as "Exec ID",
+case
+    when tr.instrument_type_id = 'E' then 'Equity'
+    when tr.instrument_type_id = 'O' then 'Option'
+    end                                                                                                  as "Security Type",
+tr.sub_strategy                                                                                          as "Sub Strategy",
+coalesce(ex.exchange_name, tr.exchange_id)                                                               as "Exchange",
+case when tr.instrument_type_id = 'O' then tcb.OSISeries else null end                                   as "OSI Symbol",
+tr.symbol                                                                                                as "Symbol",
+to_char(tcb.expiration, 'MM/DD/YYYY')                                                                    as "Exp Date",
+case
+    when tr.side = '1' then 'Buy'
+    when tr.side in ('2', '5', '6') then 'Sell'
+    end                                                                                                  as "Side",
+tr.last_qty                                                                                              as "Exec Qty",
+tr.last_px                                                                                               as "Exec Px",
+case
+    when tr.instrument_type_id = 'E' then tr.last_qty * tr.last_px
+    when tr.instrument_type_id = 'O' then tr.last_qty * tr.last_px * 100
+    end                                                                                                  as "Principal Amount",
+round((coalesce(tcb."LAST$", 0.0) + coalesce(tcb."LADR$", 0.0) + coalesce(tcb."MTF$", 0.0) + coalesce(tcb."ETF$", 0.0) +
+       coalesce(tcb."LTF$", 0.0) + coalesce(tcb."TPF$", 0.0) + coalesce(tcb."CATTF$", 0.0))::numeric,
+      8)                                                                                                 as "Execution Cost",
+round((coalesce(tcb."LAST$", 0.0) + coalesce(tcb."LADR$", 0.0) + coalesce(tcb."MTF$", 0.0) + coalesce(tcb."ETF$", 0.0) +
+       coalesce(tcb."LTF$", 0.0) + coalesce(tcb."TPF$", 0.0) + coalesce(tcb."CATTF$", 0.0) / tr.last_qty)::numeric,
+      8)                                                                                                 as "Execution Cost/Unit",
+round((coalesce(tcb."LAST$", 0.0) + coalesce(tcb."LADR$", 0.0))::numeric,
+      8)                                                                                                 as "DashCommission",
+round((coalesce(tcb."MTF$", 0.0))::numeric, 8)                                                           as "Maker/Taker Fee",
+round((coalesce(tcb."ETF$", 0.0) + coalesce(tcb."LTF$", 0.0))::numeric,
+      8)                                                                                                 as "Transaction Fee",
+round((coalesce(tcb."TPF$", 0.0))::numeric, 8)                                                           as "Trade Processing Fee",
+0.0                                                                                                      as "Royalty Fee",
+--round((coalesce(tcb."TAF$", 0.0))::numeric, 8) as "FINRA Trade Activity Fee"
+round((coalesce(tcb."CATTF$", 0.0))::numeric, 8)                                                         as "FINRA CAT Fees"
+    from billing.billing_data.tcustomer_billing_detail_all tcb
+             join billing.billing_data.fdw_dash_trade_record tr
+                  on (tr.date_id between :start_date_id and :end_date_id and
+                      tr.trading_firm_id in ('belvcaid', 'belved01', 'belved02') and tr.trade_record_id = tcb.report_id)
+             left join billing.dash_exchange_names ex on (ex.exchange_id = tr.exchange_id and ex.is_active = '1')
+    where true
+--    and tcb.date between :start_date_id and :end_date_id --convert(date, getdate()-day(getdate()-1)) and convert(date, getdate()-2)
+      and lower(tcb.billingentity) = 'belved01'
+      and lower(tcb.company) in ('belvcaid', 'belved01', 'belved02')
+      and tcb."FILLED QTY" > 0
+    order by tr.date_id, tr.trade_record_id;
+end;
+$fn$
