1144b5d841e3cf480108fb4d62a4ec12de8de6f1
Oleh Sydor <ole.sydor@gmail.com>
2025-04-04 16:00:09 +0300

    Regular auto-commit at 16:00:09 on 04/04/2025

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_130.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_130.sql
@@ -157,14 +157,29 @@ $function$
 ;
 
 
-CREATE OR REPLACE FUNCTION dwh.collect_order_exec_tree_from_root_but_street_no_fixmsg(root_order_id_ bigint)
- RETURNS TABLE(order_id bigint, root_order_id bigint, event_date_id integer, event_time timestamp without time zone, client_order_id character varying, orig_order_id bigint, parent_order_id bigint, multileg_order_id bigint, leg_ref_id character varying, exec_id bigint, fix_message_id bigint, fix_message_text text, source_type character varying)
+CREATE FUNCTION trash.collect_order_exec_tree_from_root_but_street_no_fixmsg(root_order_id_ bigint)
+    RETURNS TABLE
+            (
+                order_id          bigint,
+                root_order_id     bigint,
+                event_date_id     integer,
+                event_time        timestamp without time zone,
+                client_order_id   character varying,
+                orig_order_id     bigint,
+                parent_order_id   bigint,
+                multileg_order_id bigint,
+                leg_ref_id        character varying,
+                exec_id           bigint,
+                fix_message_id    bigint,
+                fix_message_text  text,
+                source_type       character varying
+            )
  LANGUAGE plpgsql
 AS $function$
 -- 2025-03-20 [Oleskii Haram] Moved from trash schema (original name: trash.ob_collect_order_exec_tree_from_root_but_street_no_fixmsg). Used in client_order_today.py tool.
 BEGIN
-RETURN QUERY
-WITH co AS (
+
+create temp table t_co AS (
     SELECT DISTINCT
         tree.order_id,
         root_order_id_ as root_order_id,
@@ -180,9 +195,9 @@ WITH co AS (
         -- NULL::TEXT AS fix_message_text,
         tree.source_type
     FROM dwh.collect_root_order_tree_but_street(root_order_id_) AS tree
-)
-,
-co_ex as (
+);
+
+create temp table t_co_ex as (
 	SELECT
 	    co.order_id,
 	    co.root_order_id,
@@ -197,20 +212,15 @@ co_ex as (
 	    ex.fix_message_id,
 	    --fmj.fix_message::TEXT AS fix_message_text,
 	    'exec'::VARCHAR AS source_type
-	FROM co
-	JOIN LATERAL (
-	    SELECT e.exec_id, e.exec_date_id, e.exec_time, e.fix_message_id
-	    FROM dwh.execution e
-	    WHERE e.order_id = co.order_id
-	    AND e.exec_date_id >= co.event_date_id
-	    AND e.exec_type != 'D'
-	    AND e.exec_time >= co.event_time - INTERVAL '2 seconds'
-	    AND e.fix_message_id IS NOT NULL
-		limit 100500
-	) AS ex ON true
+	FROM t_co co
+	JOIN dwh.execution ex
+	    on ex.order_id = co.order_id
+	    AND ex.exec_date_id >= co.event_date_id
+	    AND ex.exec_type != 'D'
+	    AND ex.exec_time >= co.event_time - INTERVAL '2 seconds'
+	    AND ex.fix_message_id IS NOT NULL;
 
-	UNION ALL
-
+insert into t_co_ex
 	SELECT
 	    co.order_id,
 	    co.root_order_id,
@@ -225,11 +235,11 @@ co_ex as (
 	    co.fix_message_id,
 	    --fmj.fix_message::TEXT AS fix_message_text,
 	    co.source_type
-	FROM co
+	FROM t_co co;
 
-)
-,
-co_ex_fm as (
+);
+
+with co_ex_fm as (
 	SELECT
 	    co.order_id,
 	    co.root_order_id,
@@ -247,17 +257,10 @@ co_ex_fm as (
 		case when co.exec_id is null
 			then 1
 			else
-				row_number() over (partition by co.fix_message_id rows between unbounded preceding and unbounded following)
+				row_number() over (partition by co.fix_message_id)
 			end as ern
-	FROM co_ex co
---	LEFT JOIN LATERAL (
---	    SELECT fmj.fix_message
---	    FROM fix_capture.fix_message_json fmj
---	    WHERE fmj.fix_message_id = co.fix_message_id
---	    AND fmj.date_id = co.event_date_id
---		limit  1
---	) AS fmj ON true
-) -- end co_ex_fm
+	FROM t_co_ex co
+)
 SELECT
 	    co.order_id,
 	    co.root_order_id,
