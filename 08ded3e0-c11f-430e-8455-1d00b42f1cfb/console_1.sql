74f16214f780fdf6fc2a124f19fe9d0c085c2b43
Oleh Sydor <ole.sydor@gmail.com>
2025-04-11 13:00:08 +0300

    Regular auto-commit at 13:00:08 on 11/04/2025

--- 08ded3e0-c11f-430e-8455-1d00b42f1cfb/console_1.sql
+++ 08ded3e0-c11f-430e-8455-1d00b42f1cfb/console_1.sql
@@ -1,4 +1,4 @@
-create or replace
+create --or replace
     function dash360.report_billing_traimc_summary(in_start_date_id int4, in_end_date_id int4)
     returns table
             (
@@ -8,10 +8,11 @@ create or replace
 as
 $fn$
 declare
-    l_load_id int8;
-    l_step_id int;
-    l_row_cnt integer;
-
+    l_load_id       int8;
+    l_step_id       int;
+    l_row_cnt       integer;
+    l_start_date_id date := in_start_date_id::text::date;
+    l_end_date_id   date := in_end_date_id::text::date;
 begin
 
     select nextval('public.load_timing_seq') into l_load_id;
@@ -22,116 +23,186 @@ begin
 
     drop table if exists t_base;
     create temp table t_base on commit drop as
-    select
--- tcb.date,
-tr.date_id,
-tr.trade_record_id,
-to_char(tr.trade_record_time, 'MM/DD/YYYY')                            as "Date",
-to_char(tr.trade_record_time, 'HH24:MI:SS.MS')                         as "Exec Time",
-tr.account_name                                                        as "Account",
-tr.client_id                                                           as "Client ID",
-tr.client_order_id                                                     as "Cl Ord ID",
-tr.exch_exec_id                                                        as "Exec ID",
-case
-    when tr.instrument_type_id = 'E' then 'Equity'
-    when tr.instrument_type_id = 'O' then 'Option'
-    end                                                                as "Security Type",
-tr.sub_strategy                                                        as "Sub Strategy",
-coalesce(ex.exchange_name, tr.exchange_id)                             as "Exchange",
-case when tr.instrument_type_id = 'O' then tcb.OSISeries else null end as "OSI Symbol",
-tr.symbol                                                              as "Symbol",
-to_char(tcb.expiration, 'MM/DD/YYYY')                                  as "Exp Date",
-case
-    when tr.side = '1' then 'Buy'
-    when tr.side in ('2', '5', '6') then 'Sell'
-    end                                                                as "Side",
-tr.last_qty                                                            as "Exec Qty",
-tr.last_px                                                             as "Exec Px",
-case
-    when tr.instrument_type_id = 'E' then tr.last_qty * tr.last_px
-    when tr.instrument_type_id = 'O' then tr.last_qty * tr.last_px * 100
-    end                                                                as "Principal Amount",
-round((coalesce(tcb."LAST$", 0.0) + coalesce(tcb."LADR$", 0.0) + coalesce(tcb."MTF$", 0.0) + coalesce(tcb."ETF$", 0.0) +
-       coalesce(tcb."LTF$", 0.0) + coalesce(tcb."TPF$", 0.0) + coalesce(tcb."CATTF$", 0.0))::numeric,
-      8)                                                               as "Execution Cost",
-round((coalesce(tcb."LAST$", 0.0) + coalesce(tcb."LADR$", 0.0) + coalesce(tcb."MTF$", 0.0) + coalesce(tcb."ETF$", 0.0) +
-       coalesce(tcb."LTF$", 0.0) + coalesce(tcb."TPF$", 0.0) + coalesce(tcb."CATTF$", 0.0) / tr.last_qty)::numeric,
-      8)                                                               as "Execution Cost/Unit",
-round((coalesce(tcb."LAST$", 0.0) + coalesce(tcb."LADR$", 0.0))::numeric,
-      8)                                                               as "DashCommission",
-round((coalesce(tcb."MTF$", 0.0))::numeric, 8)                         as "Maker/Taker Fee",
-round((coalesce(tcb."ETF$", 0.0) + coalesce(tcb."LTF$", 0.0))::numeric,
-      8)                                                               as "Transaction Fee",
-round((coalesce(tcb."TPF$", 0.0))::numeric, 8)                         as "Trade Processing Fee",
-0.0                                                                    as "Royalty Fee",
---round((coalesce(tcb."TAF$", 0.0))::numeric, 8) as "FINRA Trade Activity Fee"
-round((coalesce(tcb."CATTF$", 0.0))::numeric, 8)                       as "FINRA CAT Fees"
-    from billing_data.tcustomer_billing_detail_all tcb
-             join billing_data.fdw_dash_trade_record tr
-                  on (tr.date_id between in_start_date_id and in_end_date_id and
-                      tr.trading_firm_id in ('belvcaid', 'belved01', 'belved02') and tr.trade_record_id = tcb.report_id)
-             left join billing.dash_exchange_names ex on (ex.exchange_id = tr.exchange_id and ex.is_active = '1')
-    where true
-      and tcb.date between in_start_date_id::text::date and in_end_date_id::text::date --convert(date, getdate()-day(getdate()-1)) and convert(date, getdate()-2)
-      and tr.date_id between in_start_date_id and in_end_date_id
-      and lower(tcb.billingentity) = 'belved01'
-      and lower(tcb.company) in ('belvcaid', 'belved01', 'belved02')
-      and tcb."FILLED QTY" > 0;
+    select 'TRAD'                                                   as "MPID",
+           to_char(tcb."date", 'MM/dd/yyyy')                        as "Trade Date",
+           tcb.billto_id                                            as "Client ID",
+           'TOTAL'                                                  as "Rate Category",
+           case when tcb."LEG NUMBER" = '1/1' then 'N' else 'Y' end as "Spread",
+           count(distinct tcb.cl_ord_id)                            as "EXEC ORDERS",
+           sum(tcb."FILLED QTY")                                    as "EXEC QUANTITY",
+           sum(coalesce(tcb."PFOFTF$", 0.00))                       as "PFOF"
+    from billing.billing_data.tcustomer_billing_detail_all tcb
+    where tcb."date" between l_start_date_id and l_end_date_id
+      and tcb.billingentity = 'TRAIMC'
+      and tcb."FILLED QTY" > 0
+    group by "Trade Date", "Client ID", "Spread";
+
     get diagnostics l_row_cnt = row_count;
-    select public.load_log(l_load_id, l_step_id, 'dash360.report_billing_belved01_ecr data is prepared', l_row_cnt, 'O')
+    select public.load_log(l_load_id, l_step_id, 'dash360.report_billing_traimc_summary data is prepared', l_row_cnt,
+                           'O')
     into l_step_id;
-    create index on t_base (date_id, trade_record_id);
+    create index on t_base ("Trade Date", "Client ID", "Spread");
 
     return query
-        select 'Date,Exec Time,Account,Client ID,Cl Ord ID,Exec ID,Security Type,Sub Strategy,Exchange,OSI Symbol,Symbol,Exp Date,Side,Exec Qty,Exec Px,Principal Amount,Execution Cost,Execution Cost/Unit,DashCommission,Maker/Taker Fee,Transaction Fee,Trade Processing Fee,Royalty Fee,FINRA CAT Fees';
+        select 'MPID,Trade Date,Client ID,Rate Category,Spread,EXEC ORDERS,EXEC QUANTITY,PFOF';
     return query
         select array_to_string(ARRAY [
-                                   "Date",
-                                   "Exec Time",
-                                   "Account",
-                                   "Client ID",
-                                   "Cl Ord ID",
-                                   "Exec ID",
-                                   "Security Type",
-                                   "Sub Strategy",
-                                   "Exchange",
-                                   "OSI Symbol",
-                                   "Symbol",
-                                   "Exp Date",
-                                   "Side",
-                                   "Exec Qty"::text,
-                                   to_char("Exec Px", 'FM999990.0099'),
-                                   to_char("Principal Amount", 'FM999990.09999999'),
-                                   to_char("Execution Cost", 'FM999990.09999999'),
-                                   to_char("Execution Cost/Unit", 'FM999990.09999999'),
-                                   to_char("DashCommission", 'FM999990.09999999'),
-                                   to_char("Maker/Taker Fee", 'FM999990.09999999'),
-                                   to_char("Transaction Fee", 'FM999990.09999999'),
-                                   to_char("Trade Processing Fee", 'FM999990.09999999'),
-                                   to_char("Royalty Fee", 'FM999990.09999999'),
-                                   to_char("FINRA CAT Fees", 'FM999990.09999999')
+                                   "MPID",
+                                   "Trade Date",
+                                   "Client ID"::text,
+                                   "Rate Category",
+                                   "Spread",
+                                   "EXEC ORDERS"::text,
+                                   "EXEC QUANTITY"::text,
+                                   "PFOF"::text
                                    ], ',', '')
         from t_base tr
-        order by tr.date_id, tr.trade_record_id;
+        order by tr."Trade Date", tr."Client ID", tr."Spread";
 
-    select public.load_log(l_load_id, l_step_id, 'dash360.report_billing_belved01_ecr COMPLETED===', l_row_cnt, 'C')
+    select public.load_log(l_load_id, l_step_id, 'dash360.report_billing_traimc_summary COMPLETED===', l_row_cnt, 'C')
     into l_step_id;
 end ;
 $fn$;
 
+select * from dash360.report_billing_traimc_summary(in_start_date_id := 20250407, in_end_date_id := 20250409);
 
-select
-	'TRAD' as "MPID",
-	to_char(tcb."date", 'MM/dd/yyyy') as "Trade Date",
-	tcb.billto_id as "Client ID",
-	'TOTAL' as "Rate Category",
-	(case when tcb."LEG NUMBER" = '1/1' then 'N' else 'Y' end) as "Spread",
-	count(distinct tcb.cl_ord_id) as "EXEC ORDERS",
-	sum(tcb."FILLED QTY") as "EXEC QUANTITY",
-	sum(coalesce(tcb."PFOFTF$", 0.00)) as "PFOF"
-from billing.billing_data.tcustomer_billing_detail_all tcb
-where tcb."date" between :start_date_id#' and '#end_date_id#'
-	and tcb.billingentity = 'TRAIMC'
-	and tcb."FILLED QTY" > 0
-group by "Trade Date", "Client ID", "Spread"
-order by "Trade Date", "Client ID", "Spread";
+
+
+-----------------------------
+select * from dash360.report_billing_traimc_data(in_start_date_id := 20250407, in_end_date_id := 20250409);
+
+create or replace
+    function dash360.report_billing_traimc_data(in_start_date_id int4, in_end_date_id int4)
+    returns table
+            (
+                ret_row text
+            )
+    language plpgsql
+as
+$fn$
+declare
+    l_load_id       int8;
+    l_step_id       int;
+    l_row_cnt       integer;
+    l_start_date_id date := in_start_date_id::text::date;
+    l_end_date_id   date := in_end_date_id::text::date;
+begin
+
+    select nextval('public.load_timing_seq') into l_load_id;
+    l_step_id := 1;
+
+    select public.load_log(l_load_id, l_step_id, 'dash360.report_billing_traimc_data STARTED===', 0, 'O')
+    into l_step_id;
+
+    drop table if exists t_base;
+    create temp table t_base on commit drop as
+    select to_char(tcb."date", 'MM/dd/yyyy')                                                            as "Trade Date",
+           'TRAD'                                                                                       as "MPID",
+           'TOTAL'                                                                                      as "Rate Category",
+           lf.exchangecode                                                                              as "Exch",
+           tcb."B/S"                                                                                    as "B/S",
+           tcb."C/P"                                                                                    as "P/C",
+           tcb."FILLED QTY"::text                                                                       as "QTY",
+           tcb.symbol                                                                                   as "Class",
+           tcb.symbol                                                                                   as "Sym",
+           to_char(tcb.date, 'MM')                                                                      as "Mo",
+           to_char(tcb.date, 'YY')                                                                      as "Yr",
+           tcb.strike::text                                                                             as "Strike",
+           tcb.premium::text                                                                            as "Price",
+           tcb.open_close                                                                               as "O/C",
+           ''                                                                                           as "optional_data",
+           tcb.cl_ord_id                                                                                as "client_order_ID",
+           tcb."CMTA FIRM"                                                                              as "CMTA",
+           tcb.giveup                                                                                   as "Ex Firm",
+           tcb.billto_id::text                                                                          as "Client ID",
+           tcb.account                                                                                  as "Account",
+           tcb."SYMBOL TYPE"                                                                            as "Product",
+           case when tcb.penny::integer = 1 then 'Y' else 'N' end                                       as "Penny",
+           case when tcb."LEG NUMBER" = '1/1' then 'N' else 'Y' end                                     as "Spread",
+           tcb."ORDER TYPE"                                                                             as "Route",
+           tcb.liquidityflag                                                                            as "Liquidity",
+           to_char(coalesce(round("LADR"::numeric, 6), 0.00), 'FM999990.009999')                                 as "LADR",
+           to_char(coalesce(round("LADR$"::numeric, 6), 0.00), 'FM999990.009999')                                as "LADR$",
+           to_char(coalesce(round("ETF"::numeric, 6), 0.00), 'FM999990.009999')                                  as "ETF",
+           to_char(coalesce(round("ETF$"::numeric, 6), 0.00), 'FM999990.009999')                                 as "ETF$",
+           to_char(coalesce(round("MTF"::numeric, 6), 0.00), 'FM999990.009999')                                  as "MTF",
+           to_char(coalesce(round("MTF$"::numeric, 6), 0.00), 'FM999990.009999')                                 as "MTF$",
+           to_char(coalesce(round("LTF"::numeric, 6), 0.00), 'FM999990.009999')                                  as "LTF",
+           to_char(coalesce(round("LTF$"::numeric, 6), 0.00), 'FM999990.009999')                                 as "LTF$",
+           to_char(coalesce(round("SRCTF"::numeric, 6), 0.00), 'FM999990.009999')                                as "SRCTF",
+           to_char(coalesce(round("SRCTF$"::numeric, 6), 0.00), 'FM999990.009999')                               as "SRCTF$",
+           to_char(coalesce(round("PFOFTF"::numeric, 6), 0.00), 'FM999990.009999')                               as "PFOFTF",
+           to_char(coalesce(round("PFOFTF$"::numeric, 6), 0.00), 'FM999990.009999')                              as "PFOFTF$",
+           to_char(round((coalesce("LADR$"::numeric, 0.00) + coalesce("ETF$"::numeric, 0.00) + coalesce("MTF$"::numeric, 0.00) +
+                          coalesce("LTF$"::numeric, 0.00) +
+                          coalesce("SRCTF$"::numeric, 0.00) + coalesce("PFOFTF$"::numeric, 0.00)), 6),
+                   'FM999990.009999')                                                                   as "Total Charges",
+           tcb.osiseries                                                                                as "OSISeries",
+           tcb."LINK EXCH"                                                                              as "AwayExchange",
+           tcb.report_id::text                                                                          as "ReportID",
+           tcb."range"                                                                                  as "Range"
+    from billing.billing_data.tcustomer_billing_detail_all tcb
+             left outer join billing.billing.l_filterexchange lf on (tcb.exchange = lf.exchangedesc)
+    where tcb."date" BETWEEN l_start_date_id AND l_end_date_id
+      and billingentity = 'TRAIMC';
+    get diagnostics l_row_cnt = row_count;
+    select public.load_log(l_load_id, l_step_id, 'dash360.report_billing_traimc_data data is prepared', l_row_cnt,
+                           'O')
+    into l_step_id;
+    create index on t_base ("Trade Date", "ReportID");
+
+    return query
+        select 'Trade Date,MPID,Rate Category,Exch,B/S,P/C,QTY,Class,Sym,Mo,Yr,Strike,Price,O/C,optional_data,client_order_ID,CMTA,Ex Firm,Client ID,Account,Product,Penny,Spread,Route,Liquidity,LADR,LADR$,ETF,ETF$,MTF,MTF$,LTF,LTF$,SRCTF,SRCTF$,PFOFTF,PFOFTF$,Total Charges,OSISeries,AwayExchange,ReportID,Range';
+
+    return query
+        select array_to_string(ARRAY [
+                                   "Trade Date",
+                                   "MPID",
+                                   "Rate Category",
+                                   "Exch",
+                                   "B/S",
+                                   "P/C",
+                                   "QTY",
+                                   "Class",
+                                   "Sym",
+                                   "Mo",
+                                   "Yr",
+                                   "Strike",
+                                   "Price",
+                                   "O/C",
+                                   "optional_data",
+                                   "client_order_ID",
+                                   "CMTA",
+                                   "Ex Firm",
+                                   "Client ID",
+                                   "Account",
+                                   "Product",
+                                   "Penny",
+                                   "Spread",
+                                   "Route",
+                                   "Liquidity",
+                                   "LADR",
+                                   "LADR$",
+                                   "ETF",
+                                   "ETF$",
+                                   "MTF",
+                                   "MTF$",
+                                   "LTF",
+                                   "LTF$",
+                                   "SRCTF",
+                                   "SRCTF$",
+                                   "PFOFTF",
+                                   "PFOFTF$",
+                                   "Total Charges",
+                                   "OSISeries",
+                                   "AwayExchange",
+                                   "ReportID",
+                                   "Range"
+                                   ], ',', '')
+        from t_base tr
+        order by "Trade Date", "ReportID";
+
+    select public.load_log(l_load_id, l_step_id, 'dash360.report_billing_traimc_data COMPLETED===', l_row_cnt, 'C')
+    into l_step_id;
+end ;
+$fn$;
