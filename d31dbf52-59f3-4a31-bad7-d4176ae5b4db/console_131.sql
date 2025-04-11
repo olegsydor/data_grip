92b65f877cdd64913852d72b567be5b98b33fa64
Oleh Sydor <ole.sydor@gmail.com>
2025-04-11 17:00:08 +0300

    Regular auto-commit at 17:00:08 on 11/04/2025

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_131.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_131.sql
@@ -21,8 +21,8 @@
 		tf.trading_firm_name::varchar as "Trading Firm",
 		a.account_name::varchar as "Account",
 		cf.customer_or_firm_name::varchar as "Capacity",
-		o."CumQty" as "Qty",
-		o."ClOrdID" as "Parent Order Count"
+		sum(o."CumQty") as "Qty",
+		count(distinct o."ClOrdID") as "Parent Order Count"
 	from dwh.historic_order_details_storage o
 	join dwh.d_account a on (a.account_id = o."AccountID")
 	join dwh.d_trading_firm tf on (tf.trading_firm_unq_id = a.trading_firm_unq_id)
@@ -30,21 +30,33 @@
 	where "Status_Date_id" >= :l_start_quarter_date_id and "Status_Date_id" < :l_date_id
 		and o."InstrumentType" = 'O'
 		and o."CustomerOrderID" is null
-and "ClOrdID" = 'CE7-598143-83wdb'
-	and "StatusDate" = '2025-04-07'
-	union all
-select cl.create_time::date as "Period",
-tf.trading_firm_name::varchar as "Trading Firm",
-		a.account_name::varchar as "Account",
-		cf.customer_or_firm_name::varchar as "Capacity",
-		cl.order_qty as "Qty",
-		cl.client_order_id as "Parent Order Count"
-from dwh.client_order cl
-join dwh.d_account a on (a.account_id = cl.account_id)
-	join dwh.d_trading_firm tf on (tf.trading_firm_unq_id = a.trading_firm_unq_id)
-	left join dwh.d_customer_or_firm cf on (cf.customer_or_firm_id = cl.customer_or_firm_id)
-where client_order_id = 'CE7-598143-83wdb'
-	and create_time::date = '2025-04-07';
+	group by "Period", "Account", "Capacity", "Trading Firm";
 
 
 
+-- 	union all
+
+    select cl.create_date_id::text::date      as "Period",
+           tf.trading_firm_name::varchar      as "Trading Firm",
+           a.account_name::varchar            as "Account",
+           cf.customer_or_firm_name::varchar  as "Capacity",
+           sum(ex.cum_qty)                    as "Qty",
+           count(distinct cl.client_order_id) as "Parent Order Count"
+    from dwh.client_order cl
+             join dwh.d_account a on (a.account_id = cl.account_id and a.is_active)
+             join dwh.d_instrument di on di.instrument_id = cl.instrument_id
+             join dwh.d_trading_firm tf on (tf.trading_firm_unq_id = a.trading_firm_unq_id and tf.is_active)
+             left join dwh.d_customer_or_firm cf on (cf.customer_or_firm_id = cl.customer_or_firm_id)
+             left join lateral (select sum(ex.last_qty) as cum_qty
+                                from dwh.execution ex
+                                where ex.order_id = cl.order_id
+                                  and ex.exec_date_id >= cl.create_date_id
+                                  and ex.exec_type in ('F', 'G')
+                                  and ex.is_busted = 'N'
+                                limit 1) ex on true
+    where true
+      and cl.create_date_id >= :l_start_quarter_date_id
+      and cl.create_date_id < :l_date_id
+      and di.instrument_type_id = 'O'
+      and cl.parent_order_id is null
+    group by "Period", "Account", "Capacity", "Trading Firm";
