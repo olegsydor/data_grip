693c899fe6c0e7e08997c587ea7e2530cb33f30f
Oleh Sydor <ole.sydor@gmail.com>
2025-04-14 15:00:08 +0300

    Regular auto-commit at 15:00:08 on 14/04/2025

--- 9aed9572-df85-47ea-b6d8-cfc9314faeb4/console_17.sql
+++ 9aed9572-df85-47ea-b6d8-cfc9314faeb4/console_17.sql
@@ -234,7 +234,7 @@ AS SELECT NULL::text AS id,
 
 
 -- blaze7.tordermisc1_edw source
-
+drop VIEW blaze7.tordermisc1_edw;
 CREATE OR REPLACE VIEW blaze7.tordermisc1_edw
 AS SELECT NULL::text AS id,
     co.cl_ord_id AS orderid,
@@ -469,7 +469,16 @@ AS SELECT NULL::text AS id,
             WHEN co.crossing_side = 'O'::bpchar THEN co.payload #>> '{OriginatorOrder,NoLinkedOrders}'::text[]
             WHEN co.crossing_side = 'C'::bpchar THEN co.payload #>> '{ContraOrder,NoLinkedOrders}'::text[]
             ELSE NULL::text
-        END AS numberoflinkedorders
+        END AS numberoflinkedorders,
+       (co.payload ->> 'IsTiedToCombo')::char as IsTiedToCombo,
+       (co.payload ->> 'IsMerged')::char as IsMerged,
+       (SELECT co2.cl_ord_id
+           FROM blaze7.client_order co2
+           WHERE co2.order_id = (co.payload ->> 'MergedOrderId')::int8
+             and co.payload ->> 'MergedOrderId' is not null
+             AND co2.chain_id = 0
+           limit 1)                      as MergedOrderId
+
    FROM ( SELECT DISTINCT ON (cl.cl_ord_id) cl.order_id,
             cl.chain_id,
             cl.parent_order_id,
@@ -511,3 +520,313 @@ AS SELECT NULL::text AS id,
                  LIMIT 1) big ON true
           WHERE true AND (cl.record_type = ANY (ARRAY['0'::bpchar, '2'::bpchar]))
           ORDER BY cl.cl_ord_id, cl.chain_id DESC) co;
+
+
+
+
+-- blaze7.tlegs_edw source
+drop VIEW blaze7.tlegs_edw;
+CREATE OR REPLACE VIEW blaze7.tlegs_edw
+AS SELECT NULL::text AS id,
+    co.cl_ord_id AS orderid,
+    COALESCE(leg.leg_ref_id, '0'::character varying) AS legrefid,
+    COALESCE(
+        CASE
+            WHEN co.instrument_type <> 'M'::bpchar THEN co.payload ->> 'Price'::text
+            WHEN (leg.payload ->> 'LegPrice'::text) IS NOT NULL THEN leg.payload ->> 'LegPrice'::text
+            WHEN co.crossing_side = 'O'::bpchar THEN co.payload #>> '{OriginatorOrder,Price}'::text[]
+            WHEN co.crossing_side = 'C'::bpchar THEN co.payload #>> '{ContraOrder,Price}'::text[]
+            ELSE co.payload ->> 'Price'::text
+        END, '0'::text) AS price,
+    COALESCE(co.payload ->> 'NoLegs'::text, '1'::text) AS legcount,
+        CASE co.instrument_type
+            WHEN 'M'::bpchar THEN leg.payload ->> 'LegSeqNumber'::text
+            ELSE '1'::text
+        END AS legnumber,
+        CASE co.instrument_type
+            WHEN 'M'::bpchar THEN leg.payload ->> 'DashSecurityId'::text
+            ELSE co.payload ->> 'DashSecurityId'::text
+        END AS dashsecurityid,
+    COALESCE("substring"(
+        CASE co.instrument_type
+            WHEN 'M'::bpchar THEN leg.payload ->> 'DashSecurityId'::text
+            ELSE co.payload ->> 'DashSecurityId'::text
+        END, 'US:EQ:(.+)'::text), "substring"(
+        CASE co.instrument_type
+            WHEN 'M'::bpchar THEN leg.payload ->> 'DashSecurityId'::text
+            ELSE co.payload ->> 'DashSecurityId'::text
+        END, 'US:[FO|OP]{2}:(.+)_'::text)) AS basecode,
+        CASE
+            WHEN "substring"(
+            CASE co.instrument_type
+                WHEN 'M'::bpchar THEN leg.payload ->> 'DashSecurityId'::text
+                ELSE co.payload ->> 'DashSecurityId'::text
+            END, 'US:([FO|OP|EQ]{2})'::text) = 'EQ'::text THEN 'S'::text
+            WHEN "substring"(
+            CASE co.instrument_type
+                WHEN 'M'::bpchar THEN leg.payload ->> 'DashSecurityId'::text
+                ELSE co.payload ->> 'DashSecurityId'::text
+            END, 'US:([FO|OP|EQ]{2})'::text) = ANY (ARRAY['FO'::text, 'OP'::text]) THEN "substring"(
+            CASE co.instrument_type
+                WHEN 'M'::bpchar THEN leg.payload ->> 'DashSecurityId'::text
+                ELSE co.payload ->> 'DashSecurityId'::text
+            END, '[0-9]{6}(.)'::text)
+            ELSE NULL::text
+        END AS typecode,
+        CASE
+            WHEN "substring"(
+            CASE co.instrument_type
+                WHEN 'M'::bpchar THEN leg.payload ->> 'DashSecurityId'::text
+                ELSE co.payload ->> 'DashSecurityId'::text
+            END, 'US:([FO|OP|EQ]{2})'::text) = ANY (ARRAY['FO'::text, 'OP'::text]) THEN to_date("substring"(
+            CASE co.instrument_type
+                WHEN 'M'::bpchar THEN leg.payload ->> 'DashSecurityId'::text
+                ELSE co.payload ->> 'DashSecurityId'::text
+            END, '([0-9]{6})'::text), 'YYMMDD'::text)
+            ELSE NULL::date
+        END AS expirationdate,
+        CASE
+            WHEN "substring"(
+            CASE co.instrument_type
+                WHEN 'M'::bpchar THEN leg.payload ->> 'DashSecurityId'::text
+                ELSE co.payload ->> 'DashSecurityId'::text
+            END, 'US:([FO|OP|EQ]{2})'::text) = ANY (ARRAY['FO'::text, 'OP'::text]) THEN "substring"(
+            CASE co.instrument_type
+                WHEN 'M'::bpchar THEN leg.payload ->> 'DashSecurityId'::text
+                ELSE co.payload ->> 'DashSecurityId'::text
+            END, '[0-9]{6}.(.+)$'::text)::numeric
+            ELSE NULL::numeric
+        END AS strike,
+        CASE
+            WHEN co.instrument_type = 'M'::bpchar THEN leg.payload ->> 'LegSide'::text
+            WHEN co.crossing_side IS NULL THEN co.payload ->> 'Side'::text
+            WHEN co.crossing_side = 'O'::bpchar THEN co.payload #>> '{OriginatorOrder,Side}'::text[]
+            WHEN co.crossing_side = 'C'::bpchar THEN co.payload #>> '{ContraOrder,Side}'::text[]
+            ELSE NULL::text
+        END AS side,
+        CASE co.instrument_type
+            WHEN 'M'::bpchar THEN leg.payload ->> 'LegRatioQty'::text
+            ELSE '1'::text
+        END AS ratio,
+    COALESCE(
+        CASE co.instrument_type
+            WHEN 'M'::bpchar THEN leg.payload ->> 'ContractSize'::text
+            ELSE co.payload ->> 'ContractSize'::text
+        END, '1'::text) AS multiplier,
+    COALESCE(co.payload ->> 'DashTargetVega'::text, co.payload ->> 'OrderQty'::text) AS quantity,
+    ( SELECT rep.payload ->> 'CumQty'::text
+           FROM blaze7.order_report rep
+          WHERE rep.cl_ord_id::text = co.cl_ord_id::text AND rep.leg_ref_id IS NULL
+          ORDER BY rep.exec_id DESC
+         LIMIT 1) AS filled,
+        CASE
+            WHEN co.instrument_type = ANY (ARRAY['O'::bpchar, 'E'::bpchar]) THEN (COALESCE((co.payload ->> 'ContractSize'::text)::integer, 1) *
+            CASE co.payload ->> 'Side'::text
+                WHEN '1'::text THEN 1
+                ELSE '-1'::integer
+            END)::numeric * rep_last."CumQty"::bigint::numeric / 10000.0 * rep_last."AvgPx"::bigint::numeric / 10000.0
+            WHEN co.instrument_type = 'M'::bpchar THEN (COALESCE(leg.payload ->> 'ContractSize'::text, '1'::text)::integer *
+            CASE leg.payload ->> 'LegSide'::text
+                WHEN '1'::text THEN 1
+                ELSE '-1'::integer
+            END)::numeric * rep_last_exec."CumQty"::bigint::numeric / 10000.0 * rep_last_exec."AvgPx"::bigint::numeric / 10000.0
+            ELSE NULL::numeric
+        END AS invested,
+        CASE co.instrument_type
+            WHEN 'M'::bpchar THEN
+            CASE
+                WHEN (leg.payload ->> 'LegSide'::text) = '2'::text THEN '-1'::integer
+                ELSE 1
+            END * rep_last_exec."AvgPx"::bigint
+            ELSE
+            CASE
+                WHEN co.crossing_side IS NULL AND (co.payload ->> 'Side'::text) = '2'::text THEN '-1'::integer
+                WHEN co.crossing_side = 'O'::bpchar AND (co.payload #>> '{OriginatorOrder,Side}'::text[]) = '2'::text THEN '-1'::integer
+                WHEN co.crossing_side = 'C'::bpchar AND (co.payload #>> '{ContraOrder,Side}'::text[]) = '2'::text THEN '-1'::integer
+                ELSE 1
+            END * rep_last."AvgPx"::bigint
+        END::text AS avgprice,
+        CASE
+            WHEN co.instrument_type = 'E'::bpchar THEN co.payload ->> 'OrderQty'::text
+            WHEN co.instrument_type = 'M'::bpchar AND (leg.payload ->> 'LegInstrumentType'::text) = 'E'::text THEN leg.payload ->> 'LegQty'::text
+            ELSE NULL::text
+        END AS stockquantity,
+        CASE
+            WHEN co.instrument_type = 'E'::bpchar THEN rep_last."LeavesQty"
+            WHEN co.instrument_type = 'M'::bpchar AND (leg.payload ->> 'LegInstrumentType'::text) = 'E'::text THEN rep_last_exec."LeavesQty"
+            ELSE NULL::text
+        END AS stockopenquantity,
+        CASE
+            WHEN co.instrument_type = 'E'::bpchar THEN ( SELECT rep.payload ->> 'CumQty'::text
+               FROM blaze7.order_report rep
+              WHERE rep.cl_ord_id::text = co.cl_ord_id::text
+              ORDER BY rep.exec_id DESC
+             LIMIT 1)
+            WHEN co.instrument_type = 'M'::bpchar AND (leg.payload ->> 'LegInstrumentType'::text) = 'E'::text THEN rep_last_exec."CumQty"
+            ELSE NULL::text
+        END AS stockfilled,
+        CASE
+            WHEN co.instrument_type = 'E'::bpchar THEN rep_last."CanceledQty"
+            WHEN co.instrument_type = 'M'::bpchar AND (leg.payload ->> 'LegInstrumentType'::text) = 'E'::text THEN rep_last_exec."CanceledQty"
+            ELSE NULL::text
+        END AS stockcancelled,
+        CASE
+            WHEN co.instrument_type = 'O'::bpchar THEN co.payload ->> 'OrderQty'::text
+            WHEN co.instrument_type = 'M'::bpchar AND (leg.payload ->> 'LegInstrumentType'::text) = 'O'::text THEN leg.payload ->> 'LegQty'::text
+            ELSE NULL::text
+        END AS optionquantity,
+        CASE
+            WHEN co.instrument_type = 'O'::bpchar THEN rep_last."LeavesQty"
+            WHEN co.instrument_type = 'M'::bpchar AND (leg.payload ->> 'LegInstrumentType'::text) = 'O'::text THEN rep_last_exec."LeavesQty"
+            ELSE NULL::text
+        END AS optionopenquantity,
+        CASE
+            WHEN co.instrument_type = 'O'::bpchar THEN rep_last."CumQty"
+            WHEN co.instrument_type = 'M'::bpchar AND (leg.payload ->> 'LegInstrumentType'::text) = 'O'::text THEN rep_last_exec."CumQty"
+            ELSE NULL::text
+        END AS optionfilled,
+        CASE
+            WHEN co.instrument_type = 'O'::bpchar THEN rep_last."CanceledQty"
+            WHEN co.instrument_type = 'M'::bpchar AND (leg.payload ->> 'LegInstrumentType'::text) = 'O'::text THEN rep_last_exec."CanceledQty"
+            ELSE NULL::text
+        END AS optioncancelled,
+        CASE
+            WHEN co.chain_id = 0 THEN co.payload ->> 'OrderCreationTime'::text
+            ELSE f_chain."OrderCreationTime"
+        END AS mindatetime,
+        CASE co.instrument_type
+            WHEN 'M'::bpchar THEN rep_last_exec."TransactTime"
+            ELSE rep_last."TransactTime"
+        END AS maxdatetime,
+        CASE
+            WHEN co.instrument_type <> 'M'::bpchar THEN ( SELECT rep.payload ->> 'TransactTime'::text
+               FROM blaze7.order_report rep
+              WHERE rep.cl_ord_id::text = co.cl_ord_id::text AND (rep.exec_type = ANY (ARRAY['1'::bpchar, '2'::bpchar]))
+              ORDER BY rep.exec_id
+             LIMIT 1)
+            ELSE ( SELECT rep.payload ->> 'TransactTime'::text
+               FROM blaze7.order_report rep
+              WHERE rep.cl_ord_id::text = co.cl_ord_id::text AND rep.leg_ref_id::text = leg.leg_ref_id::text AND (rep.exec_type = ANY (ARRAY['1'::bpchar, '2'::bpchar]))
+              ORDER BY rep.exec_id
+             LIMIT 1)
+        END AS firstfilldatetime,
+        CASE
+            WHEN co.instrument_type <> 'M'::bpchar THEN ( SELECT rep.payload ->> 'TransactTime'::text
+               FROM blaze7.order_report rep
+              WHERE rep.cl_ord_id::text = co.cl_ord_id::text AND (rep.exec_type = ANY (ARRAY['1'::bpchar, '2'::bpchar]))
+              ORDER BY rep.exec_id DESC
+             LIMIT 1)
+            ELSE ( SELECT rep.payload ->> 'TransactTime'::text
+               FROM blaze7.order_report rep
+              WHERE rep.cl_ord_id::text = co.cl_ord_id::text AND rep.leg_ref_id::text = leg.leg_ref_id::text AND (rep.exec_type = ANY (ARRAY['1'::bpchar, '2'::bpchar]))
+              ORDER BY rep.exec_id DESC
+             LIMIT 1)
+        END AS lastfilldatetime,
+        CASE
+            WHEN COALESCE(co.payload ->> 'LinkedStageOrderId'::text, co.payload #>> '{OriginatorOrder,LinkedStageOrderId}'::text[], co.payload #>> '{ContraOrder,LinkedStageOrderId}'::text[]) IS NOT NULL THEN 'T'::text
+            ELSE ( SELECT rp.payload ->> 'BlazeOrderStatus'::text
+               FROM blaze7.order_report rp
+              WHERE rp.cl_ord_id::text = co.cl_ord_id::text AND
+                    CASE
+                        WHEN co.instrument_type = 'M'::bpchar THEN rp.leg_ref_id::text = leg.leg_ref_id::text
+                        ELSE true
+                    END
+              ORDER BY rp.exec_id DESC
+             LIMIT 1)
+        END AS statuscode,
+    co.payload ->> 'TimeInForce'::text AS timeinforcecode,
+        CASE
+            WHEN co.instrument_type = 'M'::bpchar THEN leg.payload ->> 'PositionEffect'::text
+            WHEN co.crossing_side IS NULL THEN co.payload ->> 'PositionEffect'::text
+            WHEN co.crossing_side = 'O'::bpchar THEN co.payload #>> '{OriginatorOrder,PositionEffect}'::text[]
+            WHEN co.crossing_side = 'C'::bpchar THEN co.payload #>> '{ContraOrder,PositionEffect}'::text[]
+            ELSE NULL::text
+        END AS openclose,
+    NULL::text AS systemid,
+    NULL::text AS orderidint,
+    COALESCE("substring"(
+        CASE co.instrument_type
+            WHEN 'M'::bpchar THEN leg.payload ->> 'DashSecurityId'::text
+            ELSE co.payload ->> 'DashSecurityId'::text
+        END, 'US:EQ:(.+)'::text), "substring"(
+        CASE co.instrument_type
+            WHEN 'M'::bpchar THEN leg.payload ->> 'DashSecurityId'::text
+            ELSE co.payload ->> 'DashSecurityId'::text
+        END, 'US:[FO|OP]{2}:(.+)_'::text)) AS rootcode,
+        CASE
+            WHEN (co.payload ->> 'OrigClOrdId'::text) IS NOT NULL THEN ( SELECT rep.payload ->> 'CumQty'::text
+               FROM blaze7.order_report rep
+              WHERE rep.cl_ord_id::text = orig.cl_ord_id::text AND COALESCE(rep.leg_ref_id, 'leg_ref_id'::character varying)::text = COALESCE(leg.leg_ref_id, 'leg_ref_id'::character varying)::text
+              ORDER BY rep.exec_id DESC
+             LIMIT 1)
+            ELSE NULL::text
+        END AS prevfillquantity,
+        CASE
+            WHEN co.instrument_type = 'E'::bpchar OR co.instrument_type = 'M'::bpchar AND (leg.payload ->> 'LegInstrumentType'::text) = 'E'::text AND (co.payload ->> 'OrigClOrdId'::text) IS NOT NULL THEN ( SELECT rep.payload ->> 'CumQty'::text
+               FROM blaze7.order_report rep
+              WHERE rep.cl_ord_id::text = orig.cl_ord_id::text AND COALESCE(rep.leg_ref_id, 'leg_ref_id'::character varying)::text = COALESCE(leg.leg_ref_id, 'leg_ref_id'::character varying)::text
+              ORDER BY rep.exec_id DESC
+             LIMIT 1)
+            ELSE NULL::text
+        END AS stockprevfillquantity,
+        CASE
+            WHEN co.instrument_type = 'O'::bpchar OR co.instrument_type = 'M'::bpchar AND (leg.payload ->> 'LegInstrumentType'::text) = 'O'::text AND (co.payload ->> 'OrigClOrdId'::text) IS NOT NULL THEN ( SELECT rep.payload ->> 'CumQty'::text
+               FROM blaze7.order_report rep
+              WHERE rep.cl_ord_id::text = orig.cl_ord_id::text AND NOT rep.leg_ref_id::text IS DISTINCT FROM leg.leg_ref_id::text
+              ORDER BY rep.exec_id DESC
+             LIMIT 1)
+            ELSE NULL::text
+        END AS optionprevfillquantity,
+        CASE
+            WHEN co.instrument_type = 'M'::bpchar AND (leg.payload ->> 'StitchedSingleOrderId'::text) IS NOT NULL THEN (( SELECT co2.cl_ord_id
+               FROM blaze7.client_order co2
+              WHERE co2.order_id = ((leg.payload ->> 'StitchedSingleOrderId'::text)::bigint) AND co2.chain_id = 0
+             LIMIT 1))::text
+            ELSE NULL::text
+        END AS legorderid,
+    co.order_id AS _order_id,
+    co.chain_id AS _chain_id,
+    co.db_create_time AS _db_create_time,
+    max_rep._last_mod_time,
+    co.order_trade_date AS order_trade_date_id,
+    leg.payload ->> 'IsComboLeg' as IsComboLeg
+   FROM blaze7.client_order co
+     JOIN LATERAL ( SELECT cl.order_id,
+            cl.chain_id
+           FROM blaze7.client_order cl
+          WHERE cl.cl_ord_id::text = co.cl_ord_id::text
+          ORDER BY cl.chain_id DESC
+         LIMIT 1) ch ON ch.order_id = co.order_id AND ch.chain_id = co.chain_id
+     LEFT JOIN blaze7.client_order_leg leg ON leg.order_id = co.order_id AND leg.chain_id = co.chain_id
+     LEFT JOIN LATERAL ( SELECT rep.db_create_time AS _last_mod_time
+           FROM blaze7.order_report rep
+          WHERE rep.order_id = co.order_id AND rep.chain_id = co.chain_id
+          ORDER BY rep.db_create_time DESC
+         LIMIT 1) max_rep ON true
+     LEFT JOIN LATERAL ( SELECT co2.cl_ord_id
+           FROM blaze7.client_order co2
+          WHERE co2.cl_ord_id::text = (co.payload ->> 'OrigClOrdId'::text)
+          ORDER BY co2.chain_id DESC
+         LIMIT 1) orig ON true
+     LEFT JOIN LATERAL ( SELECT co2.payload ->> 'OrderCreationTime'::text AS "OrderCreationTime"
+           FROM blaze7.client_order co2
+          WHERE co2.cl_ord_id::text = co.cl_ord_id::text AND co2.chain_id = 0) f_chain ON true
+     LEFT JOIN LATERAL ( SELECT rep.payload ->> 'CumQty'::text AS "CumQty",
+            rep.payload ->> 'AvgPx'::text AS "AvgPx",
+            rep.payload ->> 'LeavesQty'::text AS "LeavesQty",
+            rep.payload ->> 'CanceledQty'::text AS "CanceledQty",
+            rep.payload ->> 'TransactTime'::text AS "TransactTime"
+           FROM blaze7.order_report rep
+          WHERE rep.cl_ord_id::text = co.cl_ord_id::text AND COALESCE(rep.leg_ref_id::text, 'leg_ref_id'::text) = COALESCE(leg.leg_ref_id::text, 'leg_ref_id'::text)
+          ORDER BY rep.exec_id DESC
+         LIMIT 1) rep_last_exec ON true
+     LEFT JOIN LATERAL ( SELECT rep.payload ->> 'CumQty'::text AS "CumQty",
+            rep.payload ->> 'AvgPx'::text AS "AvgPx",
+            rep.payload ->> 'LeavesQty'::text AS "LeavesQty",
+            rep.payload ->> 'CanceledQty'::text AS "CanceledQty",
+            rep.payload ->> 'TransactTime'::text AS "TransactTime"
+           FROM blaze7.order_report rep
+          WHERE rep.cl_ord_id::text = co.cl_ord_id::text
+          ORDER BY rep.exec_id DESC
+         LIMIT 1) rep_last ON true
+  WHERE co.record_type = ANY (ARRAY['0'::bpchar, '2'::bpchar]);
