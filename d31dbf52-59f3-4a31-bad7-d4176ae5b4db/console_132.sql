8903bce6b3bf6ca1541e43cad63d322974475ec0
Oleh Sydor <ole.sydor@gmail.com>
2025-04-16 15:00:08 +0300

    Regular auto-commit at 15:00:08 on 16/04/2025

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_132.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_132.sql
@@ -1,6 +1,6 @@
--- DROP FUNCTION dash360.report_eod_alpaca_options_retail(int4, int4);
+-- DROP FUNCTION dash360.report_eod_alpaca_options_retail2(int4, int4);
 
-select * from dash360.report_eod_alpaca_options_retail(in_start_date_id := 20250411, in_end_date_id := 20250414, in_account_ids := '{73818,73819,73820}');
+select * from dash360.report_eod_alpaca_options_retail(in_start_date_id := 20250411, in_end_date_id := 20250414), in_account_ids := '{73818,73819,73820}');
 select * from dash360.report_eod_alpaca_options_retail(in_start_date_id := 20250411, in_end_date_id := 20250414, in_trading_firm_ids := '{"rqd","OFP0072"}');
 
 
@@ -8,10 +8,6 @@ select * from dash360.report_eod_alpaca_options_retail(in_start_date_id := 20250
 select * from dash360.report_eod_alpaca_options_retail2(in_start_date_id := 20250411, in_end_date_id := 20250414);
 
 
-        select tf.*
-        from dwh.d_account ac
-        join dwh.d_trading_firm tf on ac.trading_firm_unq_id = tf.trading_firm_unq_id
-        where account_id in (73818,73819,73820)
 
 CREATE FUNCTION dash360.report_eod_alpaca_options_retail(in_start_date_id integer, in_end_date_id integer, in_trading_firm_ids varchar[] default '{}'::varchar[], in_account_ids int4[] default '{}'::int4[])
  RETURNS TABLE(ret_row text)
@@ -113,6 +109,206 @@ begin
     get diagnostics l_row_cnt = row_count;
     select public.load_log(l_load_id, l_step_id,
                            'report_fintech_eod_alpaca_options_retail for ' || in_start_date_id::text || '-' ||
+                           in_end_date_id::text || ' COMPLETED ===', l_row_cnt, 'O')
+    into l_step_id;
+end;
+$function$
+;
+
+
+-- DROP FUNCTION dash360.report_eod_alpaca_equity_retail2(int4, int4);
+select * from dash360.report_eod_alpaca_equity_retail2(20250415, 20250415)
+except
+select * from dash360.report_eod_alpaca_equity_retail(20250415, 20250415);
+
+CREATE OR REPLACE FUNCTION dash360.report_eod_alpaca_equity_retail(in_start_date_id integer, in_end_date_id integer, in_trading_firm_ids varchar[] default '{}'::varchar[], in_account_ids int4[] default '{}'::int4[])
+ RETURNS TABLE(ret_row text)
+ LANGUAGE plpgsql
+AS $function$
+-- 20240926 SO https://dashfinancial.atlassian.net/browse/DEVREQ-4936 added Account Name
+-- 20241107 KK https://dashfinancial.atlassian.net/browse/DEVREQ-5065 Update Account Name
+-- 20241121 KK https://dashfinancial.atlassian.net/browse/DEVREQ-5065 Update Exec ID: exec_id > exch_exec_id
+-- 20250415 OS https://dashfinancial.atlassian.net/browse/DEVREQ-5905 added 2 new input parameters
+declare
+    l_load_id     int;
+    l_row_cnt     int;
+    l_step_id     int;
+    l_account_ids integer[];
+begin
+
+    select nextval('public.load_timing_seq') into l_load_id;
+    l_step_id := 1;
+    select public.load_log(l_load_id, l_step_id,
+                           'report_eod_alpaca_equity_retail for ' || in_start_date_id::text || '-' ||
+                           in_end_date_id::text || ' STARTED ===', 0, 'O')
+    into l_step_id;
+
+    if coalesce(in_account_ids, '{}') = '{}' and coalesce(in_trading_firm_ids, '{}') = '{}' then
+        select array_agg(account_id)
+        into l_account_ids
+        from dwh.d_account
+        where account_name in ('APCARET');
+    else
+        select array_agg(account_id)
+        into l_account_ids
+        from dwh.d_account
+        where true
+          and case
+                  when coalesce(in_trading_firm_ids, '{}') <> '{}'::varchar[]
+                      then trading_firm_id = ANY (in_trading_firm_ids)
+                  else true end
+          and case
+                  when coalesce(in_account_ids, '{}') <> '{}'::integer[] then account_id = ANY (in_account_ids)
+                  else true end;
+    end if;
+
+    return query
+        select 'Account Name,Trade Date,Timestamp,Symbol,Buy/Sell,Quantity,Price,Rate Category,Rate,Commission,Order ID,Exec ID,Venue,Exchange Fees';
+    return query
+        select array_to_string(ARRAY [
+                                   ac.account_name,
+                                   to_char(tr.order_process_time, 'YYYY-MM-DD'), -- as Trade date
+                                   to_char(tr.order_process_time, 'HH24:MI:SS.US'), -- as Timestamp
+            -- SYMBOL
+                                   di.symbol, -- as "SYMBOL",
+                                   case
+                                       when tr.side = '1' then 'B'
+                                       when tr.side in ('2', '5', '6') then 'S' end, -- as "SIDE",
+                                   tr.last_qty::text, -- as "QUANTITY",
+                                   to_char(tr.last_px, 'LFM99999990D009999'), -- as "PRICE",
+                                   case when tr.multileg_reporting_type = '1' then 'OUTRIGHT' else 'TIED-TO-OPTION' end,-- rate_category
+                                   to_char(case when tr.multileg_reporting_type = '1' then 0.0005 else 0 end,
+                                           'LFM90D0099'), -- rate_category
+                                   to_char(case
+                                               when tr.multileg_reporting_type = '1' then tr.last_qty * 0.0005
+                                               else 0 end, 'FM99999990D009999'),-- PFOF = rate * last_qty
+                                   tr.client_order_id::text,
+                                   tr.exch_exec_id::text,
+                                   exc.mic_code,
+                                   to_char(coalesce(tr.tcce_maker_taker_fee_amount, 0) +
+                                   coalesce(tr.tcce_trade_processing_fee_amount, 0) +
+                                   coalesce(tr.tcce_transaction_fee_amount, 0), 'FM99999999990.09999999')
+                                   ], ',', '')
+        from dwh.flat_trade_record tr
+                 join dwh.d_account ac on (ac.account_id = tr.account_id)
+                 join dwh.d_instrument di on di.instrument_id = tr.instrument_id
+                 left join dwh.d_exchange exc on exc.exchange_id = tr.exchange_id and exc.is_active
+        where tr.date_id between in_start_date_id and in_end_date_id
+          --and ac.trading_firm_id in ('alpaca','OFP0068')
+--           and ac.account_name in ('APCARET')
+          and tr.account_id = any(l_account_ids)
+          and tr.instrument_type_id = 'E'
+          and tr.is_busted = 'N'
+        order by tr.date_id, tr.trade_record_id;
+    get diagnostics l_row_cnt = row_count;
+    select public.load_log(l_load_id, l_step_id,
+                           'report_eod_alpaca_equity_retail for ' || in_start_date_id::text || '-' ||
+                           in_end_date_id::text || ' COMPLETED ===', l_row_cnt, 'O')
+    into l_step_id;
+end;
+$function$
+;
+
+
+-- DROP FUNCTION dash360.report_eod_alpaca_algo_route(int4, int4);
+select * from dash360.report_eod_alpaca_algo_route(20250415, 20250415, '{}', '{}');
+except
+select * from dash360.report_eod_alpaca_algo_route2(20250415, 20250415);
+
+CREATE OR REPLACE FUNCTION dash360.report_eod_alpaca_algo_route(in_start_date_id integer, in_end_date_id integer, in_trading_firm_ids varchar[] default '{}'::varchar[], in_account_ids int4[] default '{}'::int4[])
+ RETURNS TABLE(ret_row text)
+ LANGUAGE plpgsql
+AS $function$
+-- 20240926 SO https://dashfinancial.atlassian.net/browse/DEVREQ-4936 added Account Name
+-- 20241107 KK https://dashfinancial.atlassian.net/browse/DEVREQ-5065 Updated Account Name
+-- 20250415 OS https://dashfinancial.atlassian.net/browse/DEVREQ-5905 added 2 new input parameters
+declare
+    l_load_id     int;
+    l_row_cnt     int;
+    l_step_id     int;
+    l_account_ids int4[];
+begin
+
+    select nextval('public.load_timing_seq') into l_load_id;
+    l_step_id := 1;
+    select public.load_log(l_load_id, l_step_id,
+                           'report_eod_alpaca_algo_route for ' || in_start_date_id::text || '-' ||
+                           in_end_date_id::text || ' STARTED ===', 0, 'O')
+    into l_step_id;
+
+    if coalesce(in_account_ids, '{}') = '{}' and coalesce(in_trading_firm_ids, '{}') = '{}' then
+        select array_agg(account_id)
+        into l_account_ids
+        from dwh.d_account
+        where account_name in ('APCA');
+    else
+        select array_agg(account_id)
+        into l_account_ids
+        from dwh.d_account
+        where true
+          and case
+                  when coalesce(in_trading_firm_ids, '{}') <> '{}'::varchar[]
+                      then trading_firm_id = ANY (in_trading_firm_ids)
+                  else true end
+          and case
+                  when coalesce(in_account_ids, '{}') <> '{}'::integer[] then account_id = ANY (in_account_ids)
+                  else true end;
+    end if;
+
+
+    return query
+-- select 'Load Date,Client Short Name,Reporting Client MPID,Symbol,Side,Last Px,Last Qty,Contra Name,RLi,Li,Amount,Exec Time EST,Client Order ID,Target Strategy Name,Custom Algo,Urgency Code,External Exec ID';
+        select 'Account Name,Load Date,Client Short Name,Reporting Client MPID,Symbol,Side,Last Px,Last Qty,Li,Amount,Exec Time EST,Client Order ID,Target Strategy Name,Custom Algo,Urgency Code,External Exec ID,Venue,Exchange Fees';
+    return query
+        select array_to_string(ARRAY [
+                                   ac.account_name,
+                                   to_char(co.create_time, 'DD/MM/YYYY'), -- as "Load Date",
+                                   'APC4', -- as "Client Short Name",
+                                   'APCA', -- as "Reporting Client MPID",
+                                   di.symbol,
+                                   case
+                                       when ftr.side = '1' then 'Buy'
+                                       when ftr.side = '2' then 'Sell'
+                                       when ftr.side in ('5', '6') then 'Sell Short'
+                                       end, -- as "Side"
+                                   to_char(ftr.last_px, 'LFM99999990D009999'), -- "Last Px"
+                                   ftr.last_qty::text, -- "Last Qty"
+            -- Contra Name,
+            -- RLi
+                                   li.trade_liquidity_indicator, -- as "Li",
+                                   to_char(ftr.last_px * ftr.last_qty, 'FM99999990D009999'), -- as "Amount",
+                                   to_char(ftr.trade_record_time at time zone 'America/New_York',
+                                           'HH24:MI:SS.FF3'), -- as "Exec Time EST",
+                                   co.client_order_id, -- as "Client Order ID",
+                                   sub_strategy_desc, -- as "Target Strategy Name",
+                                   null::text, -- as "Custom Algo",
+                                   left(fmj.fix_message ->> '9002', 6), -- as "Urgency Code",
+                                   ftr.exch_exec_id, -- as "External Exec ID"
+                                   exc.mic_code,
+                                   to_char(coalesce(tcce_maker_taker_fee_amount, 0) +
+                                   coalesce(tcce_trade_processing_fee_amount, 0) +
+                                   coalesce(tcce_transaction_fee_amount, 0), 'FM99999999990.09999999')
+                                   ], ',', '')
+        select co.*
+        from dwh.client_order co
+                 join dwh.flat_trade_record ftr on ftr.order_id = co.order_id and ftr.date_id = co.create_date_id
+                 join dwh.d_account ac on ac.account_id = ftr.account_id
+                 join dwh.d_instrument di on di.instrument_id = ftr.instrument_id
+                 left join dwh.d_liquidity_indicator li on li.exchange_id = ftr.exchange_id and
+                                                           li.trade_liquidity_indicator =
+                                                           ftr.trade_liquidity_indicator and
+                                                           li.is_active
+
+                 left join fix_capture.fix_message_json fmj
+                           on fmj.fix_message_id = co.fix_message_id and fmj.date_id = co.create_date_id
+                 left join dwh.d_exchange exc on exc.exchange_id = ftr.exchange_id and exc.is_active
+        where co.ex_destination = 'ALGO'
+--           and co.account_id = any (l_account_ids)
+          and co.create_date_id between :in_start_date_id and :in_end_date_id
+        order by ftr.date_id, ftr.trade_record_id;
+    get diagnostics l_row_cnt = row_count;
+    select public.load_log(l_load_id, l_step_id,
+                           'report_eod_alpaca_algo_route for ' || in_start_date_id::text || '-' ||
                            in_end_date_id::text || ' COMPLETED ===', l_row_cnt, 'O')
     into l_step_id;
 end;
