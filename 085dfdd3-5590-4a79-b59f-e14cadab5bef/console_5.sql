d608db16e205ffa132b027712bf9195fe33ba504
Oleh Sydor <ole.sydor@gmail.com>
2025-04-30 18:00:09 +0300

    Regular auto-commit at 18:00:09 on 30/04/2025

---
+++ 085dfdd3-5590-4a79-b59f-e14cadab5bef/console_5.sql
@@ -0,0 +1,20 @@
+Select o.ID as orderID,
+       r.ID as reportid,
+       r.ExchangeTransactionID tag17,
+       feedcode as secondary_exch_exec_id,
+       *
+From LiquidPoint_EDW..TReports_EDW r
+         inner join LiquidPoint_EDW..TOrder_EDW o
+                    on r.ORderID = o.ORdeRID
+         inner join LiquidPoint_EDW..TOrderMisc1_EDW om
+                    on o.ORdeRID = om.ORdeRID and om.SystemID = o.SystemID
+         left outer join EDW_Billing.dbo.DASH_TRADE_Record dtr
+                         on dtr.client_order_id = om.CATID
+                             and dtr.secondary_exch_exec_id = r.feedcode
+                             and dtr.date_id = 20250325
+Where r.ORderID in (Select ORDERID
+                    from LiquidPoint_EDW..TORDERMisc1_EDW
+                    Where [CATID] = '20250325VSIND28939')
+  and r.Status in (151, 156)
+  and r.ID in (1407944140, 1407944308)
+order by r.ExchangeTransactionID
