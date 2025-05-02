b50c0ac11bfb59e199921d350cdb7f6461295671
Oleh Sydor <ole.sydor@gmail.com>
2025-05-02 14:00:09 +0300

    Regular auto-commit at 14:00:09 on 02/05/2025

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/DEVREQ-5964.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/DEVREQ-5964.sql
@@ -42,32 +42,35 @@ where 	true
 select * from client_order
 where parent_order_id = 100000019696533916;
 ---
-select cl.trading_firm_id, fxm.*, ex.*, cl.* --ex.*, order_qty
+select cl.trading_firm_id, ac.account_name, fxm.*, ex.*, cl.* --ex.*, order_qty
 from client_order cl
-		inner join d_account ac on ac.account_id = cl.account_id and ac.is_active = true
--- 		inner join d_trading_firm tf on tf.trading_firm_id = ac.trading_firm_id and tf.is_active = true
+         inner join d_account ac on ac.account_id = cl.account_id and ac.is_active = true
+    -- 		inner join d_trading_firm tf on tf.trading_firm_id = ac.trading_firm_id and tf.is_active = true
 --
 -- 		inner join d_fix_connection fc on fc.fix_connection_id = cl.fix_connection_id and fc.is_active = true
 -- 		inner join d_option_contract oc on oc.instrument_id = cl.instrument_id
 -- 		inner join d_option_series os on os.option_series_id  = oc.option_series_id
 -- 		left join d_time_in_force tif on tif.tif_id = cl.time_in_force_id
-        left join lateral
-	        (select j.fix_message,j.fix_message->>'143' as tag_143, j.fix_message->>'423' as tag_423,
-			        j.fix_message->>'9281' as tag_9281,j.fix_message->>'22017' as tag_22017,
-	        		j.fix_message->>'115' as tag_115, j.fix_message->>'109' as tag_109--, j.fix_message->>'5059' as tag_5059, j.fix_message->>'134' as tag_134, j.fix_message->>'135' as tag_135
-	         from fix_capture.fix_message_json j
-	         where j.fix_message_id  = cl.fix_message_id
-	         and j.date_id = :in_date_id
-	         limit 1
-	        ) fxm on true
-        left join lateral (select exch_exec_id
-                      from dwh.execution ex
-                      where ex.order_id = cl.order_id
-                        and ex.exec_type = 'F'
-                      ) ex on true
+         left join lateral
+    (select j.fix_message,
+            j.fix_message ->> '143'   as tag_143,
+            j.fix_message ->> '423'   as tag_423,
+            j.fix_message ->> '9281'  as tag_9281,
+            j.fix_message ->> '22017' as tag_22017,
+            j.fix_message ->> '115'   as tag_115,
+            j.fix_message ->> '109'   as tag_109--, j.fix_message->>'5059' as tag_5059, j.fix_message->>'134' as tag_134, j.fix_message->>'135' as tag_135
+     from fix_capture.fix_message_json j
+     where j.fix_message_id = cl.fix_message_id
+       and j.date_id = :in_date_id
+     limit 1
+    ) fxm on true
+         left join lateral (select exch_exec_id
+                            from dwh.execution ex
+                            where ex.order_id = cl.order_id
+                              and ex.exec_type = 'F'
+    ) ex on true
 
 where cl.create_date_id = :in_date_id
-
 /*		and cl.trans_type = 'D'
 		and (cl.multileg_reporting_type = '1' or (cl.sub_strategy_desc = 'VEGA' and cl.multileg_reporting_type = '2'))
 		and fc.is_high_frequency_trader = 'N'
@@ -93,10 +96,17 @@ where cl.create_date_id = :in_date_id
  */
   and case
           when cl.parent_order_id is null then true
-          when cl.parent_order_id is not null and order_qty >= 250 and cl.trading_firm_id in ('xfa', 'xfachi') and  ac.account_name <> 'XFADESKTCM' then true
-          when cl.parent_order_id is not null and order_qty >= 250 and fxm.tag_143 = 'RFAC' then true
+          when cl.parent_order_id is not null and cl.trading_firm_id in ('xfa', 'xfachi') and
+               ac.account_name <> 'XFADESKTCM' then true -- XFA
+          when cl.parent_order_id is not null and fxm.tag_143 in ('RFAC', 'DASH')
+              then true -- routed to DASH Desk (143=DASH), Casey Securities (Tag 143 = RFAC)
+          when cl.parent_order_id is not null and ac.account_name in ('TASTYSPX', 'TDSPX_BP')
+              then true -- for SPX
           else false end
-  and ac.account_id = 73660
+  and ac.account_id = 73660; -- e.g. Vision March 2025 example
+
+
+
 
 select * from dwh.execution ex
     where true
