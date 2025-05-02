ec12519761987421fecab0df6e7554ae4ab53382
Oleh Sydor <ole.sydor@gmail.com>
2025-05-02 10:00:09 +0300

    Regular auto-commit at 10:00:09 on 02/05/2025

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/DEVREQ-5964.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/DEVREQ-5964.sql
@@ -39,6 +39,49 @@ where account_id = 73660
 
 select * from client_order
 where parent_order_id = 100000019696533916
+---
+select ex.*, *
+from client_order cl
+		inner join d_account ac on ac.account_id = cl.account_id and ac.is_active = true
+		inner join d_trading_firm tf on tf.trading_firm_id = ac.trading_firm_id and tf.is_active = true
 
+		inner join d_fix_connection fc on fc.fix_connection_id = cl.fix_connection_id and fc.is_active = true
+		inner join d_option_contract oc on oc.instrument_id = cl.instrument_id
+		inner join d_option_series os on os.option_series_id  = oc.option_series_id
+		left join d_time_in_force tif on tif.tif_id = cl.time_in_force_id
+        left join lateral
+	        (select j.fix_message,--j.fix_message->>'432' as tag_432,j.fix_message->>'423' as tag_423,
+			        j.fix_message->>'9281' as tag_9281,j.fix_message->>'22017' as tag_22017,
+	        		j.fix_message->>'115' as tag_115, j.fix_message->>'109' as tag_109--, j.fix_message->>'5059' as tag_5059, j.fix_message->>'134' as tag_134, j.fix_message->>'135' as tag_135
+	         from fix_capture.fix_message_json j
+	         where j.fix_message_id  = cl.fix_message_id
+	         and j.date_id = :in_date_id
+	         limit 1
+	        ) fxm on true
 
+        left join lateral(select exch_exec_id from dwh.execution ex where ex.order_id = cl.order_id and ex.exec_type = 'F' limit 1000) ex on true
+
+		where cl.create_date_id = :in_date_id
+-- 		and cl.parent_order_id is null
+		and cl.trans_type = 'D'
+		and (cl.multileg_reporting_type = '1' or (cl.sub_strategy_desc = 'VEGA' and cl.multileg_reporting_type = '2'))
+		and fc.is_high_frequency_trader = 'N'
+		and coalesce(tf.cat_imid,'NONE') not in ('NONE','DFIN')
+		and fc.fix_comp_id not in ('IRCHNY2EQPT1INT','IRCHNY2EQPT2INT','IRCHNY2EQPT3INT','IRCHNY2OPTPT1INT')
+		and (ac.trading_firm_id not in ('BMO','dynamex01','Guggen','nbcanf','daiwa','mirae','miradelta') or fc.fix_comp_id not in ('BOOKP','BOOKP2'))
+		--IMC
+		and (ac.trading_firm_id not in ('imc01','cutler') or fc.fix_comp_id <> 'IMCCONS')
+		--
+		and coalesce(tf.cat_suppress,'N') <> 'Y'
+		and coalesce(ac.cat_suppress,'N') <> 'Y'
+		and cl.ex_destination not in ('RPTR','SQHT','WEEDN','JSEB','TRAFX','FBMS','CTDH','DASH','OUTCR','SLXX','WEX','WEXE','PRIME','UBSPP','WEXX')
+		--
+		and (cl.ex_destination not in ('BRKPT','BLAZE') or ac.account_name in ('TASTYSPX','TDSPX_BP','TDSWIM_BP','CPRFA_BP','TDA_RFA_BP','ETRADE_RFAC_BP','SCHWABTDA_RFA_BP','FIDOFP_RFA_BP','TICKRS_BP','VSIN_BP'
+			))
+		and cl.order_id not in (select order_id from compliance.rejected_parent_order where not street_is_generated)
+		--and (cl.order_id not in (select ex.order_id from execution ex where ex.exec_date_id = in_date_id and ex.is_parent_level = true and ex.exec_type = '8') or cl.sub_strategy_desc = 'DMA')
+		--and not exists (select 1 from execution ex where ex.exec_date_id = in_date_id and ex.order_id  = cl.order_Id and ex.is_parent_level = true and ex.exec_type = '8' limit 1)
+		and os.root_symbol not in (select symbol from compliance.test_symbols)
+        and cl.order_id in (100000019696533965, 100000019696533916)
+        and ex.exch_exec_id in ('l25akrts0002', 'l25akrts0000')
 
