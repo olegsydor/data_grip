bdf8e93af380cd8d2457662cd7ac362c02bce786
Oleh Sydor <ole.sydor@gmail.com>
2025-05-22 18:00:12 +0300

    Regular auto-commit at 18:00:12 on 22/05/2025

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/DEVREQ-6008.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/DEVREQ-6008.sql
@@ -1,15 +1,26 @@
 select *
-from dash360.report_compliance_avg_parent_order_count(in_date_id := 20250303
+from dash360.report_compliance_avg_parent_order_count(in_start_date_id := 20250303, in_end_date_id := 20250315
     , in_trading_firm_ids := '{alpaca,caerus}'
     , in_account_ids := '{71912, 72420, 72917}'
+     , in_instrument_type_id := null
      );
+
+select *
+from dash360.report_compliance_avg_parent_order_count(
+        in_trading_firm_ids := '{OFP0058,OFP0077,t3trade01}',
+        in_instrument_type_id := null
+     );
+
+
 
 
 -- drop function dash360.report_compliance_avg_parent_order_count
 
-create or replace function dash360.report_compliance_avg_parent_order_count(in_date_id integer default null,
-                                                                 in_trading_firm_ids character varying[] default '{}'::character varying[],
-                                                                 in_account_ids int4[] default '{}'::int4[])
+create or replace function dash360.report_compliance_avg_parent_order_count(in_start_date_id int4 default null,
+                                                                            in_end_date_id int4 default null,
+                                                                            in_trading_firm_ids character varying[] default '{}'::character varying[],
+                                                                            in_account_ids int4[] default '{}'::int4[],
+                                                                            in_instrument_type_id character DEFAULT 'O'::bpchar)
     returns table
             (
                 ret_row text
@@ -24,26 +35,16 @@ declare
     l_step_id       int;
     l_account_ids   int4[];
     l_message       text;
-    l_start_date date := case
-                             when in_date_id is not null then date_trunc('month', in_date_id::text::date)::date
-                             else date_trunc('month', current_date - '1 month'::interval)::date end;
-    l_end_date   date := case
-                             when in_date_id is not null then (
-                                 date_trunc('month', in_date_id::text::date + '1 month'::interval) - '1 day'::interval)::date
-                             else (date_trunc('month', current_date) - '1 day'::interval)::date end;
+    l_start_date    date := case
+                                when in_start_date_id is not null then in_start_date_id::text::date
+                                else date_trunc('month', current_date - '1 month'::interval)::date end;
+    l_end_date      date := case
+                                when in_end_date_id is not null then in_end_date_id::text::date
+                                else (date_trunc('month', current_date) - '1 day'::interval)::date end;
     l_start_date_id int4 := to_char(l_start_date, 'YYYYMMDD');
     l_end_date_id   int4 := to_char(l_end_date, 'YYYYMMDD');
     l_all_days      int4;
 begin
-    l_message = 'report_compliance_avg_parent_order_count for ' || l_start_date_id::text || '-' || l_end_date_id::text || ' ';
-
-    select nextval('public.load_timing_seq') into l_load_id;
-    l_step_id := 1;
-    select public.load_log(l_load_id, l_step_id,
-                           l_message || 'STARTED ===', 0, 'O')
-    into l_step_id;
-
-
     if coalesce(in_account_ids, '{}') = '{}' and coalesce(in_trading_firm_ids, '{}') = '{}' then
         l_account_ids := '{}';
     else
@@ -59,6 +60,21 @@ begin
                   when coalesce(in_account_ids, '{}') <> '{}'::integer[] then account_id = ANY (in_account_ids)
                   else true end;
     end if;
+
+    l_message =
+            'report_compliance_avg_parent_order_count, ' ||
+            case when in_account_ids = '{}' then '' else 'accounts-' || left(array_to_string(in_account_ids::int4[], ',', '')::text, 50) end ||
+            case
+                when in_trading_firm_ids = '{}' then ''
+                else 'trading_firm-' || left(array_to_string(in_trading_firm_ids::varchar[], ',', '')::text, 50) end ||
+            ' for ' || l_start_date_id::text || '-' || l_end_date_id::text ||
+            ' ';
+
+    select nextval('public.load_timing_seq') into l_load_id;
+    l_step_id := 1;
+    select public.load_log(l_load_id, l_step_id,
+                           l_message || 'STARTED ===', 0, 'O')
+    into l_step_id;
 
     select count(*)
     into l_all_days
@@ -82,9 +98,9 @@ begin
     where true
       and "Status_Date_id" >= l_start_date_id
       and "Status_Date_id" <= l_end_date_id
---       and case when in_instrument_type_id is null then true else hods."InstrumentType" = in_instrument_type_id end
+      and case when in_instrument_type_id is null then true else hods."InstrumentType" = in_instrument_type_id end
       and hods."CustomerOrderID" is null
-      and hods."AccountID" = any (l_account_ids) -- '{71912, 72420, 72917}'
+      and hods."AccountID" = any (l_account_ids) -- '{54612,54613,54690,54691,62093,62577,72945,73094}'
     group by to_char(hods."StatusDate", 'Month'), to_char(hods."StatusDate", 'YYYY'), a.account_name,
              cf.customer_or_firm_name, tf.trading_firm_name;
 
@@ -124,4 +140,12 @@ select trim(to_char(:"Period", 'Month')) May;
 select to_char(to_date(:proforma_invoice_date, 'DD/MM/YYYY'), 'Month')
 
     select to_date(:proforma_invoice_date, 'DD/MM/YYYY')
-January
+select array_to_string('{1, 2, 3}'::int4[], ',', '');
+
+select 'report_compliance_avg_parent_order_count, ' ||
+            case when :in_account_ids = '{}' then '' else 'accounts-' || left(array_to_string(:in_account_ids::int4[], ',', '')::text, 50) end ||
+            case
+                when :in_trading_firm_ids = '{}' then ''
+                else ', trading_firm-' || left(array_to_string(:in_trading_firm_ids::varchar[], ',', '')::text, 50) end ||
+            ' for ' || :l_start_date_id::text || '-' || :l_end_date_id::text ||
+            ' ';
