08c58d64944eab0c4c9916f68e25e4dd0976b901
Oleh Sydor <ole.sydor@gmail.com>
2025-06-04 16:00:08 +0300

    Regular auto-commit at 16:00:08 on 04/06/2025

--- f9447fdb-8def-4b3f-af2b-edc8e5d1d9d5/DS-10061.sql
+++ f9447fdb-8def-4b3f-af2b-edc8e5d1d9d5/DS-10061.sql
@@ -482,3 +482,187 @@ begin
 end;
 
 $fn$
+
+
+
+-- DROP FUNCTION dash360.allocations_get_accounts_config(bpchar);
+
+CREATE OR REPLACE FUNCTION dash360.allocations_get_accounts_config(in_market_type character DEFAULT 'O'::character(1))
+ RETURNS TABLE(account_id bigint, is_auto_allocate character, clearing_accounts jsonb)
+ LANGUAGE plpgsql
+ COST 1
+AS $function$
+   /*
+    13-04-2021 - MG - add is_option_auto_allocate field to output
+SY: 20240430 https://dashfinancial.atlassian.net/browse/DS-8208   The is_visible_for_manual_allocation field has been introduced
+    */
+#variable_conflict use_variable
+begin
+    return query
+        select
+            acc.account_id::bigint,
+            (case
+                when in_market_type = 'E' then acc.is_auto_allocate
+                when in_market_type = 'O' then acc.is_option_auto_allocate
+                else acc.is_auto_allocate
+            end) as is_auto_allocate,
+            jsonb_agg( jsonb_object(array['ca_number', 'def' , 'ca_name', 'oaid', 'visible'],
+                array[ca.clearing_account_number, ca.is_default , ca.clearing_account_name, ca.occ_actionable_id, ca.is_visible_for_manual_allocation::text ]))
+        from  genesis2.account acc
+        inner join genesis2.clearing_account ca
+            on acc.account_id = ca.account_id
+            and ca.is_deleted ='N'
+            and ca.market_type =in_market_type
+        where acc.is_deleted ='N'
+        group by acc.account_id,
+            (case
+                when in_market_type = 'E' then acc.is_auto_allocate
+                when in_market_type = 'O' then acc.is_option_auto_allocate
+                else acc.is_auto_allocate
+            end)
+--limit 10
+;
+
+end;
+$function$
+;
+
+
+-- DROP FUNCTION dash360.allocations_get_accounts_config(bpchar);
+select * from dash360.allocations_get_accounts_config(in_market_type := 'O');
+
+CREATE FUNCTION dash360.allocations_get_accounts_config(in_market_type character DEFAULT 'O'::character(1))
+ RETURNS TABLE(account_id bigint, is_auto_allocate character, clearing_accounts jsonb, is_intraday_auto_allocate bpchar)
+ LANGUAGE plpgsql
+ COST 1
+AS $function$
+
+-- MG: 20210413 -- add is_option_auto_allocate field to output
+-- SY: 20240430 https://dashfinancial.atlassian.net/browse/DS-8208   The is_visible_for_manual_allocation field has been introduced
+-- OS: 20250604 https://dashfinancial.atlassian.net/browse/DS-10060 added is_intraday_auto_allocate, removed #variable_conflict use_variable
+
+begin
+    return query
+        select
+            acc.account_id::bigint,
+            (case
+                when in_market_type = 'E' then acc.is_auto_allocate
+                when in_market_type = 'O' then acc.is_option_auto_allocate
+                else acc.is_auto_allocate
+            end) as is_auto_allocate,
+            jsonb_agg( jsonb_object(array['ca_number', 'def' , 'ca_name', 'oaid', 'visible'],
+                array[ca.clearing_account_number, ca.is_default , ca.clearing_account_name, ca.occ_actionable_id, ca.is_visible_for_manual_allocation::text ])),
+        acc.is_intraday_auto_allocate
+        from  genesis2.account acc
+        inner join genesis2.clearing_account ca
+            on acc.account_id = ca.account_id
+            and ca.is_deleted ='N'
+            and ca.market_type =in_market_type
+        where acc.is_deleted ='N'
+        group by acc.account_id,
+            (case
+                when in_market_type = 'E' then acc.is_auto_allocate
+                when in_market_type = 'O' then acc.is_option_auto_allocate
+                else acc.is_auto_allocate
+            end)
+--limit 10
+;
+
+end;
+$function$
+;
+
+
+-- DROP FUNCTION dash360.allocations_set_account_config(int8, jsonb, bpchar, bpchar, int4);
+
+CREATE OR REPLACE FUNCTION dash360.allocations_set_account_config(in_account_id bigint, in_clearing_accounts jsonb,
+                                                                  in_is_auto_allocate character,
+                                                                  in_instrumnt_type_id character DEFAULT 'O'::bpchar,
+                                                                  in_user_id integer DEFAULT NULL::integer,
+                                                                  in_is_intraday_auto_allocate bpchar default 'Y'::bpchar)
+    RETURNS integer
+    LANGUAGE plpgsql
+    COST 1
+AS
+$function$
+    -- MG: 20210413 add support to is_option_auto_allocate field
+-- SY: 20240430 https://dashfinancial.atlassian.net/browse/DS-8208 is_visible_for_manual_allocation  and user_id fields have been introduced
+-- OS: 20250604 https://dashfinancial.atlassian.net/browse/DS-10060 added is_intraday_auto_allocate, removed #variable_conflict use_variable
+
+
+declare
+    l_clearing_account_type smallint;
+    l_row_cnt               int;
+
+begin
+    if in_instrumnt_type_id = 'E'
+    then
+-- set is_autoallocate value
+        update account acc
+        set is_auto_allocate          = in_is_auto_allocate,
+            is_intraday_auto_allocate = in_is_intraday_auto_allocate
+        where acc.account_id = in_account_id
+          and acc.is_deleted = 'N'
+          and acc.is_auto_allocate <> in_is_auto_allocate;
+    end if;
+
+    if in_instrumnt_type_id = 'O'
+    then
+-- set is_option_auto_allocate value
+        update account acc
+        set is_option_auto_allocate   = in_is_auto_allocate,
+            is_intraday_auto_allocate = in_is_intraday_auto_allocate
+        where acc.account_id = in_account_id
+          and acc.is_deleted = 'N'
+          and acc.is_option_auto_allocate <> in_is_auto_allocate;
+    end if;
+
+-- close all current configuration for the account if any
+
+    update genesis2.clearing_account
+    set is_deleted  = 'Y',
+        delete_time = clock_timestamp(),
+        user_id     = in_user_id
+    where account_id = in_account_id
+      and market_type = in_instrumnt_type_id
+      and is_deleted = 'N';
+
+    -- get  clearing_account_type
+
+--  select case sum(case instrument_type_id when in_instrumnt_type_id then 1 else 0 end)
+--          when 1 then count(1)
+--          else 2 -- Temporary solution. For some reason account_id could be missed in account2instrument_type
+--          end  as  clearing_account_type
+--  into l_clearing_account_type
+--  from staging.account2instrument_type ait
+--  where account_id = in_account_id
+--    and instrument_type_id  in ('E', 'O');
+
+    -- Temporary logc SY:20201215 as per chat with Tim Miller
+    l_clearing_account_type := 1;
+
+
+    insert into genesis2.clearing_account (account_id, clearing_account_type, clearing_account_number, is_default,
+                                           market_type, is_deleted, cmta, clearing_account_name, occ_actionable_id,
+                                           user_id, is_visible_for_manual_allocation)
+    select in_account_id,
+           l_clearing_account_type::varchar,
+           sj ->> 'ca_number'             as clearing_account_number,
+           sj ->> 'def'                   as is_default,
+           in_instrumnt_type_id           as market_type,
+           --clock_timestamp() as  create_time,
+           'N'                            as is_deleted,
+           sj ->> 'ca_number'             as cmta,
+           coalesce(sj ->> 'ca_name', '') as clearing_account_name,
+           sj ->> 'oaid'                  as occ_actionable_id,
+           in_user_id,
+           (sj ->> 'visible')::bool       as is_visible_for_manual_allocation
+    from (select value as sj
+          from jsonb_array_elements(in_clearing_accounts)) l1;
+
+    GET DIAGNOSTICS l_row_cnt = ROW_COUNT;
+    return l_row_cnt;
+
+end;
+$function$
+;
