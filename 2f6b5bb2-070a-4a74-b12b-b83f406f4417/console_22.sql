e317e1ab144958c2b572ca0c01e94327deb5530a
Oleh Sydor <ole.sydor@gmail.com>
2025-06-06 18:00:09 +0300

    Regular auto-commit at 18:00:09 on 06/06/2025

--- 2f6b5bb2-070a-4a74-b12b-b83f406f4417/console_22.sql
+++ 2f6b5bb2-070a-4a74-b12b-b83f406f4417/console_22.sql
@@ -12,23 +12,47 @@ values (4, 1, 0.1),
 
 select * from training.clearing_account;
 
-with base as (select clearing_account_id,
+with base as (
+select clearing_account_id,
                      account_id,
                      koef,
                      :qty                                                          as qty,
                      :qty * koef                                                   as pre_sum,
                      round(:qty * koef)                                            as rnd_sum,
                      sum(round(:qty * koef)) over w                                as acc_rnd_sum,
-                     last_value(koef)
-                     over (partition by account_id order by koef
-                         rows between unbounded preceding and unbounded following) as max_koef
+                     row_number() over w                                           as rn
               from training.clearing_account
-              window w as (partition by account_id order by koef))
+              window w as (partition by account_id order by koef)
+              )
 select clearing_account_id,
        koef,
        pre_sum,
        case
-           when koef != max_koef then base.rnd_sum
-           else :qty - lag(base.acc_rnd_sum) over (partition by account_id order by koef) end
+           when rn != (select max(rn) from base) then false else true end base.rnd_sum
+           else :qty - lag(base.acc_rnd_sum) over (partition by account_id order by koef) end,
+       rn
 from base
 
+
+
+with base as (select clearing_account_id as id,
+                     account_id,
+                     koef,
+                     :qty                           as qty,
+                     :qty * koef                    as pre_sum,
+                     round(:qty * koef)             as rnd_sum,
+                     sum(round(:qty * koef)) over w as acc_rnd_sum,
+                     row_number() over w            as rn
+              from training.clearing_account
+              window w as (partition by account_id order by koef, clearing_account_id))
+select id,
+       koef,
+       pre_sum,
+       rnd_sum,
+       case
+           when rn != (select max(rn) from base) then rnd_sum
+           else :qty - lag(base.acc_rnd_sum) over (partition by account_id order by koef) end,
+       rn
+from base
+
+select ceil(10.01)
