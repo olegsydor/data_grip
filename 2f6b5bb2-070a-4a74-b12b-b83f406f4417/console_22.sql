f4a0f46074e5f4fe8ce45da30574be957e70d9be
Oleh Sydor <ole.sydor@gmail.com>
2025-06-06 19:00:08 +0300

    Regular auto-commit at 19:00:08 on 06/06/2025

--- 2f6b5bb2-070a-4a74-b12b-b83f406f4417/console_22.sql
+++ 2f6b5bb2-070a-4a74-b12b-b83f406f4417/console_22.sql
@@ -35,13 +35,13 @@ from base
 
 
 
-with base as (select clearing_account_id as id,
+with base as (select clearing_account_id            as id,
                      account_id,
                      koef,
                      :qty                           as qty,
                      :qty * koef                    as pre_sum,
-                     round(:qty * koef)             as rnd_sum,
-                     sum(round(:qty * koef)) over w as acc_rnd_sum,
+                     floor(:qty * koef)             as rnd_sum,
+                     sum(floor(:qty * koef)) over w as acc_rnd_sum,
                      row_number() over w            as rn
               from training.clearing_account
               window w as (partition by account_id order by koef, clearing_account_id))
@@ -51,8 +51,7 @@ select id,
        rnd_sum,
        case
            when rn != (select max(rn) from base) then rnd_sum
-           else :qty - lag(base.acc_rnd_sum) over (partition by account_id order by koef) end,
+           else :qty - lag(base.acc_rnd_sum)
+                       over (partition by account_id order by koef) end,
        rn
-from base
-
-select ceil(10.01)
+from base;
