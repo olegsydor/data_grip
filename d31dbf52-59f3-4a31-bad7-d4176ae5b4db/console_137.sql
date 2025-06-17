2042069abbbb132f5b06a5aee7b1b507c841ecbb
Oleh Sydor <ole.sydor@gmail.com>
2025-06-17 17:00:09 +0300

    Regular auto-commit at 17:00:09 on 17/06/2025

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_137.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_137.sql
@@ -1,18 +1,78 @@
-select *
-from dwh.client_order cl
-         join dwh.d_instrument di on di.instrument_id = cl.instrument_id
-         join dwh.target_strategy ts on ts.target_strategy_id = cl.sub_strategy_id and ts.is_active
-where true
-  and parent_order_id is not null
-  and create_date_id between 20250407 and 20250410
-  and cl.account_id in (select account_id
+select order_id
+   from data_marts.f_yield_capture fyc
+    inner join dwh.d_target_strategy dts on (dts.target_strategy_id = fyc.parent_sub_strategy_id)
+    join dwh.d_account a on fyc.account_id = a.account_id
+   where  fyc.status_date_id between  :in_start_date_id and :in_end_date_id
+      and parent_order_id is not null
+      and dts.target_strategy_name in ('SENSOR')
+and fyc.account_id in (select account_id
                         from dwh.d_trading_firm tf
                                  join dwh.d_account ac using (trading_firm_id)
                         where trading_firm_name in ('Pleasant Lake Partners', 'Stifel Nicolaus', 'Cowen Prime Services')
                           and ac.is_active
-                          and tf.is_active)
-  and di.instrument_type_id = 'E'
-  and ts.target_strategy_name = 'SENSOR'
+                          and tf.is_active);
 
 
-    select * from dwh.d_target_strategy
+
+select tr.date_id::text,                        -- as trade_dt,
+       oc.opra_symbol,                          -- as symbol,
+       sum(tr.last_qty)::text,                  -- as qty,
+       round(sum(tr.last_qty * tr.last_px) / nullif(sum(tr.last_qty), 0),
+             6)::text,                          -- as price,
+       round(sum(tr.last_qty * 0.075), 6)::text -- as comm
+
+from dwh.flat_trade_record tr
+         inner join dwh.d_account acc on (tr.account_id = acc.account_id and acc.is_active)
+         inner join dwh.d_instrument i on (i.instrument_id = tr.instrument_id)
+         inner join dwh.d_trading_firm tf on (acc.trading_firm_id = tf.trading_firm_id and tf.is_active)
+         left join dwh.d_option_contract oc on (i.instrument_id = oc.instrument_id)
+         left join dwh.d_option_series os on (oc.option_series_id = os.option_series_id)
+where true
+  and tr.date_id between :in_start_date_id and :in_end_date_id
+  and tr.instrument_type_id = 'E'
+  and tr.is_busted = 'N'
+  and acc.account_id in (select account_id
+                         from dwh.d_trading_firm tf
+                                  join dwh.d_account ac using (trading_firm_id)
+                         where
+                             trading_firm_name in ('Pleasant Lake Partners', 'Stifel Nicolaus', 'Cowen Prime Services')
+                           and ac.is_active
+                           and tf.is_active)
+  and tr.order_id in (select order_id
+                      from data_marts.f_yield_capture fyc
+                               inner join dwh.d_target_strategy dts
+                                          on (dts.target_strategy_id = fyc.parent_sub_strategy_id)
+                               join dwh.d_account a on fyc.account_id = a.account_id
+                      where fyc.status_date_id between :in_start_date_id and :in_end_date_id
+                        and parent_order_id is not null
+                        and dts.target_strategy_name in ('SENSOR')
+                        and fyc.account_id in (select account_id
+                                               from dwh.d_trading_firm tf
+                                                        join dwh.d_account ac using (trading_firm_id)
+                                               where trading_firm_name in ('Pleasant Lake Partners', 'Stifel Nicolaus',
+                                                                           'Cowen Prime Services')
+                                                 and ac.is_active
+                                                 and tf.is_active))
+group by tr.open_close, tr.side, tr.date_id, tr.order_id, oc.opra_symbol, tr.order_id
+
+
+
+select order_id, order_qty, order_price
+                      from data_marts.f_yield_capture fyc
+                               inner join dwh.d_target_strategy dts
+                                          on (dts.target_strategy_id = fyc.parent_sub_strategy_id)
+                               join dwh.d_account a on fyc.account_id = a.account_id
+                      where fyc.status_date_id between :in_start_date_id and :in_end_date_id
+                        and parent_order_id is not null
+                        and dts.target_strategy_name in ('SENSOR')
+                        and fyc.account_id in (select account_id
+                                               from dwh.d_trading_firm tf
+                                                        join dwh.d_account ac using (trading_firm_id)
+                                               where trading_firm_name in ('Pleasant Lake Partners', 'Stifel Nicolaus',
+                                                                           'Cowen Prime Services')
+                                                 and ac.is_active
+                                                 and tf.is_active);
+
+
+select * from dwh.client_order
+where order_id = 100000019928855616
