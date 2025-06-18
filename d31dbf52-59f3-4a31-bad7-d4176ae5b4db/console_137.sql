2bc894611caeedf81af4fea4baf6edda1fe8e046
Oleh Sydor <ole.sydor@gmail.com>
2025-06-18 15:00:10 +0300

    Regular auto-commit at 15:00:10 on 18/06/2025

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_137.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_137.sql
@@ -79,34 +79,47 @@ where order_id = 100000019928855616;
 
 
 
-with ord as (select distinct order_id
-             from data_marts.f_yield_capture fyc
-                      inner join dwh.d_target_strategy dts on (dts.target_strategy_id = fyc.parent_sub_strategy_id)
-                      join dwh.d_account a on fyc.account_id = a.account_id
-                      join dwh.l1_snapshot l1 on
-                 (fyc.status_date_id = l1.start_date_id
-                     and fyc.instrument_id = l1.instrument_id
-                     and fyc.routed_time <= l1.transaction_time
-                     and fyc.order_end_time >= l1.transaction_time
-                     and l1.exchange_id = 'NBBO'
-                     and case
-                             when fyc.side in ('1', '3') then l1.ask_price <= fyc.order_price -- buy order is marketable
-                             when fyc.side not in ('1', '3') then l1.bid_price >= fyc.order_price -- sell order is marketable
-                             end)
-             where fyc.status_date_id between :in_start_date_id and :in_end_date_id
-               and l1.start_date_id between :in_start_date_id and :in_end_date_id
-               and parent_order_id is not null
-               and dts.target_strategy_name = 'SENSOR'
-               and fyc.instrument_type_id = 'E'
-               and fyc.account_id in (select account_id
-                                      from dwh.d_trading_firm tf
-                                               join dwh.d_account ac using (trading_firm_id)
-                                      where trading_firm_name in ('Pleasant Lake Partners', 'Stifel Nicolaus',
-                                                                  'Cowen Prime Services')
-                                        and ac.is_active
-                                        and tf.is_active))
-select cl.create_time, cl.order_id, di.instrument_type_id, cl.side, di.symbol, cl.order_qty, cl.price
-from ord
+create table trash.so_equity_trade_file as
+select distinct order_id
+from data_marts.f_yield_capture fyc
+         inner join dwh.d_target_strategy dts on (dts.target_strategy_id = fyc.parent_sub_strategy_id)
+         join dwh.l1_snapshot l1 on
+    (fyc.status_date_id = l1.start_date_id
+        and fyc.instrument_id = l1.instrument_id
+        and fyc.routed_time <= l1.transaction_time
+        and fyc.order_end_time >= l1.transaction_time
+        and l1.exchange_id = 'NBBO'
+        and case
+                when fyc.side in ('1', '3') and l1.ask_price <= fyc.order_price
+                    then true -- buy order is marketable
+                when fyc.side not in ('1', '3') and l1.bid_price >= fyc.order_price
+                    then true -- sell order is marketable
+                else false end
+        )
+where fyc.status_date_id between :in_start_date_id and :in_end_date_id
+  and l1.start_date_id between :in_start_date_id and :in_end_date_id
+  and parent_order_id is not null
+  and dts.target_strategy_name = 'SENSOR'
+  and fyc.instrument_type_id = 'E'
+
+  and fyc.account_id in (select account_id
+                         from dwh.d_trading_firm tf
+                                  join dwh.d_account ac using (trading_firm_id)
+                         where trading_firm_name in
+                               ('Pleasant Lake Partners', 'Stifel Nicolaus', 'Cowen Prime Services')
+                           and ac.is_active
+                           and tf.is_active);
+
+
+
+select to_char(cl.create_time, 'DD/MM/YYYY'),
+       to_char(cl.create_time, 'HH24:MI:SS.MS'),
+       'Equity',
+       case when cl.side in ('1', '3') then 'Buy' else 'Sell' end,
+       di.symbol,
+       cl.order_qty,
+       cl.price
+from trash.so_equity_trade_file ord
          join dwh.client_order cl on cl.order_id = ord.order_id
          join dwh.d_instrument di on di.instrument_id = cl.instrument_id
 where cl.create_date_id between :in_start_date_id and :in_end_date_id
@@ -130,3 +143,6 @@ where tr.account_id in (select account_id
                                         and ac.is_active
                                         and tf.is_active)
 and tr.date_id between :in_start_date_id and :in_end_date_id
+
+
+select relid::regclass, index_relid::regclass, * from pg_stat_progress_create_index;
