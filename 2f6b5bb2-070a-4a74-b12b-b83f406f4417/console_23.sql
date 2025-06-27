17b68553aa384ac919c4b4ff40b9be95ed2dcd1e
Oleh Sydor <ole.sydor@gmail.com>
2025-06-27 19:00:09 +0300

    Regular auto-commit at 19:00:09 on 27/06/2025

--- 2f6b5bb2-070a-4a74-b12b-b83f406f4417/console_23.sql
+++ 2f6b5bb2-070a-4a74-b12b-b83f406f4417/console_23.sql
@@ -13,11 +13,36 @@ FROM Recur
 ORDER BY LEN(Combination), Combination;
 
 
-with recursive orig as (select N, N::text as combine
-                        from (values (1), (2), (3), (4)) as number(n)
-                        union all
-                        select n, combine||', '||n.N
-                        from orig n
-                                 join  r on r.N > n.N
-                        )
-select * from orig
+
+
+create table training.comb (
+    list_id int4,
+    list_sum int4
+);
+insert into training.comb(list_id, list_sum) values (6, 1), (7, 2), (8, 3), (9, 5), (10, 4)
+
+with recursive subset_sum as (
+    select
+        array[list_id] as ids,
+        list_id as max_id,
+        list_sum,
+        1 as depth
+    from training.comb
+
+    union all
+
+    select
+        ss.ids || c.list_id,
+        c.list_id,
+        ss.list_sum + c.list_sum,
+        ss.depth + 1
+    from subset_sum ss
+    join training.comb c
+      on c.list_id > ss.max_id
+    where ss.list_sum + c.list_sum <= :sum
+)
+select *
+from subset_sum
+where list_sum = :sum
+order by depth, ids
+limit 1;
