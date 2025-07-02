29ae9c5da6109f86d3c82ca4ce1490b3251097d0
Oleh Sydor <ole.sydor@gmail.com>
2025-07-02 19:00:09 +0300

    Regular auto-commit at 19:00:09 on 02/07/2025

--- f9447fdb-8def-4b3f-af2b-edc8e5d1d9d5/DS-10060_deploy.sql
+++ f9447fdb-8def-4b3f-af2b-edc8e5d1d9d5/DS-10060_deploy.sql
@@ -34,11 +34,15 @@ comment on column genesis2.clearing_account.default_alloc_ratio is 'Ratio used t
 
 DROP FUNCTION genesis2.auto_allocate_unallocated_trade(bpchar, int4, int4, _int4);
 
-CREATE OR REPLACE FUNCTION genesis2.auto_allocate_unallocated_trade(in_instrument_type_id character, in_allocation_type integer, in_date_id integer DEFAULT get_dateid(CURRENT_DATE), in_account_ids integer[] DEFAULT '{}'::integer[])
- RETURNS integer
- LANGUAGE plpgsql
- SET application_name TO 'ETL:  AutoAllocation'
-AS $function$
+CREATE OR REPLACE FUNCTION genesis2.auto_allocate_unallocated_trade(in_instrument_type_id character,
+                                                                    in_allocation_type integer,
+                                                                    in_date_id integer DEFAULT get_dateid(CURRENT_DATE),
+                                                                    in_account_ids integer[] DEFAULT '{}'::integer[])
+    RETURNS integer
+    LANGUAGE plpgsql
+    SET application_name TO 'ETL:  AutoAllocation'
+AS
+$function$
 --in_allocation_type = 0: options
 --in_allocation_type = 1: equities with ACC.IS_SPECIFIC_ALLOCATED = 'N'
 --in_allocation_type = 2: equities with ACC.IS_SPECIFIC_ALLOCATED = 'Y'
@@ -512,10 +516,17 @@ $function$
 
 -- DROP FUNCTION dash360.bofa_allocation_report_wrapper(int4, int4, text, bool, _int4, bool);
 
-CREATE OR REPLACE FUNCTION dash360.bofa_allocation_report_wrapper(in_start_date_id integer, in_end_date_id integer, in_exec_broker text, in_is_eod boolean DEFAULT false, in_removed_account_ids integer[] DEFAULT '{62939,263022,62810,62887,62923,63787,67949}'::integer[], in_run_intraday_option_auto_allocation boolean DEFAULT true)
- RETURNS TABLE(ret_row text)
- LANGUAGE plpgsql
-AS $function$
+CREATE OR REPLACE FUNCTION dash360.bofa_allocation_report_wrapper(in_start_date_id integer, in_end_date_id integer,
+                                                                  in_exec_broker text, in_is_eod boolean DEFAULT false,
+                                                                  in_removed_account_ids integer[] DEFAULT '{62939,263022,62810,62887,62923,63787,67949}'::integer[],
+                                                                  in_run_intraday_option_auto_allocation boolean DEFAULT true)
+    RETURNS TABLE
+            (
+                ret_row text
+            )
+    LANGUAGE plpgsql
+AS
+$function$
 declare
     l_row_cnt       int;
     l_load_id       int;
@@ -581,184 +592,206 @@ $function$
 ;
 
 
+-- DROP FUNCTION dash360.allocations_set_account_config(int8, text, bpchar, bpchar, int4, bpchar);
 
-select * from genesis2.allocation_instruction_entry
-where account_nickname is not null;
+CREATE OR REPLACE FUNCTION dash360.allocations_set_account_config(in_account_id bigint, in_clearing_accounts text, in_is_auto_allocate character DEFAULT NULL::character(1), in_instrumnt_type_id character DEFAULT 'O'::bpchar, in_user_id integer DEFAULT NULL::integer, in_is_intraday_auto_allocate character DEFAULT NULL::bpchar)
+ RETURNS integer
+ LANGUAGE plpgsql
+ COST 1
+AS $function$
+    -- MG: 20210413 add support to is_option_auto_allocate field
+-- SY: 20240430 https://dashfinancial.atlassian.net/browse/DS-8208 is_visible_for_manual_allocation  and user_id fields have been introduced
+-- OS: 20250604 https://dashfinancial.atlassian.net/browse/DS-10060 added is_intraday_auto_allocate, removed #variable_conflict use_variable
 
+
+declare
+    l_clearing_account_type smallint;
+    l_row_cnt               int;
+    l_clearing_accounts jsonb;
+
+begin
+    l_clearing_accounts := in_clearing_accounts::jsonb;
+    if in_instrumnt_type_id = 'E' and in_is_auto_allocate is not null
+    then
+-- set is_autoallocate value
+        update account acc
+        set is_auto_allocate = in_is_auto_allocate
+        where acc.account_id = in_account_id
+          and acc.is_deleted = 'N'
+          and acc.is_auto_allocate <> in_is_auto_allocate;
+    end if;
+
+    if in_instrumnt_type_id = 'O' and in_is_auto_allocate is not null
+    then
+-- set is_option_auto_allocate value
+        update account acc
+        set is_option_auto_allocate = in_is_auto_allocate
+        where acc.account_id = in_account_id
+          and acc.is_deleted = 'N'
+          and acc.is_option_auto_allocate <> in_is_auto_allocate;
+    end if;
+
+    if in_is_intraday_auto_allocate is not null then
+        update account acc
+        set is_intraday_auto_allocate = in_is_intraday_auto_allocate
+        where acc.account_id = in_account_id
+          and acc.is_deleted = 'N'
+--           and acc.is_option_auto_allocate <> in_is_auto_allocate
+        ;
+    end if;
+
+
+-- close all current configuration for the account if any
+
+    update genesis2.clearing_account
+    set is_deleted  = 'Y',
+        delete_time = clock_timestamp(),
+        user_id     = in_user_id
+    where account_id = in_account_id
+      and market_type = in_instrumnt_type_id
+      and is_deleted = 'N';
+
+    -- get  clearing_account_type
+
+--  select case sum(case instrument_type_id when in_instrumnt_type_id then 1 else 0 end)
+--          when 1 then count(1)
+--          else 2 -- Temporary solution. For some reason account_id could be missed in account2instrument_type
+--          end  as  clearing_account_type
+--  into l_clearing_account_type
+--  from staging.account2instrument_type ait
+--  where account_id = in_account_id
+--    and instrument_type_id  in ('E', 'O');
+
+    -- Temporary logc SY:20201215 as per chat with Tim Miller
+    l_clearing_account_type := 1;
+
+
+    insert into genesis2.clearing_account (account_id, clearing_account_type, clearing_account_number, is_default,
+                                           market_type, is_deleted, cmta, clearing_account_name, occ_actionable_id,
+                                           user_id, is_visible_for_manual_allocation, default_alloc_ratio)
+    select in_account_id,
+           l_clearing_account_type::varchar,
+           sj ->> 'ca_number'             as clearing_account_number,
+           sj ->> 'def'                   as is_default,
+           in_instrumnt_type_id           as market_type,
+           --clock_timestamp() as  create_time,
+           'N'                            as is_deleted,
+           sj ->> 'ca_number'             as cmta,
+           coalesce(sj ->> 'ca_name', '') as clearing_account_name,
+           sj ->> 'oaid'                  as occ_actionable_id,
+           in_user_id,
+           (sj ->> 'visible')::bool       as is_visible_for_manual_allocation,
+           coalesce((sj -> 'def_ratio')::numeric, 1)
+    from (select value as sj
+          from jsonb_array_elements(l_clearing_accounts)) l1;
+
+    GET DIAGNOSTICS l_row_cnt = ROW_COUNT;
+    return l_row_cnt;
+
+end;
+$function$
+;
+
+
+-- DROP FUNCTION dash360.allocations_get_accounts_config(bpchar, _int8);
+
+CREATE OR REPLACE FUNCTION dash360.allocations_get_accounts_config(in_market_type character DEFAULT 'O'::character(1), in_account_ids bigint[] DEFAULT '{}'::bigint[])
+ RETURNS TABLE(account_id bigint, is_auto_allocate character, clearing_accounts jsonb, is_intraday_auto_allocate character)
+ LANGUAGE plpgsql
+ COST 1
+AS $function$
+
+    -- MG: 20210413 -- add is_option_auto_allocate field to output
+-- SY: 20240430 https://dashfinancial.atlassian.net/browse/DS-8208   The is_visible_for_manual_allocation field has been introduced
+-- OS: 20250604 https://dashfinancial.atlassian.net/browse/DS-10060 added is_intraday_auto_allocate, removed #variable_conflict use_variable
+-- OS: 20250626 without ticket added new input parameter in_account_ids (back-end will call this procedure per account instead of cache)
+begin
+    return query
+        select acc.account_id::bigint,
+               (case
+                    when in_market_type = 'E' then acc.is_auto_allocate
+                    when in_market_type = 'O' then acc.is_option_auto_allocate
+                    else acc.is_auto_allocate
+                   end) as is_auto_allocate,
+               jsonb_agg(jsonb_object(array ['ca_number', 'def' , 'ca_name', 'oaid', 'visible', 'def_ratio'],
+                                      array [ca.clearing_account_number, ca.is_default , ca.clearing_account_name, ca.occ_actionable_id, ca.is_visible_for_manual_allocation::text, ca.default_alloc_ratio::text ])),
+               acc.is_intraday_auto_allocate
+        from genesis2.account acc
+                 inner join genesis2.clearing_account ca
+                            on acc.account_id = ca.account_id
+                                and ca.is_deleted = 'N'
+                                and ca.market_type = in_market_type
+        where acc.is_deleted = 'N'
+        and case when coalesce(in_account_ids, '{}') = '{}' then true else acc.account_id = any(in_account_ids) end
+        group by acc.account_id,
+                 (case
+                      when in_market_type = 'E' then acc.is_auto_allocate
+                      when in_market_type = 'O' then acc.is_option_auto_allocate
+                      else acc.is_auto_allocate
+                     end)
+--limit 10
+    ;
+
+end;
+$function$
+;
+
+
+-- DROP FUNCTION dash360.allocations_clone_account_config(int8, _int8, bpchar, int4);
+
+CREATE OR REPLACE FUNCTION dash360.allocations_clone_account_config(in_account_id bigint, clone_account_ids bigint[], in_instrumnt_type_id character DEFAULT 'O'::bpchar, in_user_id integer DEFAULT NULL::integer)
+ RETURNS integer
+ LANGUAGE plpgsql
+ COST 1
+AS $function$
+-- SY 20240826 https://dashfinancial.atlassian.net/browse/DS-8794 Input parameter in_user_id has been added
+-- SY 20240826 https://dashfinancial.atlassian.net/browse/DS-8795 is_visible_for_manual_allocation has been propagated into allocations_set_account_config as key 'visible'
+
+DECLARE
+
+    l_cnt_cln int;
+    /* The in_instrumnt_type_id variable should be implemented like input parameter to the function */
+  --  in_instrumnt_type_id character default 'O';
+
+BEGIN
+
+-- #variable_conflict use_variable
 /*
-select 5 / 2.0
-1. greater %
-2. smaller cmta
-3, primary key
+13-04-2021 - MG - add support to is_option_auto_allocate field
 */
+        select count(1)
+        into l_cnt_cln
+        from (select  dash360.allocations_set_account_config(
+                new_acc_id::bigint,
+                jsonb_agg(jsonb_object(array['ca_number', 'def' , 'ca_name', 'oaid', 'visible'],
+                            array[ca.clearing_account_number, ca.is_default , ca.clearing_account_name, ca.occ_actionable_id, ca.is_visible_for_manual_allocation::char]))::text,
+                (case
+                    when in_instrumnt_type_id = 'E' then acc.is_auto_allocate
+                    when in_instrumnt_type_id = 'O' then acc.is_option_auto_allocate
+                    else acc.is_auto_allocate
+                end),
+                in_instrumnt_type_id,
+				in_user_id,
+                acc.is_intraday_auto_allocate)
+        from clearing_account ca
+        inner join account acc on acc.account_id =ca.account_id and acc.is_deleted='N'
+        cross join unnest(clone_account_ids) new_acc_id
+        where ca.account_id = in_account_id
+        and ca.market_type = in_instrumnt_type_id
+        and ca.is_deleted = 'N'
+        group by new_acc_id,
+                 acc.is_intraday_auto_allocate,
+            (case
+                when in_instrumnt_type_id = 'E' then acc.is_auto_allocate
+                when in_instrumnt_type_id = 'O' then acc.is_option_auto_allocate
+                else acc.is_auto_allocate
+            end)) l ;
 
-1.1 -> 2
 
+       RETURN l_cnt_cln;
 
-0.33 -> 0
-0.33 -> 0
-0.33 -> 1
-
-
-;
-
-  create temp table t_aie as
-  select ai.alloc_instr_id,
-         ca.clearing_account_id,
-         ai.account_id,
-         ai.total_qty * ca.default_alloc_ratio as alloc_qty,
-         :l_date_id                            as date_id,
-         ca.occ_actionable_id
-  from genesis2.allocation_instruction ai
-           inner join genesis2.clearing_account ca on (ca.account_id = ai.account_id and ca.is_deleted = 'N' and
-                                                       ca.market_type = :in_instrument_type_id and ca.is_default = 'Y')
-  where ai.date_id = :l_date_id
-    and ai.dataset_id = :l_load_batch_id
-    and ai.is_deleted = 'N'
-  and ai.account_id = 9908
-  group by alloc_instr_id, total_qty, occ_actionable_id, ca.clearing_account_id
-  order by ca.clearing_account_id, ai.alloc_instr_id;
-
-
-create temp table t_aie as
-with base as (select ai.alloc_instr_id,
---                     ai.dataset_id,
-                     clearing_account_id,
-                     ai.account_id,
-                     ca.default_alloc_ratio,
-                     ai.total_qty                                          as qty,
-                     ai.total_qty * default_alloc_ratio                    as pre_sum,
-                     floor(ai.total_qty * default_alloc_ratio)             as rnd_sum,
-                     sum(floor(ai.total_qty * default_alloc_ratio)) over w as acc_rnd_sum,
-                     row_number() over w                                   as rn
-              from trash.so_allocation_instruction ai
-                       inner join trash.so_clearing_account ca
-                                  on (ca.account_id = ai.account_id and ca.is_deleted = 'N' and
-                                      ca.market_type = :in_instrument_type_id and ca.is_default = 'Y')
-              where ai.date_id = :l_date_id
-                and ai.dataset_id = :l_load_batch_id
-                and ai.is_deleted = 'N'
-              and ai.account_id = 9908
-              group by ai.alloc_instr_id, ai.total_qty, occ_actionable_id, ca.clearing_account_id,ai.account_id, ca.default_alloc_ratio
-              window w as ( partition by ai.alloc_instr_id, ai.account_id order by ca.default_alloc_ratio, ca.clearing_account_id )
---               order by ai.alloc_instr_id, ca.default_alloc_ratio, ca.clearing_account_id
-              )
-select alloc_instr_id,
-       clearing_account_id,
-       account_id,
---        default_alloc_ratio,
---        pre_sum,
-       qty,
-        rnd_sum,
-
-       case
-           when rn != (select max(rn) from base b where b.alloc_instr_id = base.alloc_instr_id) then rnd_sum
-           else qty - lag(base.acc_rnd_sum)
-                      over (partition by alloc_instr_id order by default_alloc_ratio) end as final_qty,
-       rn
-from base
-order by alloc_instr_id, clearing_account_id;
+END;
+$function$
 ;
 
 
--- insert into trash.so_clearing_account
-select ctid, * from trash.so_clearing_account
-where account_id = 9908
-and market_type = 'O'
-and ctid = '(109,35)'in (select  ctid from trash.so_clearing_account
-where account_id = 9908
-and market_type = 'O'
-order by 1 desc
-    limit 1)
-
-
-update trash.so_clearing_account
-set clearing_account_id = 99998
-where true
-  and market_type = 'O'
-  and ctid in (select ctid
-               from trash.so_clearing_account
-               where account_id = 9908
-                 and market_type = 'O'
-               order by 1 desc
-               limit 1)
-
-select *
- into trash.so_clearing_account
- from genesis2.clearing_account
-where is_deleted = 'N'
-  and is_default = 'Y';
-
-select ai.account_id, sum(ca.default_alloc_ratio), array_agg(ca.default_alloc_ratio)
-from genesis2.clearing_account ca
-         join lateral (select account_id
-                       from genesis2.allocation_instruction ai
-                       where true
-                         and ai.date_id = :l_date_id
-                         and ai.dataset_id = :l_load_batch_id
-                         and ai.is_deleted = 'N'
-                         and ca.account_id = ai.account_id
-                         and ca.is_deleted = 'N'
-                         and ca.market_type = :in_instrument_type_id
-                         and ca.is_default = 'Y'
-                       limit 1) ai on true
-group by ai.account_id;
-
-
-select distinct on (account_id) *
-from genesis2.allocation_instruction ai
-         join lateral (select sum(ca.default_alloc_ratio) as sum_ratio
-                       from genesis2.clearing_account ca
-                       where true
-                         and ca.account_id = ai.account_id
-                         and ca.is_deleted = 'N'
-                         and ca.market_type = :in_instrument_type_id
-                         and ca.is_default = 'Y'
-    ) ca on sum_ratio != 1
-where true
-  and ai.date_id = :l_date_id
---   and ai.dataset_id = :l_load_batch_id
-  and ai.is_deleted = 'N'
-
-drop table t_os
-create temp table t_os (ids int[], txt text);
-
-insert into t_os (ids, txt) values ('{1954227,1948382,1948459}'::int[], 'os'), ('{1948460,1947924,6}'::int[], 'so')
-
-select t_os.*--, tr.*
-from trade_record tr
-join lateral (  select unnest(ids), txt from t_os where tr.trade_record_id = any(t_os.ids) ) t_os on true
-
-
-select trade_record_id from trade_record
-where trade_record_id = 1954227
-
-
-with base as (select unnest(ids) as id, txt from t_os)
-select * from base
-join trade_record tr on tr.trade_record_id = base.id
-
-
-*/
-
-SELECT trash.auto_allocate_unallocated_trade(
-  'O'::char,
-  0,
-  20250618,
-  ARRAY[257078]::integer[]
-);
-
-
-select *
-from dash360.bofa_allocation_report_wrapper(in_start_date_id => 20250617, in_end_date_id => 20250618,
-                                            in_is_eod => case when 'No' = 'Yes' then true else false end,
-                                            in_run_intraday_option_auto_allocation => case when 'No' = 'Yes' then true else false end,
-                                            in_exec_broker => '333')
-
-
-
-select *
-from dash360.bofa_allocation_report_wrapper(in_start_date_id => 20250618, in_end_date_id => 20250618,
-                                            in_is_eod => false,
-                                            in_run_intraday_option_auto_allocation => true,
-                                            in_exec_broker => '792');
