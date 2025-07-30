ffdac1d249da5c5822111634c40dd2ddff111226
Oleh Sydor <ole.sydor@gmail.com>
2025-07-30 15:00:10 +0300

    Regular auto-commit at 15:00:10 on 30/07/2025

--- 3c921669-3ceb-41ab-badd-c173da7c7893/console.sql
+++ 3c921669-3ceb-41ab-badd-c173da7c7893/console.sql
@@ -103,30 +103,32 @@ SELECT GENESIS2_JAVA_TEST.get_commission_rate(
 FROM dual;
 
 ALTER FUNCTION GENESIS2_JAVA_TEST.GET_COMMISSION_RATE COMPILE;
-CREATE OR REPLACE FUNCTION GENESIS2_JAVA_TEST.get_commission_rate (
-    in_exchange_id           VARCHAR2,
-    in_symbol                VARCHAR2,
-    in_instrument_type       CHAR,
-    in_account_id            NUMBER,
-    in_trading_session_type  CHAR,
+CREATE OR REPLACE FUNCTION GENESIS2_JAVA_TEST.get_commission_rate(
+    in_exchange_id VARCHAR2,
+    in_symbol VARCHAR2,
+    in_instrument_type CHAR,
+    in_account_id NUMBER,
+    in_trading_session_type CHAR,
     in_price number
 ) RETURN NUMBER
-IS
+    IS
     l_touch_type_id      GENESIS2_JAVA_TEST.EX_DESTINATION_CODE.TOUCH_TYPE_ID%TYPE;
     l_rate               GENESIS2_JAVA_TEST.acct_comm_opt_rate.rate%TYPE;
 
     -- Для збереження списку symbol_list_id
     TYPE t_symbol_list_ids IS TABLE OF GENESIS2_JAVA_TEST.acct_comm_opt_rate.symbol_list_id%TYPE
         INDEX BY PLS_INTEGER;
-    l_symbol_list_id_arr  t_symbol_list_ids;
+    l_symbol_list_id_arr t_symbol_list_ids;
+    l_is_list_found      BOOLEAN := FALSE;
+        l_symbol_list_id_cnt number;
 BEGIN
     -- 1. Визначити TOUCH_TYPE_ID
     SELECT C.TOUCH_TYPE_ID
     INTO l_touch_type_id
     FROM GENESIS2_JAVA_TEST.EXCHANGE E
-         JOIN GENESIS2_JAVA_TEST.EX_DESTINATION D ON D.EXCHANGE_ID = E.EXCHANGE_ID
-         JOIN GENESIS2_JAVA_TEST.EX_DESTINATION_CODE C ON C.EX_DESTINATION_CODE = D.EX_DESTINATION_CODE
-         JOIN GENESIS2_JAVA_TEST.RISK_MGMT_TOUCH_TYPE T ON C.TOUCH_TYPE_ID = T.TOUCH_TYPE_ID
+             JOIN GENESIS2_JAVA_TEST.EX_DESTINATION D ON D.EXCHANGE_ID = E.EXCHANGE_ID
+             JOIN GENESIS2_JAVA_TEST.EX_DESTINATION_CODE C ON C.EX_DESTINATION_CODE = D.EX_DESTINATION_CODE
+             JOIN GENESIS2_JAVA_TEST.RISK_MGMT_TOUCH_TYPE T ON C.TOUCH_TYPE_ID = T.TOUCH_TYPE_ID
     WHERE E.IS_DELETED = 'N'
       AND E.IS_ACTIVE = 'Y'
       AND D.IS_DELETED = 'N'
@@ -135,38 +137,51 @@ BEGIN
       AND ROWNUM = 1;
 
     -- 2. Отримати всі релевантні symbol_list_id
-    SELECT optr.symbol_list_id
-    BULK COLLECT INTO l_symbol_list_id_arr
+    SELECT count(*)
+    INTO l_symbol_list_id_cnt
     FROM GENESIS2_JAVA_TEST.acct_comm_opt_rate optr
-         JOIN GENESIS2_JAVA_TEST.acct_comm_symbol_list sl ON optr.symbol_list_id = sl.symbol_list_id
-         JOIN GENESIS2_JAVA_TEST.symbol2acct_comm_symbol_list asl ON asl.symbol_list_id = sl.symbol_list_id
-         JOIN GENESIS2_JAVA_TEST.tf2acct_comm_symbol_list tfsl ON tfsl.symbol_list_id = sl.symbol_list_id
-         JOIN GENESIS2_JAVA_TEST.trading_firm tf ON tf.trading_firm_id = tfsl.trading_firm_id
-         JOIN GENESIS2_JAVA_TEST.account ac ON ac.trading_firm_id = tf.trading_firm_id
+             JOIN GENESIS2_JAVA_TEST.acct_comm_symbol_list sl ON optr.symbol_list_id = sl.symbol_list_id
+             JOIN GENESIS2_JAVA_TEST.symbol2acct_comm_symbol_list asl ON asl.symbol_list_id = sl.symbol_list_id
+             JOIN GENESIS2_JAVA_TEST.tf2acct_comm_symbol_list tfsl ON tfsl.symbol_list_id = sl.symbol_list_id
+             JOIN GENESIS2_JAVA_TEST.trading_firm tf ON tf.trading_firm_id = tfsl.trading_firm_id
+             JOIN GENESIS2_JAVA_TEST.account ac ON ac.trading_firm_id = tf.trading_firm_id
     WHERE sl.is_deleted = 'N'
       AND asl.symbol = in_symbol
       AND sl.instrument_type_id = in_instrument_type
       AND ac.account_id = in_account_id;
 
+
     -- 3. Знайти rate
     SELECT rate
     INTO l_rate
-    FROM (
-        SELECT optr.rate
-        FROM GENESIS2_JAVA_TEST.acct_comm_opt_rate optr
-             JOIN GENESIS2_JAVA_TEST.acct_comm_opt_premium_tier prt ON prt.tier_id = optr.tier_id
-        WHERE optr.account_id = in_account_id
-          AND optr.touch_type_id = l_touch_type_id
-          AND optr.trading_session_type = in_trading_session_type
---           AND (
---                 (l_symbol_list_id_arr.COUNT > 0 AND optr.symbol_list_id IN (SELECT COLUMN_VALUE FROM TABLE(l_symbol_list_id_arr)))
---                 OR
---                 (l_symbol_list_id_arr.COUNT = 0 AND optr.rate_scope = 'G')
---               )
-        AND optr.rate_scope = 'G'
-        and prt.min_price < in_price
-        ORDER BY prt.min_price DESC
-    )
+    FROM (SELECT optr.rate
+          FROM GENESIS2_JAVA_TEST.acct_comm_opt_rate optr
+                   JOIN GENESIS2_JAVA_TEST.acct_comm_opt_premium_tier prt ON prt.tier_id = optr.tier_id
+          WHERE optr.account_id = in_account_id
+            AND optr.touch_type_id = l_touch_type_id
+            AND optr.trading_session_type = in_trading_session_type
+            AND case
+                    when l_symbol_list_id_cnt > 0 then optr.symbol_list_id IN (SELECT optr.symbol_list_id
+                                                                                     FROM GENESIS2_JAVA_TEST.acct_comm_opt_rate optr
+                                                                                              JOIN GENESIS2_JAVA_TEST.acct_comm_symbol_list sl
+                                                                                                   ON optr.symbol_list_id = sl.symbol_list_id
+                                                                                              JOIN GENESIS2_JAVA_TEST.symbol2acct_comm_symbol_list asl
+                                                                                                   ON asl.symbol_list_id = sl.symbol_list_id
+                                                                                              JOIN GENESIS2_JAVA_TEST.tf2acct_comm_symbol_list tfsl
+                                                                                                   ON tfsl.symbol_list_id = sl.symbol_list_id
+                                                                                              JOIN GENESIS2_JAVA_TEST.trading_firm tf
+                                                                                                   ON tf.trading_firm_id = tfsl.trading_firm_id
+                                                                                              JOIN GENESIS2_JAVA_TEST.account ac
+                                                                                                   ON ac.trading_firm_id = tf.trading_firm_id
+                                                                                     WHERE sl.is_deleted = 'N'
+                                                                                       AND asl.symbol = in_symbol
+                                                                                       AND sl.instrument_type_id = in_instrument_type
+                                                                                       AND ac.account_id = in_account_id)
+                    when l_symbol_list_id_cnt = 0 then optr.rate_scope = 'G'
+                    else 1 = 2 end
+            AND optr.rate_scope = 'G'
+            and prt.min_price < in_price
+          ORDER BY prt.min_price DESC)
     WHERE ROWNUM = 1;
 
     RETURN l_rate;
