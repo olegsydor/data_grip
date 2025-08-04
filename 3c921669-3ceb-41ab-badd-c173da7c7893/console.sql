24b8e5e9f841600e07049e7ebfb05626f862a946
Oleh Sydor <ole.sydor@gmail.com>
2025-08-04 16:00:09 +0300

    Regular auto-commit at 16:00:09 on 04/08/2025

--- 3c921669-3ceb-41ab-badd-c173da7c7893/console.sql
+++ 3c921669-3ceb-41ab-badd-c173da7c7893/console.sql
@@ -110,7 +110,10 @@ CREATE OR REPLACE FUNCTION GENESIS2_QA_20100601.get_commission_rate(
     in_instrument_type CHAR,
     in_account_id NUMBER,
     in_trading_session_type CHAR,
-    in_price number
+    in_price number,
+    in_symbol_suffix varchar2 default null,
+    in_rate_type_id varchar2 default null
+
 ) RETURN NUMBER
     IS
     l_touch_type_id      GENESIS2_QA_20100601.EX_DESTINATION_CODE.TOUCH_TYPE_ID%TYPE;
@@ -149,6 +152,7 @@ BEGIN
              JOIN GENESIS2_QA_20100601.account ac ON ac.trading_firm_id = tf.trading_firm_id
     WHERE sl.is_deleted = 'N'
       AND asl.symbol = in_symbol
+      and case when asl.SYMBOL_SUFFIX is null then 'no_symbol' else asl.SYMBOL_SUFFIX end = in_symbol_suffix
       AND sl.instrument_type_id = in_instrument_type
       AND ac.account_id = in_account_id;
 
@@ -675,4 +679,116 @@ END;
 
 update GENESIS2_QA_20100601.sg_account
 set OPT_CUSTOMER_OR_FIRM = '0'
-where OPT_CUSTOMER_OR_FIRM <> '8'
+where OPT_CUSTOMER_OR_FIRM <> '8';
+
+
+drop FUNCTION GENESIS2_QA_20100601.get_commission_rate;
+CREATE OR REPLACE FUNCTION GENESIS2_QA_20100601.get_commission_rate(
+    in_exchange_id VARCHAR2,
+    in_symbol VARCHAR2,
+    in_instrument_type CHAR,
+    in_account_id NUMBER,
+    in_trading_session_type CHAR,
+    in_price number,
+    in_symbol_suffix varchar2 default null,
+    in_rate_type_id varchar2 default null
+) RETURN NUMBER
+    IS
+    l_touch_type_id      GENESIS2_QA_20100601.EX_DESTINATION_CODE.TOUCH_TYPE_ID%TYPE;
+    l_rate               GENESIS2_QA_20100601.acct_comm_opt_rate.rate%TYPE;
+
+    -- GET symbol_list_id
+    TYPE t_symbol_list_ids IS TABLE OF GENESIS2_QA_20100601.acct_comm_opt_rate.symbol_list_id%TYPE
+        INDEX BY PLS_INTEGER;
+--     l_symbol_list_id_arr t_symbol_list_ids;
+--     l_is_list_found      BOOLEAN := FALSE;
+    l_symbol_list_id_cnt number;
+BEGIN
+    -- 1. GET TOUCH_TYPE_ID
+    SELECT C.TOUCH_TYPE_ID
+    INTO l_touch_type_id
+    FROM GENESIS2_QA_20100601.EXCHANGE E
+             JOIN GENESIS2_QA_20100601.EX_DESTINATION D ON D.EXCHANGE_ID = E.EXCHANGE_ID
+             JOIN GENESIS2_QA_20100601.EX_DESTINATION_CODE C ON C.EX_DESTINATION_CODE = D.EX_DESTINATION_CODE
+             JOIN GENESIS2_QA_20100601.RISK_MGMT_TOUCH_TYPE T ON C.TOUCH_TYPE_ID = T.TOUCH_TYPE_ID
+    WHERE E.IS_DELETED = 'N'
+      AND E.IS_ACTIVE = 'Y'
+      AND D.IS_DELETED = 'N'
+      AND C.IS_DELETED = 'N'
+      AND E.EXCHANGE_ID = in_exchange_id
+      AND ROWNUM = 1;
+
+    -- 2. GET symbol_list_id
+    SELECT count(*)
+    INTO l_symbol_list_id_cnt
+    FROM GENESIS2_QA_20100601.acct_comm_opt_rate optr
+             JOIN GENESIS2_JAVA_TEST.acct_comm_symbol_list sl ON optr.symbol_list_id = sl.symbol_list_id
+             JOIN GENESIS2_QA_20100601.symbol2acct_comm_symbol_list asl ON asl.symbol_list_id = sl.symbol_list_id
+             JOIN GENESIS2_QA_20100601.acct_comm_symbol_list sl ON optr.symbol_list_id = sl.symbol_list_id
+             JOIN GENESIS2_QA_20100601.tf2acct_comm_symbol_list tfsl ON tfsl.symbol_list_id = sl.symbol_list_id
+             JOIN GENESIS2_QA_20100601.trading_firm tf ON tf.trading_firm_id = tfsl.trading_firm_id
+             JOIN GENESIS2_QA_20100601.account ac ON ac.trading_firm_id = tf.trading_firm_id
+    WHERE sl.is_deleted = 'N'
+      AND asl.symbol = in_symbol
+      and case when asl.SYMBOL_SUFFIX is null then 'no_symbol' else asl.SYMBOL_SUFFIX end = in_symbol_suffix
+      AND sl.instrument_type_id = in_instrument_type
+      AND ac.account_id = in_account_id;
+
+
+    -- 3. GET rate
+    if in_instrument_type = 'O' then
+        SELECT rate
+        INTO l_rate
+        FROM (SELECT optr.rate
+              FROM GENESIS2_QA_20100601.acct_comm_opt_rate optr
+                       JOIN GENESIS2_QA_20100601.acct_comm_opt_premium_tier prt ON prt.tier_id = optr.tier_id
+              WHERE optr.account_id = in_account_id
+                AND optr.touch_type_id = l_touch_type_id
+                AND optr.trading_session_type = in_trading_session_type
+                AND case
+                        when l_symbol_list_id_cnt > 0 and optr.symbol_list_id IN (SELECT optr.symbol_list_id
+                                                                                  FROM GENESIS2_QA_20100601.acct_comm_opt_rate optr
+                                                                                           JOIN GENESIS2_QA_20100601.acct_comm_symbol_list sl
+                                                                                                ON optr.symbol_list_id = sl.symbol_list_id
+                                                                                           JOIN GENESIS2_QA_20100601.symbol2acct_comm_symbol_list asl
+                                                                                                ON asl.symbol_list_id = sl.symbol_list_id
+                                                                                           JOIN GENESIS2_QA_20100601.tf2acct_comm_symbol_list tfsl
+                                                                                                ON tfsl.symbol_list_id = sl.symbol_list_id
+                                                                                           JOIN GENESIS2_QA_20100601.trading_firm tf
+                                                                                                ON tf.trading_firm_id = tfsl.trading_firm_id
+                                                                                           JOIN GENESIS2_QA_20100601.account ac
+                                                                                                ON ac.trading_firm_id = tf.trading_firm_id
+                                                                                  WHERE sl.is_deleted = 'N'
+                                                                                    AND asl.symbol = in_symbol
+                                                                                    and case
+                                                                                            when in_symbol_suffix is null
+                                                                                                then 'no_symbol'
+                                                                                            else asl.SYMBOL_SUFFIX end =
+                                                                                        in_symbol_suffix
+                                                                                    AND sl.instrument_type_id = in_instrument_type
+                                                                                    AND ac.account_id = in_account_id)
+                            then 1
+                        when l_symbol_list_id_cnt = 0 and optr.rate_scope = 'G' then 1
+                        else 0 end = 1
+                and prt.min_price < in_price
+              ORDER BY prt.min_price DESC)
+        WHERE ROWNUM = 1;
+    else
+        SELECT rate
+        INTO l_rate
+        FROM (SELECT optr.rate
+              FROM GENESIS2_QA_20100601.acct_comm_eqt_rate optr
+              WHERE optr.account_id = in_account_id
+                AND optr.touch_type_id = l_touch_type_id
+                and optr.rate_scope = 'G')
+        WHERE ROWNUM = 1;
+    end if;
+
+    RETURN l_rate;
+
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        RETURN NULL;
+    WHEN OTHERS THEN
+        RAISE;
+END;
--- f9447fdb-8def-4b3f-af2b-edc8e5d1d9d5/PTM changes.sql
+++ f9447fdb-8def-4b3f-af2b-edc8e5d1d9d5/PTM changes.sql
@@ -93,7 +93,7 @@ from (select null::genesis2.trade_record         as rw,
 -- DROP FUNCTION dash360.ptm_process_trades(int4, int4, jsonb);
 alter FUNCTION dash360.ptm_process_trades rename to ptm_process_trades_old;
 
-CREATE or replace FUNCTION trash.ptm_process_trades(in_date_id integer, in_user_id integer, in_change_vector jsonb)
+CREATE FUNCTION dash360.ptm_process_trades(in_date_id integer, in_user_id integer, in_change_vector jsonb)
  RETURNS bigint[]
  LANGUAGE plpgsql
 AS $function$
@@ -257,3 +257,151 @@ from trash.ptm_process_trades(in_date_id := 20250731, in_user_id := -1, in_chang
     }
   ]
 }')
+
+
+;
+
+
+-- DROP FUNCTION trash.ptm_process_trades(int4, int4, jsonb);
+
+CREATE or replace FUNCTION dash360.ptm_process_trades(in_date_id integer, in_user_id integer, in_change_vector jsonb)
+ RETURNS bigint[]
+ LANGUAGE plpgsql
+AS $function$
+-- OS 20210617 https://dashfinancial.atlassian.net/browse/DS-3519 The function has been rewritten to be able to process more than 1000 trades
+-- SY 20230524 https://dashfinancial.atlassian.net/browse/DS-6777 Performance improvement
+-- SO 20230904 https://dashfinancial.atlassian.net/browse/DS-7206 drop temp tables statments were added to prevent errors run within a single transactiongit
+-- AK 20231009 https://dashfinancial.atlassian.net/browse/DS-7304 added flat_trade_record_inherit_fees subscription creation
+-- OS 20250801 no task so far The fix to prevent creating new trade(s) from originally nusted trade
+declare
+	l_load_batch_id		int4;
+	l_step_id 			int4;
+	l_inserted 			int8[];
+	l_origin			int8[];
+	l_updated 			text;
+	l_subscribed		int4;
+	l_row_cnt			int4;
+    l_busted_trades     text;
+begin
+
+	l_step_id:=0;
+	select nextval('load_batch_load_batch_id_seq')  into l_load_batch_id;
+
+	select genesis2.load_log(l_load_batch_id::int, l_step_id, 'ptm_process_trades STARTED ====='::varchar, 0, 'S'::char)
+	into l_step_id;
+	raise INFO '%: START', clock_timestamp();
+
+    -- OS: this piece of code was added to prevent processing the trade that was already busted
+    drop table if exists t_base;
+    create temp table t_base as
+    select key::bigint as id, jsonb_array_elements(value) as val
+    from jsonb_each(in_change_vector::jsonb);
+
+    select string_agg(distinct tr.trade_record_id::text, ', ')
+    into l_busted_trades
+    from genesis2.trade_record tr
+             join t_base on t_base.id = tr.trade_record_id
+    where tr.date_id = in_date_id
+      and tr.is_busted = 'Y';
+
+    if l_busted_trades is not null then
+        raise exception 'trade_records % was\were busted before', l_busted_trades using ERRCODE = 'PTMNC';
+    end if;
+    -- end (OS)
+
+   drop table if exists whole;
+   create temp table whole on commit drop as
+ 	with nuls as
+   ( select y.id, jsonb_object_agg(y.key, null) as val from (select id, (jsonb_each_text(val)).*
+	 from t_base base) y
+	 where value = 'NULL'
+	 group by y.id)
+
+	 select base.id, base.val||
+	  jsonb_build_object(
+		'trade_record_id', nextval('trade_record_trade_record_id_seq'),
+		'db_create_time', clock_timestamp(),
+		'orig_trade_record_id', base.id,
+		'load_batch_id', l_load_batch_id,
+		'user_id', in_user_id)||
+		coalesce(nuls.val,'{}') as jsn
+	from t_base base
+	left join nuls on nuls.id = base.id;
+
+	GET DIAGNOSTICS l_row_cnt = ROW_COUNT;
+	raise INFO '%: create temp table whole % rows processed', clock_timestamp(), l_row_cnt;
+
+ drop table if exists slct;
+create temp table slct on commit drop as
+select (jsonb_populate_record(rw, jsn)).*
+from (select null::genesis2.trade_record         as rw,
+             row_to_json(tr)::jsonb || whole.jsn as jsn -- CONCAT OLD trade_record with new values
+      from whole
+               left join genesis2.trade_record tr on (tr.trade_record_id = whole.id and tr.date_id = in_date_id)) slct;
+
+GET DIAGNOSTICS l_row_cnt = ROW_COUNT;
+	raise INFO '%: create temp table slct % rows processed', clock_timestamp(), l_row_cnt;
+
+    insert into genesis2.trade_record
+    select * from slct;
+
+  	GET DIAGNOSTICS l_row_cnt = ROW_COUNT;
+	raise INFO '%:  insert into genesis2.trade_record % rows processed', clock_timestamp(), l_row_cnt;
+
+  select array_agg(slct.trade_record_id), array_agg(slct.orig_trade_record_id)
+    from slct into l_inserted, l_origin;
+
+
+   with upd as (
+	update genesis2.trade_record
+	set is_busted = 'Y'
+	where date_id = in_date_id
+		and trade_record_id = any(l_origin)
+		and is_busted = 'N'
+	returning trade_record_id as ids
+	)
+	select string_agg(ids::text, ', '),count(1)
+	from upd
+	into l_updated, l_row_cnt;
+
+	select genesis2.load_log(l_load_batch_id::int, l_step_id, 'Busted old trades: '||left(l_updated, 150), l_row_cnt, 'I')
+	into l_step_id;
+--	raise notice 'Updated: %', l_updated;
+
+	select count(1) from (
+		select genesis2.etl_subscribe(
+			in_load_batch_id 		=> t.ids::int8,
+			in_row_cnt 				=> 1,
+			in_subscription_name 	=> 'big_data.flat_trade_record',
+			in_source_table_name 	=> 'TRADE_RECORD.MANUAL_BUST',
+			in_date_id 				=> in_date_id)
+		from (select unnest(l_origin) as ids) t ) x
+	into l_subscribed;
+
+	select genesis2.load_log(l_load_batch_id::int, l_step_id, 'TRADE_RECORD.MANUAL_BUST Subscribed: ', l_subscribed, 'I')
+	into l_step_id;
+
+	select count(1) from (
+		select genesis2.etl_subscribe(
+			in_load_batch_id 		=> t.ids::int8,
+			in_row_cnt 				=> 1,
+			in_subscription_name 	=> 'flat_trade_record_inherit_fees',
+			in_source_table_name 	=> 'TRADE_LEVEL_BOOK_RECORD',
+			in_date_id 				=> in_date_id)
+		from (select unnest(l_inserted) as ids) t ) x
+	into l_subscribed;
+
+
+	select genesis2.load_log(l_load_batch_id::int, l_step_id, 'flat_trade_record_inherit_fees Subscribed: ', l_subscribed, 'I')
+	into l_step_id;
+--	raise notice 'Subscribed: %', l_subscribed;
+
+	select genesis2.load_log(l_load_batch_id::int, l_step_id, 'ptm_process_trades DONE ', 0, 'E')
+	into l_step_id;
+
+
+	return l_inserted;
+
+end;
+$function$
+;
