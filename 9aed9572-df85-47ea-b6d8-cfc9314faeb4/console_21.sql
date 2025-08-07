009009edcc0df596bcbed429773983adef0efc85
Oleh Sydor <ole.sydor@gmail.com>
2025-08-07 12:00:10 +0300

    Regular auto-commit at 12:00:10 on 07/08/2025

--- 9aed9572-df85-47ea-b6d8-cfc9314faeb4/console_21.sql
+++ 9aed9572-df85-47ea-b6d8-cfc9314faeb4/console_21.sql
@@ -1,5 +1,5 @@
 select record_type, order_id, chain_id, order_request_type, payload, md_payload
-from (with chained_orders as (select distinct chain_order_id
+from (with chained_orders as (select distinct chain_order_id, cl_ord_id
                               from blaze7.blaze7.client_order co
                               where co.order_id = 806944338171936768)
       select 0                                 as record_type,
@@ -10,7 +10,9 @@ from (with chained_orders as (select distinct chain_order_id
              null                              as md_payload
       from chained_orders cho
                inner join blaze7.blaze7.client_order co
-                          on co.chain_order_id = cho.chain_order_id and co.record_type = '0'
+                          on co.chain_order_id = cho.chain_order_id
+                                 and co.cl_ord_id = cho.cl_ord_id
+                                 and co.record_type = '0'
       union
       select 1                                  as record_type,
              orp.order_id,
@@ -20,7 +22,7 @@ from (with chained_orders as (select distinct chain_order_id
              mdr.payload::text                  as md_payload
       from chained_orders cho
                inner join blaze7.blaze7.client_order co
-                          on co.chain_order_id = cho.chain_order_id and co.record_type = '0'
+                          on co.chain_order_id = cho.chain_order_id and co.record_type = '0' and co.cl_ord_id = cho.cl_ord_id
                inner join blaze7.blaze7.order_report orp
                           on orp.order_id = co.order_id and orp.chain_id = co.chain_id and
                              orp.multileg_reporting_type in ('1', '2')
