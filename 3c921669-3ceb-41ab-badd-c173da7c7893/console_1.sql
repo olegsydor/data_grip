1750abeed17372cffd5629a51e2d2b9db7e004ae
Oleh Sydor <ole.sydor@gmail.com>
2025-08-08 16:24:05 +0300

    Regular auto-commit at 16:24:05 on 08/08/2025

--- 3c921669-3ceb-41ab-badd-c173da7c7893/console_1.sql
+++ 3c921669-3ceb-41ab-badd-c173da7c7893/console_1.sql
@@ -1,92 +1,102 @@
-CREATE OR REPLACE FUNCTION GENESIS2_QA_20100601.get_commission_rate_(
-    in_exchange_id VARCHAR2,
-    in_symbol VARCHAR2,
-    in_instrument_type CHAR,
+select GENESIS2_QA_20100601.get_sg_account(263201, 'O', '733', 'CDOE')
+from dual;
+
+
+CREATE OR REPLACE FUNCTION GENESIS2_QA_20100601.get_sg_account(
     in_account_id NUMBER,
-    in_trading_session_type CHAR,
-    in_price number,
-    in_symbol_suffix varchar2 default null,
-    in_rate_type_id varchar2 default null
-) RETURN NUMBER
-    IS
-    l_touch_type_id      GENESIS2_QA_20100601.EX_DESTINATION_CODE.TOUCH_TYPE_ID%TYPE;
-    l_rate               GENESIS2_QA_20100601.acct_comm_opt_rate.rate%TYPE;
+    in_instrument_type CHAR,
+    in_giveup VARCHAR2,
+    in_exchange_id VARCHAR2
+) RETURN varchar2
+    is
 
-    -- GET symbol_list_id
-    TYPE t_symbol_list_ids IS TABLE OF GENESIS2_QA_20100601.acct_comm_opt_rate.symbol_list_id%TYPE
-        INDEX BY PLS_INTEGER;
-    l_symbol_list_id_cnt number;
-BEGIN
-    -- 1. GET TOUCH_TYPE_ID
-    SELECT C.TOUCH_TYPE_ID
-    INTO l_touch_type_id
-    FROM GENESIS2_QA_20100601.EXCHANGE E
-             JOIN GENESIS2_QA_20100601.EX_DESTINATION D ON D.EXCHANGE_ID = E.EXCHANGE_ID
-             JOIN GENESIS2_QA_20100601.EX_DESTINATION_CODE C ON C.EX_DESTINATION_CODE = D.EX_DESTINATION_CODE
-             JOIN GENESIS2_QA_20100601.RISK_MGMT_TOUCH_TYPE T ON C.TOUCH_TYPE_ID = T.TOUCH_TYPE_ID
-    WHERE E.IS_DELETED = 'N'
-      AND E.IS_ACTIVE = 'Y'
-      AND D.IS_DELETED = 'N'
-      AND C.IS_DELETED = 'N'
-      AND E.EXCHANGE_ID = in_exchange_id
-      AND ROWNUM = 1;
+    l_opt_customer_or_firm   char;
+    l_opt_is_fix_execbrok_pr char;
+    l_opt_is_fix_clfirm_pr   char;
+    l_opt_is_fix_custfirm_pr char;
+    l_trading_firm_id        varchar2(9);
+    l_eq_mpid                varchar2(4);
+    l_opt_occ_id             varchar2(40);
+    l_exec_broker            varchar2(40);
+    l_clearing_firm          varchar2(40);
+    l_sg_sub_account         varchar2(10);
+    l_sg_mint_account        varchar2(20);
+    l_sg_sales_trader_id     varchar2(20);
+    l_opt_clearing_firm      varchar2(20);
+    l_client_cust_or_firm    varchar2(20);
+    l_opt_exec_broker        varchar2(20);
+    l_return                 varchar2(1500);
 
-    -- 2. GET symbol_list_id
-    -- 3. GET rate
-    if in_instrument_type = 'O' then
-        with cte_symbol_list as
-                 (select optr.symbol_list_id
-                  FROM GENESIS2_QA_20100601.acct_comm_opt_rate optr
-                           JOIN GENESIS2_JAVA_TEST.acct_comm_symbol_list sl ON optr.symbol_list_id = sl.symbol_list_id
-                           JOIN GENESIS2_QA_20100601.symbol2acct_comm_symbol_list asl
-                                ON asl.symbol_list_id = sl.symbol_list_id
-                           JOIN GENESIS2_QA_20100601.acct_comm_symbol_list sl ON optr.symbol_list_id = sl.symbol_list_id
-                           JOIN GENESIS2_QA_20100601.tf2acct_comm_symbol_list tfsl
-                                ON tfsl.symbol_list_id = sl.symbol_list_id
-                           JOIN GENESIS2_QA_20100601.trading_firm tf ON tf.trading_firm_id = tfsl.trading_firm_id
-                           JOIN GENESIS2_QA_20100601.account ac ON ac.trading_firm_id = tf.trading_firm_id
-                  WHERE sl.is_deleted = 'N'
-                    AND asl.symbol = in_symbol
-                    and case when in_symbol_suffix is null then asl.SYMBOL_SUFFIX else in_symbol_suffix end =
-                        asl.SYMBOL_SUFFIX
-                    AND sl.instrument_type_id = in_instrument_type
-                    AND ac.account_id = in_account_id)
-        SELECT rate
-        INTO l_rate
-        FROM (SELECT optr.rate
-              FROM GENESIS2_QA_20100601.acct_comm_opt_rate optr
-                       JOIN GENESIS2_QA_20100601.acct_comm_opt_premium_tier prt ON prt.tier_id = optr.tier_id
-              WHERE optr.account_id = in_account_id
-                AND optr.touch_type_id = l_touch_type_id
-                AND optr.trading_session_type = in_trading_session_type
-                and
-                  case when in_rate_type_id is null then optr.RATE_TYPE_ID else in_rate_type_id end = optr.RATE_TYPE_ID
-                AND case
-                        when (select count(*) from cte_symbol_list) > 0 and optr.symbol_list_id IN
-                                                                            (SELECT optr.symbol_list_id
-                                                                             FROM cte_symbol_list)
-                            then 1
-                        when (select count(*) from cte_symbol_list) = 0 and optr.rate_scope = 'G' then 1
-                        else 0 end = 1
-                and prt.min_price < in_price
-              ORDER BY prt.min_price DESC)
-        WHERE ROWNUM = 1;
-    else
-        SELECT rate
-        INTO l_rate
-        FROM (SELECT optr.rate
-              FROM GENESIS2_QA_20100601.acct_comm_eqt_rate optr
-              WHERE optr.account_id = in_account_id
-                AND optr.touch_type_id = l_touch_type_id
-                and optr.rate_scope = 'G')
-        WHERE ROWNUM = 1;
+    BEGIN
+    SELECT
+        -- acc.ACCOUNT_NAME accountName,
+        acc.OPT_CUSTOMER_OR_FIRM,
+        acc.OPT_IS_FIX_EXECBROK_PROCESSED,
+        acc.OPT_IS_FIX_CLFIRM_PROCESSED,
+        acc.OPT_IS_FIX_CUSTFIRM_PROCESSED,
+        acc.TRADING_FIRM_ID,
+        acc.eq_mpid,
+        acc.opt_occ_id
+    into l_opt_customer_or_firm, l_opt_is_fix_execbrok_pr, l_opt_is_fix_clfirm_pr, l_opt_is_fix_custfirm_pr, l_trading_firm_id,
+        l_eq_mpid, l_opt_occ_id
+    FROM GENESIS2_QA_20100601.aCCOUNT acc
+    WHERE 1 = 1
+      and acc.ACCOUNT_ID = in_account_id;
+
+    -- ExecBroker(76)
+    IF l_opt_is_fix_execbrok_pr = 'N' THEN
+        l_exec_broker := 'new\old model'; --return defaultOptExecBrokerId as OPT_EXEC_BROKER.  (!) new or old model
+    ELSE
+        l_exec_broker := 'null';
+        /* BE will handle by flag */
     end if;
 
-    RETURN l_rate;
+-- II. CustomerOrFirm(204)
+    IF l_opt_is_fix_clfirm_pr != 'N' THEN
+        l_opt_is_fix_custfirm_pr := 'null';
+    end if;
 
-EXCEPTION
+-- III. ClearingFirm(439)
+    IF in_instrument_type = 'E' or l_opt_is_fix_clfirm_pr != 'N' then
+        l_clearing_firm := null;
+    else
+        l_clearing_firm := 'find exact value';
+    end if;
+
+    --     IV. ActionableID(10440) -- Return acc.opt_occ_id
+
+
+-- V. SG Account fields
+    select coalesce(ch.SG_SUB_ACCOUNT, pa.SG_SUB_ACCOUNT),
+           coalesce(ch.SG_MINT_ACCOUNT, pa.SG_SUB_ACCOUNT),
+           coalesce(ch.SG_SALES_TRADER_ID, pa.SG_SUB_ACCOUNT)
+    into l_sg_sub_account, l_sg_mint_account, l_sg_sales_trader_id
+    from GENESIS2_QA_20100601.SG_ACCOUNT ch
+             left join GENESIS2_QA_20100601.SG_ACCOUNT pa on pa.ACCOUNT_ID = ch.SG_PARENT_ACCOUNT_ID
+    where ch.ACCOUNT_ID = in_account_id;
+
+    select '{' ||
+           '"OPT_IS_FIX_CLFIRM_PROCESSED": "'|| l_opt_is_fix_clfirm_pr || '",' ||
+           '"OPT_CLEARING_FIRM": "'|| l_opt_clearing_firm || '",' ||
+           '"OPT_IS_FIX_CUSTFIRM_PROCESSED": "'|| l_opt_is_fix_custfirm_pr || '",' ||
+           '"OPT_CUST_OR_FIRM": "'|| l_opt_customer_or_firm || '",' ||
+           '"CLIENT_CUST_OR_FIRM": "'|| l_client_cust_or_firm || '",' ||
+           '"OPT_IS_FIX_EXECBROK_PROCESSED": "'|| l_opt_is_fix_execbrok_pr || '",' ||
+           '"OPT_EXEC_BROKER": "'|| l_opt_exec_broker || '",' ||
+           '"OPT_OCC_ID": "'|| l_opt_occ_id || '",' ||
+           '"SG_SUB_ACCOUNT": "'|| l_sg_sub_account || '",' ||
+           '"SG_MINT_ACCOUNT": "'|| l_sg_mint_account || '",' ||
+           '"SG_SALES_TRADER_ID": "'|| l_sg_sales_trader_id|| '"}'
+    into l_return
+    from dual;
+
+    return l_return;
+
+    EXCEPTION
     WHEN NO_DATA_FOUND THEN
         RETURN NULL;
     WHEN OTHERS THEN
         RAISE;
 END;
+
+    {"OPT_IS_FIX_CLFIRM_PROCESSED": N,"OPT_CLEARING_FIRM": ,"OPT_IS_FIX_CUSTFIRM_PROCESSED": Y,"OPT_CUST_OR_FIRM": ,"CLIENT_CUST_OR_FIRM": ,"OPT_IS_FIX_EXECBROK_PROCESSED": N,"OPT_EXEC_BROKER": ,"OPT_OCC_ID": ,"SG_SUB_ACCOUNT": ,"SG_MINT_ACCOUNT": ,"SG_SALES_TRADER_ID": lee.cashin}{"OPT_IS_FIX_CLFIRM_PROCESSED": N,"OPT_CLEARING_FIRM": ,"OPT_IS_FIX_CUSTFIRM_PROCESSED": Y,"OPT_CUST_OR_FIRM": ,"CLIENT_CUST_OR_FIRM": ,"OPT_IS_FIX_EXECBROK_PROCESSED": N,"OPT_EXEC_BROKER": ,"OPT_OCC_ID": ,"SG_SUB_ACCOUNT": ,"SG_MINT_ACCOUNT": ,"SG_SALES_TRADER_ID": lee.cashin}
