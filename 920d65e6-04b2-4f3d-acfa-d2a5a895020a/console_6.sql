010d01ffa6b7675d38c67a9acb7cc890fd7373f2
Oleh Sydor <ole.sydor@gmail.com>
2025-08-26 13:00:11 +0300

    Regular auto-commit at 13:00:11 on 26/08/2025

---
+++ 920d65e6-04b2-4f3d-acfa-d2a5a895020a/console_6.sql
@@ -0,0 +1,39 @@
+create temp table t_alias as
+select co.cl_ord_id, account_alias
+from blaze7.client_order co
+         join lateral (select leg.order_id, leg.chain_id
+                       from blaze7.client_order_leg leg
+                       join blaze7.client_order cl on leg.order_id = cl.order_id and leg.chain_id = cl.chain_id
+                       where true
+                         and leg.payload ->> 'StitchedSingleOrderId' = co.order_id::text
+                       and cl.db_create_time >= co.db_create_time
+                       limit 1) leg on true
+         join lateral (select cl.payload->> 'AccountAlias' as account_alias
+                       from blaze7.client_order cl
+                       where cl.order_id = leg.order_id and cl.chain_id = leg.chain_id
+                       limit 1) cl on true
+where co.payload ->> 'OrderClass' = 'F'
+  and co.payload ->> 'HasStitchedOrders' = 'Y'
+  and co.db_create_time::date = '2025-08-25'
+  and co.cl_ord_id in ('f_0_3q250821', 'f_0_3m250821');
+
+
+
+select order_id, payload->>'OrderClass', payload->>'HasStitchedOrders',*
+from blaze7.client_order co
+where co.cl_ord_id in ('f_0_3q250821','f_0_3m250821');
+
+
+select payload->> 'StitchedSingleOrderId', *
+from blaze7.client_order_leg
+where payload->> 'StitchedSingleOrderId' is not null
+and payload->> 'StitchedSingleOrderId' in ('813044674737471488', '813044804358242304')
+
+select order_id, payload->>'OrderClass', payload->>'HasStitchedOrders', co.payload ->> 'AccountAlias', *
+from blaze7.client_order co
+where order_id in (813044874646388736, 813045540504731648)
+/*
+ if blaze7.client_order.order_class = 'F' and payload ->> 'HasStitchedOrders' = 'Y', then go to:
+blaze7.client_order_leg find a leg that has payload.StitchedSingleOrderId = order_id of the order in p.1, if found, then:
+go to blaze7.client_order -> payload -> AccountAlias (or OriginatorOrder.AccountAlias) for the order found on step 2
+*/
