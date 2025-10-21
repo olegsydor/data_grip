8844715d407b2ed81f2532f6dc0664d5408080c6
Oleh Sydor <ole.sydor@gmail.com>
2025-10-21 15:00:09 +0300

    Regular auto-commit at 15:00:08 on 21/10/2025

--- ceda381c-d24e-497a-a466-77ccbfbf2023/console_1.sql
+++ ceda381c-d24e-497a-a466-77ccbfbf2023/console_1.sql
@@ -1,9 +1,9 @@
 select *
 from monitoring.error_tracking
 where true
---    and regexp_replace(error_text, '\\', '') ilike '%insert or update on table "f_yield_capture_20250624%'
---         and regexp_replace(error_text, '\\', '') ilike '%l1_snapshot%'
---   and db_host = 'pgtest1.uat.dashops.net'
-and db_create_time::date = '2025-10-10'
+    and regexp_replace(error_text, '\\', '') ilike '%bigdatatail2%'
+--          and regexp_replace(error_text, '\\', '') ilike '%l1_snapshot%'
+and db_host = 'pgbigdata1.dashops.net'
+and db_create_time::date = '2025-10-21'
 -- and db_process_time is null
 order by error_tracking_id desc
