f922000149f0da14f1608aa7eb774c1c4d88b925
Oleh Sydor <ole.sydor@gmail.com>
2025-10-31 13:00:10 +0200

    Regular auto-commit at 13:00:10 on 31/10/2025

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/DEVREQ-6896.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/DEVREQ-6896.sql
@@ -884,3 +884,18 @@ end;
 $function$
 ;
 select 1/0
+
+
+-- active processes
+select * from (
+ select pid, state, application_name, user, wait_event, query_start::timestamp as query_start, age(clock_timestamp(), query_start) as age, usename, query, state
+	from pg_stat_activity
+	where true
+	and state in ('active', 'idle in transaction')
+	and query not ilike '%pg_stat_activity%'
+--	and query not ilike '%vacuum%'
+	and query not ilike '%replicat%'
+--	and query ilike '%report_rps_ofp0016_gtc%'
+	union all
+	select null, null, null as application_name, null, null, null as query_start, null as age, null, null, null) x
+	order by case when coalesce(application_name, 'Sydor') ilike '%Sydor%' then 0 else 1 end,  query_start nulls last;
