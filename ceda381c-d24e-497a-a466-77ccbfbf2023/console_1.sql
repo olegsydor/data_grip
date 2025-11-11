578802d995a111597473cad0c06ca2912b525243
Oleh Sydor <ole.sydor@gmail.com>
2025-11-11 13:00:08 +0200

    Regular auto-commit at 13:00:08 on 11/11/2025

--- ceda381c-d24e-497a-a466-77ccbfbf2023/console_1.sql
+++ ceda381c-d24e-497a-a466-77ccbfbf2023/console_1.sql
@@ -4,7 +4,7 @@ with base as (select monitoring.clean_text(error_text) as jsn, *
 --     and regexp_replace(error_text, '\\', '') ilike '%bigdatatail2%'
 --          and regexp_replace(error_text, '\\', '') ilike '%l1_snapshot%'
                 and db_host = 'pgtest1.uat.dashops.net'
-                and db_create_time::date > '2025-11-01'
+                and db_create_time::date > '2025-11-10'
 --                 and error_text ilike '%37003769%'
               )
 select jsn ->> 'ERROR'     as error,
