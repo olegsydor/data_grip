73f56414083a3edd84114e81bced502742ddddcf
Oleh Sydor <ole.sydor@gmail.com>
2025-11-24 19:00:10 +0200

    Regular auto-commit at 19:00:10 on 24/11/2025

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/DEVREQ-7157.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/DEVREQ-7157.sql
@@ -2,7 +2,7 @@ select 13 % 12
 
 
 -- DROP FUNCTION dash360.report_surveillance_socgen_parent_order_count(int4, int4, varchar);
-select * from trash.report_surveillance_socgen_parent_order_count(20251001, 20251031);
+select * from dash360.report_surveillance_socgen_parent_order_count(20251001, 20251031);
 
 select * from trash.report_surveillance_socgen_parent_order_count(20251120, 20251120, in_account_ids := '{56592,59790}');
 drop function trash.report_surveillance_socgen_parent_order_count;
@@ -271,3 +271,82 @@ select * from t_ordinary
                                       and tex.customer_or_firm_name = tor.customer_or_firm_name
                                     group by tex.status_date, tex.account_name, tex.customer_or_firm_name
                                     limit 1) tex on true;
+
+
+
+create or replace function dash360.report_surveillance_socgen_parent_order_count(in_start_date_id integer DEFAULT get_dateid((date_trunc('month'::text, (CURRENT_DATE)::timestamp with time zone))::date),
+                                                                               in_end_date_id integer DEFAULT get_dateid(CURRENT_DATE),
+                                                                               in_instrument_type character varying DEFAULT NULL::character varying)
+    RETURNS TABLE
+            (
+                "row" text
+            )
+    LANGUAGE plpgsql
+AS
+$function$
+
+declare
+    row_cnt       int4;
+    l_load_id     int;
+    l_step_id     int;
+    l_account_ids int4[];
+    l_leg_count   int4 := 8;
+begin
+    select nextval('public.load_timing_seq') into l_load_id;
+    l_step_id := 1;
+
+    select public.load_log(l_load_id, l_step_id,
+                           'report_surveillance_socgen01_parent_order_count for ' || in_start_date_id::text || '-' ||
+                           in_end_date_id::text || ' STARTED===',
+                           0, 'O')
+    into l_step_id;
+    select array_agg(account_id)
+    into l_account_ids
+    from dwh.d_account ac
+    where true
+      and trading_firm_id in ('socgenpsc', 'socgeneqd');
+
+    return query
+        select 'Period,Account,Capacity,Qty,Parent Order Count';
+
+    return query
+        with cte as (select to_char("StatusDate", 'YYYY-MM-DD')             as status_date,
+                            a.account_name,
+                            hods."ClOrdID"                                  as client_order_id,
+                            cf.customer_or_firm_name,
+                            sum(coalesce(hods."CumQty", 0))                 as cum_qty,
+                            1::int                                          as cnt,
+                            count(distinct hods."DisplayInstrumentID")::int as cnt_leg
+                     from dwh.historic_order_details_storage hods
+                              join dwh.d_account a on a.account_id = hods."AccountID"
+                              left join dwh.d_customer_or_firm cf on (cf.customer_or_firm_id = hods."CustomerOrFirm")
+                     where hods."Status_Date_id" between in_start_date_id and in_end_date_id
+                       and case
+                               when in_instrument_type is null then true
+                               else hods."InstrumentType" = in_instrument_type end
+                       and hods."CustomerOrderID" is null
+                       and hods."AccountID" = any (l_account_ids)
+                     group by status_date, account_name, client_order_id, customer_or_firm_name)
+        select array_to_string(array [
+                                   c.status_date,
+                                   c.account_name,
+                                   c.customer_or_firm_name,
+                                   sum(c.cum_qty)::text,
+                                   (sum(case
+                                            when c.cnt_leg > l_leg_count then c.cnt_leg
+                                            else c.cnt
+                                       end))::text
+                                   ], ',', '')
+        from cte c
+        group by c.status_date, c.account_name, c.customer_or_firm_name;
+    get diagnostics row_cnt = row_count;
+
+    select public.load_log(l_load_id, l_step_id,
+                           'report_surveillance_socgen01_parent_order_count for ' || in_start_date_id::text || '-' ||
+                           in_end_date_id::text || ' FINISHED===',
+                           row_cnt, 'O')
+    into l_step_id;
+
+end;
+$function$
+;
