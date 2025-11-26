a1bc63edbbd7be753404f579ba76bbd4809e0377
Oleh Sydor <ole.sydor@gmail.com>
2025-11-26 20:31:37 +0200

    Regular auto-commit at 20:31:37 on 26/11/2025

--- ceda381c-d24e-497a-a466-77ccbfbf2023/console_2.sql
+++ ceda381c-d24e-497a-a466-77ccbfbf2023/console_2.sql
@@ -1,5 +1,6 @@
 select * from dq.get_unmatched_orders(20251126,20251126,true);
 
+dq.load_internal_dc_client_order_matching();
 
 select * from (
  select pid, state, application_name, user, wait_event, query_start::timestamp as query_start, age(clock_timestamp(), query_start) as age, usename, query, state
@@ -14,3 +15,5 @@ select * from (
 	select null, null, null as application_name, null, null, null as query_start, null as age, null, null, null) x
 	order by case when coalesce(application_name, 'Sydor') ilike '%Sydor%' then 0 else 1 end,  query_start nulls last;
 
+
+select * from dq.get_unmatched_orders(20251126,20251126,false);
