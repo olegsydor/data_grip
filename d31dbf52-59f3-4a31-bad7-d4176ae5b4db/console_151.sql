bb92f1dd0dbe1bdcd228393fed22d5c5ae2f28fc
Oleh Sydor <ole.sydor@gmail.com>
2025-12-02 18:00:15 +0200

    Regular auto-commit at 18:00:15 on 02/12/2025

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_151.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_151.sql
@@ -5,47 +5,49 @@ limit 1
 
 
 
--- DROP FUNCTION staging.zabbix_monitor_gtc_last_load();
-select * from staging.zabbix_monitor_f_parent_order()
+-- DROP FUNCTION staging.zabbix_monitor_f_parent_order();
+select * from staging.zabbix_monitor_f_parent_order();
 
-create or replace function staging.zabbix_monitor_f_parent_order()
+create or replace function staging.zabbix_monitor_f_parent_order(in_date_id int4 default to_char(current_date, 'YYYYMMDD')::int4)
     returns integer
     language plpgsql
 as
 $function$
-    -- 20251202 SO
+    -- 20251202 SO https://dashfinancial.atlassian.net/browse/DS-10809
 declare
-    l_min_date          int4 := to_char(public.get_business_date_back(current_date, 2), 'YYYYMMDD')::int;
-    l_exec_time         timestamp;
-    l_subscription_time timestamp;
-    l_diff              numeric;
-    l_allow_time        int4 := 600;
+    l_min_date              int4 := in_date_id;
+    l_max_subscription_time timestamp;
+    l_min_subscription_time timestamp;
+    l_diff                  numeric;
+    l_allow_time            int4 := 600;
 begin
-    -- getting the last time from loaded execution
-    select exec_time
-    into l_exec_time
-    from dwh.execution
-    where exec_date_id >= l_min_date
-    order by exec_id desc
-    limit 1;
 
-    select min(subscribe_time)
-    into l_subscription_time
+    select coalesce(min(subscribe_time), clock_timestamp()), coalesce(max(subscribe_time), clock_timestamp())
+    into l_min_subscription_time, l_max_subscription_time
     from public.etl_subscriptions
     where source_table_name = 'execution'
       and subscription_name = 'f_parent_order'
       and not is_processed
-      and date_id >= l_min_date;
+      and date_id = l_min_date;
 
 
-    select extract(epoch from l_exec_time - l_subscription_time)::int
+    select extract(epoch from l_max_subscription_time - l_min_subscription_time)::int
     into l_diff;
-
-    return case
-               when l_diff is null then 0 -- Alarm. Something wrong
-               when l_diff > l_allow_time then 2 -- Alarm. Too long
-               else 1 end; --Ok
+--     raise notice 'diff - % (%-%)', l_diff, l_min_subscription_time, l_max_subscription_time;
+    --     return case
+--                when l_diff is null then 0 -- Alarm. Something wrong
+--                when l_diff > l_allow_time then 2 -- Alarm. Too long
+--                else 1 end; --Ok
+    return l_diff;
 end;
 $function$
 ;
 
+
+ select sum(del) as delay_in_seconds
+    from ( select is_processed, extract (epoch from max(subscribe_time))* case when is_processed then (-1) else 1 end as del
+      from public.etl_subscriptions
+      where source_table_name = 'execution'
+        and subscription_name = 'f_parent_order'
+        and date_id = 20251202
+        group by is_processed) x;
