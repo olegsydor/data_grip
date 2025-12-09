67b3367899d92ed667d5e5ec036770d84f270d82
Oleh Sydor <ole.sydor@gmail.com>
2025-12-09 18:03:02 +0200

    Regular auto-commit at 18:03:02 on 09/12/2025

---
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/DEVREQ-7253.sql
@@ -0,0 +1,191 @@
+-- https://dashfinancial.atlassian.net/browse/DEVREQ-7253
+
+-- DROP FUNCTION trash.dash360_report_trades_v3(int4, int4, bpchar, _int4, _varchar);
+
+CREATE OR REPLACE FUNCTION trash.dash360_report_trades_v3(in_start_date_id integer DEFAULT public.get_dateid(CURRENT_DATE), in_end_date_id integer DEFAULT public.get_dateid(CURRENT_DATE), in_instrument_type character DEFAULT NULL::bpchar, in_account_ids integer[] DEFAULT '{}'::integer[], in_trading_firm_ids character varying[] DEFAULT '{}'::character varying[])
+ RETURNS TABLE("Trading Firm" character varying, "Account" character varying, "Street Cl Ord ID" character varying, "Cl Ord ID" character varying, "Secondary Ord ID" character varying, "Date" text, "Time" text, "Sec Type" text, "Ex Dest" character varying, "Sub Strategy" character varying, "Fee Sensitivity" smallint, "Status" character varying, "Side" text, "O/C" text, "Symbol" character varying, "Last Qty" integer, "Leaves Qty" integer, "Last Px" numeric, "Last Mkt" character varying, "Exchange Name" character varying, "MIC Code" character varying, "Bid Qty" text, "Bid Px" text, "Ask Px" text, "Ask Qty" integer, "Exec Bid Qty" integer, "Exec Bid Px" text, "Exec Ask Px" text, "Exec Ask Qty" integer, "Liquidity Ind" character varying, "Liq Ind Description" character varying, "Cust/Firm" character varying, "Exec Broker" character varying, "CMTA" character varying, "Client ID" character varying, "Expiration" text, "Root Symbol" character varying, "Put/Call" text, "Strike" numeric, "OSI Symbol" character varying, "Dash Exec ID" bigint, "Exch Exec ID" character varying, "Secondary Exch Exec ID" character varying, "Is Mleg" text, "Is Cross" character, "Sending Firm" character varying, "Sub System" character varying, "Free Text" character varying, "Principal Amount" numeric, "Commission" numeric, "Exchange Fees" text, "Exchange Fees/Unit" text, "Execution Cost" numeric, "Execution Cost/Unit" text, "Maker/Taker Fee" numeric, "Maker/Taker Fee/Unit" text, "Transaction Fee" text, "Trade Processing Fee" text, "Spread Transaction Fee" text, "QCC Rebate" text, "Royalty Fee" text, "Option Regulatory Fee" text, "OCC Fee" text, "SEC Fee" text, "CAT Fee" text, "Equity Clearing Fee" text, "Contra Broker" character varying, "Client Commission" numeric, "Dash Liq Ind Type" character varying, "OCC AID" character varying)
+ LANGUAGE plpgsql
+AS $function$
+
+-- OS: 20251028 https://dashfinancial.atlassian.net/browse/DEVREQ-6992
+declare
+    l_load_id   int;
+    l_step_id   int;
+    l_row_count int4;
+begin
+    select nextval('public.load_timing_seq') into l_load_id;
+    l_step_id := 1;
+
+    select public.load_log(l_load_id, l_step_id,
+                           'dash360.MAREX - XFA for ' || in_start_date_id::text || ' - ' ||
+                           in_end_date_id::text ||
+                           ' STARTED===', 0, 'O')
+    into l_step_id;
+
+    return query
+        select replace(tf.trading_firm_name, ',', '')::varchar                             as "Trading Firm",
+               da.account_name                                                             as "Account",
+               tr.street_client_order_id                                                   as "Street Cl Ord ID",
+               tr.client_order_id                                                          as "Cl Ord ID",
+               tr.secondary_order_id                                                       as "Secondary Ord ID",
+               to_char(tr.trade_record_time, 'MM/DD/YYYY')                                 as "Date",
+               to_char(tr.trade_record_time, 'HH24:MI:SS.US')                              as "Time",
+               case
+                   when tr.instrument_type_id = 'E' then 'Equity'
+                   when tr.instrument_type_id = 'O' then 'Option'
+                   end                                                                     as "Sec Type",
+               case
+                   when tr.ex_destination in ('ALGO', 'SMART')
+                       then tr.ex_destination || (case
+                                                      when tr.instrument_type_id = 'O' then ' Options'
+                                                      when tr.instrument_type_id = 'E' then ' Equities'
+                                                      else '' end)
+                   else exd.ex_destination_desc
+                   end::varchar                                                            as "Ex Dest",
+               tr.sub_strategy                                                             as "Sub Strategy",
+               tr.fee_sensitivity                                                          as "Fee Sensitivity",
+               os.order_status_description                                                 as "Status",
+               --case when tr.leaves_qty = 0 then 'Filled' else 'Partially filled' end       as "Status",
+               case
+                   when tr.side = '1' then 'Buy'
+                   when tr.side = '2' then 'Sell'
+                   when tr.side in ('5', '6') then 'Sell Short'
+                   else ''
+                   end                                                                     as "Side",
+               case
+                   when tr.open_close = 'O' then 'Open'
+                   when tr.open_close = 'C' then 'Close'
+                   else '' end                                                             as "O/C",
+               hsd.display_instrument_id                                                   as "Symbol",
+               tr.last_qty                                                                 as "Last Qty",
+               tr.leaves_qty                                                               as "Leaves Qty",
+               tr.last_px                                                                  as "Last Px",
+               tr.last_mkt                                                                 as "Last Mkt",
+               ex.exchange_name                                                            as "Exchange Name",
+               ex.mic_code                                                                 as "MIC Code",
+               tr.routing_time_bid_qty::text                                               as "Bid Qty",
+               to_char(tr.routing_time_bid_price, 'FM999990.0099')                         as "Bid Px",
+               to_char(tr.routing_time_ask_price, 'FM999990.0099')                         as "Ask Px",
+               tr.routing_time_ask_qty                                                     as "Ask Qty",
+               tr.bid_qty                                                                  as "Exec Bid Qty",
+               to_char(tr.bid_price, 'FM999990.0099')                                      as "Exec Bid Px",
+               to_char(tr.ask_price, 'FM999990.0099')                                      as "Exec Ask Px",
+               tr.ask_qty                                                                  as "Exec Ask Qty",
+               tr.trade_liquidity_indicator                                                as "Liquidity Ind",
+               dlit.description                                                            as "Liq Ind Description",
+               cf.customer_or_firm_name                                                    as "Cust/Firm",
+               tr.exec_broker                                                              as "Exec Broker",
+               tr.cmta                                                                     as "CMTA",
+               tr.client_id                                                                as "Client ID",
+               to_char(hsd.maturity_date, 'MM/DD/YYYY')                                    as "Expiration",
+               coalesce(hsd.underlying_symbol, hsd.symbol)                                 as "Root Symbol",
+               case
+                   when hsd.put_call = '0' then 'Put'
+                   when hsd.put_call = '1' then 'Call'
+                   else ''
+                   end                                                                     as "Put/Call",
+               hsd.strike_px                                                               as "Strike",
+               hsd.opra_symbol                                                             as "OSI Symbol",
+               tr.exec_id                                                                  as "Dash Exec ID",
+               tr.exch_exec_id                                                             as "Exch Exec ID",
+               tr.secondary_exch_exec_id                                                   as "Secondary Exch Exec ID",
+               case
+                   when tr.multileg_reporting_type = '1'
+                       then 'N'
+                   else 'Y' end                                                            as "Is Mleg",
+               tr.is_cross_order                                                           as "Is Cross",
+               tr.fix_comp_id                                                              as "Sending Firm",
+               tr.subsystem_id                                                             as "Sub System",
+               lst_ex.exec_text                                                            as "Free Text",
+               tr.principal_amount                                                         as "Principal Amount",
+               tr.tcce_account_dash_commission_amount                                      as "Commission",
+               to_char(round(coalesce(tr.tcce_transaction_fee_amount, 0) + coalesce(tr.tcce_maker_taker_fee_amount, 0) +
+                             coalesce(tr.spread_transaction_fee_amount, 0) + coalesce(tr.tcce_royalty_fee_amount, 0) +
+                             coalesce(tr.tcce_trade_processing_fee_amount, 0) + coalesce(tr.qcc_rebate_amount, 0), 4),
+                       'FM999990.0000')                                                    as "Exchange Fees",
+               to_char(round((coalesce(tr.tcce_transaction_fee_amount, 0) +
+                              coalesce(tr.tcce_maker_taker_fee_amount, 0) +
+                              coalesce(tr.spread_transaction_fee_amount, 0) + coalesce(tr.tcce_royalty_fee_amount, 0) +
+                              coalesce(tr.tcce_trade_processing_fee_amount, 0) + coalesce(tr.qcc_rebate_amount, 0)) /
+                             nullif(tr.last_qty, 0), 4),
+                       'FM999990.0000')                                                    as "Exchange Fees/Unit",
+               tr.tcce_account_execution_cost                                              as "Execution Cost",
+               to_char(round(tr.tcce_account_execution_cost / nullif(tr.last_qty, 0), 4),
+                       'FM999990.0000')                                                    as "Execution Cost/Unit",
+               tr.tcce_maker_taker_fee_amount                                              as "Maker/Taker Fee",
+               to_char(round(tr.tcce_maker_taker_fee_amount / nullif(tr.last_qty, 0), 4),
+                       'FM999990.0000')                                                    as "Maker/Taker Fee/Unit",
+               to_char(round(tr.tcce_transaction_fee_amount, 4), 'FM999990.0000')          as "Transaction Fee",
+               to_char(round(tr.tcce_trade_processing_fee_amount, 4), 'FM999990.0000')     as "Trade Processing Fee",
+               to_char(round(tr.spread_transaction_fee_amount, 4), 'FM999990.0000')        as "Spread Transaction Fee",
+               to_char(round(qcc_rebate_amount, 8), 'FM999990.00999999')                   as "QCC Rebate",
+               to_char(round(tr.tcce_royalty_fee_amount, 4), 'FM999990.0000')              as "Royalty Fee",
+               to_char(round(tr.tcce_option_regulatory_fee_amount, 4), 'FM999990.0000')    as "Option Regulatory Fee",
+
+               case
+                   when tr.instrument_type_id = 'O'
+                       then to_char(round(tr.tcce_occ_fee_amount, 4), 'FM999990.0000') end as "OCC Fee",
+               to_char(round(tr.tcce_sec_fee_amount, 4), 'FM999990.0000')                  as "SEC Fee",
+               to_char(round(tr.cat_fee_amount, 4), 'FM999990.0000')                       as "CAT Fee",
+               case
+                   when tr.instrument_type_id = 'E'
+                       then to_char(round(tr.clearing_fee_amout, 4), 'FM999990.0000') end  as "Equity Clearing Fee",
+               tr.contra_broker                                                            as "Contra Broker",
+               tr.client_commission_rate * tr.last_qty                                     as "Client Commission",
+               dlit.liquidity_indicator_type                                               as "Dash Liq Ind Type",
+               tr.street_account_name::varchar                                             as "OCC AID"
+        from dwh.flat_trade_record tr
+                 join dwh.d_account da on (da.account_id = tr.account_id)
+                 join dwh.d_trading_firm tf on (tf.trading_firm_unq_id = da.trading_firm_unq_id)
+                 join dwh.historic_security_definition_all hsd on (hsd.instrument_id = tr.instrument_id)
+                 left join dwh.d_customer_or_firm cf
+                           on (cf.customer_or_firm_id = tr.opt_customer_firm and cf.is_active)
+                 left join dwh.d_exchange ex on ex.exchange_id = tr.exchange_id and ex.is_active
+                 left join dwh.d_ex_destination exd on exd.ex_destination_code = tr.ex_destination and
+                                                       exd.exchange_id is not distinct from tr.exchange_id and
+                                                       exd.instrument_type_id = tr.instrument_type_id and
+                                                       exd.is_active
+                 left join lateral ( select ex.exec_text, ex.order_status
+                                     from dwh.execution ex
+                                     where ex.order_id = tr.order_id
+                                       and ex.exec_date_id between in_start_date_id and in_end_date_id
+                                       and ex.order_status <> '3'
+                                     order by ex.exec_id desc
+                                     limit 1 ) lst_ex on true
+                 left join lateral (select dlit.liquidity_indicator_type, dli.description
+                                    from dwh.d_liquidity_indicator dli
+                                             inner join dwh.d_liquidity_indicator_type dlit
+                                                        on dlit.liquidity_indicator_type_id = dli.liquidity_indicator_type_id
+                                    where dli.trade_liquidity_indicator = tr.trade_liquidity_indicator
+                                      and dli.exchange_id = tr.exchange_id
+                                      and dli.is_active ) dlit on true
+                 left join dwh.execution e on (e.exec_date_id between in_start_date_id and in_end_date_id and e.exec_date_id = tr.date_id and e.exec_id = tr.exec_id)
+                 left join dwh.d_order_status os on (os.order_status = e.order_status)
+        where tr.date_id between in_start_date_id and in_end_date_id
+          and tr.is_busted <> 'Y'
+          and da.account_name not in (select * from fintech_dwh.users_fbw_tb)
+          and case when in_instrument_type is null then true else tr.instrument_type_id = in_instrument_type end
+          and case
+                  when coalesce(in_account_ids, '{}') = '{}' then true
+                  else tr.account_id = any (in_account_ids) end
+          and tr.multileg_reporting_type in ('1', '2')
+          and case
+                  when coalesce(in_trading_firm_ids, '{}') <> '{}'
+                      then da.trading_firm_id = any (in_trading_firm_ids)
+                  else true end
+        order by tr.date_id, tr.trade_record_id;
+
+    get diagnostics l_row_count = row_count;
+    select public.load_log(l_load_id, l_step_id,
+                           'dash360.MAREX - XFA for ' || in_start_date_id::text || ' - ' || in_end_date_id::text ||
+                           ' COMPLETED===', l_row_count, 'C')
+    into l_step_id;
+end;
+$function$
+;
+
+select * from dwh.d_account
+where account_name = 'TICKRSGTH';
+
+select *
+from trash.dash360_report_trades_v3(20251209, 20251209, 'O', '{74899}');
