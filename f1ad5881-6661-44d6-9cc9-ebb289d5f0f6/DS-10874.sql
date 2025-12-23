aa37fd7a535820d93c97c3d60224458499eaeca8
Oleh Sydor <ole.sydor@gmail.com>
2025-12-23 16:00:18 +0200

    Regular auto-commit at 16:00:18 on 23/12/2025

--- f1ad5881-6661-44d6-9cc9-ebb289d5f0f6/DS-10874.sql
+++ f1ad5881-6661-44d6-9cc9-ebb289d5f0f6/DS-10874.sql
@@ -1,11 +1,21 @@
 -- https://dashfinancial.atlassian.net/browse/DS-10874
-select * from dash360.get_trade_record_exchange_fees(in_date_id := 20251222, in_cl_order_id := '3_1of251222', in_dash_exec_id := 'CDAA0204-20251222', in_sec_order_id := '860011673433', in_sec_exec_id := 'npktssdk0g00', in_side := '1')
+select *
+from dash360.get_trade_record_exchange_fees(in_date_id := 20251222, in_cl_order_id := '3_1of251222',
+                                            in_dash_exec_id := '860011673436', in_sec_order_id := 'CDAA0204-20251222',
+                                            in_sec_exec_id := 'npktssdk0g02', in_side := '1');
+
+
+
+select * from staging.trade_level_book_record
+    where trade_record_id = 2347715403;
+
+
 drop function if exists dash360.get_trade_record_exchange_fees;
 create or replace function dash360.get_trade_record_exchange_fees(in_date_id int4,
-                                                                  in_cl_order_id varchar(256), --3_1of251222
-                                                                  in_dash_exec_id varchar(128), -- CDAA0204-20251222
-                                                                  in_sec_order_id varchar(256), -- 860011673433
-                                                                  in_sec_exec_id varchar(128), -- npktssdk0g00
+                                                                  in_cl_order_id varchar(256),
+                                                                  in_dash_exec_id varchar(128),
+                                                                  in_sec_order_id varchar(256),
+                                                                  in_sec_exec_id varchar(128),
                                                                   in_side char) -- 1
     returns table
             (
@@ -28,16 +38,23 @@ begin
                f.tcce_transaction_fee_amount,
                f.tcce_trade_processing_fee_amount,
                f.tcce_royalty_fee_amount,
-               true as is_exchange_fee_computed
-            ,f.client_order_id
-    ,f.secondary_order_id
-      ,f.exch_exec_id
-      , f.secondary_exch_exec_id
-      ,f.side
+               (select max_trade_record_id > f.trade_record_id
+                from staging.incrementalbilling
+                where table_name = 'MIRA_to_GENESIS2_inc') as is_exchange_fee_computed
+        --                 ,
+--                f.client_order_id
+--                 ,
+--                f.secondary_order_id
+--                 ,
+--                f.exch_exec_id
+--                 ,
+--                f.secondary_exch_exec_id
+--                 ,
+--                f.side
         from dwh.flat_trade_record f
         where true
-          and f.date_id = :in_date_id
-          and f.client_order_id = :in_cl_order_id
+          and f.date_id = in_date_id
+          and f.client_order_id = in_cl_order_id
           and f.secondary_order_id = in_sec_order_id
           and f.exch_exec_id = in_dash_exec_id
           and f.secondary_exch_exec_id = in_sec_exec_id
@@ -48,7 +65,13 @@ end;
 $fx$;
 
 
-select * from billing.IncrementalBilling
+
+import foreign schema billing limit to (incrementalbilling)
+from server postgres_uat_genesis2
+into staging
+
+select * from staging.incrementalbilling
+where table_name = 'MIRA_to_GENESIS2_inc'
 
 
 select * from information_schema.foreign_tables
