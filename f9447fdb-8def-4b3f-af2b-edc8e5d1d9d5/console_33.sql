3376fb149c9ec0a337a8f9120a473e26474af73d
Oleh Sydor <ole.sydor@gmail.com>
2026-02-03 13:00:10 +0200

    Regular auto-commit at 13:00:09 on 03/02/2026

--- f9447fdb-8def-4b3f-af2b-edc8e5d1d9d5/console_33.sql
+++ f9447fdb-8def-4b3f-af2b-edc8e5d1d9d5/console_33.sql
@@ -35,5 +35,16 @@ where application_id = 'orders'  and section_id = 'todays-blotter-section' and l
 
  select * from dash360.user_layout_settings
 
- select * from dash360.allocations_snapshot(in_date_id := 20260127)
+ select * from dash360.allocations_snapshot(in_date_id := 20260127);
 
+with dl as (select min(id) as del_id, entity_id, actionable_id, max(update_time) as delete_time
+            from blaze7_settings.entity_actionable_id
+            where not is_deleted
+            group by entity_id, actionable_id
+            having count(*) > 1)
+update blaze7_settings.entity_actionable_id eai
+set deleted_time = delete_time,
+    is_deleted   = true
+from dl
+where eai.id = dl.del_id
+  and is_deleted = false
