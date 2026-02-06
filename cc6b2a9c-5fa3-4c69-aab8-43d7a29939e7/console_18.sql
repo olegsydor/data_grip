9c709686a2a9cd6a202b39e960ce2657368ec9b5
Oleh Sydor <ole.sydor@gmail.com>
2026-02-06 15:00:14 +0200

    Regular auto-commit at 15:00:13 on 06/02/2026

--- cc6b2a9c-5fa3-4c69-aab8-43d7a29939e7/console_18.sql
+++ cc6b2a9c-5fa3-4c69-aab8-43d7a29939e7/console_18.sql
@@ -1,43 +1,55 @@
 SELECT -- 77205
-	ac.account_name as "Account",
-	TRUNC(CL.CREATE_TIME) as "Creation Date",
-	decode(I.INSTRUMENT_TYPE_ID, 'E', 'Equity', 'O', 'Option') as "Sec Type",
-	decode(CL.SIDE, '1', 'Buy', '2', 'Sell', '5', 'Sell Short', '6', 'Sell Short') as "Side",
-	CL.ORDER_QTY as "Ord Qty",
-	I.SYMBOL as "Symbol",
-	OC.STRIKE_PRICE as "Strike Px",
-	decode(OC.PUT_CALL, '1', 'C', '2', 'P') as "Put Call",
-	(OC.MATURITY_DAY || ' ' || to_char(to_date(OC.MATURITY_MONTH, 'mm'), 'Mon') || ' ' || to_char(to_date(OC.MATURITY_YEAR, 'yyyy'), 'yy')) as "Exp Date",
-	EDC.EX_DESTINATION_CODE_NAME as "Ex Dest",
-	OT.ORDER_TYPE_NAME as "Ord Type",
-	CL.PRICE as "Price",
-	CAST((SELECT SUM(LAST_QTY) FROM EXECUTION_TODAY WHERE ORDER_ID = CL.ORDER_ID AND EXEC_TYPE IN ('F','G') AND IS_BUSTED = 'N') as NUMBER(13,0)) as "Ex Qty",
-	EX.AVG_PX as "Avg Px",
-	EX.LEAVES_QTY as "Lvs Qty",
-	decode(CL.MULTILEG_REPORTING_TYPE,'1','N','2','Y') as "Is Mleg",
-	LEG.CLIENT_LEG_REF_ID as "Leg ID",
-	cl.OPEN_CLOSE as "Open/Close",
-	OC.OPRA_SYMBOL as "OSI Symbol",
-	CL.CLIENT_ORDER_ID as "Cl Ord ID",
-	CL.CLIENT_ID as "Client ID",
-	FC.FIX_COMP_ID AS "Sender Comp ID"
+    cl.ORDER_ID,
+    ac.account_name                                                                                       as "Account",
+       TRUNC(CL.CREATE_TIME)                                                                                 as "Creation Date",
+       decode(I.INSTRUMENT_TYPE_ID, 'E', 'Equity', 'O', 'Option')                                            as "Sec Type",
+       decode(CL.SIDE, '1', 'Buy', '2', 'Sell', '5', 'Sell Short', '6', 'Sell Short')                        as "Side",
+       CL.ORDER_QTY                                                                                          as "Ord Qty",
+       I.SYMBOL                                                                                              as "Symbol",
+       OC.STRIKE_PRICE                                                                                       as "Strike Px",
+       decode(OC.PUT_CALL, '1', 'C', '2', 'P')                                                               as "Put Call",
+       (OC.MATURITY_DAY || ' ' || to_char(to_date(OC.MATURITY_MONTH, 'mm'), 'Mon') || ' ' ||
+        to_char(to_date(OC.MATURITY_YEAR, 'yyyy'), 'yy'))                                                    as "Exp Date",
+       EDC.EX_DESTINATION_CODE_NAME                                                                          as "Ex Dest",
+       OT.ORDER_TYPE_NAME                                                                                    as "Ord Type",
+       CL.PRICE                                                                                              as "Price",
+       CAST((SELECT SUM(LAST_QTY)
+             FROM EXECUTION_TODAY
+             WHERE ORDER_ID = CL.ORDER_ID
+               AND EXEC_TYPE IN ('F', 'G')
+               AND IS_BUSTED = 'N') as NUMBER(13, 0))                                                        as "Ex Qty",
+       EX.AVG_PX                                                                                             as "Avg Px",
+       EX.LEAVES_QTY                                                                                         as "Lvs Qty",
+       decode(CL.MULTILEG_REPORTING_TYPE, '1', 'N', '2', 'Y')                                                as "Is Mleg",
+       LEG.CLIENT_LEG_REF_ID                                                                                 as "Leg ID",
+       cl.OPEN_CLOSE                                                                                         as "Open/Close",
+       OC.OPRA_SYMBOL                                                                                        as "OSI Symbol",
+       CL.CLIENT_ORDER_ID                                                                                    as "Cl Ord ID",
+       CL.CLIENT_ID                                                                                          as "Client ID",
+       FC.FIX_COMP_ID                                                                                        AS "Sender Comp ID"
 FROM CLIENT_ORDER_TODAY CL
-INNER JOIN INSTRUMENT I ON I.INSTRUMENT_ID = CL.INSTRUMENT_ID
-LEFT JOIN CLIENT_ORDER_TODAY ORIG ON (ORIG.ORDER_ID = CL.ORIG_ORDER_ID)
-LEFT JOIN CLIENT_ORDER_LEG LEG ON (LEG.ORDER_ID = CL.ORDER_ID)
-INNER JOIN ACCOUNT AC ON (CL.ACCOUNT_ID = AC.ACCOUNT_ID)
-LEFT JOIN OPTION_CONTRACT OC on (OC.INSTRUMENT_ID = CL.INSTRUMENT_ID)
-LEFT JOIN OPTION_SERIES OS on (OC.OPTION_SERIES_ID = OS.OPTION_SERIES_ID)
-INNER JOIN EXECUTION_TODAY EX ON (CL.ORDER_ID = EX.ORDER_ID)
-INNER JOIN ORDER_STATUS ORS ON ORS.ORDER_STATUS = EX.ORDER_STATUS
-LEFT JOIN EX_DESTINATION_CODE EDC on (CL.ex_destination = EDC.ex_destination_CODE and EDC.is_deleted <> 'Y')
-LEFT JOIN ORDER_TYPE OT ON (OT.ORDER_TYPE_ID = CL.ORDER_TYPE)
-LEFT JOIN FIX_CONNECTION FC ON (FC.FIX_CONNECTION_ID = CL.FIX_CONNECTION_ID)
+         INNER JOIN INSTRUMENT I ON I.INSTRUMENT_ID = CL.INSTRUMENT_ID
+         LEFT JOIN CLIENT_ORDER_TODAY ORIG ON (ORIG.ORDER_ID = CL.ORIG_ORDER_ID)
+         LEFT JOIN CLIENT_ORDER_LEG LEG ON (LEG.ORDER_ID = CL.ORDER_ID)
+         INNER JOIN ACCOUNT AC ON (CL.ACCOUNT_ID = AC.ACCOUNT_ID)
+         LEFT JOIN OPTION_CONTRACT OC on (OC.INSTRUMENT_ID = CL.INSTRUMENT_ID)
+         LEFT JOIN OPTION_SERIES OS on (OC.OPTION_SERIES_ID = OS.OPTION_SERIES_ID)
+         INNER JOIN EXECUTION_TODAY EX ON (CL.ORDER_ID = EX.ORDER_ID)
+         INNER JOIN ORDER_STATUS ORS ON ORS.ORDER_STATUS = EX.ORDER_STATUS
+         LEFT JOIN EX_DESTINATION_CODE EDC on (CL.ex_destination = EDC.ex_destination_CODE and EDC.is_deleted <> 'Y')
+         LEFT JOIN ORDER_TYPE OT ON (OT.ORDER_TYPE_ID = CL.ORDER_TYPE)
+         LEFT JOIN FIX_CONNECTION FC ON (FC.FIX_CONNECTION_ID = CL.FIX_CONNECTION_ID)
 WHERE CL.PARENT_ORDER_ID IS NULL
-	AND CL.TRANS_TYPE IN ('D','G')
-	AND CL.TIME_IN_FORCE in ('1','6')
-	AND CL.MULTILEG_REPORTING_TYPE in ('1','2')
-	AND (trunc(i.last_trade_date) >= trunc(SYSDATE) OR i.last_trade_date IS NULL)
-	AND NOT EXISTS (SELECT 1 FROM EXECUTION_TODAY WHERE ORDER_ID = CL.ORDER_ID AND ORDER_STATUS IN ('2','4','8') and (TEXT <>'Instrument expiration' OR TEXT is NULL))
-	AND EX.EXEC_ID = (SELECT MAX(EXEC_ID) FROM EXECUTION_TODAY WHERE ORDER_ID = CL.ORDER_ID AND ORDER_STATUS <> '3')
-and rownum < 10
+  AND CL.TRANS_TYPE IN ('D', 'G')
+  AND CL.TIME_IN_FORCE in ('1', '6')
+  AND CL.MULTILEG_REPORTING_TYPE in ('1', '2')
+  AND (trunc(i.last_trade_date) >= trunc(SYSDATE) OR i.last_trade_date IS NULL)
+  AND NOT EXISTS (SELECT 1
+                  FROM EXECUTION_TODAY
+                  WHERE ORDER_ID = CL.ORDER_ID
+                    AND ORDER_STATUS IN ('2', '4', '8')
+                    and (TEXT <> 'Instrument expiration' OR TEXT is NULL))
+  AND EX.EXEC_ID = (SELECT MAX(EXEC_ID) FROM EXECUTION_TODAY WHERE ORDER_ID = CL.ORDER_ID AND ORDER_STATUS <> '3')
+  and cl.CREATE_TIME > (date '2026-01-23')
+-- and cl.ORDER_ID in (414795391607396407,414795391607396408,414795391607396409,414795391607396410,414795391618919403,414795391618919404,414795391618919405,414795391618919406,414795391647238854,414795391647238855,414795391647238856,414795391647238857,414795395922273334,414795395922273335,414795395922273336,414795395922273337,414795395953737275,414795395953737276,414795395953737277,414795395953737278,414795395968414191,414795395968414192,414795395968414193,414795395968414194,414795395982044570,414795395982044571,414795395982044572,414795395982044573,414795404552055800,414795404552055801,414795404552055802,414795404552055803,414795404563605918,414795404563605919,414795404563605920,414795404563605921,414795705143141639,414796370920745578,414796405254267307,414796675757521111,414796705883113308,414796731649767952,414796731649767953,414797027970008812,414797027970008813,414797131056559549,414797131056559550,414797178343139576,414797178343139577,414797234185063570,414797234185063571,414797655070887000,414797749545481788,414797788226400896,414798110369921749,414798599938539934,414799016528330599,414799016528330600,414799016528330601,414799016528330602,414799033686179276,414799033686179277,414799166909857107,414799171232087344,414799338657171205,414799489025063740,414799489039743110,414799489079593196,414799493312692549,414799497580398448,414799497625482424,414799566334475185,414799579164853845,414799630806173616,414799678044521071,414799678044521072,414799678044521073,414799678044521074,414799682310129750,414799682310129751,414799682310129752,414799682310129753,414799682324807001,414799682324807002,414799682324807003,414799682324807004,414799699524602212,414799699524602213,414799699524602214,414799699524602215,414799708088322425,414799905652618118,414799905652618119,414799970050915226,414799970050915227,414799970050915228,414799970050915229,414799978620932548,414799978620932549,414799978620932550)
+
--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_161.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_161.sql
@@ -1,10 +1,33 @@
--- DROP FUNCTION dash360.get_active_parent_gtc_orders(_int4, int4, int4, _varchar);
-
-CREATE OR REPLACE FUNCTION dash360.report_gtc_impacted_active(in_start_date_id integer DEFAULT (to_char((CURRENT_DATE)::timestamp with time zone, 'YYYYMMDD'::text))::integer,
+CREATE or replace FUNCTION dash360.report_gtc_impacted_active(in_start_date_id integer DEFAULT (to_char((CURRENT_DATE)::timestamp with time zone, 'YYYYMMDD'::text))::integer,
                                                               in_end_date_id integer DEFAULT (to_char((CURRENT_DATE)::timestamp with time zone, 'YYYYMMDD'::text))::integer)
- RETURNS TABLE(account_name character varying, creation_date timestamp without time zone, ord_status character varying, sec_type character, side character, symbol character varying, ex_dest character varying, ord_qty integer, ex_qty numeric, ord_type character, price numeric, avg_px numeric, lvs_qty bigint, is_mleg text, leg_id character varying, open_close character, dash_id character varying, cl_ord_id character varying, orig_cl_ord_id character varying, occ_data text, osi_symbol character varying, client_id character varying, subsystem character varying, strike_px numeric, put_call character, exp_year smallint, exp_month smallint, exp_day smallint, order_id bigint, sender_comp_id character varying)
- LANGUAGE plpgsql
-AS $function$
+    RETURNS TABLE
+            (
+                "Account"        varchar(30),
+                "Creation Date"  date,
+                "Sec Type"       text,
+                "Side"           text,
+                "Ord Qty"        int4,
+                "Symbol"         varchar(10),
+                "Strike Px"      numeric(12, 4),
+                "Put Call"       text,
+                "Exp Date"       text,
+                "Ex Dest"        varchar(256),
+                "Ord Type"       varchar(255),
+                "Price"          numeric(12, 4),
+                "Ex Qty"         numeric,
+                "Avg Px"         numeric,
+                "Lvs Qty"        int8,
+                "Is Mleg"        text,
+                "Leg ID"         varchar(30),
+                "Open/Close"     bpchar(1),
+                "OSI Symbol"     varchar(30),
+                "Cl Ord ID"      varchar(256),
+                "Client ID"      varchar(255),
+                "Sender Comp ID" varchar(30)
+            )
+    LANGUAGE plpgsql
+AS
+$function$
     -- 2026-02-06 SO: https://dashfinancial.atlassian.net/browse/DS-11064
 declare
     row_cnt           int4;
@@ -26,87 +49,138 @@ begin
                            in_end_date_id::text || ' STARTED===',
                            0, 'O')
     into l_step_id;
-  select to_char(to_date(:maturity_year::text ||:maturity_day::text || :maturity_month::text, 'YYYYMMDD'), 'DD Mon YYYY')
 
-    return query
-        select ac.account_name              as "Account",
-               cl.create_time::date               as "Creation Date",
-               case di.instrument_type_id when 'E' then 'Equity' when 'O' then 'Option' end as "Sec Type",
-               case cl.side when '1' then 'Buy'
-                   when '2' then 'Sell' when '5' then 'Sell Short' when '6' then 'Sell Short' end                      as "Side",
-               cl.order_qty                 as "Ord Qty",
-               di.symbol                    as "Symbol",
-               oc.strike_price              as "Strike Px",
-               case oc.put_call when '1' then 'C' when '2' then 'P' end                 as put_call,
-               to_char(to_date(oc.maturity_year::text ||oc.maturity_day::text || oc.maturity_month::text, 'YYYYMMDD'), 'DD Mon YYYY')             as exp_day,
-               dex.ex_destination_code_name as ex_dest,
-               cl.order_type_id             as ord_type,
-               cl.price                     as price,
-               exl.ex_qty,
-               ex.avg_px                    as avg_px,
-               ex.leaves_qty                as lvs_qty,
-               case
-                   when cl.multileg_reporting_type = '1' then 'N'
-                   when cl.multileg_reporting_type = '2'
-                       then 'Y' end         as is_mleg,
-               cl.co_client_leg_ref_id		as leg_id,
-               cl.open_close                as open_close,
-               oc.opra_symbol               as osi_symbol,
-               cl.client_order_id           as cl_ord_id,
-               cl.client_id_text            as client_id,
-               fc.fix_comp_id               as sender_comp_id
+    drop table if exists t_gtc;
+    create temp table if not exists t_gtc as
+    select cl.order_id,
+           ac.account_name                                                                  as "Account",
+           cl.create_time::date                                                             as "Creation Date",
+           case di.instrument_type_id when 'E' then 'Equity' when 'O' then 'Option' end     as "Sec Type",
+           case cl.side
+               when '1' then 'Buy'
+               when '2' then 'Sell'
+               when '5' then 'Sell Short'
+               when '6' then 'Sell Short' end                                               as "Side",
+           cl.order_qty                                                                     as "Ord Qty",
+           di.symbol                                                                        as "Symbol",
+           oc.strike_price                                                                  as "Strike Px",
+           case oc.put_call when '1' then 'C' when '2' then 'P' end                         as "Put Call",
+           to_char(to_date(lpad(oc.maturity_year::text, 4, '0') || lpad(oc.maturity_month::text, 2, '0') ||
+                           lpad(oc.maturity_day::text, 2, '0'), 'YYYYMMDD'), 'DD Mon YYYY') as "Exp Date",
+           dex.ex_destination_code_name                                                     as "Ex Dest",
+           ot.order_type_name                                                               as "Ord Type",
+           cl.price                                                                         as "Price",
+           exl.ex_qty                                                                       as "Ex Qty",
+           ex.avg_px                                                                        as "Avg Px",
+           ex.leaves_qty                                                                    as "Lvs Qty",
+           case
+               when cl.multileg_reporting_type = '1' then 'N'
+               when cl.multileg_reporting_type = '2'
+                   then 'Y' end                                                             as "Is Mleg",
+           cl.co_client_leg_ref_id                                                          as "Leg ID",
+           cl.open_close                                                                    as "Open/Close",
+           oc.opra_symbol                                                                   as "OSI Symbol",
+           cl.client_order_id                                                               as "Cl Ord ID",
+           cl.client_id_text                                                                as "Client ID",
+           fc.fix_comp_id                                                                   as "Sender Comp ID"
+    from dwh.gtc_order_status gtc
+             join dwh.client_order cl on cl.order_id = gtc.order_id and cl.create_date_id = gtc.create_date_id
+             join dwh.d_instrument di on di.instrument_id = cl.instrument_id
+             inner join dwh.d_account ac on (cl.account_id = ac.account_id)
+             join lateral (select ex.exec_id as exec_id,
+                                  ex.avg_px,
+                                  ex.leaves_qty,
+                                  ex.order_status
+                           from dwh.execution ex
+                           where gtc.order_id = ex.order_id
+                             and ex.order_status <> '3'
+                             and ex.exec_date_id >= gtc.create_date_id
+                             and ex.exec_date_id <= in_end_date_id
+                           order by ex.exec_time desc
+                           limit 1) ex on true
+             inner join dwh.d_order_status ors on ors.order_status = ex.order_status
+             left join lateral (select sum(ex.last_qty) as ex_qty
+                                from dwh.execution ex
+                                where ex.exec_date_id >= gtc.create_date_id
+                                  and ex.exec_date_id <= in_end_date_id
+                                  and ex.order_id = cl.order_id
+                                  and ex.exec_type in ('F', 'G')
+                                  and ex.is_busted = 'N'
+                                limit 1) exl on true
+             left join dwh.d_option_contract oc on oc.instrument_id = cl.instrument_id
+             left join dwh.d_fix_connection fc on fc.fix_connection_id = cl.fix_connection_id
+             left join dwh.d_ex_destination_code dex on dex.ex_destination_code = cl.ex_destination and dex.is_active
+             left join dwh.d_order_type ot on ot.order_type_id = cl.order_type_id
 
-        from dwh.gtc_order_status gtc
-                 join dwh.client_order cl on cl.order_id = gtc.order_id and cl.create_date_id = gtc.create_date_id
-                 join dwh.d_instrument di on di.instrument_id = cl.instrument_id
-                 inner join dwh.d_account ac on (cl.account_id = ac.account_id)
-                 join lateral (select ex.exec_id as exec_id,
-                                      ex.avg_px,
-                                      ex.leaves_qty,
-                                      ex.order_status
-                               from dwh.execution ex
-                               where gtc.order_id = ex.order_id
-                                 and ex.order_status <> '3'
-                                 and ex.exec_date_id >= gtc.create_date_id
-                               order by ex.exec_time desc
-                               limit 1) ex on true
-                 inner join dwh.d_order_status ors on ors.order_status = ex.order_status
-                 left join lateral (select sum(ex.last_qty) as ex_qty
-                                    from dwh.execution ex
-                                    where ex.exec_date_id >= gtc.create_date_id
-                                      and ex.order_id = cl.order_id
-                                      and ex.exec_type in ('F', 'G')
-                                      and ex.is_busted = 'N'
-                                    limit 1) exl on true
-
-                 left join dwh.d_option_contract oc on oc.instrument_id = cl.instrument_id
---                  left join dwh.d_sub_system ss on ss.sub_system_unq_id = cl.sub_system_unq_id
-                 left join dwh.d_fix_connection fc on fc.fix_connection_id = cl.fix_connection_id
-                 left join dwh.d_ex_destination_code dex on dex.ex_destination_code = cl.ex_destination and dex.is_active
-
-        where cl.parent_order_id is null
-          and gtc.create_date_id <= in_start_date_id
-          and (gtc.close_date_id is null
-                    -- the code below has been added to provide the same performance in the case we use the report for CURRENT date
-           or (case
+    where cl.parent_order_id is null
+      and gtc.create_date_id <= in_start_date_id
+      and (gtc.close_date_id is null
+        or (case
                 when l_is_current_date then false
                 else gtc.close_date_id is not null and close_date_id > in_end_date_id end))
-          -- end of
-          and cl.trans_type in ('D', 'G')
-          and cl.time_in_force_id in ('1', '6')
-          and cl.multileg_reporting_type in ('1', '2')
---           and case when coalesce(in_account_ids, '{}') = '{}' then true else gtc.account_id = any (in_account_ids) end
---           and case when l_account_ids = '{}' then true else gtc.account_id = any (l_account_ids) end
-    order by gtc.order_id;
-    get diagnostics row_cnt = row_count;
-
+      and cl.trans_type in ('D', 'G')
+      and cl.time_in_force_id in ('1', '6')
+      and cl.multileg_reporting_type in ('1', '2');
 
     select public.load_log(l_load_id, l_step_id,
-                           'get_active_parent_gtc_orders for ' || in_start_date_id::text || '-' ||
-                           in_end_date_id::text || ' FINISHED===',
+                           'report_gtc_impacted_active for ' || in_start_date_id::text || '-' ||
+                           in_end_date_id::text || ' calculated',
+                           row_cnt, 'O')
+    into l_step_id;
+    create index on t_gtc (order_id);
+
+    select public.load_log(l_load_id, l_step_id,
+                           'report_gtc_impacted_active for ' || in_start_date_id::text || '-' ||
+                           in_end_date_id::text || ' indexed',
+                           row_cnt, 'O')
+    into l_step_id;
+    return query
+        SELECT t_gtc."Account",
+               t_gtc."Creation Date",
+               t_gtc."Sec Type",
+               t_gtc."Side",
+               t_gtc."Ord Qty",
+               t_gtc."Symbol",
+               t_gtc."Strike Px",
+               t_gtc."Put Call",
+               t_gtc."Exp Date",
+               t_gtc."Ex Dest",
+               t_gtc."Ord Type",
+               t_gtc."Price",
+               t_gtc."Ex Qty",
+               t_gtc."Avg Px",
+               t_gtc."Lvs Qty",
+               t_gtc."Is Mleg",
+               t_gtc."Leg ID",
+               t_gtc."Open/Close",
+               t_gtc."OSI Symbol",
+               t_gtc."Cl Ord ID",
+               t_gtc."Client ID",
+               t_gtc."Sender Comp ID"
+        FROM t_gtc
+        order by order_id;
+
+    select public.load_log(l_load_id, l_step_id,
+                           'report_gtc_impacted_active for ' || in_start_date_id::text || '-' ||
+                           in_end_date_id::text || ' COMPLETED ===== ',
                            row_cnt, 'O')
     into l_step_id;
 
-   end;
+
+end;
 $function$
 ;
+
+drop table t_os;
+create table t_os as select * from dash360.report_gtc_impacted_active();
+
+select string_agg(order_id::text, ',')
+from (select order_id
+      from t_gtc
+      order by order_id
+      offset 50000 limit 100) a;
+
+
+
+select * from t_gtc
+    where order_id in (100000025246467247,100000025246471898,100000025246476613)
