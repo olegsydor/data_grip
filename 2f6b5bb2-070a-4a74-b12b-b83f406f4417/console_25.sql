95b2d3eae9ea467c0a255ed42bca4773e62e5a84
Oleh Sydor <ole.sydor@gmail.com>
2026-02-18 20:00:09 +0200

    Regular auto-commit at 20:00:09 on 18/02/2026

--- 2f6b5bb2-070a-4a74-b12b-b83f406f4417/console_25.sql
+++ 2f6b5bb2-070a-4a74-b12b-b83f406f4417/console_25.sql
@@ -109,3 +109,5 @@ from way.add_connection(in_parent_entity_id := 221::int4,
                         in_child_entity_ids := '{92}'::int4[],
                         in_year := 2009::int2, in_month := 05::int2, in_day := null::int2,
                         in_is_exact_date := false);
+
+alter table way.event add constraint parent_entity_id_child_entity_id_unq unique (parent_entity_id,child_entity_id);
