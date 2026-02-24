3769bd2188d8bc38bdd74edf01d9dc0c428ca61d
Oleh Sydor <ole.sydor@gmail.com>
2026-02-24 15:00:10 +0200

    Regular auto-commit at 15:00:10 on 24/02/2026

--- 18bcd7c0-0384-48d3-8bcb-2dd86af4323f/HFT DISCREP.sql
+++ 18bcd7c0-0384-48d3-8bcb-2dd86af4323f/HFT DISCREP.sql
@@ -355,3 +355,6 @@ where msg_type not in ('1', '5')
 order by cl_ord_id, load_batch_id
 
 ;
+alter table inc_hft.hft_incremental_files
+    add column if not exists is_str_modif_processed bool not null default false;
+comment on column inc_hft.hft_incremental_files.is_str_modif_processed is 'Swithced into true as soon as the process of filling the very orig message 35=D';
