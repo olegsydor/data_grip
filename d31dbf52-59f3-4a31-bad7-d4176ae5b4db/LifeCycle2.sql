e77bec5ccf6c70740aa946dcafb5f9973b3c0594
Oleh Sydor <ole.sydor@gmail.com>
2026-02-26 19:00:12 +0200

    Regular auto-commit at 19:00:12 on 26/02/2026

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/LifeCycle2.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/LifeCycle2.sql
@@ -44,13 +44,28 @@ where cl.parent_order_id is null
   and case when :in_instrument_type is null then true else di.instrument_type_id = :in_instrument_type end
   and case when :in_exclude_eos = 'N' then true else fc.is_high_frequency_trader = 'N' end
   and case when :in_fix_comp_ids = '{}' then true else coalesce(fc.fix_comp_id, '') = any (:in_fix_comp_ids) end
-  and case when :in_client_order_ids = '{}' then true else cl.client_order_id = any (:in_client_order_ids) end
-  and cl.trans_type <> 'F';
+  and cl.trans_type <> 'F'
+  and cl.trans_type in ('D', 'G')
+  and cl.multileg_reporting_type in ('1', '2')
+;
+analyze t_base;
 
+-- street orders
+insert into t_base
+select cl.*
+from dwh.client_order cl
+         join t_base par on cl.parent_order_id = cl.order_id
+    and cl.create_date_id between :l_date_begin_id and :l_date_end_id
+    and case
+            when coalesce(:in_account_ids, '{}') = '{}' then true
+            else cl.account_id = any (:in_account_ids) end
+    and cl.parent_order_id is not null
+    and cl.trans_type in ('D', 'G')
+    and cl.multileg_reporting_type in ('1', '2');
 
 -- parent cancels
 
--- street orders
+
 
 -- strett cancels
 
