3922cc44f06f0a55dd92273640b884bb7d539938
Oleh Sydor <ole.sydor@gmail.com>
2026-02-26 18:00:15 +0200

    Regular auto-commit at 18:00:15 on 26/02/2026

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/LifeCycle2.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/LifeCycle2.sql
@@ -12,8 +12,41 @@ where true
           when coalesce(:in_client_order_ids, '{}') = '{}' then true
           else gtc.client_order_id = any (:in_client_order_ids) end;
 
+-- obo routes
+create temp table t_route as
+select *
+from (values ('D', 'New Order', 1),
+             ('D', 'Order Route', 2),
+             ('G', 'Order Modify', 1),
+             ('G', 'Order Modify Route', 2))
+         as t(trans_type, order_type_value, rn)
+where true
+  and case when :in_include_routes = 'Y' then true else rn = 1 end;
+
 
 -- parent orders
+drop table if exists t_base;
+create temp table t_base as
+select cl.*
+from dwh.client_order cl
+         join dwh.d_account ac on ac.account_id = cl.account_id and ac.is_active
+         join dwh.d_instrument di on di.instrument_id = cl.instrument_id and di.is_active
+         left join dwh.d_fix_connection fc
+                   on fc.fix_connection_id = cl.fix_connection_id and fc.is_active = true
+where cl.parent_order_id is null
+  and cl.create_date_id between :l_date_begin_id and :l_date_end_id
+  and case
+          when coalesce(:in_account_ids, '{}') = '{}' then true
+          else cl.account_id = any (:in_account_ids) end
+  and case
+          when coalesce(:in_parent_order_ids, '{}') = '{}' then true
+          else cl.order_id = any (:in_parent_order_ids) end
+  and case when :in_instrument_type is null then true else di.instrument_type_id = :in_instrument_type end
+  and case when :in_exclude_eos = 'N' then true else fc.is_high_frequency_trader = 'N' end
+  and case when :in_fix_comp_ids = '{}' then true else coalesce(fc.fix_comp_id, '') = any (:in_fix_comp_ids) end
+  and case when :in_client_order_ids = '{}' then true else cl.client_order_id = any (:in_client_order_ids) end
+  and cl.trans_type <> 'F';
+
 
 -- parent cancels
 
