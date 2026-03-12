51bbeaea26c4b05764434a77d3303c768bbdd391
Oleh Sydor <ole.sydor@gmail.com>
2026-03-12 11:00:13 +0200

    Regular auto-commit at 11:00:13 on 12/03/2026

--- f9447fdb-8def-4b3f-af2b-edc8e5d1d9d5/DS-11244.sql
+++ f9447fdb-8def-4b3f-af2b-edc8e5d1d9d5/DS-11244.sql
@@ -24,3 +24,52 @@ comment on column genesis2.ptm_alloc_drop_message_status.msg_type is 'Message ty
 comment on column genesis2.ptm_alloc_drop_message_status.msg_status is 'N - new, S - sent, A - accepted, R - rejected, P - partial accepted, I - internally rejected';
 comment on column genesis2.ptm_alloc_drop_message_status.reject_reason is 'Reject reason';
 comment on column genesis2.ptm_alloc_drop_message_status.orig_trade_record_id is 'Linked orig trade_record_id';
+
+
+-- DROP FUNCTION dash360.ptm_alloc_drop_message_status_init(int8, bpchar);
+create function dash360.ptm_alloc_drop_message_status_init(in_orig_trade_record_id bigint, in_msg_type character)
+    returns integer
+    language plpgsql
+as
+$fn$
+declare
+    l_ptm_drop_message_status_id int4;
+begin
+    if in_msg_type in ('N', 'C') and not exists (select null
+                                                 from genesis2.ptm_alloc_drop_message_status
+                                                 where orig_trade_record_id = in_orig_trade_record_id
+                                                   and msg_type = in_msg_type) then
+        insert into genesis2.ptm_alloc_drop_message_status(orig_trade_record_id, msg_type)
+        values (in_orig_trade_record_id, in_msg_type)
+        returning ptm_drop_message_status_id into l_ptm_drop_message_status_id;
+    else
+        l_ptm_drop_message_status_id := -1;
+    end if;
+    return l_ptm_drop_message_status_id;
+end;
+$fn$
+;
+comment on function dash360.ptm_alloc_drop_message_status_init(int8, bpchar) is 'Insert data into ptm_alloc_drop_message_status';
+
+
+-- DROP FUNCTION dash360.ptm_alloc_drop_message_status_update(int4, bpchar, text);
+
+create function dash360.ptm_alloc_drop_message_status_update(in_ptm_drop_message_status_id integer,
+                                                             in_msg_status character, in_reject_reason text)
+    returns integer
+    language plpgsql
+as
+$function$
+declare
+    l_ptm_drop_message_status_id int4;
+begin
+    update genesis2.ptm_alloc_drop_message_status adms
+    set msg_status    = in_msg_status,
+        reject_reason = in_reject_reason
+    where adms.ptm_drop_message_status_id = in_ptm_drop_message_status_id
+    returning in_ptm_drop_message_status_id into l_ptm_drop_message_status_id;
+
+    return l_ptm_drop_message_status_id;
+end;
+$function$
+;
