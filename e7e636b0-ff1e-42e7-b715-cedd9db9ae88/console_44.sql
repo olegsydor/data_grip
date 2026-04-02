082b81a9554bf43989f6d9a1940389f53720e157
Oleh Sydor <ole.sydor@gmail.com>
2026-04-02 14:00:09 +0300

    Regular auto-commit at 14:00:09 on 02/04/2026

--- e7e636b0-ff1e-42e7-b715-cedd9db9ae88/console_44.sql
+++ e7e636b0-ff1e-42e7-b715-cedd9db9ae88/console_44.sql
@@ -8,4 +8,16 @@ with client_groups as (select CustomerID,
                      join client_groups g using (CustomerID))
 select array_agg(CustomerID), grp, count(*)
 from nt
+group by grp;
+
+with client_groups as (select CustomerID,
+                              cnt,
+                              ntile(10) over (order by CustomerID, cnt) as grp
+                       from (select CustomerID as CustomerID,
+                                    count(*) as cnt
+                             from billing.TBillingOrderDetail_Daily_inc
+                             group by CustomerID) t)
+
+select array_agg(distinct CustomerID), grp, sum(cnt)
+from client_groups
 group by grp
