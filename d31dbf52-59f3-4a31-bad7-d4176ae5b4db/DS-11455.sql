54b5a7c4eec44c3d927c7229456001a09089622c
Oleh Sydor <ole.sydor@gmail.com>
2026-04-27 18:00:09 +0300

    Regular auto-commit at 18:00:09 on 27/04/2026

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/DS-11455.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/DS-11455.sql
@@ -1,4 +1,15 @@
 -- https://dashfinancial.atlassian.net/browse/DEVREQ-8133
 -- https://dashfinancial.atlassian.net/browse/DS-11455
 
-drop function if exists dash360.
+create or replace function dash360.execution_tradestation(in_start_date_id integer DEFAULT public.get_dateid(CURRENT_DATE),
+                                                       in_end_date_id integer DEFAULT public.get_dateid(CURRENT_DATE),
+                                                       in_trading_firm_ids character varying[] DEFAULT '{}'::character varying [])
+returns table (ret_row text)
+language plpgsql
+as $fx$
+    declare
+
+    begin
+
+    end;
+    $fx$
