dea558358657d1dbf0969d0e456bde425f31b1e6
Oleh Sydor <ole.sydor@gmail.com>
2026-05-07 18:00:10 +0300

    Regular auto-commit at 18:00:10 on 07/05/2026

--- 2f6b5bb2-070a-4a74-b12b-b83f406f4417/console_26.sql
+++ 2f6b5bb2-070a-4a74-b12b-b83f406f4417/console_26.sql
@@ -295,3 +295,42 @@ select filename,
                   and start_processing is not null
 
 
+CREATE TABLE dwh.d_liquidity_indicator (
+	sq_liquidity_indicator serial4 NOT NULL,
+	description varchar(256) NULL,
+	exchange_id varchar(6) NOT NULL,
+	is_grey varchar(1) NULL,
+	liquidity_indicator_type_id int2 NULL,
+	trade_liquidity_indicator varchar(256) NOT NULL,
+	is_active bool NULL,
+	start_date date NULL,
+	end_date date NULL,
+	CONSTRAINT d_liquidity_indicator_pkey PRIMARY KEY (sq_liquidity_indicator)
+);
+
+insert into dwh.d_liquidity_indicator (description, exchange_id, is_grey, liquidity_indicator_type_id,
+                                       trade_liquidity_indicator, is_active, start_date)
+values ('Early Session, Removes Liquidity, Displayed Retail Order (All Tapes)', 'EPRL', 'N', 2, 'eR', TRUE, to_date('27-04-26', 'DD-MM-YY')),
+       ('Late Session, Removes Liquidity, Displayed Retail Order (All Tapes)', 'EPRL', 'N', 2, 'fR', TRUE, to_date('27-04-26', 'DD-MM-YY')),
+       ('Early Session, Removes Liquidity, Non-Displayed Retail Order (All Tapes)', 'EPRL', 'Y', 2, 'er', TRUE,
+        to_date('27-04-26', 'DD-MM-YY')),
+       ('Late Session, Removes Liquidity, Non-Displayed Retail Order (All Tapes)', 'EPRL', 'Y', 2, 'fr', TRUE,
+        to_date('27-04-26', 'DD-MM-YY')),
+       ('Removes Retail Liquidity, Displayed Order (All Tapes)', 'EPRL', 'N', 2, 'RT', TRUE, to_date('27-04-26', 'DD-MM-YY')),
+       ('Early Session, Removes Retail Liquidity, Displayed Order (All Tapes)', 'EPRL', 'N', 2, 'eT', TRUE, to_date('27-04-26', 'DD-MM-YY')),
+       ('Late Session, Removes Retail Liquidity, Displayed Order (All Tapes)', 'EPRL', 'N', 2, 'fT', TRUE, to_date('27-04-26', 'DD-MM-YY')),
+       ('Removes Retail Liquidity, Non-Displayed Order (All Tapes)', 'EPRL', 'Y', 2, 'Rt', TRUE, to_date('27-04-26', 'DD-MM-YY')),
+       ('Early Session, Removes Retail Liquidity, Non-Displayed Order (All Tapes)', 'EPRL', 'Y', 2, 'et', TRUE,
+        to_date('27-04-26', 'DD-MM-YY')),
+       ('Late Session, Removes Retail Liquidity, Non-Displayed Order (All Tapes)', 'EPRL', 'Y', 2, 'ft', TRUE,
+        to_date('27-04-26', 'DD-MM-YY')),
+       ('Early Session, Removes Liquidity at Midpoint, Midpoint Peg Order (All Tapes)', 'EPRL', 'Y', 2, 'ep', TRUE,
+        to_date('27-04-26', 'DD-MM-YY')),
+       ('Late Session, Removes Liquidity at Midpoint, Midpoint Peg Order (All Tapes)', 'EPRL', 'Y', 2, 'fp', TRUE,
+        to_date('27-04-26', 'DD-MM-YY')),
+       ('Not Applicable', 'EPRL', 'N', NULL, 0, TRUE, to_date('27-04-26', 'DD-MM-YY')),
+       ('NBBO Setter', 'EPRL', 'N', 1, 1, TRUE, to_date('27-04-26', 'DD-MM-YY')),
+       ('NBBO Joiner', 'EPRL', 'N', 1, 2, TRUE, to_date('27-04-26', 'DD-MM-YY')),
+       ('NBBO First Joiner', 'EPRL', 'N', 1, 3, TRUE, to_date('27-04-26', 'DD-MM-YY'));
+
+select * from dwh.d_liquidity_indicator
