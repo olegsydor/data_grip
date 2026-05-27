b5c9e38fd6edb564c20c30a048871c787a8f7799
Oleh Sydor <ole.sydor@gmail.com>
2026-05-27 17:00:10 +0300

    Regular auto-commit at 17:00:10 on 27/05/2026

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_172.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_172.sql
@@ -1,22 +1,15 @@
+create temp table t_os as
 select *
 from trash.so_report_rps_s3(in_start_date_id := 20260401, in_end_date_id := 20260401, in_account_ids := '{77005}',
-                         in_is_multi_leg := 'N')
+                            in_is_multi_leg := 'N');
 
+select * from t_os
+where ret_row ilike '%440160506409384778%'
 
-
-CREATE or replace FUNCTION trash.so_report_rps_s3(in_start_date_id integer, in_end_date_id integer,
-                                    in_account_ids integer[] DEFAULT '{}'::integer[],
-                                    in_is_multi_leg character DEFAULT 'N'::bpchar,
-                                    in_trading_firm_ids character varying[] DEFAULT '{}'::character varying[],
-                                    in_exclude_blaze boolean DEFAULT true,
-                                    in_cl_ord_ids varchar(256)[] default '{}'::varchar(256)[])
-    RETURNS TABLE
-            (
-                ret_row text
-            )
-    LANGUAGE plpgsql
-AS
-$function$
+CREATE OR REPLACE FUNCTION trash.so_report_rps_s3(in_start_date_id integer, in_end_date_id integer, in_account_ids integer[] DEFAULT '{}'::integer[], in_is_multi_leg character DEFAULT 'N'::bpchar, in_trading_firm_ids character varying[] DEFAULT '{}'::character varying[], in_exclude_blaze boolean DEFAULT true)
+ RETURNS TABLE(ret_row text)
+ LANGUAGE plpgsql
+AS $function$
     -- 2024-04-23 SO: https://dashfinancial.atlassian.net/browse/DS-8251 added in_trading_firm_ids as an input parameter
     -- SO 20240523 https://dashfinancial.atlassian.net/browse/DEVREQ-4264 add coalesce to account\trading firm input parameters
     -- SO 20250219 https://dashfinancial.atlassian.net/browse/DS-9608 Performance improvement
@@ -113,7 +106,7 @@ drop table if exists t_report;
                        end
                end || '|' ||
            case
-               when not l_is_multileg then 'DFIN'
+               when not l_is_multileg then coalesce(cl.exchange_id, 'DFIN')
                else
                    case
                        when cl.parent_order_id is null then 'DFIN'
@@ -213,7 +206,6 @@ drop table if exists t_report;
     where true
       and case when l_account_ids = '{}' then true else cl.account_id = any (l_account_ids) end
       and cl.create_date_id between in_start_date_id and in_end_date_id
-      and case when in_cl_ord_ids = '{}' then true else cl.client_order_id = any(in_cl_ord_ids) end
       and cl.trans_type <> 'F'
       and case when l_is_multileg then cl.parent_order_id is null else true end
       and case
@@ -250,7 +242,6 @@ drop table if exists t_report;
 --                           and cl.order_id = ex.order_id
       and case when l_account_ids = '{}' then true else cl.account_id = any (l_account_ids) end
       and cl.trans_type <> 'F'
-      and case when in_cl_ord_ids = '{}' then true else cl.client_order_id = any(in_cl_ord_ids) end
       and case when in_exclude_blaze then coalesce(cl.ex_destination, '') not ilike 'blaze' else true end
       and case when in_exclude_blaze then coalesce(cl.exchange_id, '') not ilike 'blaze' else true end;
 
@@ -276,7 +267,6 @@ drop table if exists t_report;
       and cl.create_date_id < in_start_date_id
       and gtc.close_date_id is null
       and case when l_account_ids = '{}' then true else cl.account_id = any (l_account_ids) end
-      and case when in_cl_ord_ids = '{}' then true else cl.client_order_id = any(in_cl_ord_ids) end
       and cl.trans_type <> 'F'
       and case when in_exclude_blaze then coalesce(cl.ex_destination, '') not ilike 'blaze' else true end
       and case when in_exclude_blaze then coalesce(cl.exchange_id, '') not ilike 'blaze' else true end;
@@ -465,191 +455,3 @@ drop table if exists t_report;
 end;
 $function$
 ;
-
-
-  select
-      ac.broker_dealer_mpid,
-      'NO'                                      as record_type,
-           coalesce(cl.parent_order_id, cl.order_id) as order_id,
-           to_char(cl.process_time, 'HH24MISSFF3')   as time_id,
-           cl.client_order_id                        as record_id,
-           1                                         as record_type_id,
-           'O' , -- || '|' ||
-           case
-               when ((:l_is_multileg and cl.multileg_reporting_type = '3') or
-                     (not :l_is_multileg and cl.parent_order_id is null)) then 'NO'
-               else 'RO'
-               end , -- || '|' ||
-           cl.client_order_id , -- || '|' ||
-           cl.order_id::text , -- || '|' || --source_order_id
-           case
-               when :l_is_multileg and cl.multileg_reporting_type = '2' then cl.client_order_id::text
-               when not :l_is_multileg then coalesce(cl.parent_order_id::text, '')
-               else ''
-               end , -- || '|' || --source_parent_id
-           coalesce(cl.orig_order_id::text, '') , -- || '|' ||
-               --
-           case
-               when not :l_is_multileg
-                   then ''
-               else
-                   case
-                       when cl.multileg_reporting_type = '3' then cl.order_id::text
-                       when cl.multileg_reporting_type = '2' then cl.multileg_order_id::text
-                       end
-               end , -- || '|' || --
-           case
-               when not :l_is_multileg then ac.broker_dealer_mpid
-               else
-                   case
-                       when cl.parent_order_id is null and ac.broker_dealer_mpid = 'NONE' then ''
-                       when cl.parent_order_id is null then ac.broker_dealer_mpid
-                       else 'DFIN'
-                       end
-               end , -- || '|' ||
-           case
-               when not :l_is_multileg then coalesce(cl.exchange_id, 'DFIN')
-               else
-                   case
-                       when cl.parent_order_id is null then 'DFIN'
-                       else coalesce(exc.mic_code, exc.eq_mpid, '') end
-               end
-               , -- || '|' ||
-           '' , -- || '|' ||
-           '' , -- || '|' ||
-           case cl.multileg_reporting_type when '3' then '' else i.instrument_type_id end , -- || '|' ||
-           case i.instrument_type_id when 'E' then i.display_instrument_id when 'O' then oc.opra_symbol else '' end ||
-           '|' ||
-           '' , -- || '|' || --primary Exchange
-           case cl.side when '1' then 'B' when '2' then 'S' when '5' then 'SS' when '6' then 'SSE' else '' end ||
-           '|' || --OrderAction
-           to_char(cl.process_time, 'YYYYMMDD') || 'T' || to_char(cl.process_time, 'HH24MISSFF3') , -- || '|' ||
-           ot.order_type_short_name , -- || '|' || --order_type
-           case
-               when not :l_is_multileg then cl.order_qty::text
-               else
-                   case when cl.multileg_reporting_type = '3' then '' else cl.order_qty::text end
-               end , -- || '|' || --order_volume
-           coalesce(to_char(cl.price, 'FM99990D0099'),'') , -- || '|' ||
-           coalesce(to_char(cl.stop_price, 'FM99990D0099'), '') , -- || '|' ||
-           tif.tif_short_name , -- || '|' ||
-           case
-               when not :l_is_multileg then
-                   coalesce(to_char(cl.expire_time, 'YYYYMMDD'), '') || 'T' ||
-                   coalesce(to_char(cl.expire_time, 'HH24MISSFF3'), '')
-               else
-                   case
-                       when cl.expire_time is not null then
-                           coalesce(to_char(cl.expire_time, 'YYYYMMDD'), '') || 'T' ||
-                           coalesce(to_char(cl.expire_time, 'HH24MISSFF3'), '')
-                       when cl.time_in_force_id = '6' then
-                           (select coalesce(fmj.fix_message ->> '432', '') || 'T235959000'
-                            from fix_capture.fix_message_json fmj
-                            where fix_message_id = cl.fix_message_id
-                              and fmj.date_id = cl.create_date_id
-                            limit 1)
-                       else ''
-                       end
-               end , -- || '|' || --22
-           '0' , -- || '|' || --PRE_MARKET_IND
-           '' , -- || '|' ||
-           '0' , -- || '|' || --POST_MARKET_IND
-           '' , -- || '|' ||
-           case
-               when cl.parent_order_id is null then case cl.sub_strategy_desc when 'DMA' then '1' else '0' end
-               else case po.sub_strategy_desc when 'DMA' then '1' else '0' end
-               end , -- || '|' || --DIRECTED_ORDER_IND
-           case
-               when (cl.parent_order_id is null or :l_is_multileg)
-                   then case cl.sub_strategy_desc when 'SMOKE' then '1' else '0' end
-               else case po.sub_strategy_desc when 'SMOKE' then '1' else '0' end
-               end , -- || '|' || --NON_DISPLAY_IND
-           '0' , -- || '|' || --DO_NOT_REDUCE
-           case cl.exec_instruction when 'G' then '1' else '0' end , -- || '|' ||
-           case cl.exec_instruction when '1' then '1' else '0' end , -- || '|' || --NOT_HELD_IND [31]
-           '0' , -- || '|' || --[32]
-           '0' , -- || '|' || --[33]
-           '0' , -- || '|' || --[34]
-           '' , -- || '|' || --[35]
-           '' , -- || '|' || --[36]
-           '' , -- || '|' || --[37]
-           '' , -- || '|' || --[38]
-           case
-               when :l_is_multileg then coalesce(cl.ex_destination, '')
-               else '' end , -- || '|' || --[39]
-           case
-               when (:l_is_multileg and cl.multileg_reporting_type = '3') then coalesce(cl.no_legs::text, '')
-               else '' end , -- || '|' || --[40]
-           '' , -- || '|' || --[41]
-           '' , -- || '|' || --[42]
-           '' , -- || '|' || --[43]
-           '' , -- || '|' || --[44]
-           '' , -- || '|' || --[45]
-           '' , -- || '|' || --[46]
-           '' , -- || '|' || --[47]
-           '' --[48]
-                                                     as REC
-    from dwh.client_order cl
-             inner join dwh.d_account ac on ac.account_id = cl.account_id
-             inner join dwh.d_instrument i on i.instrument_id = cl.instrument_id and i.is_active
-             left join lateral (select po.sub_strategy_desc
-                                from dwh.client_order po
-                                where po.order_id = cl.parent_order_id
-                                  and po.create_date_id <= cl.create_date_id
-                                limit 1) po on true
-             left join dwh.d_option_contract oc on oc.instrument_id = i.instrument_id and oc.is_active
-             left join dwh.d_option_series os on os.option_series_id = oc.option_series_id and os.is_active
-             left join dwh.d_order_type ot on ot.order_type_id = cl.order_type_id
-             left join dwh.d_time_in_force tif on tif.tif_id = cl.time_in_force_id
-             left join lateral (select *
-                                from dwh.d_exchange exc
-                                where exc.exchange_id = cl.exchange_id and exc.is_active
-                                limit 1) exc on true
-    where true
---       and cl.account_id = 77005
---       and cl.create_date_id between :in_start_date_id and :in_end_date_id
---       and cl.trans_type <> 'F'
---       and cl.client_order_id in ('gTD00xKQRW4J2')
-     and cl.parent_order_id = 440160506409384778
---       and case when :l_is_multileg then cl.parent_order_id is null else true end
---       and case
---               when :l_is_multileg then cl.multileg_reporting_type in ('2', '3')
---               else cl.multileg_reporting_type = '1' end
---       and case when in_exclude_blaze then coalesce(cl.ex_destination, '') not ilike 'blaze' else true end
---       and case when in_exclude_blaze then coalesce(cl.exchange_id, '') not ilike 'blaze' else true end
-
-
-      select trans_type, * from dwh.client_order cl
-          where true
---       and cl.client_order_id in ('gTD00xKQRW4J2')
-and parent_order_id = 440160506409384778
-
-          DCCC1989-20260331
-
-
-        select case
-                   when record_type = 'H' then rec || '|' ||
-                                               :in_start_date_id::text || 'T' || min_time || '|' || --Starting Event
-                                               :in_end_date_id::text || 'T' || max_time || '|' || --Ending Event
-                                               'DFIN' || '|' ||
---                                                'DAIN' || '|' ||
-                                               (select coalesce(cat_imid, '')
-                                                from dwh.d_account
-                                                         join dwh.d_trading_firm using (trading_firm_id)
-                                                where true
-                                                  and case
-                                                          when l_account_ids = '{}' then true
-                                                          else account_id = any (l_account_ids) end
-                                                  and cat_imid is not null
-                                                limit 1) || '|' ||
-                                               'dashtradedesk@iongroup.com' || '|' ||
-                                               ''
-                   else rec
-                   end
-        from (select min(time_id) over () as min_time,
-                     max(time_id) over () as max_time,
-                     record_type,
-                     rec
-              from t_report
-
-              order by order_id, time_id, record_id, record_type_id) x;
