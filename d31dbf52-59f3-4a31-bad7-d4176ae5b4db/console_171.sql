2572b20fe26fb118bacc996b025c9b6b6a615435
Oleh Sydor <ole.sydor@gmail.com>
2026-06-15 20:00:22 +0300

    Regular auto-commit at 20:00:21 on 15/06/2026

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_171.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/console_171.sql
@@ -949,3 +949,124 @@ where true
                   when coalesce(:in_account_ids, '{}') <> '{}'::integer[] then account_id = ANY (:in_account_ids)
                   else true end;
 ---------------------
+
+   drop table if exists tmp_base;
+    create temp table tmp_base as
+-- head and legs
+    select 'NO' as noro,
+           cl.create_date_id,
+           cl.parent_order_id,
+           null as parent_client_order_id,
+           cl.order_id,
+           cl.process_time,
+           cl.client_order_id,
+           cl.instrument_id,
+           cl.account_id,
+           cl.trans_type,
+           cl.multileg_reporting_type,
+           cl.multileg_order_id,
+           cl.time_in_force_id
+    from dwh.client_order cl
+    where true
+--       and client_order_id = '00214105960ESNY1'
+      and cl.create_date_id between :in_start_date_id and :in_end_date_id
+--       and case when :l_account_ids = '{}' then true else cl.account_id = any (:l_account_ids) end
+      and cl.trans_type <> 'F'
+      and cl.time_in_force_id not in ('1', '6')
+      and cl.client_order_id = '00214105960ESNY1'
+      and case
+              when :l_is_multileg then cl.multileg_reporting_type in ('2', '3')
+              else cl.multileg_reporting_type = '1' end
+--       and case when in_exclude_blaze then cl.ex_destination is distinct from 'blaze' else true end
+--       and case when in_exclude_blaze then cl.exchange_id is distinct from 'blaze' else true end;
+;
+
+    get diagnostics l_row_cnt = row_count;
+    select public.load_log(l_load_id, l_step_id, l_msg || '  daily orders added  ===', l_row_cnt, 'O')
+    into l_step_id;
+
+    -- gtc parent orders
+-- head and legs
+    drop table if exists tmp_base_gtc;
+    create temp table tmp_base_gtc as
+    select 'NO' as noro,
+           cl.create_date_id,
+           cl.parent_order_id,
+           null as parent_client_order_id,
+           cl.order_id,
+           cl.process_time,
+           cl.client_order_id,
+           cl.instrument_id,
+           cl.account_id,
+           cl.trans_type,
+           cl.multileg_reporting_type,
+           cl.multileg_order_id,
+           cl.time_in_force_id
+    from dwh.client_order cl
+             join dwh.gtc_order_status gtc using (create_date_id, order_id)
+    where true
+--         and order_id = 416550869289618526
+--       and cl.client_order_id = '00214105960ESNY1'
+      and cl.parent_order_id is null
+      and case when :l_account_ids = '{}' then true else cl.account_id = any (:l_account_ids) end
+      and case when :l_account_ids = '{}' then true else gtc.account_id = any (:l_account_ids) end
+      and cl.trans_type <> 'F'
+      and gtc.create_date_id > :l_gtc_min_date_id
+      and cl.time_in_force_id in ('1', '6')
+      and cl.create_date_id < :in_start_date_id
+      and (gtc.close_date_id is null or gtc.close_date_id > :in_end_date_id)
+      and case
+              when :l_is_multileg then cl.multileg_reporting_type in ('2', '3')
+              else cl.multileg_reporting_type = '1' end
+       and cl.client_order_id = '00214105960ESNY1'
+      and case when in_exclude_blaze then cl.ex_destination is distinct from 'blaze' else true end
+      and case when in_exclude_blaze then cl.exchange_id is distinct from 'blaze' else true end;
+
+    get diagnostics l_row_cnt = row_count;
+    select public.load_log(l_load_id, l_step_id, l_msg || '  gtc orders added  ===', l_row_cnt, 'O')
+    into l_step_id;
+
+    insert into tmp_base
+    select *
+    from tmp_base_gtc;
+
+    analyse tmp_base;
+    create index on tmp_base (order_id, create_date_id);
+
+-- streets of legs
+    drop table if exists tmp_base_str;
+    create temp table tmp_base_str as
+    select 'RO',
+           cl.create_date_id,
+           cl.parent_order_id,
+           tmp.client_order_id as parent_client_order_id,
+           cl.order_id,
+           cl.process_time,
+           cl.client_order_id,
+           cl.instrument_id,
+           cl.account_id,
+           cl.trans_type,
+           cl.multileg_reporting_type,
+           cl.multileg_order_id,
+           cl.time_in_force_id
+    from dwh.client_order cl
+             join tmp_base tmp on tmp.order_id = cl.parent_order_id
+    where true
+      and cl.parent_order_id is not null
+--       and case when l_account_ids = '{}' then true else cl.account_id = any (l_account_ids) end
+      and cl.create_date_id >= tmp.create_date_id
+      and cl.create_date_id > :l_gtc_min_date_id
+      and case
+              when :l_is_multileg then cl.multileg_reporting_type in ('2')
+              else cl.multileg_reporting_type = '1' end
+      and case when in_exclude_blaze then cl.ex_destination is distinct from 'blaze' else true end
+      and case when in_exclude_blaze then cl.exchange_id is distinct from 'blaze' else true end;
+    get diagnostics l_row_cnt = row_count;
+    select public.load_log(l_load_id, l_step_id, l_msg || '  street orders added  ===', l_row_cnt, 'O')
+    into l_step_id;
+
+    insert into tmp_base
+    select *
+    from tmp_base_str;
+
+    create index on tmp_base (order_id, create_date_id);
