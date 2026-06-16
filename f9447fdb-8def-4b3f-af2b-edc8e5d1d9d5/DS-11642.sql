a2abd9f1813e75bee88f3b0a44f04840aa995153
Oleh Sydor <ole.sydor@gmail.com>
2026-06-16 15:00:08 +0300

    Regular auto-commit at 15:00:08 on 16/06/2026

--- f9447fdb-8def-4b3f-af2b-edc8e5d1d9d5/DS-11642.sql
+++ f9447fdb-8def-4b3f-af2b-edc8e5d1d9d5/DS-11642.sql
@@ -1385,7 +1385,8 @@ genesis2.get_manual_broker(in_subsystem_id := tr.subsystem_id, in_date_id := tr.
                                             and tl.trade_record_id = tr.trade_record_id) L1
                                     where rn = 1) ccru on true
 
-        where tr.is_busted = 'N'
+        where true
+--             tr.is_busted = 'N'
           and tr.date_id = :l_date_id
           and a.alloc_instr_id = :in_alloc_instr_id;
 
@@ -1399,4 +1400,158 @@ select manual_broker
         from staging.trade_record_blaze7
         where date_id = 20260615
           and trade_record_id = 2348069241
-          and manual_broker is not null
+          and manual_broker is not null;
+
+
+select * from dash360.allocations_instruction_commission_rate(-126289, 20260612)
+-- DROP FUNCTION dash360.allocations_instruction_commission_rate(int4, int4);
+
+CREATE OR REPLACE FUNCTION dash360.allocations_instruction_commission_rate(in_alloc_instr_id integer,
+                                                                           in_date_id integer DEFAULT get_dateid(CURRENT_DATE))
+    RETURNS TABLE
+            (
+                date_id                  integer,
+                trade_record_id          bigint,
+                account_id               integer,
+                instrument_id            bigint,
+                side                     character,
+                open_close               character,
+                avg_px                   numeric,
+                exec_qty                 integer,
+                display_instrument_id    character varying,
+                last_trade_date          date,
+                instrument_type_id       character,
+                alloc_instr_id           integer,
+                alloc_time               timestamp without time zone,
+                is_allocated             boolean,
+                is_bundle                boolean,
+                cmta                     character varying,
+                exec_broker              character varying,
+                principal_amount         numeric,
+                client_commission_rate   numeric,
+                username                 character varying,
+                client_commission_amount numeric,
+                client_order_id          character varying,
+                clearing_submitted_away  character,
+                client_order_status      character,
+                broker_commission_rate   numeric,
+                broker_commission_amount numeric,
+                manual_broker_code       character varying
+            )
+    LANGUAGE plpgsql
+    COST 1
+AS
+$function$
+    -- SO 20260319 https://dashfinancial.atlassian.net/browse/DS-11284 Support client_order_id in dash360.allocations_instruction_commission_rate
+    -- SO 20260323 https://dashfinancial.atlassian.net/browse/DS-11290 Support client_order_status in dash360.allocations_instruction_commission_rate
+    -- SO 20260414 https://dashfinancial.atlassian.net/browse/DS-11390 Return client_order_status for Trade Records from OMS (OMS_EDW) as filled (2)
+    -- SO 20260616 https://dashfinancial.atlassian.net/browse/DS-11642 Add broker commissions rate and amount
+    -- SO 20260616 https://dashfinancial.atlassian.net/browse/DS-11642 Add manual_broker_code
+declare
+    l_sg_accounts int8[];
+begin
+
+    /*
+select array_agg(account_id)
+--     into l_sg_accounts
+from genesis2.account
+where false
+*/
+    return query
+        select a.date_id,
+               null::bigint                                                  as trade_record_id,
+               a.account_id,
+               a.instrument_id,
+               a.side,
+               a.open_close,
+               a.avg_px,
+               a.total_qty                                                   as exec_qty,
+               i.display_instrument_id,
+               i.last_trade_date::date,
+               i.instrument_type_id,
+               a.alloc_instr_id,
+               a.create_time                                                 as alloc_time,
+               true                                                          as is_allocated,
+               true                                                          as is_bundle,
+               null::character varying                                       as cmta,
+               null::character varying                                       as exec_broker,
+               case i.instrument_type_id
+                   when 'O' then a.total_qty * a.avg_px * os.contract_multiplier
+                   else a.total_qty * a.avg_px
+                   end                                                       as principal_amount,
+               ccr.ccru_rate                                                 as client_commission_rate,
+               ui.user_name,
+               ccru_amount                                                   as client_commission_amount,
+               (case
+                    when array_length(ccr.client_order_id, 1) > 1 then '-'
+                    else ccr.client_order_id[1] end)::character varying(256) as client_order_id,
+               a.clearing_submitted_away::character,
+               (case
+                    when array_length(ccr.client_order_status, 1) > 1 then '-'
+                    else ccr.client_order_status[1] end)::character          as client_order_status,
+               ccr.brok_rate                                                 as broker_commission_rate,
+               ccr.brok_amount                                               as broker_commission_rate,
+               case
+                   when array_length(ccr.manual_broker, 1) > 1 then '-'
+                   else ccr.client_order_id[1] end                           as manual_broker_code
+
+        from allocation_instruction a
+                 inner join instrument i on (a.instrument_id = i.instrument_id)
+                 left join user_identifier ui on a.created_by_user_id = ui.user_id
+                 left join option_contract oc on i.instrument_id = oc.instrument_id
+                 left join option_series os on oc.option_series_id = os.option_series_id
+
+
+                 left join lateral (select sum(l1.ccru_rate * tr.last_qty) / nullif(sum(tr.last_qty), 0)                       as ccru_rate,
+                                           sum(ccru_amount)                                                                    as ccru_amount,
+                                           string_agg(distinct tr.exec_broker, ', ')                                           as exec_broker,
+                                           array_agg(distinct tr.client_order_id)                                              as client_order_id,
+                                           array_agg(distinct fpo.order_status)                                                as client_order_status,
+                                           sum(l1.brok_rate * tr.last_qty) / nullif(sum(tr.last_qty), 0)                       as brok_rate,
+                                           sum(brok_amount)                                                                    as brok_amount,
+                                           array_agg(distinct
+                                                     genesis2.get_manual_broker(in_subsystem_id := tr.subsystem_id,
+                                                                                in_date_id := tr.date_id,
+                                                                                in_trade_record_id := tr.trade_record_id,
+                                                                                in_fix_message_id := tr.trade_fix_message_id)) as manual_broker
+                                    from genesis2.alloc_instr2trade_record alt
+                                             inner join genesis2.trade_record tr
+                                                        on alt.trade_record_id = tr.trade_record_id and alt.date_id = tr.date_id
+
+                                             left join lateral (
+                                        select max(case when book_record_type_id = 'CCRU' then l0.rate end)   as ccru_rate,
+                                               max(case when book_record_type_id = 'CCRU' then l0.amount end) as ccru_amount,
+                                               max(case when book_record_type_id = 'BROK' then l0.rate end)   as brok_rate,
+                                               max(case when book_record_type_id = 'BROK' then l0.amount end) as brok_amount
+                                        from (select rate,
+                                                     tl.amount,
+                                                     book_record_type_id,
+                                                     row_number()
+                                                     over (partition by tl.trade_record_id , book_record_type_id , billing_entity order by cr.priority ) as rn
+                                              from genesis2.trade_level_book_record tl
+                                                       inner join genesis2.book_record_creator cr
+                                                                  on tl.book_record_creator_id = cr.book_record_creator_id
+                                              where tl.date_id = in_date_id
+                                                AND tl.book_record_type_id in ('CCRU', 'BROK')
+                                                and tl.trade_record_id = alt.trade_record_id) l0
+                                        where true
+                                          and (l0.rn = 1 or l0.rn is null)
+                                        ) l1 on true
+                                             left join lateral (select distinct case when tr.subsystem_id = 'OMS_EDW' then '2' else fpo.order_status end
+                                                                from staging.f_parent_order fpo
+                                                                where fpo.status_date_id = in_date_id
+                                                                  and fpo.parent_order_id = tr.order_id) fpo on true
+                                    where alt.alloc_instr_id = a.alloc_instr_id
+                                      and tr.is_busted = 'N'
+                                      and alt.date_id = in_date_id
+                                      and tr.date_id = in_date_id
+                                    limit 1
+            ) ccr on true
+
+
+        where a.alloc_instr_id = in_alloc_instr_id
+          and a.is_deleted = 'N';
+
+end;
+$function$
+;
