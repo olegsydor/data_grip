933b5ef5d67e55b000dd046767dd49e7ee991851
Oleh Sydor <ole.sydor@gmail.com>
2026-06-16 13:00:09 +0300

    Regular auto-commit at 13:00:09 on 16/06/2026

--- f9447fdb-8def-4b3f-af2b-edc8e5d1d9d5/DS-11642.sql
+++ f9447fdb-8def-4b3f-af2b-edc8e5d1d9d5/DS-11642.sql
@@ -1175,6 +1175,167 @@ where date_id = 20260612;
 
 select *
 from dash360.get_data_for_allocation_drop(-126289, 20260612);
--126290
--126291
--126292
+
+
+
+-- DROP FUNCTION dash360.allocations_instruction_trades(int4);
+
+CREATE OR REPLACE FUNCTION dash360.allocations_instruction_trades(in_alloc_instr_id integer)
+    RETURNS TABLE
+            (
+                date_id                  integer,
+                trade_record_id          bigint,
+                account_id               integer,
+                instrument_id            bigint,
+                side                     character,
+                open_close               character,
+                avg_px                   numeric,
+                exec_qty                 integer,
+                display_instrument_id    character varying,
+                last_trade_date          date,
+                instrument_type_id       character,
+                cmta                     character varying,
+                exec_broker              character varying,
+                principal_amount         numeric,
+                client_commission_rate   numeric,
+                blaze_account_alias      character varying,
+                street_exec_time         timestamp without time zone,
+                expiration_date          timestamp without time zone,
+                opt_customer_firm        character,
+                reported_status          character,
+                reported_time            timestamp without time zone,
+                claimed_by               integer,
+                claim_status             character,
+                is_prev_reported         boolean,
+                client_commission_amount numeric,
+                client_order_id          character varying,
+                client_order_status      character,
+                broker_commission_rate   numeric,
+                broker_commission_amount numeric,
+                manual_broker_code       character varying
+            )
+    LANGUAGE plpgsql
+    COST 1
+AS
+$function$
+    --l_date_id := in_date_id;
+    --VP 20231101 https://dashfinancial.atlassian.net/browse/DS-7479
+    -- OS 20241227 https://dashfinancial.atlassian.net/browse/DS-9337 Add new input and output parameters
+    -- OS 20250116 https://dashfinancial.atlassian.net/browse/DS-9337 changes in report_time using is_billed in trade_record
+    -- OS 20260123 no ticket yet added client_commission_amount
+    -- OS 20260309 https://dashfinancial.atlassian.net/browse/DS-11204 Support client_order_id in Allocation Procedures
+    -- OS 20260317 https://dashfinancial.atlassian.net/browse/DS-11268 Support client_order_status for Allocation Instructions - allocation_snapshot, allocation_instruction_trades, allocation_insttuction_delete
+    -- SO 20260414 https://dashfinancial.atlassian.net/browse/DS-11390 Return client_order_status for Trade Records from OMS (OMS_EDW) as filled (2)
+    -- SO 20260609 https://dashfinancial.atlassian.net/browse/DS-11642 Add broker commissions rate and amount
+    -- SO 20260616 https://dashfinancial.atlassian.net/browse/DS-11642 Add manual_broker_code
+declare
+    l_date_id integer;
+begin
+
+    select ai.date_id
+    from genesis2.allocation_instruction ai
+    where ai.alloc_instr_id = in_alloc_instr_id
+    into l_date_id;
+
+    return query
+        select tr.date_id,
+               tr.trade_record_id::bigint,
+               tr.account_id::integer,
+               tr.instrument_id::int8,
+               tr.side,
+               tr.open_close,
+               tr.last_px                                                                       as avg_px,
+               tr.last_qty                                                                      as exec_qty,
+               i.display_instrument_id,
+               i.last_trade_date::date,
+               i.instrument_type_id,
+               tr.cmta,
+               tr.exec_broker,
+               case i.instrument_type_id
+                   when 'O' then tr.last_qty * tr.last_px * os.contract_multiplier
+                   else tr.last_qty * tr.last_px
+                   end                                                                          as principal_amount,
+               CCRU.ccru_rate                                                                   as client_commission_rate,
+               tr.blaze_account_alias,
+               coalesce(tr.street_trade_record_time, tr.trade_record_time)                      as street_exec_time,
+               ----------------
+               i.last_trade_date                                                                as expiration_date,
+               tr.opt_customer_firm,
+--                coalesce(bar.to_report, btr.to_report)                      as reported_status,
+               case when tr.is_billed = 'R' then 'R'::char end                                  as reported_status,
+               case
+                   when tr.is_billed = 'R' then coalesce(/*bar.db_create_time,*/ (select bar.db_create_time
+                                                                                  from dash_reporting.bofa_allocation_report bar
+                                                                                           join genesis2.alloc_instr2trade_record aitr
+                                                                                                on aitr.date_id = bar.date_id and aitr.alloc_instr_id = bar.alloc_instr_id
+                                                                                           join genesis2.trade_record tri
+                                                                                                on tri.date_id =
+                                                                                                   bar.date_id and
+                                                                                                   tri.trade_record_id =
+                                                                                                   aitr.trade_record_id
+                                                                                  where true
+--                           and tri.exch_exec_id = tr.exch_exec_id
+                                                                                    and tri.exec_id = tr.exec_id
+                                                                                    and tri.is_billed = 'R'
+                                                                                  order by 1
+                                                                                  limit 1)) end as reported_time,
+               null::int4                                                                       as claimed_by,
+               null::character                                                                  as claim_status,
+               case when tr.is_billed = 'R' then true else false end                            as is_prev_reported,
+               CCRU.ccru_amount                                                                 as client_commission_amount,
+               tr.client_order_id,
+               case
+                   when tr.subsystem_id is distinct from 'OMS_EDW' then
+                       (select fpo.order_status
+                        from staging.f_parent_order fpo
+                        where fpo.status_date_id = l_date_id
+                          and fpo.parent_order_id = tr.order_id
+                        limit 1)
+                   else '2' end::character                                                      as client_order_status,
+               CCRU.brok_rate                                                                   as broker_commission_rate,
+               CCRU.brok_amount                                                                 as broker_commission_amount,
+               genesis2.get_manual_broker(in_subsystem_id := tr.subsystem_id, in_date_id := tr.date_id,
+                                          in_trade_record_id := tr.trade_record_id,
+                                          in_fix_message_id := tr.trade_fix_message_id)         as manual_broker_code
+        from genesis2.trade_record tr
+                 inner join genesis2.instrument i on (tr.instrument_id = i.instrument_id)
+                 inner join genesis2.alloc_instr2trade_record ai2tr on (ai2tr.trade_record_id = tr.trade_record_id)
+                 inner join genesis2.allocation_instruction a on (a.alloc_instr_id = ai2tr.alloc_instr_id)
+                 left join lateral (select to_report, btr.db_create_time
+                                    from dash_reporting.bofa_trade_record btr
+                                    where btr.trade_record_id = tr.trade_record_id
+                                      and btr.date_id = tr.date_id
+                                    limit 1) btr on true
+                 left join lateral (select to_report, bar.db_create_time
+                                    from dash_reporting.bofa_allocation_report bar
+                                    where bar.alloc_instr_id = ai2tr.alloc_instr_id
+                                      and bar.date_id = ai2tr.date_id
+                                    limit 1) bar on true
+
+                 left join genesis2.option_contract oc on i.instrument_id = oc.instrument_id
+                 left join genesis2.option_series os on oc.option_series_id = os.option_series_id
+                 left join lateral (select max(case when book_record_type_id = 'CCRU' then L1.rate end)   as ccru_rate,
+                                           max(case when book_record_type_id = 'CCRU' then l1.amount end) as ccru_amount,
+                                           max(case when book_record_type_id = 'BROK' then L1.rate end)   as brok_rate,
+                                           max(case when book_record_type_id = 'BROK' then l1.amount end) as brok_amount
+                                    from (SELECT tl.trade_record_id,
+                                                 book_record_type_id,
+                                                 row_number()
+                                                 over (partition by tl.trade_record_id , book_record_type_id , billing_entity order by cr.priority ) as rn,
+                                                 tl.rate,
+                                                 tl.amount
+                                          FROM genesis2.trade_level_book_record tl
+                                                   inner join genesis2.book_record_creator cr
+                                                              on tl.book_record_creator_id = cr.book_record_creator_id
+                                          WHERE tl.date_id = l_date_id
+                                            AND book_record_type_id in ('CCRU', 'BROK')
+                                            and tl.trade_record_id = tr.trade_record_id) L1
+                                    where rn = 1) ccru on true
+
+        where tr.is_busted = 'N'
+          and tr.date_id = l_date_id
+          and a.alloc_instr_id = in_alloc_instr_id;
+
+end;
+$function$
+;
