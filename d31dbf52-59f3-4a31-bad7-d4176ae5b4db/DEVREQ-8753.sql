12d5ae92c7adbe43770ce8a835852ac63c9f9725
Oleh Sydor <ole.sydor@gmail.com>
2026-07-23 18:00:16 +0300

    Regular auto-commit at 18:00:15 on 23/07/2026

--- d31dbf52-59f3-4a31-bad7-d4176ae5b4db/DEVREQ-8753.sql
+++ d31dbf52-59f3-4a31-bad7-d4176ae5b4db/DEVREQ-8753.sql
@@ -1,7 +1,10 @@
 -- DEVREQ-8753
 
-select * from dash360.report_fintech_venue_breakdown_by_symbol(20260722, 20260722, null, '{73494}', null)
-create function dash360.report_fintech_venue_breakdown_by_symbol(in_start_date_id integer,
+select *
+from dash360.report_fintech_venue_breakdown_by_symbol(in_start_date_id := 20260723, in_end_date_id := 20260723,
+                                                      in_trading_firm_ids := null, in_account_ids := '{73494}',
+                                                      in_instrument_type_id := 'O')
+create or replace function dash360.report_fintech_venue_breakdown_by_symbol(in_start_date_id integer,
                                                                             in_end_date_id integer,
                                                                             in_trading_firm_ids varchar(9)[],
                                                                             in_account_ids int8[],
@@ -44,25 +47,29 @@ begin
                   else true end;
     end if;
 
-    select array_to_string(array [
-                               to_char(cl.create_time, 'YYYY/MM/DD'), -- Trade Date
-                               di.instrument_type_id, -- Security Type
-                               display_instrument_id, -- Symbol
-                               exc.exchange_name, -- Venue
-                               to_char(sum(cl.order_qty), 'FM99990D0099'),-- Order Qty
-                               to_char(sum(last_qty), 'FM99990D0099'),-- Last Qty
-                               to_char(100.0 * sum(last_qty) / sum(cl.order_qty), '990.99%')
-                               ], ',', '')
-    from dwh.client_order cl
-             join dwh.execution ex on ex.order_id = cl.order_id and not ex.is_parent_level
-             join dwh.d_instrument di on di.instrument_id = cl.instrument_id
-             join dwh.d_exchange exc on exc.exchange_id = cl.exchange_id
-    where true
-      and cl.parent_order_id is not null
-      and cl.create_date_id between in_start_date_id and in_end_date_id
-      and case when in_instrument_type_id is null then true else di.instrument_type_id = in_instrument_type_id end
-      and case when l_account_ids = '{}' then true else cl.account_id = any (l_account_ids) end
-    group by create_time, di.instrument_type_id, display_instrument_id, exc.exchange_name;
+    return query
+        select 'Trade Date,Sec Type,Symbol,Venue,Order Qty,Fill Qty,Fill Rate';
+
+    return query
+        select array_to_string(array [
+                                   to_char(cl.create_time, 'YYYY/MM/DD'), -- Trade Date
+                                   di.instrument_type_id, -- Security Type
+                                   display_instrument_id, -- Symbol
+                                   exc.exchange_name, -- Venue
+                                   to_char(sum(cl.order_qty), 'FM9999999'),-- Order Qty
+                                   to_char(sum(last_qty), 'FM9999999'),-- Last Qty
+                                   to_char(round(100.0 * sum(last_qty) / sum(cl.order_qty), 2), 'FM990.00%')
+                                   ], ',', '')
+        from dwh.client_order cl
+                 join dwh.execution ex on ex.order_id = cl.order_id and not ex.is_parent_level
+                 join dwh.d_instrument di on di.instrument_id = cl.instrument_id
+                 join dwh.d_exchange exc on exc.exchange_id = cl.exchange_id
+        where true
+          and cl.parent_order_id is not null
+          and cl.create_date_id between in_start_date_id and in_end_date_id
+          and case when in_instrument_type_id is null then true else di.instrument_type_id = in_instrument_type_id end
+          and case when l_account_ids = '{}' then true else cl.account_id = any (l_account_ids) end
+        group by create_time, di.instrument_type_id, display_instrument_id, exc.exchange_name;
 
     select public.load_log(l_load_id, l_step_id,
                            'report_fintech_venue_breakdown_by_symbol for ' || in_start_date_id::text || '-' ||
@@ -72,3 +79,7 @@ begin
 end;
 $function$
 ;
+
+select to_char(round(100.0 * 500 / 500, 2), '990.99%')
+
+' 100.00%'
