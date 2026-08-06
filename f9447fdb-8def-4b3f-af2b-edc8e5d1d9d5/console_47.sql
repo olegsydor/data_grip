7890786c71e29450c086453074046c34e1ca209d
Oleh Sydor <ole.sydor@gmail.com>
2026-08-06 15:00:10 +0300

    Regular auto-commit at 15:00:10 on 06/08/2026

--- f9447fdb-8def-4b3f-af2b-edc8e5d1d9d5/console_47.sql
+++ f9447fdb-8def-4b3f-af2b-edc8e5d1d9d5/console_47.sql
@@ -1,19 +1,8 @@
--- blaze7.v_lifecycle_order source
+drop foreign table staging.v_lifecycle_order;
 
-CREATE OR REPLACE VIEW blaze7.v_lifecycle_order
-AS SELECT DISTINCT ON (co.order_id, co.chain_order_id) co.order_id,
-    co.chain_order_id AS lifecycle_orderid,
-    ( SELECT
-                CASE
-                    WHEN ((rep.payload ->> 'OrderStatus'::text) = ANY (ARRAY['2'::text, '4'::text, 'P'::text, '3'::text])) AND (rep.payload ->> 'BlazeOrderStatus'::text) <> 't'::text THEN 'F'::text
-                    ELSE 'N'::text
-                END AS lifecycle_orderid_status
-           FROM blaze7.order_report rep
-          WHERE rep.order_id = co.order_id AND rep.leg_ref_id IS NULL
-          ORDER BY rep.exec_id DESC
-         LIMIT 1) AS lifecycle_orderid_status
-   FROM blaze7.client_order co
-  WHERE true AND co.parent_order_id IS NULL AND co.db_create_time >= CURRENT_DATE AND co.db_create_time <= (CURRENT_DATE + '1 day'::interval);
+IMPORT FOREIGN SCHEMA blaze7 LIMIT TO (v_lifecycle_order)
+FROM SERVER blaze7_uat
+into staging
 
 drop table if exists genesis2.blaze_lifecycle_order;
 create table if not exists genesis2.blaze_lifecycle_order
@@ -22,35 +11,47 @@ create table if not exists genesis2.blaze_lifecycle_order
         constraint blaze_lifecycle_order_pk primary key,
     lifecycle_orderid        int8        null,
     lifecycle_orderid_status char(1)     not null,
-    last_exec_id             varchar(30) not null
+    last_exec_id             varchar(30) not null,
+    db_create_time           timestamp default clock_timestamp(),
+    db_update_time           timestamp
 );
 create index blaze_lifecycle_order_last_exec_id_idx on genesis2.blaze_lifecycle_order (last_exec_id);
 
-create or replace function
-insert into genesis2.blaze_lifecycle_order (parent_order_id, lifecycle_orderid, lifecycle_orderid_status)
-select order_id, lifecycle_orderid, lifecycle_orderid_status
-from staging.v_lifecycle_order vl
-on conflict (parent_order_id)
-    do update set lifecycle_orderid        = excluded.lifecycle_orderid,
-                  lifecycle_orderid_status = excluded.lifecycle_orderid_status
-where blaze_lifecycle_order.lifecycle_orderid_status is distinct from excluded.lifecycle_orderid_status
-   or blaze_lifecycle_order.lifecycle_orderid is distinct from excluded.lifecycle_orderid;
+create or replace function genesis2.load_blaze_lifecycle_order(in_last_exec_id varchar(30) default null)
+    returns int4
+    language plpgsql
+as
+$fn$
+    -- 20260806 SO https://dashfinancial.atlassian.net/browse/DS-11870
+declare
+    l_last_exec_id varchar(30);
+    l_row_cnt      int4;
+begin
+    select coalesce(in_last_exec_id, max(last_exec_id), '')
+    into l_last_exec_id
+    from genesis2.blaze_lifecycle_order;
+
+    insert into genesis2.blaze_lifecycle_order (parent_order_id, lifecycle_orderid, lifecycle_orderid_status,
+                                                last_exec_id)
+    select order_id, lifecycle_orderid, lifecycle_orderid_status, exec_id
+    from staging.v_lifecycle_order vl
+    where exec_id > l_last_exec_id
+    on conflict (parent_order_id)
+        do update set lifecycle_orderid        = excluded.lifecycle_orderid,
+                      lifecycle_orderid_status = excluded.lifecycle_orderid_status,
+                      last_exec_id             = excluded.last_exec_id,
+                      db_update_time           = clock_timestamp()
+    where blaze_lifecycle_order.lifecycle_orderid is distinct from excluded.lifecycle_orderid
+       or blaze_lifecycle_order.lifecycle_orderid_status != excluded.lifecycle_orderid_status
+       or blaze_lifecycle_order.last_exec_id != excluded.last_exec_id;
+    get diagnostics l_row_cnt = row_count;
+    return l_row_cnt;
+end;
+$fn$;
+
+select * from genesis2.load_blaze_lifecycle_order('');
+select * from genesis2.load_blaze_lifecycle_order();
 
 
 select * from genesis2.blaze_lifecycle_order
-
--- DROP FUNCTION genesis2.load_away_trade(text, text);
-
-CREATE OR REPLACE FUNCTION genesis2.load_away_trade(in_min_report_id text DEFAULT NULL::text, in_max_report_id text DEFAULT NULL::text)
- RETURNS integer
- LANGUAGE plpgsql
-AS $function$
--- 20241217 SO added account_name_gvp and mic_code
-declare
-    l_last_loaded_report_id text;
-    l_maxt_report_id        text;
-    l_row_cnt               int4;
-    l_load_id               int4;
-    l_step_id               int4;
-
-begin
+WHERE parent_order_id = 647855791876882432 AND lifecycle_orderid = 647855791876882432
