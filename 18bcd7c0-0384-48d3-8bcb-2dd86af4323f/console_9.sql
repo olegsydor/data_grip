5dacf74776143dc8b9015241fecb43d2ef3f071d
Oleh Sydor <ole.sydor@gmail.com>
2026-08-18 19:00:08 +0300

    Regular auto-commit at 19:00:08 on 18/08/2026

---
+++ 18bcd7c0-0384-48d3-8bcb-2dd86af4323f/console_9.sql
@@ -0,0 +1,18 @@
+drop table t_os;
+
+create temp table t_os as
+with tm as (
+    select * from generate_series(11, 17, 1) as x(hr)
+)
+select dl.date_id, tm.hr, to_char(sum(dl.loaded_rows), 'FM9,999,999,990')
+from loader.files fl
+   join loader.daily_load dl using (date_id, file_id)
+   join tm on true
+where dl.date_id >= 20260701
+  and true
+and end_processing < (fl.date_id::text::date + '1 hour'::interval * tm.hr)
+group by dl.date_id, tm.hr;
+
+select * from t_os
+where hr = 12
+order by 3 desc
